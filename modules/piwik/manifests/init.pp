b399acf93eb6cc2fa8ec6a94b6350e684a25f260
Southparkfan <southparkfan223@hotmail.com>
2015-07-23 16:21:18 +0200

    Fix piwik module

--- modules/piwik/manifests/init.pp
+++ modules/piwik/manifests/init.pp
@@ -3,6 +3,16 @@ class piwik {
     include ::apache
     include ::apache::mod::expires
     include ::apache::mod::php5
+
+    $packages = [
+        'php5-curl',
+        'php5-mysqlnd',
+        'php5-gd'
+    ]
+
+    package { $packages:
+        ensure => present,
+    }
     
     git::clone { 'piwik':
         directory => '/srv/piwik',
@@ -10,6 +20,13 @@ class piwik {
         branch    => 'master', # FIXME: shouldn't clone master
         owner     => 'www-data',
         group     => 'www-data',
+    }
+
+    exec { "curl -sS https://getcomposer.org/installer | php && php composer.phar install":
+        creates => '/srv/piwik/composer.phar',
+        cwd     => '/srv/piwik',
+        path    => '/usr/bin',
+        user    => 'www-data',
     }
 
     apache::site { 'piwik.miraheze.org':
