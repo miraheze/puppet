fb36d7f1d4765d344579c9fdaa5c94048f5b02f8
Southparkfan <Southparkfan@users.noreply.github.com>
2015-08-02 17:22:22 +0200

    Add apt::source for nginx apt repo

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,7 +1,18 @@
 # nginx
 #
 class nginx {
-    package { [ 'nginx-light', 'nginx-common' ]: }
+    include ::apt
+    apt::source { 'nginx_apt':
+        comment  => 'nginx apt repo',
+        location => 'http://nginx.org/packages/debian',
+        release  => 'jessie',
+        repos    => 'nginx',
+        key      => '573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62',
+    }
+    
+    package { 'nginx':
+        ensure => present,
+    }
 
     service { 'nginx':
         ensure     => 'running',
