470fe99a56400d31f618987abe2e4f9ec8d4591e
John F. Lewis <johnflewis93@gmail.com>
2015-09-02 18:40:46 +0100

    restrict access to parsoid to mw1 only

--- modules/role/manifests/parsoid.pp
+++ modules/role/manifests/parsoid.pp
@@ -3,7 +3,8 @@ class role::parsoid {
 
     ufw::allow { 'parsoid':
         proto => 'tcp',
-        port => 8142,
+        port  => 8142,
+        from  => '185.52.1.75',
     }
 
     motd::role { 'role::parsoid':
