1bb3c891926ccb7026e18b4723c9c853b3f10dad
Southparkfan <Southparkfan@users.noreply.github.com>
2015-09-18 23:07:22 +0200

    disable jobqueue cron

--- modules/mediawiki/manifests/cron.pp
+++ modules/mediawiki/manifests/cron.pp
@@ -1,11 +1,12 @@
 # class: mediawiki::cron
 class mediawiki::cron {
-    cron { 'jobqueue':
-        ensure  => present,
-        command => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblist/all.dblist /srv/mediawiki/w/maintenance/runJobs.php > /var/log/mediawiki/cron/jobqueue.log',
-        user    => 'www-data',
-        minute  => '*/10',
-    }
+    # https://github.com/miraheze/mw-config/issues/114
+    #cron { 'jobqueue':
+    #    ensure  => present,
+    #    command => '/usr/local/bin/foreachwikiindblist /srv/mediawiki/dblist/all.dblist /srv/mediawiki/w/maintenance/runJobs.php > /var/log/mediawiki/cron/jobqueue.log',
+    #    user    => 'www-data',
+    #    minute  => '*/10',
+    #}
 
     cron { 'purge_checkuser':
         ensure  => present,
