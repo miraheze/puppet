14e53119f574468341e0f9b53767ebd6f7687deb
Southparkfan <Southparkfan@users.noreply.github.com>
2015-09-20 18:44:42 +0200

    Add parsoid for cinemawiki

--- modules/parsoid/files/settings.js
+++ modules/parsoid/files/settings.js
@@ -3,6 +3,7 @@
 exports.setup = function( parsoidConfig ) {
 	// wiki end points
 	parsoidConfig.setInterwiki( 'cbmediawiki', 'https://cbmedia.miraheze.org/w/api.php' );
+	parsoidConfig.setInterwiki( 'cinemawiki', 'https://cinema.miraheze.org/w/api.php' );
 	parsoidConfig.setInterwiki( 'clicordiwiki', 'https://clicordi.miraheze.org/w/api.php' );
 	parsoidConfig.setInterwiki( 'extloadwiki', 'https://extload.miraheze.org/w/api.php' );
 	parsoidConfig.setInterwiki( 'genwiki', 'https://gen.miraheze.org/w/api.php' );
