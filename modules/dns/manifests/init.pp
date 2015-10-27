35593fab2b14a70b864ac3649146bdd034b59f08
Southparkfan <Southparkfan@users.noreply.github.com>
2015-10-27 19:40:26 +0100

    Install GDNSD from stretch release instead of jessie's

--- modules/dns/manifests/init.pp
+++ modules/dns/manifests/init.pp
@@ -1,8 +1,25 @@
 # dns
-#
 class dns {
+    include ::apt
+    
+    apt::source { 'debian_stretch':
+        comment     => 'Debian Stretch APT'
+        location    => 'http://ftp.debian.org/debian',
+        release     => 'stretch',
+        repos       => 'main contrib non-free',
+    }
+
+    # Debian Jessie has GDNSD 2.1, but we need 2.2 so we use stretch for this one
+    file { '/etc/apt/preferences':
+        ensure  => present,
+        source  => 'puppet:///modules/dns/preferences.apt',
+        require => Apt::Source['debian_stretch'],
+        notify  => Exec['apt_update'],
+    }
+        
     package { 'gdnsd':
-        ensure => installed,
+        ensure  => installed,
+        require => File['/etc/apt/preferences'],
     }
 
     service { 'gdnsd':
