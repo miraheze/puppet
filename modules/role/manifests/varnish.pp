5cbdd37d6269231a0c9b4f73d2815e3fa610a97c
Southparkfan <southparkfan223@hotmail.com>
2015-12-18 17:55:32 +0100

    Allow direct Varnish access from mw1, bypass nginx for PURGE

--- modules/role/manifests/varnish.pp
+++ modules/role/manifests/varnish.pp
@@ -1,16 +1,23 @@
 class role::varnish {
-  include ::varnish
+    include ::varnish
 
-  ufw::allow { 'http port tcp':
-      proto => 'tcp',
-      port  => 80,
-  }
+    ufw::allow { 'http port tcp':
+        proto => 'tcp',
+        port  => 80,
+    }
 
-  ufw::allow { 'https port tcp':
-      proto => 'tcp',
-      port  => 443,
-  }
-  motd::role { 'role::varnish':
-    description => 'Varnish caching server',
-  }
+    ufw::allow { 'https port tcp':
+        proto => 'tcp',
+        port  => 443,
+    }
+
+    ufw::allow { 'Direct Varnish access mw1':
+        proto => 'tcp',
+        port  => 81,
+        from  => '185.52.1.75',
+    }
+
+    motd::role { 'role::varnish':
+        description => 'Varnish caching server',
+    }
 }
