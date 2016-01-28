603ef372a697f0075bc82831992c47f3d8278251
NDKilla-public <jildosx@gmail.com>
2016-01-28 14:27:47 -0500

    parsoid config for airwiki

--- modules/parsoid/files/settings.js
+++ modules/parsoid/files/settings.js
@@ -4,6 +4,7 @@ exports.setup = function( parsoidConfig ) {
 	// Wiki end points
 	parsoidConfig.setInterwiki( '8stationwiki', 'https://8station.miraheze.org/w/api.php' );
 	parsoidConfig.setInterwiki( 'adnovumwiki', 'https://adnovum.miraheze.org/w/api.php' );
+	parsoidConfig.setInterwiki( 'airwiki', 'https://air.miraheze.org/w/api.php' );
 	parsoidConfig.setInterwiki( 'allthetropeswiki', 'https://allthetropes.miraheze.org/w/api.php' );
 	parsoidConfig.setInterwiki( 'aktposwiki', 'https://aktpos.miraheze.org/w/api.php' );
 	parsoidConfig.setInterwiki( 'applebranchwiki', 'https://applebranch.miraheze.org/w/api.php' );
