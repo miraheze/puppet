7c27c40b83b26ef17d556eeb7226b4d2b58a6fff
John F. Lewis <johnflewis93@gmail.com>
2016-03-16 21:36:21 +0000

    allow managing remote home dir via puppet

--- modules/users/manifests/user.pp
+++ modules/users/manifests/user.pp
@@ -24,6 +24,19 @@ define users::user(
         allowdupe  => false,
     }
 
+    if $ensure == 'present' {
+        file { "/home/${name}":
+            ensure => 'present',
+            source => "puppet:///modules/users/home/${name}/",
+            recurse => 'remote',
+            mode => '0644',
+            owner => $name,
+            group => $gid,
+            force => true,
+            require => User[$name],
+        }
+    }
+
     if !is_array($ssh_keys) {
         fail("${name} is not a valid ssh_keys array: ${ssh_keys}")
     }
