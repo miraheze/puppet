40397a77bfe554c5a94f5811d1e5d032a2b101ca
Southparkfan <Southparkfan@users.noreply.github.com>
2016-03-18 19:41:49 +0100

    add valentinaprojectwiki parsoid conf

--- modules/parsoid/templates/settings.js
+++ modules/parsoid/templates/settings.js
@@ -13,6 +13,7 @@ exports.setup = function( parsoidConfig ) {
 	parsoidConfig.setInterwiki( 'permanentfuturelabwiki', 'https://permanentfuturelab.wiki/w/api.php');
 	parsoidConfig.setInterwiki( 'spiralwiki', 'https://spiral.wiki/w/api.php' );
 	parsoidConfig.setInterwiki( 'universebuildwiki', 'https://universebuild.com/w/api.php' );
+	parsoidConfig.setInterwiki( 'valentinaprojectwiki', 'https://wiki.valentinaproject.org/w/api.php' );
 
 	// Don't load WMF wikis
 	parsoidConfig.loadWMF = false;
