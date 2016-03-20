3abe5e37a5117d7983a3252acc659fa8ad4f2464
Yongmin Hong <revi@miraheze.org>
2016-03-20 21:02:23 +0900

    Add oyeavdelingenwiki for parsoid
    For the VE, requested in wiki creation request 709.

--- modules/parsoid/manifests/init.pp
+++ modules/parsoid/manifests/init.pp
@@ -58,6 +58,7 @@ class parsoid {
                 'ndtest',
                 'newtrend',
                 'nwp',
+                'oyeavdelingen',
                 'partup',
                 'pflanzen',
                 'pq',
