56bc814609d72d75851127f0456c50a6987cd1d3
John F. Lewis <johnflewis93@gmail.com>
2016-04-11 22:38:19 +0100

    add phabricator git repo management

--- modules/phabricator/manifests/init.pp
+++ modules/phabricator/manifests/init.pp
@@ -1,9 +1,34 @@
 class phabricator {
-	include ::apache::mod::ssl
+    include ::apache::mod::ssl
     include ::apache::mod::php5
 
     apache::site { 'phabricator.miraheze.org':
         ensure => present,
         source => 'puppet:///modules/phabricator/apache.conf',
+    }
+
+    file { '/srv/phab':
+        ensure => directory,
+    }
+
+    git::clone { 'arcanist':
+        ensure    => present,
+        directory => '/srv/phab/arcanist',
+        origin    => 'https://github.com/phacility/arcanist.git',
+        require   => File['/srv/phab'],
+    }
+
+    git::clone { 'libphutil':
+        ensure    => present,
+        directory => '/srv/phab/libphutil',
+        origin    => 'https://github.com/phacility/libphutil.git',
+        require   => File['/srv/phab'],
+    }
+
+    git::clone { 'phabricator':
+        ensure    => present,
+        directory => '/srv/phab/phabricator',
+        origin    => 'https://github.com/phacility/phabricator.git',
+        require   => File['/srv/phab'],
     }
 }
