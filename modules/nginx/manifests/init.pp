45e462be14c9cb07d77adce7ac53e26f25d46f78
Southparkfan <Southparkfan@users.noreply.github.com>
2016-05-03 12:08:06 +0200

    nginx: ensure Apache is absent
    Somehow Apache is installed on the appservers. Because Apache is restarted before nginx, this leads into a race condition where the appservers need manual maintenance before they can be put into production again.

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,15 +1,20 @@
 # nginx
-#
 class nginx {
     package { 'nginx':
         ensure  => present,
     }
 
+    # Ensure Apache is absent: https://phabricator.miraheze.org/T253
+    package { 'apache2':
+        ensure  => absent,
+    }
+
     service { 'nginx':
-        ensure     => 'running',
-        enable     => true,
-        provider   => 'debian',
-        hasrestart => true,
+        ensure      => 'running',
+        enable      => true,
+        provider    => 'debian',
+        hasrestart  => true,
+        require     => Package['apache2'],
     }
 
     file { '/etc/logrotate.d/nginx':
