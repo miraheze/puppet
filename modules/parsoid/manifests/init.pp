b9fadc28141ce8829ab3c3fcb0557c8dcd76863b
John F. Lewis <johnflewis93@gmail.com>
2016-05-20 18:51:44 +0100

    parsoid fishcolatorwiki

--- modules/parsoid/manifests/init.pp
+++ modules/parsoid/manifests/init.pp
@@ -57,6 +57,7 @@ class parsoid {
                 'essway',
                 'etpo',
                 'ezdmf',
+                'fishpercolator',
                 'foodsharinghamburg',
                 'games',
                 'gen',
