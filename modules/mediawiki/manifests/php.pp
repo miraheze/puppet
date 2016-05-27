0343fa8214b2f0a49afdf83c8149d416ab9457c7
Southparkfan <Southparkfan@users.noreply.github.com>
2016-05-27 21:51:20 +0200

    Fix usual failures by SPF™

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -1,19 +1,20 @@
 # mediawiki::php
+class mediawiki::php {
 	file { '/etc/php5/fpm/php-fpm.conf':
 		ensure => 'present',
-        mode   => 0755,
-        source => 'puppet:///modules/mediawiki/php/php-fpm.conf',
-    }
-
-    file { '/etc/php5/fpm/pool.d/www.conf':
-        ensure => 'present',
-        mode   => 0755,
-        source => 'puppet:///modules/mediawiki/php/www.conf',
-    }
-
-    file { '/etc/php5/fpm/php.ini':
-        ensure => present,
-        mode   => 0755,
-        source => 'puppet:///modules/mediawiki/php/php.ini',
-    }
+		mode   => 0755,
+		source => 'puppet:///modules/mediawiki/php/php-fpm.conf',
+	}
+	
+	file { '/etc/php5/fpm/pool.d/www.conf':
+		ensure => 'present',
+		mode   => 0755,
+		source => 'puppet:///modules/mediawiki/php/www.conf',
+	}
+	
+	file { '/etc/php5/fpm/php.ini':
+		ensure => present,
+		mode   => 0755,
+		source => 'puppet:///modules/mediawiki/php/php.ini',
+	}
 }
