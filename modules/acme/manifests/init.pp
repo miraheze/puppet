87b0deb219f5c85c28e51f164400cc86a6375fc1
John F. Lewis <johnflewis93@gmail.com>
2016-07-08 18:50:05 +0100

    add account.key through puppet

--- modules/acme/manifests/init.pp
+++ modules/acme/manifests/init.pp
@@ -11,4 +11,9 @@ class acme {
         source => 'puppet:///modules/acme/ssl-certificate',
         mode   => '0555',
     }
+
+    file { '/root/acme-tiny/account.key':
+        ensure => present,
+        source => 'puppet:///private/acme/account.key',
+    }
 }
