a5a3f5202abd0fca6efdff8d7e5d1ec6bfb7a782
Southparkfan <southparkfan223@hotmail.com>
2016-07-13 12:19:34 +0200

    Revert

--- modules/irc/templates/logbot/config.py
+++ modules/irc/templates/logbot/config.py
@@ -56,10 +56,7 @@ author_map = {
 }
 
 # Map irc nick to title of the user (how the bot addresses the user)
-title_map = {
-    "example": "Master",
-    "SPF|Cloud": "I love you! <3"
-}
+title_map = { "example": "Master" }
 
 # Scheme and wiki hostname to connect to
 wiki_connection = ("https", "meta.miraheze.org")
