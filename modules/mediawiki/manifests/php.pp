04c41c0b4a1f83efe42a9388117d1496963a8280
Southparkfan <southparkfan223@hotmail.com>
2016-07-30 23:16:07 +0200

    Restart php5-fpm after config changes

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -19,21 +19,29 @@ class mediawiki::php {
         ensure => present,
     }
 
+    service { 'php5-fpm':
+        ensure => running,
+        require => Package['php5-fpm'],
+    }
+
     file { '/etc/php5/fpm/php-fpm.conf':
         ensure => 'present',
         mode   => 0755,
         source => 'puppet:///modules/mediawiki/php/php-fpm.conf',
+        notify => Service['php5-fpm'],
     }
 	
     file { '/etc/php5/fpm/pool.d/www.conf':
         ensure => 'present',
         mode   => 0755,
         source => 'puppet:///modules/mediawiki/php/www.conf',
+        notify => Service['php5-fpm'],
     }
 	
     file { '/etc/php5/fpm/php.ini':
         ensure => present,
         mode   => 0755,
         source => 'puppet:///modules/mediawiki/php/php.ini',
+        notify => Service['php5-fpm'],
     }
 }
