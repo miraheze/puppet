ecda0bc1c52b0057419c6aab2773591d506b95ba
John F. Lewis <johnflewis93@gmail.com>
2016-08-03 16:36:31 +0100

    add if for redirecturl clause

--- modules/ssl/manifests/hiera/certs.pp
+++ modules/ssl/manifests/hiera/certs.pp
@@ -2,14 +2,22 @@
 define ssl::hiera::certs (
     $url,
     $ca,
+    $redirect,
+    $sslname,
 ) {
-    file { "/etc/ssl/certs/${url}.crt":
-        ensure => 'present',
-        source => "puppet:///modules/ssl/certificates/${url}.crt",
+    if $sslname {
+        $sslurl = $sslname
+    } else {
+        $sslurl = $url
     }
 
-    file { "/etc/ssl/private/${url}.key":
+    file { "/etc/ssl/certs/${sslurl}.crt":
         ensure => 'present',
-        source => "puppet:///private/ssl/${url}.key",
+        source => "puppet:///modules/ssl/certificates/${sslurl}.crt",
+    }
+
+    file { "/etc/ssl/private/${sslurl}.key":
+        ensure => 'present',
+        source => "puppet:///private/ssl/${sslurl}.key",
     }
 }
