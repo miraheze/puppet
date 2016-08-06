9de2aa614c9f8fbd236b9688bdcb0fd13d4d58e0
Samuel Tarling <samtarling@users.noreply.github.com>
2016-08-06 12:35:09 +0100

    Update init.pp

--- modules/phabricator/manifests/init.pp
+++ modules/phabricator/manifests/init.pp
@@ -2,6 +2,8 @@ class phabricator {
     include ::apache::mod::ssl
     include ::apache::mod::php5
 
+    $password = hiera('passwords::irc::mirahezebots')
+
     apache::site { 'phabricator.miraheze.org':
         ensure => present,
         source => 'puppet:///modules/phabricator/apache.conf',
