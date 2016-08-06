4300e4d8a00494e89c405d69b256d8504f28d630
John F. Lewis <johnflewis93@gmail.com>
2016-08-06 14:24:02 +0100

    cachet puppetisation changes

--- modules/cachet/manifests/init.pp
+++ modules/cachet/manifests/init.pp
@@ -1,8 +1,11 @@
 class cachet {
     include ::apache::mod::ssl
     include ::apache::mod::php5
+
     $database_password = hiera('passwords::db::cachet')
     $redis_password = hiera('passwords::redis::master')
+    $cachet_key = hiera('passwords::cachet::key')
+    $noreply_password = hiera('passwords::mail::noreply')
     
     apache::site { 'status.miraheze.org':
         ensure => present,
@@ -24,6 +27,14 @@ class cachet {
         environment => 'HOME=/srv/cachet',
         user        => 'www-data',
         require     => Git::Clone['cachet'],
+    }
+
+    exec { 'php composer.phar require predis/predis':
+        cwd         => '/srv/cachet',
+        path        => '/usr/bin',
+        environment => 'HOME=/srv/cachet',
+        user        => 'www-data',
+        require     => Exec['Cachet composer'],
     }
 
    file { '/srv/cachet/.env':
