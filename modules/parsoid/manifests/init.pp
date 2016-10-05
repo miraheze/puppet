febee06ac20ced49b2be61ec0379090b7dc5127f
ImBoPhil <josh.rolandez@gmail.com>
2016-10-05 20:14:33 +0200

    enable parsoid (mylogicwiki) VE

--- modules/parsoid/manifests/init.pp
+++ modules/parsoid/manifests/init.pp
@@ -144,6 +144,7 @@ class parsoid {
                 'musiclibrary',
                 'musictabs',
                 'mydegree',
+                'mylogic',
                 'ndn',
                 'neuronpedia',
                 'newarkmanor',
