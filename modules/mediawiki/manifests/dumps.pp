523bf05464706d0f62858af2eb8f6158dd4f3ce5
NDKilla <justin22622@gmail.com>
2016-10-06 13:28:40 -0400

    address PR

--- modules/mediawiki/manifests/dumps.pp
+++ modules/mediawiki/manifests/dumps.pp
@@ -2,13 +2,13 @@
 #
 # Cron jobs of select wiki dumps
 class mediawiki::dumps {
-    cron { 'sterbalssundrystudieswiki':
+    cron { 'Export sterbalssundrystudieswiki xml dump monthly':
         ensure   => present,
         command  => '/usr/bin/nice -n19 /usr/bin/php /srv/mediawiki/w/maintenance/dumpBackups.php --wiki sterbalssundrystudieswiki --logs --full > /mnt/mediawiki-static/dumps/sterbalssundrystudieswiki.xml',
         user     => 'www-data',
-        minute   => absent,
-        hour     => absent,
-        month    => absent,
+        minute   => '0',
+        hour     => '0',
+        month    => '*',
         monthday => 1,
     }
 }
