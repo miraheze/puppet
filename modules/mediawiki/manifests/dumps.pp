4333f46779fd68e6029cae7bec1926d5678f29aa
Justin <NDKilla@users.noreply.github.com>
2016-10-06 10:14:56 -0400

    update to spaces over tabs

--- modules/mediawiki/manifests/dumps.pp
+++ modules/mediawiki/manifests/dumps.pp
@@ -2,15 +2,13 @@
 #
 # Cron jobs of select wiki dumps
 class mediawiki::cron {
-	if $::hostname == 'cp1' {
-	    cron { 'update_database_lists':
-        	ensure		=> present,
-        	command		=> '/usr/bin/php /srv/mediawiki/w/maintenance/dumpBackups.php --wiki sterbalssundrystudieswiki --logs --full',
-        	user		=> 'www-data',
-        	minute		=> absent,
-        	hour		=> absent,
-        	month		=> absent,
-        	monthday	=> 1,
-    	}
-	}    	
+    cron { 'update_database_lists':
+        ensure   => present,
+        command  => '/usr/bin/php /srv/mediawiki/w/maintenance/dumpBackups.php --wiki sterbalssundrystudieswiki --logs --full',
+        user     => 'www-data',
+        minute   => absent,
+        hour     => absent,
+        month    => absent,
+        monthday => 1,
+    }
 }
