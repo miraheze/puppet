39226745d7ba317e33bbae443d4e58714a06edc6
Justin <NDKilla@users.noreply.github.com>
2016-10-06 10:11:01 -0400

    Create dumps.pp

---
+++ modules/mediawiki/manifests/dumps.pp
@@ -0,0 +1,16 @@
+# class: mediawiki::dumps
+#
+# Cron jobs of select wiki dumps
+class mediawiki::cron {
+	if $::hostname == 'cp1' {
+	    cron { 'update_database_lists':
+        	ensure		=> present,
+        	command		=> '/usr/bin/php /srv/mediawiki/w/maintenance/dumpBackups.php --wiki sterbalssundrystudieswiki --logs --full',
+        	user		=> 'www-data',
+        	minute		=> absent,
+        	hour		=> absent,
+        	month		=> absent,
+        	monthday	=> 1,
+    	}
+	}    	
+}
