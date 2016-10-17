027b4930f03c5b77eca492cb1079c38e3a20629a
LegoFan4000 <paulfkeffer@gmail.com>
2016-10-17 07:41:44 -0400

    Enable parsoid on karniarutheniawiki

--- modules/parsoid/manifests/init.pp
+++ modules/parsoid/manifests/init.pp
@@ -124,6 +124,7 @@ class parsoid {
                 'izanagi',
                 'janesskillspack',
                 'justinbieber',
+                'karniaruthenia'
                 'kassai',
                 'kinderacic',
                 'korach',
