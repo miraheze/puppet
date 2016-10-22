1f3d3572de6040062bc5c98111286138fa278036
Reception123 <motoc.luca@gmail.com>
2016-10-22 19:15:52 +0200

    add amaninfowiki dump
    DO NOT MERGE! No confirmation of dumping to static.miraheze.org

--- modules/mediawiki/manifests/dumps.pp
+++ modules/mediawiki/manifests/dumps.pp
@@ -10,5 +10,14 @@ class mediawiki::dumps {
         hour     => '0',
         month    => '*',
         monthday => 1,
+    cron { 'Export amaninfowiki xml dump monthly':
+        ensure   => present,
+        command  => '/usr/bin/nice -n19 /usr/bin/php /srv/mediawiki/w/maintenance/dumpBackups.php --wiki amaninfowiki --logs --full > /mnt/mediawiki-static/dumps/amaninfowiki.xml',
+        user     => 'www-data',
+        minute   => '0',
+        hour     => '0',
+        month    => '0',
+        week     => '2',
+        monthday => 0,
     }
 }
