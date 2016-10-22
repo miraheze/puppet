6e10199e571012c82cb5367558284bf6d9c9ccd6
Reception123 <motoc.luca@gmail.com>
2016-10-22 19:21:31 +0200

    DumpBackups->DumpBackup

--- modules/mediawiki/manifests/dumps.pp
+++ modules/mediawiki/manifests/dumps.pp
@@ -4,7 +4,7 @@
 class mediawiki::dumps {
     cron { 'Export sterbalssundrystudieswiki xml dump monthly':
         ensure   => present,
-        command  => '/usr/bin/nice -n19 /usr/bin/php /srv/mediawiki/w/maintenance/dumpBackups.php --wiki sterbalssundrystudieswiki --logs --full > /mnt/mediawiki-static/dumps/sterbalssundrystudieswiki.xml',
+        command  => '/usr/bin/nice -n19 /usr/bin/php /srv/mediawiki/w/maintenance/dumpBackup.php --wiki sterbalssundrystudieswiki --logs --full > /mnt/mediawiki-static/dumps/sterbalssundrystudieswiki.xml',
         user     => 'www-data',
         minute   => '0',
         hour     => '0',
