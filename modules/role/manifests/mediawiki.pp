4d95ce67c3de13e0466121b233c7f1246068e6e3
Southparkfan <southparkfan223@hotmail.com>
2016-10-26 18:12:50 +0200

    Cleanup mw* firewall rules

--- modules/role/manifests/mediawiki.pp
+++ modules/role/manifests/mediawiki.pp
@@ -2,14 +2,14 @@ class role::mediawiki {
     include ::mediawiki
     include nfs::client
 
-    ufw::allow { 'http port tcp':
-        proto => 'tcp',
-        port  => 80,
-    }
-
-    ufw::allow { 'https port tcp':
-        proto => 'tcp',
-        port  => 443,
+    $cp_ips = ['81.4.124.61', '107.191.126.23']
+    
+    $cp_ips.each |String $cp_ip| {
+        ufw::allow { "https port ${cp_ip} tcp":
+            proto => 'tcp',
+            port  => 443,
+            from  => $cp_ip,
+        }
     }
 
     motd::role { 'role::mediawiki':
