20aee6d952a3caf3a980db4ae779b8e4d1987a87
John F. Lewis <johnflewis93@gmail.com>
2017-01-01 18:17:49 +0000

    wikistats list: exclude private

--- modules/mediawiki/files/wikistats/wikistats-format-list.sh
+++ modules/mediawiki/files/wikistats/wikistats-format-list.sh
@@ -3,5 +3,7 @@
 while read line
 do
 	wiki=`/bin/echo $line | /usr/bin/cut -d "|" -f1`
-	/bin/echo ${wiki%????}
+	if ! /bin/grep -q "$wiki" /srv/mediawiki/dblist/private.dblist; then
+		/bin/echo ${wiki%????}
+	fi
 done < $1
