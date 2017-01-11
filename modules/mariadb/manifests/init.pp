b3f928fd5bf19597f0d29453d3ff75c99058b86c
Southparkfan <southparkfan223@hotmail.com>
2017-01-11 11:08:34 -0500

    Create tmp directory for mariadb

--- modules/mariadb/manifests/init.pp
+++ modules/mariadb/manifests/init.pp
@@ -2,4 +2,11 @@
 class mariadb {
     include mariadb::config
     include mariadb::packages
+
+    file { '/var/tmp/mariadb':
+        ensure  => directory,
+        owner   => 'mysql',
+        group   => 'mysql',
+        mode    => '0644',
+    }
 }
