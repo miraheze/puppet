7728583504ba4cb38803ce1deb794dd6cf25f511
John F. Lewis <johnflewis93@gmail.com>
2017-02-14 18:13:46 +0000

    new cron run

--- modules/base/manifests/puppet.pp
+++ modules/base/manifests/puppet.pp
@@ -1,20 +1,29 @@
 # class base::puppet
 class base::puppet {
     cron { 'puppet-run-no-force':
+        ensure  => absent,
         command => "/root/puppet-run &> /var/log/puppet-run.log",
         user    => 'root',
         minute  => [ 10, 20, 30, 40, 50 ],
     }
 
     cron { 'puppet-run-force':
+        ensure  => absent,
         command => "/root/puppet-run -f &> /var/log/puppet-run.log",
         user    => 'root',
         hour    => '*',
         minute  => '0',
     }
 
+    cron { 'puppet-agent':
+        command => '/usr/bin/puppet agent -t',
+        user    => 'root',
+        hour    => '*',
+        minute  => '*/10',
+    }
+
     file { '/root/puppet-run':
-        ensure => present,
+        ensure => absent,
         source => 'puppet:///modules/base/puppet/puppet-run',
         mode   => '0775',
     }
@@ -28,7 +37,7 @@ class base::puppet {
     }
 
     file { '/root/id_rsa':
-        ensure => present,
+        ensure => absent,
         source => 'puppet:///private/ssh/id_rsa',
         mode   => '0400',
     }
