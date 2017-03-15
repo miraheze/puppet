02321502ecec02f7544671ff054203f41f848c23
Reception123 <motoc.luca@gmail.com>
2017-03-15 19:15:26 +0100

    corrections

--- modules/mediawiki/manifests/dumps.pp
+++ modules/mediawiki/manifests/dumps.pp
@@ -63,13 +63,13 @@ class mediawiki::dumps {
         month    => '*',
         monthday => ['1', '8', '15', '22', '29'],
     }
-      cron { 'Export scruffywiki xml dump weekly':
+      cron { 'Export icmscholarswiki xml dump weekly':
         ensure   => present,
         command  => '/usr/bin/nice -n19 /usr/bin/php /srv/mediawiki/w/maintenance/dumpBackup.php --wiki icmscholarswiki --logs --full > /mnt/mediawiki-static/privatedumps/icmscholarswiki.xml',
         user     => 'www-data',
         minute   => '0',
         hour     => '0',
         month    => '*',
-        monthday => ['1', '8', '15', '22', '29'],
+        monthday => ['15', '30'],
     }
 }
