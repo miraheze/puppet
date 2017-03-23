0994c2e5ff5eda0edbe03dea31414acbeda5884a
John F. Lewis <johnflewis93@gmail.com>
2017-03-23 18:59:59 +0000

    mw: reduce log storage from 12 weeks to 6 weeks

--- modules/mediawiki/manifests/init.pp
+++ modules/mediawiki/manifests/init.pp
@@ -138,11 +138,13 @@ class mediawiki {
     }
 
     logrotate::rotate { 'mediawiki_wikilogs':
-        logs => '/var/log/mediawiki/*.log',
+        logs   => '/var/log/mediawiki/*.log',
+        rotate => '6',
     }
 
     logrotate::rotate { 'mediawiki_debuglogs':
-        logs => '/var/log/mediawiki/debuglogs/*.log',
+        logs   => '/var/log/mediawiki/debuglogs/*.log',
+        rotate => '6',
     }
     
     exec { 'Math texvccheck':
