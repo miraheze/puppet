f2f9411de043cd3c452b1a03e8fec6c682e3ad4b
Southparkfan <southparkfan223@hotmail.com>
2017-10-13 13:35:29 -0400

    Revert "fix icinga not purging old hosts"
    This reverts commit 178163a8c8158af252e5750b977f76aad21024fb.

--- modules/icinga/manifests/host.pp
+++ modules/icinga/manifests/host.pp
@@ -11,6 +11,7 @@ define icinga::host (
             host_name             => $title,
             address               => $ip,
             contact_groups        => $contacts,
+            target                => '/etc/icinga/config/puppet_hosts.cfg',
         },
     }
 
