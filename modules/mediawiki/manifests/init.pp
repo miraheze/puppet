2620b58fa19c174b556b497f29a588a336cec668
Reception123 <utilizator.receptie123@gmail.com>
2017-10-30 17:21:52 +0100

    fixes

--- modules/mediawiki/manifests/init.pp
+++ modules/mediawiki/manifests/init.pp
@@ -192,14 +192,15 @@ class mediawiki(
         require     => Git::Clone['MediaWiki core'],
     }
     exec { 'ExtensionMessageFiles':
-        command     => 'php mergeMessageFileList.php --wiki loginwiki --output /srv/mediawiki/config/ExtensionMessageFiles.php',
+        command     => 'php /srv/mediawiki/w/maintenance/mergeMessageFileList.php --wiki loginwiki --output /srv/mediawiki/config/ExtensionMessageFiles.php',
         creates     => '/srv/mediawiki/config/ExtensionMessageFiles.php',
-        cwd         => '/srv/mediawiki/config/',
+        cwd         => '/srv/mediawiki/config',
         path        => '/usr/bin',
         environment => 'HOME=/srv/mediawiki/config',
         user        => 'www-data',
         require     => Git::Clone['MediaWiki core'],
     }
+    
     icinga::service { 'mediawiki_rendering':
         description   => 'MediaWiki Rendering',
         check_command => 'check_mediawiki!meta.miraheze.org',
