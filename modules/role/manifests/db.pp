3c893dcf87e3763d8d79b10fc529571fa39173a2
Reception123 <utilizator.receptie123@gmail.com>
2017-11-02 18:34:21 +0100

    syntax fix

--- modules/role/manifests/db.pp
+++ modules/role/manifests/db.pp
@@ -41,8 +41,8 @@ class role::db {
     }
 	
     ufw:allow { 'mysql port mw3':
-        proto => 'tcp'
-        port => '3306'
+        proto => 'tcp',
+        port => '3306',
         from => '81.4.121.113',
     }
 
