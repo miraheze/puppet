6f6734e82bc855b434aa253496631e52e24e8b37
paladox <paladox@users.noreply.github.com>
2018-01-13 14:10:44 +0000

    Remove requires_os from puppetdb

--- modules/puppetmaster/manifests/puppetdb/client.pp
+++ modules/puppetmaster/manifests/puppetdb/client.pp
@@ -1,8 +1,6 @@
 # == Class puppetmaster::puppetdb::client
 # Configures a puppetmaster to work as a puppetdb client
 class puppetmaster::puppetdb::client($host, $port=443) {
-    # Only 3.5+ puppetmasters can work with our version of puppetdb
-    requires_os('debian >= jessie')
 
     # We are hosting puppetdb on puppetmaster so this
     # is already going to be installed
