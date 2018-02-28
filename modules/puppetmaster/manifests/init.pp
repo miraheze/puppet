99fd5b8f2504785c0bc7b4455c6a010819cf2fbc
paladox <paladox@users.noreply.github.com>
2018-02-28 18:54:22 +0000

    Update puppet-master.conf.erb (#608)
    * Update puppet-master.conf.erb
    
    * Update puppet-master.conf.erb
    
    * Update puppet-master.conf.erb
    
    * Update init.pp
    
    * Update init.pp

--- modules/puppetmaster/manifests/init.pp
+++ modules/puppetmaster/manifests/init.pp
@@ -209,6 +209,7 @@ class puppetmaster(
 
     service { 'puppetmaster':
         ensure => stopped,
+        before => Service['apache2'],
     }
 
     include ::apache::mod::rewrite 
@@ -217,6 +218,18 @@ class puppetmaster(
     apache::site { 'puppet-master':
         ensure => present,
         content => template("puppetmaster/puppet-master.conf.erb"),
+    }
+
+    # Place an empty puppet-master.conf file to prevent creation of this file
+    # at package install time. Apache breaks if that happens. T179102
+    file { '/etc/apache2/sites-available/puppet-master.conf':
+        ensure  => present,
+        content => '# This file intentionally left blank by puppet'
+    }
+    file { '/etc/apache2/sites-enabled/puppet-master.conf':
+        ensure  => link,
+        target  => '/etc/apache2/sites-available/puppet-master.conf',
+        require => File['/etc/apache2/sites-available/puppet-master.conf'],
     }
 
     ufw::allow { 'puppetmaster':
