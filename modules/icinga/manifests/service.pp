1895993a4e67efd58d359924638620d170b80dcb
paladox <paladox@users.noreply.github.com>
2018-03-04 15:37:33 +0000

    Fix exported resources in services.pp

--- modules/icinga/manifests/service.pp
+++ modules/icinga/manifests/service.pp
@@ -1,45 +1,47 @@
-# exports service monitoring for icinga
-define icinga::service (
-    $description,
+# proxy to an exported nagios_service definition
+# Used as a workaround of https://tickets.puppetlabs.com/browse/PUP-6698
+define icinga::exported_nagios_service (
+    $ensure,
+    $host_name,
+    $service_description,
     $check_command,
-    $host           = $::hostname,
-    $retries        = 3,
-    $ensure         = present,
-    $check_interval = 2,
-    $retry_interval = 1,
-    $contacts       = hiera('contactgroups', 'ops'),
-    $event_handler  = undef,
-  ) {
-    $description_safe = regsubst($description, '[`~!$%^&*"|\'<>?,()=]', '-', 'G')
-
-    $service = {
-        "${::hostname} ${title}" => {
-            ensure                 => $ensure,
-            host_name              => $host,
-            service_description    => $description_safe,
-            check_command          => $check_command,
-            max_check_attempts     => $retries,
-            check_interval         => $check_interval,
-            retry_interval         => $retry_interval,
-            check_period           => '24x7',
-            notification_interval  => 0,
-            notification_period    => '24x7',
-            notification_options   => 'w,u,c,r',
-            contact_groups         => $contacts,
-            passive_checks_enabled => 1,
-            active_checks_enabled  => 1,
-            is_volatile            => 0,
-            check_freshness        => 0,
-            event_handler          => $event_handler,
-            target                 => '/etc/icinga/config/puppet_services.cfg',
-            mode                   => '0444',
-            use                    => 'generic-service',
-        },
-    }
-
-    if defined(Class['icinga']) {
-        create_resources(nagios_service, $service)
-    } else {
-        create_resources('icinga::exported_nagios_service', $service)
+    $max_check_attempts,
+    $check_interval,
+    $retry_interval,
+    $check_period,
+    $notification_interval,
+    $notification_period,
+    $notification_options,
+    $contact_groups,
+    $passive_checks_enabled,
+    $active_checks_enabled,
+    $is_volatile,
+    $check_freshness,
+    $event_handler,
+    $target,
+    $mode,
+    $use,
+) {
+    @@nagios_service { $title:
+        ensure                 => $ensure,
+        host_name              => $host_name,
+        service_description    => $service_description,
+        check_command          => $check_command,
+        max_check_attempts     => $max_check_attempts,
+        check_interval         => $check_interval,
+        retry_interval         => $retry_interval,
+        check_period           => $check_period,
+        notification_interval  => $notification_interval,
+        notification_period    => $notification_period,
+        notification_options   => $notification_options,
+        contact_groups         => $contact_groups,
+        passive_checks_enabled => $passive_checks_enabled,
+        active_checks_enabled  => $active_checks_enabled,
+        is_volatile            => $is_volatile,
+        check_freshness        => $check_freshness,
+        event_handler          => $event_handler,
+        target                 => $target,
+        mode                   => $mode,
+        use                    => $use,
     }
 }
