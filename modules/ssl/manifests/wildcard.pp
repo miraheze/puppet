9cc7ae6a262c108a565370ce4e29efc0bc37b3a2
John Lewis <johnflewis93@gmail.com>
2018-04-05 14:34:37 +0100

    wildard->wildcard

--- modules/ssl/manifests/wildcard.pp
+++ modules/ssl/manifests/wildcard.pp
@@ -3,12 +3,12 @@ class ssl::wildcard {
     if !defined(File['/etc/ssl/certs/wildcard.miraheze.org.crt']) {
         file { '/etc/ssl/certs/wildcard.miraheze.org.crt':
             ensure => 'present',
-            source => 'puppet:///ssl/certificates/wildard.miraheze.org.crt',
+            source => 'puppet:///ssl/certificates/wildcard.miraheze.org.crt',
         }
 
-        file { '/etc/ssl/private/wildard.miraheze.org.key':
+        file { '/etc/ssl/private/wildcard.miraheze.org.key':
             ensure => 'present',
-            source => 'puppet:///ssl-keys/wildard.miraheze.org.key',
+            source => 'puppet:///ssl-keys/wildcard.miraheze.org.key',
         }
     }
 }
