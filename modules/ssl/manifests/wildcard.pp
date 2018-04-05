253559c042675771fbf891c58cb147d9f169f54c
paladox <paladox@users.noreply.github.com>
2018-04-05 18:00:25 +0100

    Change !defined to defined
    This fixes an issue where it try to add the file again leading to puppet errors as you carn't do the same thing twice.

--- modules/ssl/manifests/wildcard.pp
+++ modules/ssl/manifests/wildcard.pp
@@ -1,6 +1,6 @@
 # class: ssl::wildcard
 class ssl::wildcard {
-    if !defined(File['/etc/ssl/certs/wildcard.miraheze.org.crt']) {
+    if defined(File['/etc/ssl/certs/wildcard.miraheze.org.crt']) {
         file { '/etc/ssl/certs/wildcard.miraheze.org.crt':
             ensure => 'present',
             source => 'puppet:///ssl/certificates/wildcard.miraheze.org.crt',
