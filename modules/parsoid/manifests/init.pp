ba6fdf3c320dcc8fcf900de01149dc4759755fff
paladox <paladox@users.noreply.github.com>
2018-04-11 00:14:29 +0100

    Downgrade parsoid to 0.8.0 temporarily due to breaking changes with 0.9.0

--- modules/parsoid/manifests/init.pp
+++ modules/parsoid/manifests/init.pp
@@ -1,18 +1,31 @@
 # class: parsoid
 class parsoid {
-    include apt
+    #include apt
     include nginx
 
     $wikis = loadyaml('/etc/puppet/parsoid/parsoid.yaml')
 
-    apt::source { 'parsoid':
-        location => 'https://releases.wikimedia.org/debian',
-        release  => 'jessie-mediawiki',
-        repos    => 'main',
-        key      => {
-            'id'     => 'A6FD76E2A61C5566D196D2C090E9F83F22250DD7',
-            'server' => 'hkp://keyserver.ubuntu.com:80',
-        },
+    # can be redone once we are on mediawiki 1.31
+    #apt::source { 'parsoid':
+    #    location => 'https://releases.wikimedia.org/debian',
+    #    release  => 'jessie-mediawiki',
+    #    repos    => 'main',
+    #    key      => {
+    #        'id'     => 'A6FD76E2A61C5566D196D2C090E9F83F22250DD7',
+    #        'server' => 'hkp://keyserver.ubuntu.com:80',
+    #    },
+    ##}
+    
+    exec { "install_parsoid":
+        command => '/usr/bin/curl -o /opt/parsoid_0.8.0all_all.deb https://people.wikimedia.org/~ssastry/parsoid/debs/parsoid_0.8.0all_all.deb',
+        unless  => '/bin/ls /opt/parsoid_0.8.0all_all.deb',
+    }
+
+    package { "parsoid":
+        provider => dpkg,
+        ensure   => present,
+        source   => '/opt/puppetdb_4.4.0-1~wmf1_all.deb',
+        require  => Package['default-jdk'],
     }
 
     include ssl::wildcard
@@ -34,9 +47,12 @@ class parsoid {
         monitor => false,
     }
 
-    package { 'parsoid':
-        ensure  => present,
-        require => Apt::Source['parsoid'],
+    package { "parsoid":
+        provider => dpkg,
+        ensure   => present,
+        source   => '/opt/puppetdb_4.4.0-1~wmf1_all.deb',
+        require  => Exec['install_parsoid'],
+        # require => Apt::Source['parsoid'],
     }
 
     service { 'parsoid':
