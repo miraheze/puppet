738bea424a8e18ac7f0e952bb79236211a16cd4c
paladox <paladox@users.noreply.github.com>
2018-04-11 00:47:38 +0100

    Add a defined if check for httpd::mod

--- modules/httpd/manifests/mod.pp
+++ modules/httpd/manifests/mod.pp
@@ -1,7 +1,9 @@
 define httpd::mod(
     $modules = [],
 ) {
-    httpd::mod_conf { $modules:
-        ensure => present
+    if !defined(Httpd::Mod_conf($modules)) {
+        httpd::mod_conf { $modules:
+            ensure => present
+        }
     }
 }
