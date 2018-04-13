28ea739f15fcd1091e03bc49e374150afab76b88
paladox <paladox@users.noreply.github.com>
2018-04-13 23:33:56 +0100

    fix icinga file permission

--- modules/icinga/manifests/service.pp
+++ modules/icinga/manifests/service.pp
@@ -38,6 +38,15 @@ define icinga::service (
     }
 
     if defined(Class['icinga']) {
+        file { '/etc/icinga/config/puppet_services.cfg':
+           ensure  => present,
+           owner   => 'icinga',
+           group   => 'icinga', 
+           mode    => '0644',
+        }
+    }
+
+    if defined(Class['icinga']) {
         create_resources(nagios_service, $service)
     } else {
         create_resources('icinga::exported_nagios_service', $service)
