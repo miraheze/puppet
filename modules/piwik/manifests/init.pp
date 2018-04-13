1e1a0b49c807a5bf4ab4bce3601dfb62e6cf0cf7
paladox <paladox@users.noreply.github.com>
2018-04-13 17:01:08 +0100

    Support php7.0 in piwik module

--- modules/piwik/manifests/init.pp
+++ modules/piwik/manifests/init.pp
@@ -1,20 +1,17 @@
 # class: piwik
 class piwik(
+    # use php7 on stretch+
     $modules = ['expires', 'rewrite', 'ssl', 'php5']
 ) {
     include ::httpd
 
-    $packages = [
-        'php5-curl',
-        'php5-mysqlnd',
-        'php5-gd'
-    ]
-
-    package { $packages:
-        ensure => present,
+    if os_version('debian >= stretch') {
+        $php_version = '7'
+        require_package('php7.0-curl', 'php7.0-mysqlnd', 'php7.0-gd', 'libapache2-mod-php7.0')
+    } else {
+        $php_version = '5'
+        require_package('php5-curl', 'php5-mysqlnd', 'php5-gd', 'libapache2-mod-php5')
     }
-
-    require_package('libapache2-mod-php5')
 
     git::clone { 'piwik':
         directory          => '/srv/piwik',
@@ -40,24 +37,41 @@ class piwik(
         monitor => true,
     }
 
-    file { '/etc/php5/apache2/conf.d/20-piwik.ini':
-        ensure  => present,
-        source  => 'puppet:///modules/piwik/20-piwik.ini',
-        notify  => Exec['apache2_test_config_and_restart'],
-        require => Package['libapache2-mod-php5'],
-    }
+    if os_version('debian >= stretch') {
+        file { '/etc/php/7.0/apache2/conf.d/20-piwik.ini':
+            ensure  => present,
+            source  => 'puppet:///modules/piwik/20-piwik.ini',
+            notify  => Exec['apache2_test_config_and_restart'],
+            require => Package['libapache2-mod-php7'],
+        }
 
-    file_line { 'enable_php_opcache':
-        line    => 'opcache.enable=1',
-        match   => '^;?opcache.enable\s*\=',
-        path    => '/etc/php5/apache2/php.ini',
-        notify  => Exec['apache2_test_config_and_restart'],
-        require => Package['libapache2-mod-php5'],
+        file_line { 'enable_php_opcache':
+            line    => 'opcache.enable=1',
+            match   => '^;?opcache.enable\s*\=',
+            path    => '/etc/php/7.0/apache2/php.ini',
+            notify  => Exec['apache2_test_config_and_restart'],
+            require => Package['libapache2-mod-php7'],
+        }
+    } else {
+        file { '/etc/php5/apache2/conf.d/20-piwik.ini':
+            ensure  => present,
+            source  => 'puppet:///modules/piwik/20-piwik.ini',
+            notify  => Exec['apache2_test_config_and_restart'],
+            require => Package['libapache2-mod-php5'],
+        }
+
+        file_line { 'enable_php_opcache':
+            line    => 'opcache.enable=1',
+            match   => '^;?opcache.enable\s*\=',
+            path    => '/etc/php5/apache2/php.ini',
+            notify  => Exec['apache2_test_config_and_restart'],
+            require => Package['libapache2-mod-php5'],
+        }
     }
 
     httpd::mod { 'piwik_apache':
         modules => $modules,
-        require => Package['libapache2-mod-php5'],
+        require => Package["libapache2-mod-php${php_version}"],
     }
 
     $salt = hiera('passwords::piwik::salt')
