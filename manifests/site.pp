10cf3afbc596c7a65c571612d5762e8d9c7a901c
Paladox <thomasmulhall410@yahoo.com>
2018-04-15 00:55:42 +0100

    Install salt-minions and salt-master on misc3 (trialing)

--- manifests/site.pp
+++ manifests/site.pp
@@ -49,6 +49,8 @@ node 'misc2.miraheze.org' {
 node 'misc3.miraheze.org' {
     include base
     include role::parsoid
+    include role::salt::masters
+    include role::salt::minions
 }
 
 node /^mw[123]\.miraheze\.org$/ {
