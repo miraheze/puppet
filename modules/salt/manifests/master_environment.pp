bf2af067863a424c9bfd135ed36c363e9a4a8c69
Paladox <thomasmulhall410@yahoo.com>
2018-04-15 01:27:36 +0100

    Re add defined check in salt::master_environment

--- modules/salt/manifests/master_environment.pp
+++ modules/salt/manifests/master_environment.pp
@@ -4,40 +4,50 @@ define salt::master_environment(
     $salt_pillar_roots,
     $salt_module_roots,
     $salt_returner_roots,
-) {
+){
 
-    file { $salt_state_roots[$title]:
-        ensure => directory,
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
+    if ! defined(File[$salt_state_roots) {
+        file { $salt_state_roots[$title]:
+            ensure => directory,
+            mode   => '0755',
+            owner  => 'root',
+            group  => 'root',
+        }
     }
 
-    file { $salt_file_roots[$title]:
-        ensure => directory,
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
+    if ! defined(File[$salt_file_roots) {
+        file { $salt_file_roots:
+            ensure => directory,
+            mode   => '0755',
+            owner  => 'root',
+            group  => 'root',
+        }
     }
 
-    file { $salt_pillar_roots[$title]:
-        ensure => directory,
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
+    if ! defined(File[$salt_pillar_roots) {
+        file { $salt_pillar_roots[$title]:
+            ensure => directory,
+            mode   => '0755',
+            owner  => 'root',
+            group  => 'root',
+        }
     }
 
-    file { $salt_module_roots[$title]:
-        ensure => directory,
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
+    if ! defined(File[$salt_module_roots) {
+        file { $salt_module_roots[$title]:
+            ensure => directory,
+            mode   => '0755',
+            owner  => 'root',
+            group  => 'root',
+        }
     }
 
-    file { $salt_returner_roots[$title]:
-        ensure => directory,
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
+    if ! defined(File[$salt_returner_roots) {
+        file { $salt_returner_roots[$title]:
+            ensure => directory,
+            mode   => '0755',
+            owner  => 'root',
+            group  => 'root',
+        }
     }
 }
