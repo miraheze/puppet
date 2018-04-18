313abee2287f74ea818a288a41f5b8e73b2ade8e
paladox <paladox@users.noreply.github.com>
2018-04-18 14:27:24 +0100

    Fix nfs-common on stretch
    See http://blog.ruanbekker.com/blog/2017/12/09/unmask-a-masked-service-in-systemd/

--- modules/nfs/manifests/client.pp
+++ modules/nfs/manifests/client.pp
@@ -3,4 +3,15 @@ class nfs::client {
     package { 'nfs-common':
         ensure => present,
     }
+
+    exec { 'nfs-common reload systemd':
+        command     => '/bin/systemctl daemon-reload',
+        refreshonly => true,
+    }
+
+    file { '/lib/systemd/system/nfs-common.service':
+        ensure  => absent,
+        notify => Exec['nfs-common reload systemd'],
+        require => Package['nfs-common'],
+    }
 }
