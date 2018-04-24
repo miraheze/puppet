9b765b0bcb6a9c27a1cabfa423685d65a52cb673
paladox <paladox@users.noreply.github.com>
2018-04-24 16:49:10 +0100

    Fix icinga2::object::host params
    notification needs to be var.notification

--- modules/icinga2/manifests/custom/hosts.pp
+++ modules/icinga2/manifests/custom/hosts.pp
@@ -1,7 +1,6 @@
 define icinga2::custom::hosts (
   $ensure   = present,
   $ip       = $::ipaddress,
-  $vars     = undef,
   $contacts = hiera('contactgroups', [ 'icingaadmins', 'ops' ]),
 ) {
     icinga2::object::host { $title:
@@ -9,10 +8,11 @@ define icinga2::custom::hosts (
         import  => 'generic-host',
         address => $ip,
         target  => '/etc/icinga2/conf.d/puppet_hosts.conf',
-        vars    => $vars,
-        notification => {
-            mail => {
-                groups => $contacts,
+        vars    => {
+            notification => {
+                mail => {
+                    groups => $contacts,
+                },
             },
         },
     }
