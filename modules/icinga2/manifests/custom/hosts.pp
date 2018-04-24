03748a7dfeffabfa9fe43b7b40c463f824492ba9
paladox <paladox@users.noreply.github.com>
2018-04-24 23:00:12 +0100

    Update hosts.pp

--- modules/icinga2/manifests/custom/hosts.pp
+++ modules/icinga2/manifests/custom/hosts.pp
@@ -1,7 +1,7 @@
 define icinga2::custom::hosts (
   $ensure   = present,
   $ip       = $::ipaddress,
-  $contacts = hiera('contactgroups', [ 'icingaadmins', 'irc', 'ops' ]),
+  $contacts = hiera('contactgroups', [ 'icingaadmins', 'ops' ]),
 ) {
     @@icinga2::object::host { $title:
         ensure  => $ensure,
