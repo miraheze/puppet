97e7ce8e5c91ba18f69ca0cf72c69c8a5b5aa6a4
paladox <paladox@users.noreply.github.com>
2018-04-25 21:22:54 +0100

    Update icinga2.pp

--- modules/role/manifests/icinga2.pp
+++ modules/role/manifests/icinga2.pp
@@ -2,15 +2,15 @@
 class role::icinga2 {
     include ::icinga2::custom::conf
 
-    #ufw::allow { 'icinga2 http':
-    #    proto => 'tcp',
-    #    port  => '80',
-    #}
+    ufw::allow { 'icinga2 http':
+        proto => 'tcp',
+        port  => '80',
+    }
 
-    #ufw::allow { 'icinga2 https':
-    #    proto => 'tcp',
-    #    port  => '443',
-    #}
+    ufw::allow { 'icinga2 https':
+        proto => 'tcp',
+        port  => '443',
+    }
 
     motd::role { 'role::icinga2':
         description => 'central monitoring server',
