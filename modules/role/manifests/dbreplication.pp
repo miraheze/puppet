b942ece3ef3cadf8020bd9279813288aa678ace7
Southparkfan <southparkfan223@hotmail.com>
2018-04-27 13:27:08 +0200

    Tone down to the bare essentials

--- modules/role/manifests/dbreplication.pp
+++ modules/role/manifests/dbreplication.pp
@@ -2,29 +2,9 @@
 class role::dbreplication {
     include mariadb::packages
 
-    $mediawiki_password = hiera('passwords::db::mediawiki')
-    $wikiadmin_password = hiera('passwords::db::wikiadmin')
-    $piwik_password = hiera('passwords::db::piwik')
-    $phabricator_password = hiera('passwords::db::phabricator')
-
     class { 'mariadb::config':
         config   => 'mariadb/config/mw.cnf.erb',
         password => hiera('passwords::db::root'),
-    }
-
-    file { '/etc/mysql/miraheze/mediawiki-grants.sql':
-        ensure  => present,
-        content => template('mariadb/grants/mediawiki-grants.sql.erb'),
-    }
-
-    file { '/etc/mysql/miraheze/piwik-grants.sql':
-        ensure  => present,
-        content => template('mariadb/grants/piwik-grants.sql.erb'),
-    }
-
-    file { '/etc/mysql/miraheze/phabricator-grants.sql':
-        ensure  => present,
-        content => template('mariadb/grants/phabricator-grants.sql.erb'),
     }
 
     ufw::allow { 'mysql port db2':
@@ -43,15 +23,6 @@ class role::dbreplication {
         proto => 'tcp',
         port  => '3306',
         from  => '81.4.109.166',
-    }
-
-    # Create a user to allow db transfers between servers
-    users::user { 'dbcopy':
-        ensure      => present,
-        uid         => 3000,
-        ssh_keys    => [
-            'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDTRdK2A00FRVV60Db+qJLspRw+mYnsG3X+MK3UR6JuK6bmueXA03Y1QNAxGsMIJarvTpuzEU30v/zh4NuQFCCX7vBKQfFxV32SyTIT7OQQpdzh0VlHzGQPq2Oz0fcDDxvCm5cldPZkq/rdQu5Qt395LHSLsiu7hblErlaUfFJ8UPIpIzi87NfaCvZiEad+kcqR5ELoK3LKUbu7vtv+UoCjSzc4eD/OFIuIhXFNk0TlRJppG5XxgnKgL3B1ho/x8i3f6mTwu6zx3IX6tO+0GN00nLVRbOGhZhvDuM2iSeQCKaQ0SbXRsn+DIEt2fUQT5D9xP1uTKB5+/NgWb0L4vVvd/a7rjpVniKWQjzJUxiel4/AjBudDwImP5wN7t8P3+4zYa/ooL8qe15nv40J66LuzRT0MNV4NCjNTrv2lOBMVz+cMy+xFDUtChleoABBQence8iqUvmZ2cH7GrK5IiKbRTjyIesfPmd+ewcRXmIQ0Y/UXTYi1oJqVP+pslQDa3aTgJGSgWvwbRFmQRHwLodAv3QXYT3KKbdPiynEvZ6A7qPkULGfeZ/W/R/JEr70csnHqKqvkz81jnqM9MFw2oDwU2vlhoHBhea8A+SJv38wAAuzpbcTzNQP8feXgKWnHavP6uRDxO8KUbV4LTt2Fveb+livtCGidU4wBtagDfTkgzQ== root@db3'
-        ],
     }
 
     file { '/etc/ssl/private':
