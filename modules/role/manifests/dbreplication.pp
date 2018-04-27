22f56c83a3e18a2323e8c37e37626de2009899cf
Southparkfan <southparkfan223@hotmail.com>
2018-04-27 13:28:53 +0200

    Add mysql instance @ bacula1 for db[x] -> bacula1 MySQL replication (#732)
    * Create dbreplication.pp
    
    * Update db.pp
    
    * Update site.pp
    
    * Tone down to the bare essentials
    
    * Update dbreplication.pp

---
+++ modules/role/manifests/dbreplication.pp
@@ -0,0 +1,46 @@
+# class: role::dbreplication
+class role::dbreplication {
+    include mariadb::packages
+
+    class { 'mariadb::config':
+        config   => 'mariadb/config/mw.cnf.erb',
+        password => hiera('passwords::db::root'),
+    }
+
+    ufw::allow { 'mysql port db2':
+        proto => 'tcp',
+        port  => '3306',
+        from  => '81.4.125.112',
+    }
+
+    ufw::allow { 'mysql port db3':
+        proto => 'tcp',
+        port  => '3306',
+        from  => '81.4.127.157',
+    }
+
+    ufw::allow { 'mysql port db4':
+        proto => 'tcp',
+        port  => '3306',
+        from  => '81.4.109.166',
+    }
+
+    file { '/etc/ssl/private':
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'mysql',
+        mode	=> '0750',
+    }
+
+    include ssl::wildcard
+
+    if $::virtual == 'kvm' {
+        sysctl::parameters { 'avoid swap usage':
+            values  => { 'vm.swappiness' => 1, },
+        }
+    }
+    
+    motd::role { 'role::dbreplication':
+        description => 'replication backup database server',
+    }
+}
