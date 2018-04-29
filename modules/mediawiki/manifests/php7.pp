80940167608817ba2290e1d9652bd13cbdd73787
Paladox <thomasmulhall410@yahoo.com>
2018-04-29 21:33:03 +0100

    Update some php7 packages names in mediawiki

--- modules/mediawiki/manifests/php7.pp
+++ modules/mediawiki/manifests/php7.pp
@@ -1,21 +1,20 @@
 # mediawiki::php7
 class mediawiki::php7 {
     $packages = [
-        'php-pear',
-        'php-mail',
+        'php-imagick',
         'php-luasandbox',
+        'php-mail',
+        'php-pear',
+        'php-redis',
         'php7.0',
         'php7.0-curl',
         'php7.0-fpm',
         'php7.0-gd',
-        'php7.0-imagick',
         'php7.0-intl',
         'php7.0-json',
         'php7.0-mbstring',
         'php7.0-mcrypt',
         'php7.0-mysql',
-        'php7.0-mysqlnd',
-        'php7.0-redis',
     ]
 
     package { $packages:
