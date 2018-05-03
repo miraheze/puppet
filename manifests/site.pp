9cfcae77c1ec00da4770419aba73a1e58edb9b00
paladox <paladox@users.noreply.github.com>
2018-05-03 18:29:08 +0100

    Install role::electron on misc3

--- manifests/site.pp
+++ manifests/site.pp
@@ -57,6 +57,7 @@ node 'misc2.miraheze.org' {
 node 'misc3.miraheze.org' {
     include base
     include role::parsoid
+    include role::electron
     include role::restbase
     include role::salt::masters
     include role::salt::minions
