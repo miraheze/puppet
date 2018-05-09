617be2b0f18c9c635dfe5cc215b4de4dc873a7b9
paladox <paladox@users.noreply.github.com>
2018-05-09 11:57:06 +0100

    Move location

--- modules/acme/manifests/init.pp
+++ modules/acme/manifests/init.pp
@@ -6,13 +6,20 @@ class acme {
         origin    => 'https://github.com/diafygi/acme-tiny.git',
     }
 
+    file { '/root/ssl':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0770',
+    }
+
     file { '/root/ssl-certificate':
         ensure => present,
         source => 'puppet:///modules/acme/ssl-certificate',
         mode   => '0555',
     }
 
-    file { '/root/acme-tiny/account.key':
+    file { '/root/account.key':
         ensure => present,
         source => 'puppet:///private/acme/account.key',
         require => Git::Clone['acme-tiny'],
