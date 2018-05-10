293a75c28dd895a8b0cdd0c33b1f78d5df8fa43c
paladox <paladox@users.noreply.github.com>
2018-05-10 11:55:56 +0100

    Add icinga monitoring for port 10044 (#746)
    * Add icinga monitoring for port 10044
    
    * Update mathoid.pp

--- modules/role/manifests/mathoid.pp
+++ modules/role/manifests/mathoid.pp
@@ -2,6 +2,12 @@
 class role::mathoid {
     include ::mathoid
 
+    ufw::allow { 'mathoid monitoring':
+        proto => 'tcp',
+        port  => 10044,
+        from  => '185.52.1.76',
+    }
+
     motd::role { 'role::mathoid':
         description => 'Mediawiki Mathoid Service',
     }
