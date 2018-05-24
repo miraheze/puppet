c44d0dc34bfd12ccba19bddfb0cb9d622414ac2c
John Lewis <johnflewis93@gmail.com>
2018-05-24 02:45:59 +0100

    add ssl-auto-renew stuff to acme'

--- modules/acme/manifests/init.pp
+++ modules/acme/manifests/init.pp
@@ -24,4 +24,31 @@ class acme {
         source => 'puppet:///private/acme/account.key',
         require => Git::Clone['acme-tiny'],
     }
+
+    file { '/var/lib/nagios/ssl-phabricator':
+        ensure => present,
+        source => 'puppet:///modules/acme/ssl-phabricator',
+        owner  => 'nagios',
+        group  => 'nagios',
+    }
+
+    file { '/var/lib/nagios/LE.crt':
+        ensure => present,
+        source => 'puppet:///modules/acme/LE.crt',
+        owner  => 'nagios',
+        group  => 'nagios',
+    }
+
+    file { '/var/lib/nagios/id_rsa':
+        ensure => present,
+        source => 'puppet:///private/acme/id_rsa',
+        owner  => 'nagios',
+        group  => 'nagios',
+        mode   => '0400',
+    }
+
+    sudo::user { 'nrpe_ssl-certificate':
+        user       => 'nagios',
+        privileges => [ 'ALL = NOPASSWD: /root/ssl-certificate' ],
+    }
 }
