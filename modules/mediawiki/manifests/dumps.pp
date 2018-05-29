138b55a16b937683ca3b2fcc72e6b3b74a2b88bf
Reception123 <utilizator.receptie123@gmail.com>
2018-05-29 19:50:30 +0200

    add montly dump for renaissancewiki

--- modules/mediawiki/manifests/dumps.pp
+++ modules/mediawiki/manifests/dumps.pp
@@ -135,6 +135,16 @@ class mediawiki::dumps {
         month    => '*',
         monthday => ['15', '30'],
     }
+    
+    cron { 'Export renaissancewiki xml dump monthly':
+        ensure   => present,
+        command  => '/usr/bin/nice -n19 /usr/bin/php /srv/mediawiki/w/maintenance/dumpBackup.php --wiki renaissancewiki --logs --full --uploads > /mnt/mediawiki-static/dumps/renaissancewiki.xml',
+        user     => 'www-data',
+        minute   => '0',
+        hour     => '0',
+        month    => '*',
+        monthday => '1',
+    }
 
     cron { 'Export scruffywiki xml dump weekly':
         ensure   => present,
