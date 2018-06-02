bed289cbcbd474522da6c11161d216df1156f950
paladox <paladox@users.noreply.github.com>
2018-06-02 01:22:31 +0100

    Setup oauth_composer exec

--- modules/mediawiki/manifests/extensionsetup.pp
+++ modules/mediawiki/manifests/extensionsetup.pp
@@ -56,6 +56,16 @@ class mediawiki::extensionsetup {
         require     => Git::Clone['MediaWiki core'],
     }
 
+    exec { 'oauth_composer':
+        command     => 'curl -sS https://getcomposer.org/installer | php && php composer.phar install',
+        creates     => '/srv/mediawiki/w/extensions/OAuth/composer.phar',
+        cwd         => '/srv/mediawiki/w/extensions/OAuth',
+        path        => '/usr/bin',
+        environment => 'HOME=/srv/mediawiki/w/extensions/OAuth',
+        user        => 'www-data',
+        require     => Git::Clone['MediaWiki core'],
+    }
+
     # Ensure widgets template directory is read/writeable by webserver if mediawiki is cloned
     file { '/srv/mediawiki/w/extensions/Widgets/compiled_templates':
         ensure  => directory,
