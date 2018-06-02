cabadc99061aa3795d678057e95753e8f43d8830
paladox <paladox@users.noreply.github.com>
2018-06-02 15:26:52 +0100

    fix script

--- modules/icinga/files/ssl-renew.sh
+++ modules/icinga/files/ssl-renew.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ssh -i /home/nagiosre/.ssh/id_rsa nagiosre@mw1.miraheze.org  '/var/lib/nagios/ssl-acme'
+SERVICEDISPLAYNAME="$1"
+SERVICEDESC="$2"
+SERVICESTATE="$3"
+
+ssh -i /home/nagiosre/.ssh/id_rsa nagiosre@mw1.miraheze.org  "/var/lib/nagios/ssl-acme ${SERVICEDISPLAYNAME} ${SERVICEDESC} ${SERVICESTATE}"
