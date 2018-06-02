56dbb04d3859f800f5c685d5ac9a7a62aa563b65
paladox <paladox@users.noreply.github.com>
2018-06-02 17:36:01 +0100

    Update ssl-renew.sh

--- modules/icinga/files/ssl-renew.sh
+++ modules/icinga/files/ssl-renew.sh
@@ -4,4 +4,4 @@ SERVICEDISPLAYNAME="$1"
 SERVICEDESC="$2"
 SERVICESTATE="$3"
 
-ssh -t -i /home/nagiosre/.ssh/id_rsa nagiosre@mw1.miraheze.org  "/var/lib/nagios/ssl-acme ${SERVICEDISPLAYNAME} ${SERVICEDESC} ${SERVICESTATE}"
+ssh -t -i /var/lib/nagios/id_rsa2 nagiosre@mw1.miraheze.org  "/var/lib/nagios/ssl-acme ${SERVICEDISPLAYNAME} ${SERVICEDESC} ${SERVICESTATE}"
