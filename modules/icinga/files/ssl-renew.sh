7ab31dc7e9cd626586cc053410ec505ab372f6f7
paladox <paladox@users.noreply.github.com>
2018-06-02 23:31:03 +0100

    Update script to make sure it will only run when it is in hard check mode

--- modules/icinga/files/ssl-renew.sh
+++ modules/icinga/files/ssl-renew.sh
@@ -2,5 +2,7 @@
 
 SERVICEDISPLAYNAME="$1"
 SERVICESTATE="$2"
+SERVICESTATETYPE="$3"
+SERVICEATTEMPT="$4"
 
-/usr/bin/ssh -t -i  /var/lib/nagios/id_rsa2 nagiosre@mw1.miraheze.org  "/var/lib/nagios/ssl-acme -s ${SERVICESTATE} -u ${SERVICEDISPLAYNAME}" >> /var/log/icinga/ssl-let.log 2>&1
+/usr/bin/ssh -t -i  /var/lib/nagios/id_rsa2 nagiosre@mw1.miraheze.org  "/var/lib/nagios/ssl-acme -a ${SERVICEATTEMPT} -s ${SERVICESTATE} -t {SERVICESTATETYPE} -u ${SERVICEDISPLAYNAME}" >> /var/log/icinga/ssl-let.log 2>&1
