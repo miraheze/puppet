102a0c174264165bbfc6c3ce00a69dd2b07cd6b7
paladox <paladox@users.noreply.github.com>
2018-06-05 19:19:48 +0100

    Replace all extension composer setup with mediawiki_core_composer

--- modules/mediawiki/manifests/extensionsetup.pp
+++ modules/mediawiki/manifests/extensionsetup.pp
@@ -1,79 +1,19 @@
 # MediaWiki extension setup
 class mediawiki::extensionsetup {
+    exec { 'mediawiki_core_composer':
+        command     => 'curl -sS https://getcomposer.org/installer | php && php composer.phar install',
+        creates     => '/srv/mediawiki/w/composer.phar',
+        cwd         => '/srv/mediawiki/w',
+        path        => '/usr/bin',
+        environment => 'HOME=/srv/mediawiki/w',
+        user        => 'www-data',
+        require     => Git::Clone['MediaWiki core'],
+    }
+
     exec { 'Math texvccheck':
         command => '/usr/bin/make --directory=/srv/mediawiki/w/extensions/Math/texvccheck',
         creates => '/srv/mediawiki/w/extensions/Math/texvccheck/texvccheck',
         require => [ Git::Clone['MediaWiki core'], Package['ocaml'] ],
-    }
-
-    exec { 'wikibase_composer':
-        command     => 'curl -sS https://getcomposer.org/installer | php && php composer.phar install',
-        creates     => '/srv/mediawiki/w/extensions/Wikibase/composer.phar',
-        cwd         => '/srv/mediawiki/w/extensions/Wikibase',
-        path        => '/usr/bin',
-        environment => 'HOME=/srv/mediawiki/w/extensions/Wikibase',
-        user        => 'www-data',
-        require     => Git::Clone['MediaWiki core'],
-    }
-
-    exec { 'maps_composer':
-        command     => 'curl -sS https://getcomposer.org/installer | php && php composer.phar install',
-        creates     => '/srv/mediawiki/w/extensions/Maps/composer.phar',
-        cwd         => '/srv/mediawiki/w/extensions/Maps',
-        path        => '/usr/bin',
-        environment => 'HOME=/srv/mediawiki/w/extensions/Maps',
-        user        => 'www-data',
-        require     => Git::Clone['MediaWiki core'],
-    }
-
-    exec { 'flow_composer':
-        command     => 'curl -sS https://getcomposer.org/installer | php && php composer.phar install',
-        creates     => '/srv/mediawiki/w/extensions/Flow/composer.phar',
-        cwd         => '/srv/mediawiki/w/extensions/Flow',
-        path        => '/usr/bin',
-        environment => 'HOME=/srv/mediawiki/w/extensions/Flow',
-        user        => 'www-data',
-        require     => Git::Clone['MediaWiki core'],
-    }
-
-    exec { 'cookiewarning_composer':
-        command     => 'curl -sS https://getcomposer.org/installer | php && php composer.phar install',
-        creates     => '/srv/mediawiki/w/extensions/CookieWarning/composer.phar',
-        cwd         => '/srv/mediawiki/w/extensions/CookieWarning',
-        path        => '/usr/bin',
-        environment => 'HOME=/srv/mediawiki/w/extensions/CookieWarning',
-        user        => 'www-data',
-        require     => Git::Clone['MediaWiki core'],
-    }
-
-    exec { 'syntaxhighlight_geshi_composer':
-        command     => 'curl -sS https://getcomposer.org/installer | php && php composer.phar install',
-        creates     => '/srv/mediawiki/w/extensions/SyntaxHighlight_GeSHi/composer.phar',
-        cwd         => '/srv/mediawiki/w/extensions/SyntaxHighlight_GeSHi',
-        path        => '/usr/bin',
-        environment => 'HOME=/srv/mediawiki/w/extensions/SyntaxHighlight_GeSHi',
-        user        => 'www-data',
-        require     => Git::Clone['MediaWiki core'],
-    }
-
-    exec { 'oauth_composer':
-        command     => 'curl -sS https://getcomposer.org/installer | php && php composer.phar install',
-        creates     => '/srv/mediawiki/w/extensions/OAuth/composer.phar',
-        cwd         => '/srv/mediawiki/w/extensions/OAuth',
-        path        => '/usr/bin',
-        environment => 'HOME=/srv/mediawiki/w/extensions/OAuth',
-        user        => 'www-data',
-        require     => Git::Clone['MediaWiki core'],
-    }
-
-    exec { 'templatestyles_composer':
-        command     => 'curl -sS https://getcomposer.org/installer | php && php composer.phar install',
-        creates     => '/srv/mediawiki/w/extensions/TemplateStyles/composer.phar',
-        cwd         => '/srv/mediawiki/w/extensions/TemplateStyles',
-        path        => '/usr/bin',
-        environment => 'HOME=/srv/mediawiki/w/extensions/TemplateStyles',
-        user        => 'www-data',
-        require     => Git::Clone['MediaWiki core'],
     }
 
     # Ensure widgets template directory is read/writeable by webserver if mediawiki is cloned
