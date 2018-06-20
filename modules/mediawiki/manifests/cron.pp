a61a6ee6e3520f3bce3f5e56de02a75258fb761b
John F. Lewis <johnflewis93@gmail.com>
2018-06-20 08:54:44 +0100

    remove useless cron absents

--- modules/mediawiki/manifests/cron.pp
+++ modules/mediawiki/manifests/cron.pp
@@ -17,19 +17,4 @@ class mediawiki::cron {
         minute  => '0',
         hour    => '23',
     }
-
-    $php7_2 = hiera('mediawiki::use_php_7_2', false)
-    if $php7_2 {
-        cron { 'restart_php72fpm':
-            ensure  => absent,
-            command => '/usr/sbin/service php7.2-fpm restart',
-            minute  => '*/10',
-        }
-    } else {
-        cron { 'restart_php70fpm':
-            ensure  => absent,
-            command => '/usr/sbin/service php7.0-fpm restart',
-            minute  => '*/10',
-        }
-    }
 }
