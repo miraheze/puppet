7c493f0895f02f51367fb4c2c843629923b88a3a
paladox <paladox@users.noreply.github.com>
2018-06-29 19:28:39 +0100

    Add role::swift

---
+++ modules/role/manifests/swift.pp
@@ -0,0 +1,32 @@
+# role: swift
+class role::swift {
+    include ::swift
+
+    ufw::allow { 'swift mw1':
+        proto => 'tcp',
+        port  => 8080,
+        from  => '185.52.1.75',
+    }
+
+    ufw::allow { 'swift mw2':
+        proto => 'tcp',
+        port  => 8080,
+        from  => '185.52.2.113',
+    }
+
+    ufw::allow { 'swift mw3':
+        proto => 'tcp',
+        port  => 8080,
+        from  => '81.4.121.113',
+    }
+
+    ufw::allow { 'swift test1':
+        proto => 'tcp',
+        port  => 8080,
+        from  => '185.52.2.243',
+    }
+
+    motd::role { 'role::swift':
+        description => 'Openstack Swift Object storage Proxy',
+    }
+}
