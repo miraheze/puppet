043ad58fef8f4423732e60c6f86fc5127a8b62c0
paladox <paladox@users.noreply.github.com>
2018-07-04 22:41:52 +0100

    Update swift.pp

--- modules/role/manifests/swift.pp
+++ modules/role/manifests/swift.pp
@@ -3,27 +3,27 @@ class role::swift {
     include ::swift
 
     # http
-    ufw::allow { 'swift mw1 80':
+    ufw::allow { 'swift mw1 8080':
         proto => 'tcp',
-        port  => 80,
+        port  => 8080,
         from  => '185.52.1.75',
     }
 
-    ufw::allow { 'swift mw2 80':
+    ufw::allow { 'swift mw2 8080':
         proto => 'tcp',
-        port  => 80,
+        port  => 8080,
         from  => '185.52.2.113',
     }
 
-    ufw::allow { 'swift mw3 80':
+    ufw::allow { 'swift mw3 8080':
         proto => 'tcp',
-        port  => 80,
+        port  => 8080,
         from  => '81.4.121.113',
     }
 
-    ufw::allow { 'swift test1 80':
+    ufw::allow { 'swift test1 8080':
         proto => 'tcp',
-        port  => 80,
+        port  => 8080,
         from  => '185.52.2.243',
     }
     
@@ -50,6 +50,31 @@ class role::swift {
         proto => 'tcp',
         port  => 443,
         from  => '185.52.2.243',
+    }
+
+    # nfs1 access swift1
+    ufw::allow { 'swift swift1 6000':
+        proto => 'tcp',
+        port  => 6000,
+        from  => '81.4.124.61',
+    }
+
+    ufw::allow { 'swift swift1 6001':
+        proto => 'tcp',
+        port  => 6001,
+        from  => '81.4.124.61',
+    }
+
+    ufw::allow { 'swift swift1 6002':
+        proto => 'tcp',
+        port  => 6002,
+        from  => '81.4.124.61',
+    }
+
+    ufw::allow { 'swift swift1 6003':
+        proto => 'tcp',
+        port  => 6003,
+        from  => '81.4.124.61',
     }
 
     motd::role { 'role::swift':
