b0116cdd30727966ecbf627866116b41d6e95205
paladox <paladox@users.noreply.github.com>
2018-07-06 01:48:28 +0100

    Remove nfs client from mw*

--- modules/role/manifests/mediawiki.pp
+++ modules/role/manifests/mediawiki.pp
@@ -1,7 +1,6 @@
 # role: mediawiki
 class role::mediawiki {
     include ::mediawiki
-    include nfs::client
 
     ufw::allow { 'http port tcp':
         proto => 'tcp',
@@ -17,16 +16,7 @@ class role::mediawiki {
         description => 'MediaWiki server',
     }
 
-    file { '/mnt/mediawiki-static':
-        ensure => directory,
-    }
-
-    mount { '/mnt/mediawiki-static':
-        ensure  => mounted,
-        device  => '81.4.124.61:/srv/mediawiki-static',
-        fstype  => 'nfs',
-        options => 'rw',
-        atboot  => true,
-        require => File['/mnt/mediawiki-static'],
+    package { 'nfs-common':
+        ensure => purged,
     }
 }
