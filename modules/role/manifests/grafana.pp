6ed9262bc9c7a4bd245f9e03fa1a1831fbc1b25a
paladox <paladox@users.noreply.github.com>
2018-07-08 00:12:07 +0100

    Update grafana.pp

--- modules/role/manifests/grafana.pp
+++ modules/role/manifests/grafana.pp
@@ -2,20 +2,6 @@
 class role::grafana {
     include ::grafana
 
-    if !defined(Ufw::Allow['icinga http']) {
-        ufw::allow { 'icinga http':
-            proto => 'tcp',
-            port  => '80',
-        }
-    }
-
-    if !defined(Ufw::Allow['icinga https']) {
-        ufw::allow { 'icinga https':
-            proto => 'tcp',
-            port  => '443',
-        }
-    }
-
     ufw::allow { 'grafana tcp':
         proto => 'tcp',
         port  => 2003,
