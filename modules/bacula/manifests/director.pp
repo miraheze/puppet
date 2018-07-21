7c9ca53fd3f4c27d6e9b5f750ca99b5b8a60c4bc
paladox <paladox@users.noreply.github.com>
2018-07-22 00:30:21 +0100

    Update director.pp

--- modules/bacula/manifests/director.pp
+++ modules/bacula/manifests/director.pp
@@ -119,24 +119,24 @@ class bacula::director {
             },
         }
 
-        icinga2::custom::services { 'Bacula Misc4 Lizardfs':
+        icinga2::custom::services { 'Bacula Misc4 Lizardfs Master':
             check_command => 'nrpe',
             vars          => {
                 nrpe_command => 'check_bacula_misc4_lizardfs',
             },
         }
 
-        icinga2::custom::services { 'Bacula Lizardfs Lizardfs':
+        icinga2::custom::services { 'Bacula Lizardfs1 Lizardfs Chunkserver':
             check_command => 'nrpe',
             vars          => {
-                nrpe_command => 'check_bacula_lizardfs_lizardfs',
+                nrpe_command => 'check_bacula_lizardfs_lizardfs_chunkserver1',
             },
         }
 
-        icinga2::custom::services { 'Bacula Lizardfs2 Lizardfs':
+        icinga2::custom::services { 'Bacula Lizardfs2 Lizardfs Chunkserver':
             check_command => 'nrpe',
             vars          => {
-                nrpe_command => 'check_bacula_lizardfs2_lizardfs',
+                nrpe_command => 'check_bacula_lizardfs2_lizardfs_chunkserver2',
             },
         }
 
@@ -175,18 +175,18 @@ class bacula::director {
         }
 
         icinga::service { 'bacula_misc4_lizardfs':
-            description   => 'Bacula - /var/lib/lizardfs misc4',
+            description   => 'Bacula - misc4 Lizardfs master',
             check_command => 'check_nrpe_1arg!check_bacula_misc4_lizardfs',
         }
 
         icinga::service { 'bacula_lizardfs_lizardfs':
-            description   => 'Bacula - /var/lib/lizardfs lizardfs',
-            check_command => 'check_nrpe_1arg!check_bacula_lizardfs_lizardfs',
+            description   => 'Bacula - lizardfs1 Lizardfs Chunkserver1',
+            check_command => 'check_nrpe_1arg!check_bacula_lizardfs1_lizardfs_chunkserver1',
         }
 
         icinga::service { 'bacula_lizardfs_lizardfs2':
-            description   => 'Bacula - /var/lib/lizardfs lizardfs2',
-            check_command => 'check_nrpe_1arg!check_bacula_lizardfs_lizardfs2',
+            description   => 'Bacula - lizardfs2 Lizardfs Chunkserver2',
+            check_command => 'check_nrpe_1arg!check_bacula_lizardfs2_lizardfs2_chunkserver2',
         }
 
         icinga::service { 'bacula_phabstatic':
