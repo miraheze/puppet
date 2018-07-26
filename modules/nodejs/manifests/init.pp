8b107706c77eee3bfb314226d4de504da56fef2b
paladox <paladox@users.noreply.github.com>
2018-07-26 15:29:21 +0100

    Update init.pp

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -8,6 +8,14 @@ class nodejs {
         release  => "${::lsbdistcodename}",
         repos    => 'main',
         key      => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
+        notify   => Exec['apt_update_nodejs'],
+    }
+ 
+    # First installs can trip without this
+    exec {'apt_update_nodejs':
+        command     => '/usr/bin/apt-get update',
+        refreshonly => true,
+        logoutput   => true,
     }
 
     require_package('nodejs')
