ea615648924940d9abed054759be2673a8a61168
paladox <paladox@users.noreply.github.com>
2018-07-28 18:57:04 +0100

    Update icinga2.pp

--- modules/role/manifests/icinga2.pp
+++ modules/role/manifests/icinga2.pp
@@ -2,14 +2,18 @@
 class role::icinga2 {
     include ::icinga2
 
-    ufw::allow { 'icinga2 http':
-        proto => 'tcp',
-        port  => '80',
+    if !defined(Exec['ufw-allow-tcp-from-any-to-any-port-80']) {
+        ufw::allow { 'icinga2 http':
+            proto => 'tcp',
+            port  => '80',
+        }
     }
 
-    ufw::allow { 'icinga2 https':
-        proto => 'tcp',
-        port  => '443',
+    if !defined(Exec['ufw-allow-tcp-from-any-to-any-port-443']) {
+        ufw::allow { 'icinga2 https':
+            proto => 'tcp',
+            port  => '443',
+        }
     }
 
     motd::role { 'role::icinga2':
