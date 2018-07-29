0f992c15d360b8fb55d472726d5fa24e1cc84755
paladox <paladox@users.noreply.github.com>
2018-07-29 20:52:26 +0100

    Add ServicesSettings

--- modules/mediawiki/manifests/init.pp
+++ modules/mediawiki/manifests/init.pp
@@ -114,6 +114,14 @@ class mediawiki(
         require => Git::Clone['MediaWiki config'],
     }
 
+    file { '/srv/mediawiki/config/ServicesSettings.php':
+        ensure  => 'present',
+        content => template('mediawiki/ServicesSettings.php'),
+        require => Git::Clone['MediaWiki config'],
+    }
+
+    $service_wikis = loadyaml('/etc/puppet/services/services.yaml')
+
     file { '/usr/local/bin/foreachwikiindblist':
         ensure => 'present',
         mode   => '0755',
