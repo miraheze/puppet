d470394611663cf00fc6f2a600232c0df7a72a1f
paladox <paladox@users.noreply.github.com>
2018-08-01 21:32:05 +0100

    Move pushServices.sh cron to it's own class.

--- modules/mediawiki/files/bin/pushServices.sh
+++ modules/mediawiki/files/bin/pushServices.sh
@@ -4,20 +4,16 @@
 set -e
 set -u
 
-fqn=$(hostname -f)
-
-if [[ "${fqn}" == "mw1.miraheze.org" ]];
-then
-        if [ ! -d /srv/mediawiki/services/ ]; then
-                cd /srv/mediawiki/ && GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -i /var/lib/nagios/id_rsa -F /dev/null' git clone git@github.com:miraheze/services.git && cd /srv/mediawiki/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /var/lib/nagios/id_rsa -F /dev/null"
-        else
-                cd /srv/mediawiki/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /var/lib/nagios/id_rsa -F /dev/null" && git reset --hard origin/master && git pull
-        fi
-        git -C /srv/mediawiki/services/ config user.email "noreply@miraheze.org"
-        git -C /srv/mediawiki/services/ config user.name "MirahezeSSLBot"
-        git -C /srv/mediawiki/services/ add -A --all
-        git -C /srv/mediawiki/services/ commit -m "Update config for wiki's"
-        git -C /srv/mediawiki/services/ push origin master
+if [ ! -d /srv/mediawiki/services/ ]; then
+        cd /srv/mediawiki/ && GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -i /var/lib/nagios/id_rsa -F /dev/null' git clone git@github.com:miraheze/services.git && cd /srv/mediawiki/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /var/lib/nagios/id_rsa -F /dev/null"
+else
+        cd /srv/mediawiki/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /var/lib/nagios/id_rsa -F /dev/null" && git reset --hard origin/master && git pull
 fi
+git -C /srv/mediawiki/services/ config user.email "noreply@miraheze.org"
+git -C /srv/mediawiki/services/ config user.name "MirahezeSSLBot"
+/usr/bin/php /srv/mediawiki/w/extensions/MirahezeMagic/maintenance/addWikiToServices.php --wiki=metawiki
+git -C /srv/mediawiki/services/ add -A --all
+git -C /srv/mediawiki/services/ commit -m "Update config for wiki's"
+git -C /srv/mediawiki/services/ push origin master
 
 exit 0
