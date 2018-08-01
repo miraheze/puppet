6d1cfb3de832f0042f82cae083834154eeeee08a
paladox <paladox@users.noreply.github.com>
2018-08-01 23:28:08 +0100

    Update pushServices.sh

--- modules/mediawiki/files/bin/pushServices.sh
+++ modules/mediawiki/files/bin/pushServices.sh
@@ -9,11 +9,17 @@ if [ ! -d /srv/mediawiki/services/ ]; then
 else
         cd /srv/mediawiki/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /var/lib/nagios/id_rsa -F /dev/null" && git reset --hard origin/master && git pull
 fi
+
 git -C /srv/mediawiki/services/ config user.email "noreply@miraheze.org"
+
 git -C /srv/mediawiki/services/ config user.name "MirahezeSSLBot"
+
 /usr/bin/php /srv/mediawiki/w/extensions/MirahezeMagic/maintenance/addWikiToServices.php --wiki=metawiki
+
 git -C /srv/mediawiki/services/ add -A --all
+
 git -C /srv/mediawiki/services/ commit -m "Update config for wiki's"
+
 git -C /srv/mediawiki/services/ push origin master
 
 exit 0
