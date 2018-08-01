a27d4087091e53887a0c7c10f36b49f71c0d03ad
paladox <paladox@users.noreply.github.com>
2018-08-01 21:52:07 +0100

    Create services_cron.pp

---
+++ modules/mediawiki/manifests/services_cron.pp
@@ -0,0 +1,10 @@
+# class: mediawiki::services_cron
+class mediawiki::services_cron {
+      cron { 'generate_services':
+          ensure  => present,
+          command => '/bin/bash /usr/local/bin/pushServices.sh',
+          user    => 'www-data',
+          minute  => '*/10',
+          hour    => '*',
+      }
+}
