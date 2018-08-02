fb876804daf6af343ed265991d05e09e1e0858c7
Paladox <thomasmulhall410@yahoo.com>
2018-08-02 17:55:08 +0100

    Update ssl and services related things
    * This moves a ssh key out of acme and into a services_cron class for use
      by the services script.
    
    * Update location of where to git clone services repo to update and push it.

--- modules/acme/manifests/init.pp
+++ modules/acme/manifests/init.pp
@@ -51,15 +51,7 @@ class acme {
         ensure => present,
         source => 'puppet:///private/acme/id_rsa',
         owner  => 'nagiosre',
-        group  => 'www-data',
-        mode   => '0440',
-    }
-
-    file { '/var/lib/nagios/id_rsa2':
-        ensure => present,
-        source => 'puppet:///private/acme/id_rsa',
-        owner  => 'nagiosre',
-        group  => 'www-data',
+        group  => 'nagiosre',
         mode   => '0400',
     }
 
--- modules/mediawiki/files/bin/pushServices.sh
+++ modules/mediawiki/files/bin/pushServices.sh
@@ -4,22 +4,22 @@
 set -e
 set -u
 
-if [ ! -d /srv/mediawiki/services/ ]; then
-        cd /srv/mediawiki/ && GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -i /var/lib/nagios/id_rsa -F /dev/null' git clone git@github.com:miraheze/services.git && cd /srv/mediawiki/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /var/lib/nagios/id_rsa -F /dev/null"
+if [ ! -d /srv/services/services/ ]; then
+        cd /srv/services/ && GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -i /srv/services/id_rsa -F /dev/null' git clone git@github.com:miraheze/services.git && cd /srv/services/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /srv/services/id_rsa -F /dev/null"
 else
-        cd /srv/mediawiki/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /var/lib/nagios/id_rsa -F /dev/null" && git reset --hard origin/master && git pull
+        cd /srv/services/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /srv/services/id_rsa -F /dev/null" && git reset --hard origin/master && git pull
 fi
 
-git -C /srv/mediawiki/services/ config user.email "noreply@miraheze.org"
+git -C /srv/services/services/ config user.email "noreply@miraheze.org"
 
-git -C /srv/mediawiki/services/ config user.name "MirahezeSSLBot"
+git -C /srv/services/services/ config user.name "MirahezeSSLBot"
 
 /usr/bin/php /srv/mediawiki/w/extensions/MirahezeMagic/maintenance/addWikiToServices.php --wiki=metawiki
 
-git -C /srv/mediawiki/services/ add -A --all
+git -C /srv/services/services/ add -A --all
 
-git -C /srv/mediawiki/services/ commit -m "BOT: Updating services config for wikis"
+git -C /srv/services/services/ commit -m "BOT: Updating services config for wikis"
 
-git -C /srv/mediawiki/services/ push origin master
+git -C /srv/services/services/ push origin master
 
 exit 0
