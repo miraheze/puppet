f0a7b86a19837a34f7a3b9eab66fc93a54660c9a
paladox <paladox@users.noreply.github.com>
2018-08-02 17:41:31 +0100

    Update pushServices.sh

--- modules/mediawiki/files/bin/pushServices.sh
+++ modules/mediawiki/files/bin/pushServices.sh
@@ -5,9 +5,9 @@ set -e
 set -u
 
 if [ ! -d /srv/mediawiki/services/ ]; then
-        cd /srv/mediawiki/ && GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -i /var/lib/nagios/id_rsa -F /dev/null' git clone git@github.com:miraheze/services.git && cd /srv/mediawiki/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /var/lib/nagios/id_rsa -F /dev/null"
+        cd /srv/mediawiki/ && GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -i /srv/services/id_rsa -F /dev/null' git clone git@github.com:miraheze/services.git && cd /srv/mediawiki/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /srv/services/id_rsa -F /dev/null"
 else
-        cd /srv/mediawiki/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /var/lib/nagios/id_rsa -F /dev/null" && git reset --hard origin/master && git pull
+        cd /srv/mediawiki/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /srv/services/id_rsa -F /dev/null" && git reset --hard origin/master && git pull
 fi
 
 git -C /srv/mediawiki/services/ config user.email "noreply@miraheze.org"
