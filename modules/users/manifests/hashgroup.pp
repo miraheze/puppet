250d6a2ec989ae9b2a083bf25df62251c53ba184
paladox <paladox@users.noreply.github.com>
2018-08-05 15:10:26 +0100

    Update hashgroup.pp

--- modules/users/manifests/hashgroup.pp
+++ modules/users/manifests/hashgroup.pp
@@ -1,15 +1,21 @@
-# class: user::hashgroup
+# A defined type for group creation / user realization from hash
+#
+# === Parameters
+#
+# [*name*]
+#  Hash group name
+#
 define users::hashgroup(
-    $phash={},
 )
 {
+
     #explicit error as otherwise it goes forward later
     #complaining of 'invalid hash' which is hard to track down
-    if !has_key($phash['groups'], $name) {
+    if !has_key($::users::data['groups'], $name) {
         fail("${name} is not a valid group name")
     }
 
-    $gdata = $phash['groups'][$name]
+    $gdata = $::users::data['groups'][$name]
     if has_key($gdata, 'posix_name') {
         $group_name = $gdata['posix_name']
     } else {
