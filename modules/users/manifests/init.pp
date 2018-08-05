3b869e32f5fe5658e5642c7d072755a5ae83cea5
paladox <paladox@users.noreply.github.com>
2018-08-05 15:08:52 +0100

    Update init.pp

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -1,31 +1,59 @@
-# class: user
+# Creates groups, users, and sudo permissions all from yaml for valid passed group name
+#
+# === Parameters
+#
+# [*$groups*]
+#  Array of valid groups (defined in yaml) to create with associated members
+#
+# [*$always_groups*]
+#  Array of valid groups to always run
+#
+
 class users(
     $groups=[],
-    $always_groups=['ops'],
+    $always_groups=['absent', 'ops'],
 )
 {
+    include ::sudo
+
     $module_path = get_module_path($module_name)
-    $data = loadyaml("${module_path}/data/data.yaml")
+    $base_data = loadyaml("${module_path}/data/data.yaml")
+    # Fill the all-users group with all active users
+    $data = add_all_users($base_data)
+
     $uinfo = $data['users']
     $users = keys($uinfo)
 
-    # making sure to include always_groups
+    #making sure to include always_groups
     $all_groups = concat($always_groups, $groups)
 
-    # this custom function eliminates the need for virtual users
+    #this custom function eliminates the need for virtual users
     $user_set = unique_users($data, $all_groups)
 
+
+    file { '/usr/local/sbin/enforce-users-groups':
+        ensure => file,
+        mode   => '0555',
+        source => 'puppet:///modules/users/enforce-users-groups.sh',
+    }
+
     users::hashgroup { $all_groups:
-        phash  => $data,
         before => Users::Hashuser[$user_set],
     }
 
     users::hashuser { $user_set:
-        phash  => $data,
         before => Users::Groupmembers[$all_groups],
     }
 
     users::groupmembers { $all_groups:
-        phash  => $data,
+        before => Exec['enforce-users-groups-cleanup'],
+    }
+
+    # Declarative gotcha: non-defined users can get left behind
+    # Here we cleanup anyone not in a supplementary group above a certain UID
+    exec { 'enforce-users-groups-cleanup':
+        command   => '/usr/local/sbin/enforce-users-groups',
+        unless    => '/usr/local/sbin/enforce-users-groups dryrun',
+        logoutput => true,
     }
 }
