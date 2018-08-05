341ca7ec03636b6bd371f53fd6653f557811f550
paladox <paladox@users.noreply.github.com>
2018-08-05 02:54:36 +0100

    Update redis.pp

--- modules/role/manifests/redis.pp
+++ modules/role/manifests/redis.pp
@@ -2,7 +2,7 @@
 class role::redis {
     class { '::redis':
         password  => hiera('passwords::redis::master'),
-        maxmemory => '600mb',
+        maxmemory => '640mb',
     }
 
     ufw::allow { 'redis':
