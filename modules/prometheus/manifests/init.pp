2cdf99031ca2d89877a563ce9dea50ee1060a43c
paladox <paladox@users.noreply.github.com>
2018-08-17 01:32:23 +0100

    Add icinga check to make sure that prometheus is running

--- modules/prometheus/manifests/init.pp
+++ modules/prometheus/manifests/init.pp
@@ -17,4 +17,18 @@ class prometheus {
         require   => Package['prometheus'],
         subscribe => File['/etc/prometheus/prometheus.yml'],
     }
+
+    if hiera('base::monitoring::use_icinga2', false) {
+        icinga2::custom::services { 'Prometheus':
+            check_command => 'tcp',
+            vars          => {
+                tcp_port    => '9090',
+            },
+        }
+    } else {
+        icinga::service { 'prometheus':
+            description   => 'Prometheus',
+            check_command => 'check_tcp!9090',
+        }
+    }
 }
