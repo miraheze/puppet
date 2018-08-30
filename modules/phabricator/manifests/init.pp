3488b71cd318a1e159d6b777ea2b7f9643f74530
paladox <paladox@users.noreply.github.com>
2018-08-30 02:41:54 +0100

    Update init.pp

--- modules/phabricator/manifests/init.pp
+++ modules/phabricator/manifests/init.pp
@@ -120,14 +120,6 @@ class phabricator {
         require => [File['/etc/systemd/system/phd.service'], File['/srv/phab/phabricator/conf/local/local.json']],
     }
 
-    icinga2::custom::services { 'phabricator.miraheze.org HTTP':
-        check_command => 'check_http',
-        vars         => {
-            address  => "phabricator.miraheze.org",
-            http_ssl => false,
-        },
-    }
-
     if hiera('base::monitoring::use_icinga2', false) {
         icinga2::custom::services { 'phd':
             check_command => 'nrpe',
