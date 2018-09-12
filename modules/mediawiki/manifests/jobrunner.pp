5fc585c0450f10f31e441e0d96a9e6d5ea1cdab4
paladox <paladox@users.noreply.github.com>
2018-09-12 20:49:58 +0100

    Add sudo rules for check_jobqueue

--- modules/mediawiki/manifests/jobrunner.pp
+++ modules/mediawiki/manifests/jobrunner.pp
@@ -108,4 +108,9 @@ class mediawiki::jobrunner {
             nrpe_command => 'check_jobqueue',
         },
     }
+
+    sudo::user { 'nrpe_sudo_check_jobqueue':
+        user       => 'nagios',
+        privileges => [ 'ALL = NOPASSWD: /usr/lib/nagios/plugins/check_jobqueue' ],
+    }
 }
