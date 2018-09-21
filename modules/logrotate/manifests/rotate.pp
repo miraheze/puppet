911b596e46f856151888616337838c751335762b
paladox <paladox@users.noreply.github.com>
2018-09-21 20:20:27 +0100

    Update rotate.pp

--- modules/logrotate/manifests/rotate.pp
+++ modules/logrotate/manifests/rotate.pp
@@ -2,10 +2,10 @@
 #
 # Simple class to help make logrotation files easily
 define logrotate::rotate(
-    $logs   = undef,
-    $time   = 'weekly',
-    $rotate = '12',
-    $delay  = true,
+    Optional[String] $logs   = undef,
+    Enum['daily', 'weekly', 'monthly'] $time   = 'weekly',
+    String $rotate = '12',
+    Boolean $delay  = true,
 ) {
     file { "log_rotate_${title}":
         ensure  => present,
