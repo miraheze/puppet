ee69512f0406e1847b3498135b8da2394a4bc0ad
paladox <paladox@users.noreply.github.com>
2018-09-21 21:29:07 +0100

    Convert redis to puppet types per T3635

--- modules/redis/manifests/init.pp
+++ modules/redis/manifests/init.pp
@@ -1,10 +1,10 @@
 # class: redis
 class redis (
-    $port = 6379,
-    $maxmemory = '512mb',
-    $maxmemory_policy = 'volatile-lfu',
-    $maxmemory_samples = 5,
-    $password = false,
+    Integer$port = 6379,
+    String $maxmemory = '512mb',
+    String $maxmemory_policy = 'volatile-lfu',
+    Integer $maxmemory_samples = 5,
+    Boolean $password = false,
 ) {
     apt::pin { 'debian_stretch_backports_redis':
         priority   => 740,
