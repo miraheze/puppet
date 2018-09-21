d68e7319ace5a173e6dc7a66bdc4c8a14cf648be
paladox <paladox@users.noreply.github.com>
2018-09-21 22:12:36 +0100

    Update allow_multi.pp

--- modules/ufw/manifests/allow_multi.pp
+++ modules/ufw/manifests/allow_multi.pp
@@ -1,9 +1,9 @@
 # ufw::allow_multi
 define ufw::allow_multi(
-    $proto='tcp',
-    $port='all',
-    $ip='',
-    $from_array=['any']
+    String $proto = 'tcp',
+    String $port = 'all',
+    String $ip = '',
+    Array[String] $from_array = ['any'],
 ) {
     $from_array.each |String $from| {
         ufw::allow { "ufw allow from ${from} to ${port}":
