61bb969498024f95f66fdd78698f5aa181a68a81
paladox <paladox@users.noreply.github.com>
2018-09-23 19:25:18 +0100

    More fixes to ssl module (#851)
    * More fixes to ssl module
    
    * Update certs.pp
    
    * Update wildcard.pp

--- modules/ssl/manifests/wildcard.pp
+++ modules/ssl/manifests/wildcard.pp
@@ -1,16 +1,27 @@
 # class: ssl::wildcard
 class ssl::wildcard {
+
+    if defined(Package['nginx']) {
+        $restart = 'nginx'
+    } else {
+        $restart = 'apache2'
+    }
+
     if !defined(File['wildcard.miraheze.org']) {
         file { 'wildcard.miraheze.org':
             ensure => 'present',
             source => 'puppet:///ssl/certificates/wildcard.miraheze.org.crt',
             path   => '/etc/ssl/certs/wildcard.miraheze.org.crt',
+            notify => Exec["${restart}-syntax"],
         }
+    }
 
+    if !defined(File['wildcard.miraheze.org_private']) {
         file { 'wildcard.miraheze.org_private':
             ensure => 'present',
             source => 'puppet:///ssl-keys/wildcard.miraheze.org.key',
             path   => '/etc/ssl/private/wildcard.miraheze.org.key',
+            notify => Exec["${restart}-syntax"],
         }
     }
 }
