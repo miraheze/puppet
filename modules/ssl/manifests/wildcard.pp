78a41ede1ab1f4c93bd94c94f7f3b003a587218e
paladox <paladox@users.noreply.github.com>
2018-09-23 19:24:35 +0100

    Update wildcard.pp

--- modules/ssl/manifests/wildcard.pp
+++ modules/ssl/manifests/wildcard.pp
@@ -1,10 +1,18 @@
 # class: ssl::wildcard
 class ssl::wildcard {
+
+    if defined(Package['nginx']) {
+        $restart = 'nginx'
+    } else {
+        $restart = 'apache2'
+    }
+
     if !defined(File['wildcard.miraheze.org']) {
         file { 'wildcard.miraheze.org':
             ensure => 'present',
             source => 'puppet:///ssl/certificates/wildcard.miraheze.org.crt',
             path   => '/etc/ssl/certs/wildcard.miraheze.org.crt',
+            notify => Exec["${restart}-syntax"],
         }
     }
 
@@ -13,6 +21,7 @@ class ssl::wildcard {
             ensure => 'present',
             source => 'puppet:///ssl-keys/wildcard.miraheze.org.key',
             path   => '/etc/ssl/private/wildcard.miraheze.org.key',
+            notify => Exec["${restart}-syntax"],
         }
     }
 }
