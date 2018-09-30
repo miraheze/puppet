c34d0c37f2f079b0bd5d970a146eeda36c386ce6
paladox <paladox@users.noreply.github.com>
2018-09-30 16:34:01 +0100

    Puppet log to file

--- modules/base/manifests/puppet.pp
+++ modules/base/manifests/puppet.pp
@@ -1,14 +1,24 @@
 # class base::puppet
 class base::puppet {
+    
+    require_package('puppet', 'facter')
+
+    # facter needs this for proper "virtual"/"is_virtual" resolution
+    require_package('virt-what')
 
     $puppetmaster_hostname = hiera('puppetmaster_hostname', 'puppet1.miraheze.org')
     $puppetmaster_version = hiera('puppetmaster_version', 4)
 
     cron { 'puppet-agent':
-        command => '/usr/bin/puppet agent -tv',
+        command => '/usr/bin/puppet agent -tv >> /var/log/puppet/puppet.log',
         user    => 'root',
         hour    => '*',
         minute  => '*/10',
+    }
+
+    logrotate::conf { 'puppet':
+        ensure => present,
+        source => 'puppet:///modules/base/puppet/puppet.logrotate.conf',
     }
 
     file { '/root/puppet-run':
