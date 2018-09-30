39ae75c06b25d54dded0a4fbf2735b4b717b4084
paladox <paladox@users.noreply.github.com>
2018-09-30 16:36:42 +0100

    Puppet log to file (#859)
    * Puppet log to file
    
    * Update puppet.pp
    
    * Create puppet.logrotate.conf

--- modules/base/manifests/puppet.pp
+++ modules/base/manifests/puppet.pp
@@ -1,14 +1,31 @@
 # class base::puppet
 class base::puppet {
+    
+    require_package('puppet', 'facter')
+
+    # facter needs this for proper "virtual"/"is_virtual" resolution
+    require_package('virt-what')
 
     $puppetmaster_hostname = hiera('puppetmaster_hostname', 'puppet1.miraheze.org')
     $puppetmaster_version = hiera('puppetmaster_version', 4)
 
+    file { '/var/log/puppet':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
     cron { 'puppet-agent':
-        command => '/usr/bin/puppet agent -tv',
+        command => '/usr/bin/puppet agent -tv >> /var/log/puppet/puppet.log',
         user    => 'root',
         hour    => '*',
         minute  => '*/10',
+    }
+
+    logrotate::conf { 'puppet':
+        ensure => present,
+        source => 'puppet:///modules/base/puppet/puppet.logrotate.conf',
     }
 
     file { '/root/puppet-run':
