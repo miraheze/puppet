47f4eb214042a777456f9356bb1f3ad25818d7d4
paladox <paladox@users.noreply.github.com>
2018-10-05 12:42:56 +0100

    Fix script

--- modules/icinga2/files/scripts/ssl-renew.sh
+++ modules/icinga2/files/scripts/ssl-renew.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 curl -X POST -H "Content-Type: application/json" -d '{
-  "SERVICEATTEMPT": "${SERVICEATTEMPT}",
-  "SERVICESTATE": "${SERVICESTATE}",
-  "SERVICESTATETYPE": "${SERVICESTATETYPE}",
-  "SERVICEDESC": "${SERVICEDESC}"
+  "SERVICEATTEMPT": "'${1}'",
+  "SERVICESTATE": "'${2}'",
+  "SERVICESTATETYPE": "'${3}'",
+  "SERVICEDESC": "'${4}'"
 }' http://185.52.1.75:5000/renew >> /var/log/icinga2/ssl-let.log 2>&1
