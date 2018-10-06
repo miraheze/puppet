46551e149dbd7505ccd498a193492870f62a80c6
paladox <paladox@users.noreply.github.com>
2018-10-06 18:34:40 +0100

    Add prometheus::blackbox_exporter (#871)
    * Add prometheus::blackbox_exporter
    
    * Create blackbox_exporter.yml.erb

---
+++ modules/prometheus/manifests/blackbox_exporter.pp
@@ -0,0 +1,27 @@
+# Prometheus black box metrics exporter. See also
+# https://github.com/prometheus/blackbox_exporter
+#
+# This does 'active' checks over TCP / UDP / ICMP / HTTP / DNS
+# and reports status to the prometheus scraper
+# from https://github.com/wikimedia/puppet/blob/b347052863d4d2e87b37d6c2d9f44f833cfd9dc2/modules/prometheus/manifests/blackbox_exporter.pp
+
+class prometheus::blackbox_exporter {
+
+    require_package('prometheus-blackbox-exporter')
+
+    file { '/etc/prometheus/blackbox.yml':
+        ensure  => present,
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'root',
+        content => template('prometheus/blackbox_exporter.yml.erb'),
+        notify  => Service['prometheus-blackbox-exporter'],
+    }
+
+    service { 'prometheus-blackbox-exporter':
+        ensure     => running,
+        hasrestart => true,
+        provider   => 'systemd',
+        require    => Package['prometheus-blackbox-exporter'],
+    }
+}
