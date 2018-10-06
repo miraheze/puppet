9b2f090785bbcfb26293855b06f6a6683e58849c
paladox <paladox@users.noreply.github.com>
2018-10-06 16:14:15 +0100

    Add prometheus::node_gdnsd to prometheus

---
+++ modules/prometheus/manifests/node_gdnsd.pp
@@ -0,0 +1,36 @@
+# = Class: prometheus::node_gdnsd
+#
+# Periodically export gdnsd stats via node-exporter textfile collector.
+#
+# Why not a gdnsd_exporter?
+#
+# In WMF's case there aren't a lot of machines with gdnsd deployed as of Dec 2016.
+# Also, having an exporter would have the added benefit of being able to
+# aggregate stats on other dimensions rather than per-cluster or per-site.
+# from https://github.com/wikimedia/puppet/blob/production/modules/prometheus/manifests/node_gdnsd.pp
+
+class prometheus::node_gdnsd (
+    Stdlib::Ensure $ensure = 'present',
+    String $outfile = '/var/lib/prometheus/node.d/gdnsd.prom',
+) {
+
+    require_package( [
+        'python-prometheus-client',
+        'python-requests',
+    ] )
+
+    file { '/usr/local/bin/prometheus-gdnsd-stats':
+        ensure => file,
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/prometheus/usr/local/bin/prometheus-gdnsd-stats',
+    }
+
+    # Collect every minute
+    cron { 'prometheus_gdnsd_stats':
+        ensure  => $ensure,
+        user    => 'root',
+        command => "/usr/local/bin/prometheus-gdnsd-stats --outfile ${outfile}",
+    }
+}
