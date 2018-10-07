2a9bcb3f5688bd795e1c031113b4a5f4cb6204e0
Paladox <thomasmulhall410@yahoo.com>
2018-10-07 22:11:55 +0100

    Add php-fpm prometheus exporter

--- modules/prometheus/manifests/node_gdnsd.pp
+++ modules/prometheus/manifests/node_gdnsd.pp
@@ -24,7 +24,7 @@ class prometheus::node_gdnsd (
         mode   => '0555',
         owner  => 'root',
         group  => 'root',
-        source => 'puppet:///modules/prometheus/usr/local/bin/prometheus-gdnsd-stats',
+        source => 'puppet:///modules/prometheus/gdnsd/prometheus-gdnsd-stats',
     }
 
     # Collect every minute
---
+++ modules/prometheus/manifests/php_fpm.pp
@@ -0,0 +1,31 @@
+# = Class: prometheus::php_fpm
+#
+class prometheus::php_fpm {
+
+    file { '/usr/local/bin/prometheus-phpfpm-exporter':
+        ensure => file,
+        mode   => '0555',
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/prometheus/php_fpm/phpfpm_exporter',
+    }
+
+    exec { 'prometheus-php-fpm reload systemd':
+        command     => '/bin/systemctl daemon-reload',
+        refreshonly => true,
+    }
+
+    file { '/etc/systemd/system/prometheus-php-fpm.service':
+        ensure => present,
+        source => 'puppet:///modules/prometheus/prometheus-php-fpm.systemd',
+        notify => Exec['prometheus-php-fpm reload systemd'],
+    }
+
+    service { 'prometheus-php-fpm':
+        ensure  => 'running',
+        require => [
+            File['/etc/systemd/system/prometheus-php-fpm.service'],
+            File['/usr/local/bin/prometheus-phpfpm-exporter']
+        ],
+    }
+}
