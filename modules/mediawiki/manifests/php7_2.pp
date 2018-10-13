9ea6dc46fb162fcf66836b86b691807ca1d5fe40
paladox <paladox@users.noreply.github.com>
2018-10-13 14:50:20 +0100

    Update php7_2.pp

--- modules/mediawiki/manifests/php7_2.pp
+++ modules/mediawiki/manifests/php7_2.pp
@@ -1,48 +1,6 @@
-# mediawiki::php7_2
-class mediawiki::php7_2 {
-    include ::apt
-
-    if !defined(Apt::Source['php72_apt']) {
-        apt::key { 'php72_key':
-          id     => 'DF3D585DB8F0EB658690A554AC0E47584A7A714D',
-          source => 'https://packages.sury.org/php/apt.gpg',
-        }
-
-        apt::source { 'php72_apt':
-          location => 'https://packages.sury.org/php/',
-          release  => "${::lsbdistcodename}",
-          repos    => 'main',
-          notify   => Exec['apt_update_php'],
-        }
-
-        # First installs can trip without this
-        exec {'apt_update_php':
-            command     => '/usr/bin/apt-get update',
-            refreshonly => true,
-            logoutput   => true,
-        }
-    }
-
-    $packages = [
-        'php-mail',
-        'php-igbinary',
-        'php-imagick',
-        'php-luasandbox',
-        'php-pear',
-        'php-redis',
-        'php7.2',
-        'php7.2-curl',
-        'php7.2-dba',
-        'php7.2-fpm',
-        'php7.2-gd',
-        'php7.2-intl',
-        'php7.2-json',
-        'php7.2-mbstring',
-        'php7.2-mysql',
-        'php7.2-xml',
-    ]
-    
-    require_package($packages)
+# mediawiki::php
+class mediawiki::php {
+    include ::php
 
     service { 'php7.2-fpm':
         ensure  => running,
@@ -53,14 +11,6 @@ class mediawiki::php7_2 {
         ensure  => 'present',
         mode    => '0755',
         source  => 'puppet:///modules/mediawiki/php/php-fpm-7.2.conf',
-        require => Package['php7.2-fpm'],
-        notify  => Service['php7.2-fpm'],
-    }
-
-    file { '/etc/php/7.2/fpm/pool.d/www.conf':
-        ensure  => 'present',
-        mode    => '0755',
-        source  => 'puppet:///modules/mediawiki/php/www-7.2.conf',
         require => Package['php7.2-fpm'],
         notify  => Service['php7.2-fpm'],
     }
