fae71f82ba1ee241c3580ecc57d63ade7a820bd9
paladox <paladox@users.noreply.github.com>
2018-10-28 00:08:38 +0100

    Move RottenLinks cron to jobrunner class

--- modules/mediawiki/manifests/cron.pp
+++ modules/mediawiki/manifests/cron.pp
@@ -17,15 +17,4 @@ class mediawiki::cron {
         minute  => '0',
         hour    => '23',
     }
-
-    if $::fqdn === "mw1.miraheze.org" {
-        cron { 'RotenLinks updateExternalLinks.php on all wikis':
-            ensure  => present,
-            command => '/usr/bin/nice -19 /usr/local/bin/foreachwikiindblist /srv/mediawiki/dblist/all.dblist /srv/mediawiki/w/extensions/RottenLinks/maintenance/updateExternalLinks.php',
-            user    => 'www-data',
-            minute  => '*',
-            hour    => '23',
-            weekday => ['0', '4'],
-        }
-    }
 }
