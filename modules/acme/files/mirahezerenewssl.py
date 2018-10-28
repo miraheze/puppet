8c626429f5b79584e3b4220be74ba33fa959abbc
paladox <paladox@users.noreply.github.com>
2018-10-28 13:57:28 +0000

    Fix file locking in mirahezerenewssl

--- modules/acme/files/mirahezerenewssl.py
+++ modules/acme/files/mirahezerenewssl.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 from flask import Flask
 from flask import request
 import os
-import time
 
 app = Flask(__name__)
 
@@ -20,18 +19,18 @@ def post():
     lock = FileLock(filename)
 
     while not lock_acquired:
-        try:
-            with lock.acquire():
-                os.system("/var/lib/nagios/ssl-acme -s {} -t {} -u {} >> /var/log/acme/ssl-renew.log 2>&1".format(
+        with lock:
+            lock.acquire()
+            try:
+                os.system("/var/lib/nagios/ssl-acme -s {} -t {} -u {} > /var/log/acme/ssl-renew.log 2>&1".format(
                     content['SERVICESTATE'],
                     content['SERVICESTATETYPE'],
                     content['SERVICEDESC']
                 ))
-                time.sleep(2)
                 lock_acquired = True
-        finally:
-            lock.release()
-            lock_acquired = True
+            finally:
+                lock.release()
+                lock_acquired = True
     return '', 204
 
 app.run(host='0.0.0.0', port=5000, threaded=True)
