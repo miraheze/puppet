da2801b4cb6292836a80e649e61e28d49fee4435
paladox <paladox@users.noreply.github.com>
2018-11-12 21:16:21 +0000

    Update minion.pp

--- modules/salt/manifests/minion.pp
+++ modules/salt/manifests/minion.pp
@@ -67,6 +67,7 @@ class salt::minion(
     # will start it with the correct settings
     package { 'salt-minion':
         ensure  => present,
+        require => Apt::Source['salt_apt'],
     }
 
     service { 'salt-minion':
