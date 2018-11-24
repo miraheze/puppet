73a21605b449b953b1e675a72b51f80e47aca595
paladox <paladox@users.noreply.github.com>
2018-11-24 18:29:06 +0000

    letsencrypt: Improve readability of ssl-certificate (#901)
    Also some more improvements:
    
    * Fixes a issue where -c was expecting you to supply a value.
    
    * Fixes a issue where CSR failed to generate due to escaping issues.
    
    * Adds a -q (--quiet) flag to quiet output when supplied.
    
    * Adds -v (--version) flag to output the version.

--- modules/letsencrypt/files/ssl-certificate.py
+++ modules/letsencrypt/files/ssl-certificate.py
@@ -7,60 +7,123 @@
 
 import argparse
 import os
+import subprocess
  
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser(description="Script to generate LetsEncrypt certs or generate a CSR.")
 ap.add_argument("-c", "--csr", required=False,
-    help="generates a CSR")
+    action="store_true", default=False, help="generates a CSR")
 ap.add_argument("-d", "--domain", required=True,
     help="name of the domain")
 ap.add_argument("-g", "--generate", required=False,
     action="store_true", default=False, help="generates LetsEncrypt SSL Certificate")
+ap.add_argument("-q", "--quiet", required=False,
+    action="store_true", default=False, help="makes script quieter")
 ap.add_argument("-r", "--renew", required=False,
     action="store_true", default=False, help="renews LetsEncrypt SSL Certificate")
 ap.add_argument("-s", "--secondary", required=False,
     help="allows you to add other domains to the same cert, eg www.<domain>")
+ap.add_argument("-v", "--version", action="version", version="%(prog)s 1.0")
 ap.add_argument("-w", "--wildcard", required=False,
     action="store_true", default=False, help="auths against DNS supporting wildcards")
 args = vars(ap.parse_args())
 
-domain = args['domain']
+class SslCertificate:
+    def __init__(self):
+        self.csr = args['csr']
+        self.domain = args['domain']
+        self.generate = args['generate']
+        if args['quiet']:
+            self.quiet = "-q"
+        else:
+            self.quiet = ""
+        self.renew = args['renew']
+        if args['secondary']:
+            self.secondary_domain = " -d " +  args['secondary']
+        else:
+            self.secondary_domain = ""
+        self.wildcard = args['wildcard']
 
-if args['secondary']:
-    secondary_domain = " -d " +  args['secondary']
-else:
-    secondary_domain = ""
+    def on_init(self):
+        if self.csr:
+            self.generate_csr()
+        elif self.generate and not self.renew:
+            self.generate_letsencrypt_certificate()
+        elif not self.generate and self.renew:
+            self.renew_letsencrypt_certificate()
 
-if args["csr"]:
-    secondary_domain = secondary_domain.replace(" -d ", "")
+    def generate_csr(self):
+        if self.secondary_domain:
+            secondary_domain = self.secondary_domain.replace(" -d ", ",DNS:")
+        else:
+            secondary_domain = self.secondary_domain
 
-    # Generate the private key
-    os.system("openssl genrsa 2048 > /root/ssl/{}.key".format(domain))
-    print("Private key generated at: /root/ssl/{}.key".format(domain))
+        # Generate the private key
+        os.system("openssl genrsa 2048 > /root/ssl/{0}.key".format(self.domain))
 
-    # Generate the CSR
-    os.system("openssl req -new -sha256 -key /root/ssl/{0}.key -subj \"/C=NL/ST=Netherlands/L=Netherlands/O=Miraheze/CN={0}\" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf \"[SAN]\nsubjectAltName=DNS:{0}{1}\")) > /root/ssl/{0}.csr".format(domain, secondary_domain))
-    print("CSR generated at: /root/ssl/{0}.csr".format(domain))
+        if not self.quiet:
+            print("Private key generated at: /root/ssl/{0}.key".format(self.domain))
 
-    print("Not generating an SSL certificate. Use CSR below to send to the requestor")
-    os.system("cat /root/ssl/{0}.csr".format(domain))
+        # Generate the CSR
+        subprocess.call([
+            'bash',
+            '-c',
+            'openssl req -new -sha256 -key /root/ssl/{0}.key -subj \"/C=NL/ST=Netherlands/L=Netherlands/O=Miraheze/CN={0}\" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf \"[SAN]\nsubjectAltName=DNS:{0}{1}\")) > /root/ssl/{0}.csr'.format(self.domain, secondary_domain),
+        ])
 
-if args["generate"] and not args["renew"]:
-    if args["wildcard"]:
-        print("Generating Wildcard SSL certificate with LetsEncrypt")
-        os.system("/usr/bin/certbot certonly --manual --preferred-challenges dns-01 -d {0} {1} --no-verify-ssl".format(domain, secondary_domain))
-        print("LetsEncrypt certificate at: /etc/letsencrypt/live/{0}/fullchain.pem".format(domain))
-    else:
-        print("Generating SSL certificate with LetsEncrypt")
-        os.system("/usr/bin/certbot -q --noninteractive certonly -a webroot -d {0} {1} --no-verify-ssl".format(domain, secondary_domain))
-        print("LetsEncrypt certificate at: /etc/letsencrypt/live/{0}/fullchain.pem".format(domain))
-    os.system("cat /etc/letsencrypt/live/{0}/fullchain.pem".format(domain))
-elif not args["generate"] and args["renew"]:
-    # note that if you do *.domain.org then the cert name is domain.org
-    print("Re-generating a new SSL cert for {0}".format(domain))
-    if args["wildcard"]:
-        os.system("/usr/bin/certbot renew --cert-name {0} --no-verify-ssl --force-renewal --expand".format(domain))
-    else:
-        os.system("/usr/bin/certbot -q --noninteractive renew --cert-name {0} --no-verify-ssl --force-renewal --expand".format(domain))
-    print("LetsEncrypt certificate at: /etc/letsencrypt/live/{0}/fullchain.pem".format(domain))
-    os.system("cat /etc/letsencrypt/live/{0}/fullchain.pem".format(domain))
+        if not self.quiet:
+            print("CSR generated at: /root/ssl/{0}.csr".format(self.domain))
+
+        if not self.quiet:
+            print("Not generating an SSL certificate. Use CSR below to send to the requestor")
+
+        os.system("/bin/cat /root/ssl/{0}.csr".format(self.domain))
+
+    def generate_letsencrypt_certificate(self):
+        if self.wildcard:
+            if not self.quiet:
+                print("Generating Wildcard SSL certificate with LetsEncrypt")
+
+            os.system("/usr/bin/certbot certonly --manual --preferred-challenges dns-01 -d {0} {1} --no-verify-ssl".format(self.domain, self.secondary_domain))
+
+            if not self.quiet:
+                print("LetsEncrypt certificate at: /etc/letsencrypt/live/{0}/fullchain.pem".format(self.domain))
+        else:
+            if not self.quiet:
+                print("Generating SSL certificate with LetsEncrypt")
+
+            os.system("/usr/bin/certbot {1} --noninteractive certonly -a webroot -d {0} {2} --no-verify-ssl".format(self.domain, self.quiet, self.secondary_domain))
+
+            if not self.quiet:
+                print("LetsEncrypt certificate at: /etc/letsencrypt/live/{0}/fullchain.pem".format(self.domain))
+
+        os.system("/bin/cat /etc/letsencrypt/live/{0}/fullchain.pem".format(self.domain))
+
+        if not self.quiet:
+            print("LetsEncrypt private key is at: /etc/letsencrypt/live/{0}/privkey.pem".format(self.domain))
+
+    def renew_letsencrypt_certificate(self):
+        if self.wildcard:
+            if not self.quiet:
+                print("Re-generating a new wildcard SSL cert for {0}".format(self.domain))
+
+            os.system("/usr/bin/certbot --no-verify-ssl --reuse-key certonly --manual --preferred-challenges dns-01 -d {0} {1}".format(self.domain, self.secondary_domain))
+
+            if not self.quiet:
+                print("LetsEncrypt certificate at: /etc/letsencrypt/live/{0}/fullchain.pem".format(self.domain))
+        else:
+            if not self.quiet:
+                print("Re-generating a new SSL cert for {0}".format(self.domain))
+
+            os.system("/usr/bin/certbot --force-renewal --expand  --no-verify-ssl {1} --noninteractive renew --cert-name {0}".format(self.domain, self.quiet))
+
+            if not self.quiet:
+                print("LetsEncrypt certificate at: /etc/letsencrypt/live/{0}/fullchain.pem".format(self.domain))
+
+        os.system("/bin/cat /etc/letsencrypt/live/{0}/fullchain.pem".format(self.domain))
+
+        if not self.quiet:
+            print("LetsEncrypt private key is at: /etc/letsencrypt/live/{0}/privkey.pem".format(self.domain))
+
+cert = SslCertificate()
+cert.on_init()
