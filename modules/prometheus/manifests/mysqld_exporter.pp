8eec2fbca3a64e547a1aeff251f2b7cbd4d94c43
paladox <paladox@users.noreply.github.com>
2018-11-29 00:13:20 +0000

    Create mysqld_exporter.pp (#911)
    * Create mysqld_exporter.pp
    
    * Create prometheus-mysqld-exporter.erb
    
    * Create my.cnf.erb
    
    * Update mysqld_exporter.pp

---
+++ modules/prometheus/manifests/mysqld_exporter.pp
@@ -0,0 +1,39 @@
+# = Class: prometheus::mysqld_exporter
+#
+class prometheus::mysqld_exporter {
+
+    # should use the version from sid
+    require_package('prometheus-mysqld-exporter')
+
+    $exporter_password = hiera('passwords::db::exporter')
+
+    file { '/etc/default/prometheus-mysqld-exporter':
+        content => template('prometheus/prometheus-mysqld-exporter.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        require => Package['prometheus-mysqld-exporter'],
+    }
+
+    file { '/var/lib/prometheus/.my.cnf':
+        content => template('prometheus/my.cnf.erb'),
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        require => Package['prometheus-mysqld-exporter'],
+    }
+
+    service { ' prometheus-mysqld-exporter':
+        ensure  => 'running',
+        require => [
+            File['/etc/default/prometheus-mysqld-exporter'],
+            File['/var/lib/prometheus/.my.cnf'],
+        ],
+    }
+
+    ufw::allow { 'prometheus mysql monitoring':
+        proto   => 'tcp',
+        port    => '9104',
+        from    => '81.4.127.174',
+    }
+}
