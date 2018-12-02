3af746c0e0d7dd32ea08e7660e9c21614555180d
paladox <paladox@users.noreply.github.com>
2018-12-02 01:55:54 +0000

    Add newline

--- modules/mediawiki/files/robots.php
+++ modules/mediawiki/files/robots.php
@@ -7,13 +7,13 @@ echo "# Disallow API and special pages" . "\r\n";
 echo "User-agent: *" . "\r\n";
 echo "Disallow: /w/api.php" . "\r\n";
 echo "Disallow: /w/index.php?title=Special:" . "\r\n";
-echo "Disallow: /wiki/Special:" . "\r\n";
+echo "Disallow: /wiki/Special:" . "\r\n\n";
 
 # Throttle YandexBot
 echo "# Throttle YandexBot" . "\r\n";
 echo "User-Agent: YandexBot" . "\r\n";
-echo "Crawl-Delay: 2.5" . "\r\n";
+echo "Crawl-Delay: 2.5" . "\r\n\n";
 
 # Dynamic sitemap url
 echo "# Dynamic sitemap url" . "\r\n";
-echo "Sitemap: https://static.miraheze.org/sitemaps/$_SERVER[HTTP_HOST]/sitemap.xml" . "\r\n";
+echo "Sitemap: https://static.miraheze.org/sitemaps/$_SERVER[HTTP_HOST]/sitemap.xml" . "\r\n\n";
