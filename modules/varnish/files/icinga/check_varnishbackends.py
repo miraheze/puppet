91a61a6d2ccc1d3b586469ad81844b4d8594cf6e
paladox <paladox@users.noreply.github.com>
2018-12-02 21:41:26 +0000

    check_varnishbackends: Convert optparse.OptionParser to argparse.ArgumentParser

--- modules/varnish/files/icinga/check_varnishbackends.py
+++ modules/varnish/files/icinga/check_varnishbackends.py
@@ -24,50 +24,50 @@
 #   13 Apr 2016 - Jeoffrey BAUVIN
 #   Migrate to Varnish 4.1
 
+import argparse
 import sys
-import optparse
 import subprocess
 
 def runcommand(command, exit_on_fail=True):
     try:
-      process = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
-      output, unused_err = process.communicate()
-      retcode = process.poll()
-      return output
+        process = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        return output
 
     except OSError as e:
-      print("Error: Executing command failed,  does it exist?")
-      sys.exit(2)
+        print("Error: Executing command failed,  does it exist?")
+        sys.exit(2)
 
 
 def main(argv):
-  o = optparse.OptionParser(conflict_handler="resolve", description="Nagios plugin to check varnish backend health.")
-  o.add_option('-H', '--host', action='store', type='string', dest='host', default='127.0.0.1', help='The ip varnishadm is listening on')
-  o.add_option('-P', '--port', action='store', type='int', dest='port', default=6082, help='The port varnishadm is listening on')
-  o.add_option('-s', '--secret', action='store', type='string', dest='secret', default='/etc/varnish/secret', help='The path to the secret file')
-  o.add_option('-p', '--path', action='store', type='string', dest='path', default='/usr/bin/varnishadm', help='The path to the varnishadm binary')
+    o = argparse.ArgumentParser(conflict_handler="resolve", description="Nagios plugin to check varnish backend health.")
+    o.add_argument('-H', '--host', action='store', dest='host', default='127.0.0.1', help='The ip varnishadm is listening on')
+    o.add_argument('-P', '--port', action='store', dest='port', default=6082, help='The port varnishadm is listening on')
+    o.add_argument('-s', '--secret', action='store', dest='secret', default='/etc/varnish/secret', help='The path to the secret file')
+    o.add_argument('-p', '--path', action='store', dest='path', default='/usr/bin/varnishadm', help='The path to the varnishadm binary')
 
-  options=o.parse_args()[0]
-  command = runcommand("%(path)s -S %(secret)s -T %(host)s:%(port)s backend.list" % options.__dict__)
-  backends = command.split(b"\n")
-  backends_healthy, backends_sick = [], []
-  for line in backends:
-    if line.startswith(b"boot") and line.find(b"test")==-1:
-      if line.find(b"Healthy") != -1:
-        backends_healthy.append(line.split(b" ")[0].replace(b"boot.", b""))
-      else:
-        backends_sick.append(line.split(b" ")[0].replace(b"boot.", b""))
+    options= o.parse_args()
+    command = runcommand("%(path)s -S %(secret)s -T %(host)s:%(port)s backend.list" % options.__dict__)
+    backends = command.split(b"\n")
+    backends_healthy, backends_sick = [], []
+    for line in backends:
+        if line.startswith(b"boot") and line.find(b"test")==-1:
+            if line.find(b"Healthy") != -1:
+                backends_healthy.append(line.split(b" ")[0].replace(b"boot.", b""))
+            else:
+                backends_sick.append(line.split(b" ")[0].replace(b"boot.", b""))
 
-  if backends_sick:
-    print("%s backends are down.  %s" % (len(backends_sick), " ".join(backends_sick)))
-    sys.exit(2)
+    if backends_sick:
+        print(("%s backends are down.  %s" % (len(backends_sick), " ".join(backends_sick))))
+        sys.exit(2)
 
-  if not backends_sick and not backends_healthy:
-    print("No backends detected.  If this is an error, see readme.txt")
-    sys.exit(1)
+    if not backends_sick and not backends_healthy:
+        print("No backends detected.  If this is an error, see readme.txt")
+        sys.exit(1)
 
-  print("All %s backends are healthy" % (len(backends_healthy)))
-  sys.exit(0)
+    print(("All %s backends are healthy" % (len(backends_healthy))))
+    sys.exit(0)
 
 
 if __name__ == "__main__":
