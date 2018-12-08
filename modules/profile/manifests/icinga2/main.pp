4748e4a38da0e6bd32baba34315f21dbf18de25d
paladox <paladox@users.noreply.github.com>
2018-12-08 23:49:39 +0000

    Remove duplicate variables doing the same thing

--- modules/profile/manifests/icinga2/main.pp
+++ modules/profile/manifests/icinga2/main.pp
@@ -2,7 +2,7 @@ class profile::icinga2::main (
     String $icinga2_db_host                 = hiera('icinga_ido_db_host', 'db4.miraheze.org'),
     String $icinga2_db_name                 = hiera('icinga_ido_db_name', 'icinga'),
     String $icinga2_db_user                 = hiera('icinga_ido_user_name', 'icinga2'),
-    String $icinga2_db_password             = hiera('passwords::icinga_ido'),
+    String $ido_db_user_password            = hiera('passwords::icinga_ido'),
     String $mirahezebots_password           = hiera('passwords::irc::mirahezebots'),
     String $icingaweb2_db_host              = hiera('icingaweb_db_host', 'db4.miraheze.org'),
     String $icingaweb2_db_name              = hiera('icingaweb_db_name', 'icingaweb2'),
@@ -11,14 +11,13 @@ class profile::icinga2::main (
     String $icingaweb2_ido_db_host          = hiera('icinga_ido_db_host', 'db4.miraheze.org'),
     String $icingaweb2_ido_db_name          = hiera('icinga_ido_db_name', 'icinga'),
     String $icingaweb2_ido_db_user_name     = hiera('icinga_ido_user_name', 'icinga2'),
-    String $icingaweb2_ido_db_user_password = hiera('passwords::icinga_ido'),
     String $icingaweb2_icinga_api_password  = hiera('passwords::icinga_api'),
 ) {
     class { '::monitoring':
         db_host               => $icinga2_db_host,
         db_name               => $icinga2_db_name,
         db_user               => $icinga2_db_user,
-        db_password           => $icinga2_db_password,
+        db_password           => $ido_db_user_password ,
         mirahezebots_password => $mirahezebots_password,
     }
 
@@ -30,7 +29,7 @@ class profile::icinga2::main (
         ido_db_host           => $icingaweb2_ido_db_host,
         ido_db_name           => $icingaweb2_ido_db_name,
         ido_db_user_name      => $icingaweb2_ido_db_user_name,
-        ido_db_user_password  => $icingaweb2_ido_db_user_password,
+        ido_db_user_password  => $ido_db_user_password ,
         icinga_api_password   => $icingaweb2_icinga_api_password,
     }
 }
