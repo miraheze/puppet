be6b978a7e087c36df94fa865ad9bdaa4b6065f7
paladox <paladox@users.noreply.github.com>
2018-12-09 23:41:05 +0000

    Use ensure_resource_duplicate in role roundcubemail

--- modules/role/manifests/roundcubemail.pp
+++ modules/role/manifests/roundcubemail.pp
@@ -5,4 +5,8 @@ class role::roundcubemail {
     }
 
     include ::profile::roundcubemail::main
+
+    ensure_resource_duplicate('ufw::allow', 'roundcubemail http', {'proto' => 'tcp', 'port' => '80'})
+
+    ensure_resource_duplicate('ufw::allow', 'roundcubemail https', {'proto' => 'tcp', 'port' => '443'})
 }
