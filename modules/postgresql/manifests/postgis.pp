7845cae98a4a925c89cf6d941a2da394ec5e193d
paladox <paladox@users.noreply.github.com>
2018-12-10 00:21:34 +0000

    Update postgis.pp

--- modules/postgresql/manifests/postgis.pp
+++ modules/postgresql/manifests/postgis.pp
@@ -13,7 +13,7 @@
 #     include postgresql::postgis
 #
 class postgresql::postgis(
-    Stdlib::Ensure $ensure = 'present',
+    Mlib::Ensure $ensure = 'present',
     String $postgresql_postgis_package = $::lsbdistcodename ? {
         'stretch' => 'postgresql-9.6-postgis-2.3',
         'jessie'  => 'postgresql-9.4-postgis-2.3',
