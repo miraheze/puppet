5a031595b6d253d872eb1e4af061dd72dde607fc
paladox <paladox@users.noreply.github.com>
2018-12-10 00:09:38 +0000

    Update icinga2.pp

--- modules/role/manifests/icinga2.pp
+++ modules/role/manifests/icinga2.pp
@@ -6,17 +6,7 @@ class role::icinga2 {
 
     include ::profile::icinga2::main
 
-    if !defined(Exec['ufw-allow-tcp-from-any-to-any-port-80']) {
-        ufw::allow { 'icinga2 http':
-            proto => 'tcp',
-            port  => '80',
-        }
-    }
+    ensure_resource_duplicate('ufw::allow', 'roundcubemail http', {'proto' => 'tcp', 'port' => '80'})
 
-    if !defined(Exec['ufw-allow-tcp-from-any-to-any-port-443']) {
-        ufw::allow { 'icinga2 https':
-            proto => 'tcp',
-            port  => '443',
-        }
-    }
+    ensure_resource_duplicate('ufw::allow', 'roundcubemail https', {'proto' => 'tcp', 'port' => '443'})
 }
