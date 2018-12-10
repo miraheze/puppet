357bf9dcd3c82f34cf549d6a634217a2d3d67091
paladox <paladox@users.noreply.github.com>
2018-12-10 00:12:17 +0000

    Update grafana.pp

--- modules/role/manifests/grafana.pp
+++ modules/role/manifests/grafana.pp
@@ -16,7 +16,7 @@ class role::grafana {
         port  => 2004,
     }
 
-    ensure_resource_duplicate('ufw::allow', 'roundcubemail http', {'proto' => 'tcp', 'port' => '80'})
+    ensure_resource_duplicate('ufw::allow', 'grafana http', {'proto' => 'tcp', 'port' => '80'})
 
-    ensure_resource_duplicate('ufw::allow', 'roundcubemail https', {'proto' => 'tcp', 'port' => '443'})
+    ensure_resource_duplicate('ufw::allow', 'grafana https', {'proto' => 'tcp', 'port' => '443'})
 }
