5176f5963a44fb93970814698531d9b829259ef1
paladox <paladox@users.noreply.github.com>
2018-12-23 18:36:19 +0000

    Update puppet.pp

--- modules/base/manifests/puppet.pp
+++ modules/base/manifests/puppet.pp
@@ -7,7 +7,7 @@ class base::puppet {
     if $puppetmaster_version == 6 {
         apt::source { 'puppetlabs':
             location => 'http://apt.puppetlabs.com',
-            repos    => 'main',
+            repos    => 'puppet6',
             key      => {
                 'id'     => '6F6B15509CF8E59E6E469F327F438280EF8D349F',
                 'server' => 'pgp.mit.edu',
