7daab66543d3aa6dc315dd4ff440f40b4c014f95
paladox <paladox@users.noreply.github.com>
2018-12-25 17:01:59 +0000

    Increase redis max memory to 660mb

--- modules/role/manifests/redis.pp
+++ modules/role/manifests/redis.pp
@@ -2,7 +2,7 @@
 class role::redis {
     class { '::redis':
         password  => hiera('passwords::redis::master'),
-        maxmemory => '640mb',
+        maxmemory => '660mb',
     }
 
     ufw::allow { 'redis':
