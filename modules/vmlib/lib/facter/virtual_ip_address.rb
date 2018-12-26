54303a0b241b25b05266c4563f59500c3a1ad3d1
paladox <paladox@users.noreply.github.com>
2018-12-26 04:55:14 +0000

    Update virtual_ip_address.rb

--- modules/vmlib/lib/facter/virtual_ip_address.rb
+++ modules/vmlib/lib/facter/virtual_ip_address.rb
@@ -6,7 +6,7 @@ Facter.add(:virtual_ip_address) do
     if Facter.value('virtual') == 'kvm'
       ip = Facter.value('ipaddress')
     else
-      ip Socket.ip_address_list[2].ip_address
+      ip = Socket.ip_address_list[2].ip_address
     end
   end
 end
