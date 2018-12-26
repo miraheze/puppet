c2720fbdb570f408d6c8a4d40055cfd184c8fc78
paladox <paladox@users.noreply.github.com>
2018-12-26 04:33:31 +0000

    Update virtual_ip_address.rb

--- modules/vmlib/lib/facter/virtual_ip_address.rb
+++ modules/vmlib/lib/facter/virtual_ip_address.rb
@@ -1,8 +1,8 @@
 require 'socket'
 
-Facter.add(:virtual_processor_count) do
+Facter.add(:virtual_ip_address) do
   has_weight 100
   setcode do
-    processors = Integer(Socket.ip_address_list[2].ip_address)
+    ip = Socket.ip_address_list[2].ip_address
   end
 end
