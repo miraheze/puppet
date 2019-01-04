62376a7248979cb32f1f42ef1793a89ddec7b414
paladox <paladox@users.noreply.github.com>
2019-01-04 02:26:08 +0000

    Update wiki_monitoring.pp

--- modules/profile/manifests/wiki_monitoring.pp
+++ modules/profile/manifests/wiki_monitoring.pp
@@ -1,9 +1,6 @@
 # monitoring for user wikis
 class profile::wiki_monitoring {
 
-    # to add a wiki to monitoring just add the short name to this array
-    $monitored_wikis = [ 'meta', 's23' ]
-
     monitoring::wiki {'s23': }
     
     monitoring::wiki { 'meta':
