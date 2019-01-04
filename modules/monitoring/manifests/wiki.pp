03fbbf3461ce8894a8220eaa80ff2a199adb677f
paladox <paladox@users.noreply.github.com>
2019-01-04 02:59:49 +0000

    Update wiki.pp

--- modules/monitoring/manifests/wiki.pp
+++ modules/monitoring/manifests/wiki.pp
@@ -8,13 +8,13 @@ define monitoring::wiki (
     $enable_ssl   = true,
 ) {
     $wikifqdn = "${name}.${domain}"
-    $testuri  = "${protocol}://${name}.${domain}/wiki/${testpage}"
+    $testuri  = "/wiki/${testpage}"
     $protocol_string = upcase($protocol)
 
     monitoring::services {"${wikifqdn} ${protocol_string}":
         check_command => 'check_http',
         vars            => {
-            host        => $wikifqdn,
+            address     => $wikifqdn,
             http_expect => "HTTP/${http_version} 200",
             http_ssl    => $enable_ssl ? {
                 true    => true,
