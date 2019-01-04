3574f5861178b8814a86d8a72e5e7dcb264efafb
mutante <mutante@s23.org>
2019-01-03 17:26:24 -0800

    create new profile for monitoring of user wikis (#961)
    use the new defined type created in #946 to add monitoring for the meta wiki and s23 wiki. use a a defined type and array to iterate over so code does not have to be repeated and it's really easy to add more wikis

---
+++ modules/profile/manifests/wiki_monitoring.pp
@@ -0,0 +1,9 @@
+# monitoring for user wikis
+class profile::wiki_monitoring {
+
+    # to add a wiki to monitoring just add the short name to this array
+    $monitored_wikis = [ 'meta', 's23' ]
+
+    monitoring::wiki { $monitored_wikis: }
+
+}
