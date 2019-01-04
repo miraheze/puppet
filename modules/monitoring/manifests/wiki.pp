ae6df64a99c66df64b74b4c71ca73a071cf4fd19
paladox <paladox@users.noreply.github.com>
2019-01-04 02:27:40 +0000

    Update wiki.pp (#963)
    * Update wiki.pp
    
    * Update wiki.pp

--- modules/monitoring/manifests/wiki.pp
+++ modules/monitoring/manifests/wiki.pp
@@ -1,13 +1,11 @@
 define monitoring::wiki (
     $ensure       = present,
-    $name         = undef,
     $contacts     = hiera('contactgroups', [ 'icingaadmins', 'ops' ]),
-    $protocol     = 'https',
     $domain       = 'miraheze.org',
     $testpage     = 'Main_Page',
     $http_version = '1.1',
+    $enable_ssl    = true,
 ) {
-
     $wikifqdn = "${name}.${domain}"
     $testuri  = "${protocol}://${name}.${domain}/wiki/${testpage}"
     $protocol_string = upcase($protocol)
@@ -15,8 +13,9 @@ define monitoring::wiki (
     monitoring::services {"${wikifqdn} ${protocol_string}":
         check_command => 'check_http',
         vars          => {
+            host        => $wikifqdn,
             http_expect => "HTTP/${http_version} 200",
-            http_ssl    => true,
+            http_ssl    => $enable_ssl ? true : false,
             http_vhost  => $wikifqdn,
             http_uri    => $testuri,
         },
