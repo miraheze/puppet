ac26a4657b4e6d6744e6e5deddd79f1899861fd6
thomasmulhall410 <thomasmulhall410@yahoo.com>
2019-01-07 14:31:39 +0000

    Revert "Fix key error in ircecho"
    This reverts commit 32084688ba356d4dabd6019abc6d85e4df992eef.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I28b821f4ae2cfb047ee1a4cdf1d4a43d383b152f

--- modules/monitoring/files/bot/ircecho.py
+++ modules/monitoring/files/bot/ircecho.py
@@ -217,7 +217,7 @@ args = vars(ap.parse_args())
 
 chans = args['channel']
 nickname = args['nickname']
-nickname_pass = args['nickname_pass']
+nickname_pass = args['nickname-pass']
 server = args['server'].split(':')[0]
 try:
     ssl = args['server'].split(':')[1].startswith('+')
