1d5bd78b6e302c3a7545c0cbda96fc314bca650e
Paladox <thomasmulhall410@yahoo.com>
2019-01-11 14:13:14 +0000

    Revert "Increase redis max memory to 660mb"
    This reverts commit 7daab66543d3aa6dc315dd4ff440f40b4c014f95.

--- modules/role/manifests/redis.pp
+++ modules/role/manifests/redis.pp
@@ -2,7 +2,7 @@
 class role::redis {
     class { '::redis':
         password  => hiera('passwords::redis::master'),
-        maxmemory => '660mb',
+        maxmemory => '640mb',
     }
 
     ufw::allow { 'redis':
