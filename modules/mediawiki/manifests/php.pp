1428fcd3fedb32a2ac629936fc9747e6a71e8243
paladox <paladox@users.noreply.github.com>
2019-01-19 23:27:08 +0000

    Update php.pp

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -1,25 +1,28 @@
 # mediawiki::php
 class mediawiki::php {
-    ensure_resource_duplicate('class', '::php::php_fpm', {
+    ensure_resource_duplicate('class', 'php::php_fpm', {
         'config'  => {
-            'display_errors'            => 'Off',
-            'error_log'                 => '/var/log/mediawiki/debuglogs/php-error.log',
-            'error_reporting'           => 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
-            'log_errors'                => 'On',
-            'max_execution_time'        => 230,
-            'opcache'                   => {
+            'display_errors'              => 'Off',
+            'error_log'                   => '/var/log/mediawiki/debuglogs/php-error.log',
+            'error_reporting'             => 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
+            'log_errors'                  => 'On',
+            'max_execution_time'          => 230,
+            'opcache'                     => {
                 'enable'                  => 1,
                 'memory_consumption'      => 256,
                 'interned_strings_buffer' => 64,
                 'max_accelerated_files'   => 32531,
                 'revalidate_freq'         => 60,
             },
-            'post_max_size'       => '250M',
-            'register_argc_argv'  => 'Off',
-            'request_order'       => 'GP',
-            'track_errors'        => 'Off',
-            'upload_max_filesize' => '250M',
-            'variables_order'     => 'GPCS',
+            'mysqli'                      => {
+                'allow_local_infile'      => 0,
+            },
+            'post_max_size'               => '250M',
+            'register_argc_argv'          => 'Off',
+            'request_order'               => 'GP',
+            'track_errors'                => 'Off',
+            'upload_max_filesize'         => '250M',
+            'variables_order'             => 'GPCS',
         },
         'version' => hiera('php::php_version', '7.2'),
     })
