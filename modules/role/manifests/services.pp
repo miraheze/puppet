3e5a8cd74506d3c1c49e99d1b87ed91a16b37e01
paladox <paladox@users.noreply.github.com>
2019-02-20 16:42:47 +0000

    Update services.pp

--- modules/role/manifests/services.pp
+++ modules/role/manifests/services.pp
@@ -1,66 +1,73 @@
 # role: services
 class role::services {
-    include ::profile::citoid
+    if hiera('enable_citoid', true) {
+		include ::profile::citoid
 
-    include ::profile::electron
+		ufw::allow { 'citoid monitoring':
+			proto => 'tcp',
+			port  => 6927,
+			from  => '185.52.1.76',
+		}
 
-    include ::profile::parsoid
+		ufw::allow { 'zotero monitoring':
+			proto => 'tcp',
+			port  => 1969,
+			from  => '185.52.1.76',
+		}
+	}
 
-    include ::profile::restbase
+    if hiera('enable_electron', true) {
+		include ::profile::electron
 
+		ufw::allow { 'electron monitoring':
+			proto => 'tcp',
+			port  => 3000,
+			from  => '185.52.1.76',
+		}
+	}
 
-    ufw::allow { 'citoid monitoring':
-        proto => 'tcp',
-        port  => 6927,
-        from  => '185.52.1.76',
-    }
+    if hiera('enable_parsoid', true) {
+		include ::profile::parsoid
 
-    ufw::allow { 'zotero monitoring':
-        proto => 'tcp',
-        port  => 1969,
-        from  => '185.52.1.76',
-    }
+		ufw::allow { 'parsoid mw1':
+			proto => 'tcp',
+			port  => 443,
+			from  => '185.52.1.75',
+		}
 
-    ufw::allow { 'electron monitoring':
-        proto => 'tcp',
-        port  => 3000,
-        from  => '185.52.1.76',
-    }
+		ufw::allow { 'parsoid mw2':
+			proto => 'tcp',
+			port  => 443,
+			from  => '185.52.2.113',
+		}
+		ufw::allow { 'parsoid mw3':
+			proto => 'tcp',
+			port  => 443,
+			from  => '81.4.121.113',
+		}
 
-    ufw::allow { 'parsoid mw1':
-        proto => 'tcp',
-        port  => 443,
-        from  => '185.52.1.75',
-    }
+		ufw::allow { 'parsoid monitoring':
+			proto => 'tcp',
+			port  => 8142,
+			from  => '185.52.1.76',
+		}
 
-    ufw::allow { 'parsoid mw2':
-        proto => 'tcp',
-        port  => 443,
-        from  => '185.52.2.113',
-    }
-    ufw::allow { 'parsoid mw3':
-        proto => 'tcp',
-        port  => 443,
-        from  => '81.4.121.113',
-    }
+		ufw::allow { 'parsoid test1':
+			proto => 'tcp',
+			port  => 443,
+			from  => '185.52.2.243',
+		}
+	}
 
-    ufw::allow { 'parsoid monitoring':
-        proto => 'tcp',
-        port  => 8142,
-        from  => '185.52.1.76',
-    }
-	
-    ufw::allow { 'parsoid test1':
-        proto => 'tcp',
-        port  => 443,
-        from  => '185.52.2.243',
-    }
+    if hiera('enable_restbase', true) {
+		include ::profile::restbase
 
-    ufw::allow { 'restbase monitoring':
-        proto => 'tcp',
-        port  => 7231,
-        from  => '185.52.1.76',
-    }
+		ufw::allow { 'restbase monitoring':
+			proto => 'tcp',
+			port  => 7231,
+			from  => '185.52.1.76',
+		}
+	}
 
     motd::role { 'role::services':
         description => 'Hosting MediaWiki services (citoid, electron, parsoid, restbase etc)',
