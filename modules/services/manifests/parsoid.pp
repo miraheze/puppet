964da92540ef6f6bca7979d95cfeffa9b1796aa5
paladox <paladox@users.noreply.github.com>
2019-02-23 17:56:06 +0000

    parsoid: Use version 0.10.0 instead of master

--- modules/services/manifests/parsoid.pp
+++ modules/services/manifests/parsoid.pp
@@ -21,7 +21,7 @@ class services::parsoid {
         ensure    => present,
         directory => '/srv/parsoid',
         origin    => 'https://github.com/wikimedia/parsoid.git',
-        branch    => 'master',
+        branch    => 'v0.10.0',
         owner     => 'root',
         group     => 'root',
         mode      => '0755',
