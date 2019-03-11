cde857d31fa6c98305d5dc8c4bda5b894cce5c30
Southparkfan <southparkfan223@hotmail.com>
2019-03-11 02:15:31 +0100

    Configure heap size in ES

--- modules/role/manifests/elasticsearch.pp
+++ modules/role/manifests/elasticsearch.pp
@@ -3,7 +3,13 @@ class role::elasticsearch {
     include ::java
 
     class { 'elasticsearch': }
-    elasticsearch::instance { 'es-01': }
+    # https://www.elastic.co/guide/en/elasticsearch/reference/master/heap-size.html
+    elasticsearch::instance { 'es-01':
+        jvm_options => [
+            '-Xms512m',
+            '-Xmx512m',
+        ]
+    }
 
     motd::role { 'role::elasticsearch':
         description => 'elasticsearch server',
