041ce46a77c23c879fad1105fd96d55b0c1c70c0
paladox <paladox@users.noreply.github.com>
2019-04-04 21:50:02 +0100

    php: Move apt repo back

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -11,8 +11,33 @@ class php(
 ) {
     requires_os('debian >= stretch')
 
+    if !defined(Apt::Source['php_apt']) {
+        file { '/etc/apt/trusted.gpg.d/php.gpg':
+            ensure => present,
+            source => 'puppet:///modules/php/key/php.gpg',
+        }
+
+        apt::source { 'php_apt':
+            location => 'https://packages.sury.org/php/',
+            release  => "${::lsbdistcodename}",
+            repos    => 'main',
+            require  => File['/etc/apt/trusted.gpg.d/php.gpg'],
+            notify   => Exec['apt_update_php'],
+        }
+
+        # First installs can trip without this
+        exec {'apt_update_php':
+            command     => '/usr/bin/apt-get update',
+            refreshonly => true,
+            logoutput   => true,
+        }
+    }
+
     # We need php-common everywhere
-    require_package("php${version}-common", "php${version}-opcache")
+    package { [ "php${version}-common", "php${version}-opcache" ]:
+        ensure  => $ensure,
+        require => Apt::Source['php_apt'],
+    }
 
     $config_dir = "/etc/php/${version}"
 
