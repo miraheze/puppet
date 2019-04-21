f62931cf79deb3e99cef750a108cc818c55fc60e
thomasmulhall410 <thomasmulhall410@yahoo.com>
2019-04-21 14:38:59 +0000

    Fix tweeks
    Change-Id: Iceb1341d9b215f93ca079f326224de5bc243d707

--- modules/phabricator/manifests/init.pp
+++ modules/phabricator/manifests/init.pp
@@ -9,7 +9,7 @@ class phabricator {
             'error_log'                 => '/var/log/php/php.log',
             'error_reporting'           => 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
             'log_errors'                => 'On',
-            'max_execution_time'        => 70,
+            'max_execution_time'        => 230,
             'opcache'                   => {
                 'enable'                  => 1,
                 'memory_consumption'      => 256,
