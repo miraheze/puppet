85b95aceec1ffec888767a93b2883c57c968d185
Southparkfan <southparkfan223@hotmail.com>
2019-05-04 23:24:58 +0200

    Fix vpncloud module

--- modules/vpncloud/manifests/init.pp
+++ modules/vpncloud/manifests/init.pp
@@ -1,6 +1,6 @@
 # class: vpncloud
 class vpncloud(
-    Optional[string] $server_ip = undef,
+    String $server_ip = undef,
 ){
     file { '/opt/vpncloud_0.9.1_xenial_amd64.deb':
         ensure  => present,
