9ab0aab0c16de43513dea67e6eba49e657554907
paladox <paladox@users.noreply.github.com>
2019-07-11 22:37:09 +0100

    Update fileLockScript.sh

--- modules/mediawiki/files/bin/fileLockScript.sh
+++ modules/mediawiki/files/bin/fileLockScript.sh
@@ -3,16 +3,17 @@
 # To run this script do the following fileLockScript.sh <lock_name> "<script>"
 
 LOCK_FILE=$1
-if [ -f "$LOCK_FILE" ]; then
-    # Lock file already exists, exit the script
-    echo "An instance of this script is already running"
-    exit 1
+
+if ( set -o noclobber; echo "$$" > "$LOCK_FILE") 2> /dev/null; then
+
+        trap 'rm -f "$LOCK_FILE"; exit $?' INT TERM EXIT
+
+        # do stuff here
+        $2
+
+        # clean up after yourself, and release your trap
+        rm -f "$LOCK_FILE"
+        trap - INT TERM EXIT
+else
+        echo "Lock Exists: $lockfile owned by $(cat $LOCK_FILE)"
 fi
-# Create the lock file
-echo "Locked" > "$LOCK_FILE"
-
-# Do the normal stuff
-$2
-
-# clean-up before exit
-rm "$LOCK_FILE"
