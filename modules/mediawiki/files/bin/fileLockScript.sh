329ae282efda4d25028c486e5a8d418fcda371f3
paladox <paladox@users.noreply.github.com>
2019-07-11 22:37:34 +0100

    jobrunner: Add new script that file locks (and convert RottenLinks script to use it) (#1026)
    * jobrunner: Use flock when running RottenLink maintenance script
    
    * Create fileLockScript.sh
    
    * Update init.pp
    
    * Update jobrunner.pp
    
    * Update fileLockScript.sh

---
+++ modules/mediawiki/files/bin/fileLockScript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# To run this script do the following fileLockScript.sh <lock_name> "<script>"
+
+LOCK_FILE=$1
+
+if ( set -o noclobber; echo "$$" > "$LOCK_FILE") 2> /dev/null; then
+
+        trap 'rm -f "$LOCK_FILE"; exit $?' INT TERM EXIT
+
+        # do stuff here
+        $2
+
+        # clean up after yourself, and release your trap
+        rm -f "$LOCK_FILE"
+        trap - INT TERM EXIT
+else
+        echo "Lock Exists: $lockfile owned by $(cat $LOCK_FILE)"
+fi
