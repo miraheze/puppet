e35b5602a2a8791645067812137f7dc14973f132
paladox <paladox@users.noreply.github.com>
2019-07-19 17:36:05 +0100

    Update wildcard.pp

--- modules/ssl/manifests/wildcard.pp
+++ modules/ssl/manifests/wildcard.pp
@@ -1,11 +1,13 @@
 # class: ssl::wildcard
-class ssl::wildcard {
+class ssl::wildcard (
+    $ssl_path = '/etc/ssl',
+) {
 
     if !defined(File['wildcard.miraheze.org']) {
         file { 'wildcard.miraheze.org':
             ensure => 'present',
             source => 'puppet:///ssl/certificates/wildcard.miraheze.org.crt',
-            path   => '/etc/ssl/certs/wildcard.miraheze.org.crt',
+            path   => "${ssl_path}/certs/wildcard.miraheze.org.crt",
         }
     }
 
@@ -13,8 +15,7 @@ class ssl::wildcard {
         file { 'wildcard.miraheze.org_private':
             ensure => 'present',
             source => 'puppet:///ssl-keys/wildcard.miraheze.org.key',
-            path   => '/etc/ssl/private/wildcard.miraheze.org.key',
+            path   => "${ssl_path}/private/wildcard.miraheze.org.key",
         }
     }
 }
-
