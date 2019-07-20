5b3e5b12c658616d7dc1e8704bd908bf3b99ba39
paladox <paladox@users.noreply.github.com>
2019-07-20 03:18:02 +0100

    redis: Lower heap by 100mb

--- modules/role/manifests/redis.pp
+++ modules/role/manifests/redis.pp
@@ -2,7 +2,7 @@
 class role::redis {
     class { '::redis':
         password  => hiera('passwords::redis::master'),
-        maxmemory => '1450mb',
+        maxmemory => '1300mb',
     }
 
     ufw::allow { 'redis':
