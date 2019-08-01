93d066070894906bb2e185ea977feeb2c98cb6e3
paladox <paladox@users.noreply.github.com>
2019-08-01 01:10:30 +0100

    Add icinga grants

--- modules/mariadb/manifests/config.pp
+++ modules/mariadb/manifests/config.pp
@@ -96,6 +96,7 @@ class mariadb::config(
         vars          => {
             mysql_username => 'icinga',
             mysql_database => 'icinga',
+            mysql_password => $icinga_database_password,
             mysql_ssl      => true,
         },
     }
