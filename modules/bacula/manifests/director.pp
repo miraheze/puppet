0e71d98779acaf9edf6f8a95f80e46b700846d7c
paladox <paladox@users.noreply.github.com>
2019-08-04 15:00:05 +0100

    Add "Bacula Databases db5" monitoring check

--- modules/bacula/manifests/director.pp
+++ modules/bacula/manifests/director.pp
@@ -106,6 +106,14 @@ class bacula::director {
         },
     }
 
+    monitoring::services { 'Bacula Databases db5':
+        check_command => 'nrpe',
+        vars          => {
+            nrpe_command => 'check_bacula_databasesdb5',
+            nrpe_timeout => '60s',
+        },
+    }
+
     monitoring::services { 'Bacula Static':
         check_command => 'nrpe',
         vars          => {
