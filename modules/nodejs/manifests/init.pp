60ee1f398da250b1e6f26ae19dc8f9ecb45cf44a
paladox <paladox@users.noreply.github.com>
2019-09-12 19:19:09 +0100

    nodejs: Ping nodejs to the official nodejs apt repo
    This will also install npm.

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -2,12 +2,19 @@
 class nodejs {
     include ::apt
 
+    apt::pin { 'nodejs_pin':
+        priority        => 600,
+        label           => 'NodeJS',
+        origin          => 'deb.nodesource.com'
+    }
+
     apt::source { 'nodejs_apt':
         comment  => 'NODEJS',
         location => 'https://deb.nodesource.com/node_8.x',
         release  => "${::lsbdistcodename}",
         repos    => 'main',
         key      => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
+        require  => Apt::Pin['nodejs_pin'],
         notify   => Exec['apt_update_nodejs'],
     }
  
