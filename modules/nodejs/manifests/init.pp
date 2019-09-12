6dadb2c930d08392535076a677f0dd5ba35f7246
paladox <paladox@users.noreply.github.com>
2019-09-12 21:48:12 +0100

    Update init.pp

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -2,19 +2,18 @@
 class nodejs {
     include ::apt
 
-    apt::pin { 'nodejs_pin':
-        priority        => 600,
-        origin          => 'deb.nodesource.com'
-    }
-
     apt::source { 'nodejs_apt':
         comment  => 'NODEJS',
         location => 'https://deb.nodesource.com/node_8.x',
         release  => "${::lsbdistcodename}",
         repos    => 'main',
         key      => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
-        require  => Apt::Pin['nodejs_pin'],
         notify   => Exec['apt_update_nodejs'],
+    }
+
+    apt::pin { 'nodejs_pin':
+        priority        => 600,
+        origin          => 'deb.nodesource.com'
     }
  
     # First installs can trip without this
@@ -22,6 +21,7 @@ class nodejs {
         command     => '/usr/bin/apt-get update',
         refreshonly => true,
         logoutput   => true,
+        require     => Apt::Pin['nodejs_pin'],
         before      => Package['nodejs'],
     }
 
