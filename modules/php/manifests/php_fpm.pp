ae029ce7e4aa163a34af0c90779cffcbc23f1e46
paladox <paladox@users.noreply.github.com>
2019-09-28 00:00:39 +0100

    php: Reduce emergency_restart_interval to 30s

--- modules/php/manifests/php_fpm.pp
+++ modules/php/manifests/php_fpm.pp
@@ -147,7 +147,7 @@ class php::php_fpm(
     }
 
     $base_fpm_config = {
-        'emergency_restart_interval'  => '60s',
+        'emergency_restart_interval'  => '30s',
         'emergency_restart_threshold' => $facts['virtual_processor_count'],
         'error_log'                   => "/var/log/php${version}-fpm.log",
         'process.priority'            => -19,
