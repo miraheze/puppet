277f63e9be6c106e72a61834ccc1c0849a0e0859
paladox <paladox@users.noreply.github.com>
2019-10-12 14:24:23 +0100

    Update init.pp

--- modules/matomo/manifests/init.pp
+++ modules/matomo/manifests/init.pp
@@ -41,7 +41,7 @@ class matomo {
             'upload_max_filesize' => '100M',
             'variables_order'     => 'GPCS',
         },
-        'fpm_min_child' => 3,
+        'fpm_min_child' => 4,
         'version' => hiera('php::php_version', '7.3'),
     })
 
