8a6e958fc48cbb67613ed12701bf13326adf2c8a
paladox <paladox@users.noreply.github.com>
2019-10-16 23:17:55 +0100

    monitoring: Support custom ips in hosts (#1106)
    * monitoring: Support custom ips in hosts
    
    * Update lizardfs6.yaml
    
    * Update hosts.pp
    
    * Update hosts.pp
    
    * Update lizardfs6.yaml
    
    * Update hosts.pp

--- modules/monitoring/manifests/hosts.pp
+++ modules/monitoring/manifests/hosts.pp
@@ -1,20 +1,26 @@
 define monitoring::hosts (
-    $ensure   = present,
-    $contacts = hiera('contactgroups', [ 'icingaadmins', 'ops' ]),
+    $ensure      = present,
+    $contacts    = hiera('contactgroups', [ 'icingaadmins', 'ops' ]),
+    # Defaults to use the variables below.
+    $ipaddress = hiera('monitoring::hosts::ipaddress', undef),
 ) {
 
-   # If on a container instead of a physical machine or real VM,
-    # use the custom fact to get the IP.
-    if $facts['virtual'] == 'openvz' {
-        $ip = $facts['virtual_ip_address']
+    if $ipaddress != undef {
+        $ipaddresses = $ipaddress
     } else {
-        $ip = $facts['ipaddress']
+        # If on a container instead of a physical machine or real VM,
+        # use the custom fact to get the IP.
+        if $facts['virtual'] == 'openvz' {
+            $ipaddresses = $facts['virtual_ip_address']
+        } else {
+            $ipaddresses = $facts['ipaddress']
+        }
     }
 
     @@icinga2::object::host { $title:
         ensure  => $ensure,
         import  => ['generic-host'],
-        address => $ip,
+        address => $ipaddresses,
         target  => '/etc/icinga2/conf.d/puppet_hosts.conf',
         vars    => {
             notification => {
