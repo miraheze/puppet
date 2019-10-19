daef45d02a07299d499a22004717f6a99507c6e2
paladox <paladox@users.noreply.github.com>
2019-10-19 14:59:13 +0100

    Update mediawiki.pp

--- modules/role/manifests/mediawiki.pp
+++ modules/role/manifests/mediawiki.pp
@@ -46,6 +46,6 @@ class role::mediawiki {
 
     ::lizardfs::client { '/mnt/mediawiki-static':
         create_mountpoint => true,
-        options           => 'big_writes,nosuid,nodev,noatime,mfsioretries=4,mfswriteworkers=30,mfsattrcacheto=60.0,mfsentrycacheto=60.0,mfsdirentrycacheto=60.0,mfsdirentrycachesize=3000',
+        options           => 'big_writes,nosuid,nodev,noatime,mfsattrcacheto=30.0,mfsentrycacheto=30.0,mfsdirentrycacheto=30.0,mfsdirentrycachesize=3000',
     }
 }
