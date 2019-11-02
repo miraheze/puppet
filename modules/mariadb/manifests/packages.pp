a231d68d2b6c8cd48be4a78aba10028040932a6b
paladox <paladox@users.noreply.github.com>
2019-11-02 00:26:40 +0000

    Update packages.pp

--- modules/mariadb/manifests/packages.pp
+++ modules/mariadb/manifests/packages.pp
@@ -7,17 +7,12 @@ class mariadb::packages(
         ensure => present,
     }
 
-    $apt_key = $version ? {
-      '10.2' => '177F4010FE56CA3336300305F1656F24C74CD1D8',
-      '10.3' => '177F4010FE56CA3336300305F1656F24C74CD1D8',
-    }
-
     apt::source { 'mariadb_apt':
         comment     => 'MariaDB stable',
         location    => "http://ams2.mirrors.digitalocean.com/mariadb/repo/${version}/debian",
         release     => "${::lsbdistcodename}",
         repos       => 'main',
-        key         => $apt_key,
+        key         => '177F4010FE56CA3336300305F1656F24C74CD1D8',
     }
 
     package { "mariadb-server-${version}':
