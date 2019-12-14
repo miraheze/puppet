26571d6d4a4a4d75674b38afe1c22777156864e4
paladox <paladox@users.noreply.github.com>
2019-12-14 15:21:29 +0000

    Remove elastica_composer

--- modules/mediawiki/manifests/extensionsetup.pp
+++ modules/mediawiki/manifests/extensionsetup.pp
@@ -120,16 +120,6 @@ class mediawiki::extensionsetup {
         require     => Git::Clone['MediaWiki core'],
     }
 
-    exec { 'elastica_composer':
-        command     => 'curl -sS https://getcomposer.org/installer | php && php composer.phar install',
-        creates     => '/srv/mediawiki/w/extensions/Elastica/composer.phar',
-        cwd         => '/srv/mediawiki/w/extensions/Elastica',
-        path        => '/usr/bin',
-        environment => 'HOME=/srv/mediawiki/w/extensions/Elastica',
-        user        => 'www-data',
-        require     => Git::Clone['MediaWiki core'],
-    }
-
     # Ensure widgets template directory is read/writeable by webserver if mediawiki is cloned
     file { '/srv/mediawiki/w/extensions/Widgets/compiled_templates':
         ensure  => directory,
