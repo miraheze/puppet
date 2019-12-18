45dccb9fcc3a2917718e4a0df9dc55856f978d3f
paladox <paladox@users.noreply.github.com>
2019-12-18 16:38:14 +0000

    redis: increase memory

--- modules/role/manifests/redis.pp
+++ modules/role/manifests/redis.pp
@@ -2,7 +2,7 @@
 class role::redis {
     class { '::redis':
         password  => hiera('passwords::redis::master'),
-        maxmemory => '1300mb',
+        maxmemory => '1700mb',
     }
 
     ufw::allow { 'redis':
