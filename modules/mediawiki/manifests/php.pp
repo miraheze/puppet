35f192ca6bbe988ac7d78a0f12510fddaa0aedcf
paladox <paladox@users.noreply.github.com>
2020-01-02 19:10:52 +0000

    mediawiki: Make fpm_min_restart_threshold configurable
    Also increase fpm_min_restart_threshold to 32 for lizardfs6

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -1,6 +1,7 @@
 # mediawiki::php
 class mediawiki::php (
     $php_fpm_childs = hiera('mediawiki::php::fpm::childs', 8),
+    $fpm_min_restart_threshold = hiera('mediawiki::php::fpm::fpm_min_restart_threshold', 6),
     $php_version = hiera('php::php_version', '7.2'),
     Optional[Boolean] $use_tideways = undef,
 ) {
@@ -31,7 +32,7 @@ class mediawiki::php (
             'request_terminate_timeout_track_finished' => 'yes',
         },
         'fpm_min_child' => $php_fpm_childs,
-        'fpm_min_restart_threshold' => 6,
+        'fpm_min_restart_threshold' => $fpm_min_restart_threshold,
         'version' => $php_version
     })
 
