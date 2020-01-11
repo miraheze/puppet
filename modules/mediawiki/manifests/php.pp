cc5b94c2ea453296236e249a6255ba758eccab9b
paladox <paladox@users.noreply.github.com>
2020-01-11 05:46:42 +0000

    Update php.pp

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -13,8 +13,8 @@ class mediawiki::php (
             'log_errors'                => 'On',
             'opcache'                   => {
                 'enable'                  => 1,
-                'interned_strings_buffer' => 40,
-                'memory_consumption'      => 200,
+                'interned_strings_buffer' => 50,
+                'memory_consumption'      => 256,
                 'max_accelerated_files'   => 20000,
                 'max_wasted_percentage'   => 10,
                 'validate_timestamps'     => 1,
