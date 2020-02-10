8a4db592c59491394994b8347f630211fa94cfab
Paladox <thomasmulhall410@yahoo.com>
2020-02-10 23:53:36 +0000

    Add sysfs module
    This is copied from https://github.com/wikimedia/puppet/tree/e0b15db0f5b5205456a26551babcd99bbed09439/modules/sysfs

---
+++ modules/sysfs/manifests/init.pp
@@ -0,0 +1,35 @@
+# == Class: sysfs
+#
+# This Puppet module provides 'sysfs::conffile' and 'sysfs::parameters'
+# resources which manages kernel parameters using /etc/sysfs.d files
+# and the sysfsutils service.
+#
+class sysfs {
+    package { 'sysfsutils':
+        ensure => present,
+    }
+
+    file { '/etc/sysfs.d':
+        ensure  => directory,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        recurse => true,
+        purge   => true,
+        force   => true,
+    }
+
+    service { 'sysfsutils':
+        ensure     => running,
+        # Does not have any permanent process, so prevent puppet from
+        # attempting to restart a service which is not meant to be running.
+        # sysfs::conffile() notify the service to trigger the restart whenever
+        # configuration files change.
+        hasstatus  => false,
+        status     => '/bin/true',
+        # Have it running on boot
+        enable     => true,
+        # Quicker restart
+        hasrestart => true,
+    }
+}
