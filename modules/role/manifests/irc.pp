5dc3f7123f1142996230172d4a695361f5ee50d6
paladox <paladox@users.noreply.github.com>
2020-02-11 19:48:19 +0000

    Fix dupes (#1202)
    * Fix dupes
    
    * Update irc.pp
    
    * Update varnish.pp

--- modules/role/manifests/irc.pp
+++ modules/role/manifests/irc.pp
@@ -42,61 +42,61 @@ class role::irc {
     }
     
     # new servers
-    ufw::allow { 'ircrcbot port mw4':
+    ufw::allow { 'ircrcbot port mw4 ipv4':
         proto => 'udp',
         port  => '5070',
         from  => '51.89.160.128',
     }
 
-    ufw::allow { 'ircrcbot port mw4':
+    ufw::allow { 'ircrcbot port mw4 ipv6':
         proto => 'udp',
         port  => '5070',
         from  => '2001:41d0:800:1056::3',
     }
 
-    ufw::allow { 'ircrcbot port mw5':
+    ufw::allow { 'ircrcbot port mw5 ipv4':
         proto => 'udp',
         port  => '5070',
         from  => '51.89.160.133',
     }
 
-    ufw::allow { 'ircrcbot port mw5':
+    ufw::allow { 'ircrcbot port mw5 ipv6':
         proto => 'udp',
         port  => '5070',
         from  => '2001:41d0:800:1056::8',
     }
 
-    ufw::allow { 'ircrcbot port mw6':
+    ufw::allow { 'ircrcbot port mw6 ipv4':
         proto => 'udp',
         port  => '5070',
         from  => '51.89.160.136',
     }
 
-    ufw::allow { 'ircrcbot port mw6':
+    ufw::allow { 'ircrcbot port mw6 ipv6':
         proto => 'udp',
         port  => '5070',
         from  => '2001:41d0:800:105a::4',
     }
 
-    ufw::allow { 'ircrcbot port mw7':
+    ufw::allow { 'ircrcbot port mw7 ipv4':
         proto => 'udp',
         port  => '5070',
         from  => '51.89.160.137',
     }
 
-    ufw::allow { 'ircrcbot port mw7':
+    ufw::allow { 'ircrcbot port mw7 ipv6':
         proto => 'udp',
         port  => '5070',
         from  => '2001:41d0:800:105a::5',
     }
 
-    ufw::allow { 'ircrcbot port test2':
+    ufw::allow { 'ircrcbot port test2 ipv4':
         proto => 'udp',
         port  => '5070',
         from  => '51.77.107.211',
     }
 
-    ufw::allow { 'ircrcbot port test2':
+    ufw::allow { 'ircrcbot port test2 ipv6':
         proto => 'udp',
         port  => '5070',
         from  => '2001:41d0:800:105a::3',
--- modules/role/manifests/varnish.pp
+++ modules/role/manifests/varnish.pp
@@ -36,49 +36,50 @@ class role::varnish {
         from  => '81.4.121.113',
     }
 
-    ufw::allow { 'Direct Varnish access mw4':
+    # new servers
+    ufw::allow { 'Direct Varnish access mw4 ipv4':
         proto => 'tcp',
         port  => 81,
         from  => '51.89.160.128',
     }
 
-    ufw::allow { 'Direct Varnish access mw4':
+    ufw::allow { 'Direct Varnish access mw4 ipv6':
         proto => 'tcp',
         port  => 81,
         from  => '2001:41d0:800:1056::3',
     }
 
-    ufw::allow { 'Direct Varnish access mw5':
+    ufw::allow { 'Direct Varnish access mw5 ipv4':
         proto => 'tcp',
         port  => 81,
         from  => '51.89.160.133',
     }
 
-    ufw::allow { 'Direct Varnish access mw5':
+    ufw::allow { 'Direct Varnish access mw5 ipv6':
         proto => 'tcp',
         port  => 81,
         from  => '2001:41d0:800:1056::8',
     }
 
-    ufw::allow { 'Direct Varnish access mw6':
+    ufw::allow { 'Direct Varnish access mw6 ipv4':
         proto => 'tcp',
         port  => 81,
         from  => '51.89.160.136',
     }
 
-    ufw::allow { 'Direct Varnish access mw6':
+    ufw::allow { 'Direct Varnish access mw6 ipv6':
         proto => 'tcp',
         port  => 81,
         from  => '2001:41d0:800:105a::4',
     }
 
-    ufw::allow { 'Direct Varnish access mw7':
+    ufw::allow { 'Direct Varnish access mw7 ipv4':
         proto => 'tcp',
         port  => 81,
         from  => '51.89.160.137',
     }
 
-    ufw::allow { 'Direct Varnish access mw7':
+    ufw::allow { 'Direct Varnish access mw7 ipv6':
         proto => 'tcp',
         port  => 81,
         from  => '2001:41d0:800:105a::5',
