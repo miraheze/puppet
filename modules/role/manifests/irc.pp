5371760880873cbbc5820240ba8592a52876c674
paladox <paladox@users.noreply.github.com>
2020-02-11 19:30:52 +0000

    Update irc.pp

--- modules/role/manifests/irc.pp
+++ modules/role/manifests/irc.pp
@@ -40,6 +40,67 @@ class role::irc {
         port  => '5070',
         from  => '185.52.2.243',
     }
+    
+    # new servers
+    ufw::allow { 'ircrcbot port mw4':
+        proto => 'udp',
+        port  => '5070',
+        from  => '51.89.160.128',
+    }
+
+    ufw::allow { 'ircrcbot port mw4':
+        proto => 'udp',
+        port  => '5070',
+        from  => '2001:41d0:800:1056::3',
+    }
+
+    ufw::allow { 'ircrcbot port mw5':
+        proto => 'udp',
+        port  => '5070',
+        from  => '51.89.160.133',
+    }
+
+    ufw::allow { 'ircrcbot port mw5':
+        proto => 'udp',
+        port  => '5070',
+        from  => '2001:41d0:800:1056::8',
+    }
+
+    ufw::allow { 'ircrcbot port mw6':
+        proto => 'udp',
+        port  => '5070',
+        from  => '51.89.160.136',
+    }
+
+    ufw::allow { 'ircrcbot port mw6':
+        proto => 'udp',
+        port  => '5070',
+        from  => '2001:41d0:800:105a::4',
+    }
+
+    ufw::allow { 'ircrcbot port mw7':
+        proto => 'udp',
+        port  => '5070',
+        from  => '51.89.160.137',
+    }
+
+    ufw::allow { 'ircrcbot port mw7':
+        proto => 'udp',
+        port  => '5070',
+        from  => '2001:41d0:800:105a::5',
+    }
+
+    ufw::allow { 'ircrcbot port test2':
+        proto => 'udp',
+        port  => '5070',
+        from  => '51.77.107.211',
+    }
+
+    ufw::allow { 'ircrcbot port test2':
+        proto => 'udp',
+        port  => '5070',
+        from  => '2001:41d0:800:105a::3',
+    }
 
     motd::role { 'role::irc':
         description => 'IRC bots server',
