1156fe8419313bc17a0ed5bb2c7e4f849dc011a0
paladox <paladox@users.noreply.github.com>
2020-02-12 20:51:33 +0000

    monitoring: Fix installing mariadb-client

--- modules/monitoring/manifests/init.pp
+++ modules/monitoring/manifests/init.pp
@@ -13,7 +13,33 @@ class monitoring (
         allowdupe => false,
     }
 
-    include ::mariadb::packages
+    $version = hiera('mariadb::version', '10.2')
+    apt::source { 'mariadb_apt':
+        comment     => 'MariaDB stable',
+        location    => "http://ams2.mirrors.digitalocean.com/mariadb/repo/${version}/debian",
+        release     => "${::lsbdistcodename}",
+        repos       => 'main',
+        key         => '177F4010FE56CA3336300305F1656F24C74CD1D8',
+        notify      => Exec['apt_update_mariadb'],
+    }
+
+    apt::pin { 'mariadb_pin':
+        priority        => 600,
+        origin          => 'ams2.mirrors.digitalocean.com'
+    }
+
+    # First installs can trip without this
+    exec {'apt_update_mariadb':
+        command     => '/usr/bin/apt-get update',
+        refreshonly => true,
+        logoutput   => true,
+        require     => Apt::Pin['mariadb_pin'],
+    }
+
+    package { "mariadb-client-${version}":
+        ensure  => present,
+        require => Apt::Source['mariadb_apt'],
+    }
 
     class { '::icinga2':
       manage_repo => true,
