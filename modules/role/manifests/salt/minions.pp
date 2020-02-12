24799608426b0fa14a1cd4d2fc56f5b702c98905
paladox <paladox@users.noreply.github.com>
2020-02-12 16:54:50 +0000

    salt: Update salt master finger for new servers

--- modules/role/manifests/salt/minions.pp
+++ modules/role/manifests/salt/minions.pp
@@ -2,9 +2,13 @@ class role::salt::minions(
     $salt_master = 'misc4.miraheze.org',
     $salt_master_key = false
 ) {
-    # stretch's salt-minion uses SHA256 instead of MD5 by default.
+    # stretch's/buster's salt-minion uses SHA256 instead of MD5 by default.
     # while it's possible to set 'hash_type: md5', this is preferrable
-    $master_finger = '28:62:d0:61:2c:76:3c:15:51:6a:43:1b:bb:e1:99:a4:e8:82:b5:47:fc:3c:44:b5:16:84:86:f4:5d:22:69:9d'
+    if hiera('new_servers', false) {
+        $master_finger = 'a8:b0:35:99:7c:2e:d5:5e:16:cc:ee:cc:2e:2c:b5:dd:72:f0:de:49:82:e5:bb:07:53:15:34:0b:50:62:5a:aa'
+    } else {
+        $master_finger = '28:62:d0:61:2c:76:3c:15:51:6a:43:1b:bb:e1:99:a4:e8:82:b5:47:fc:3c:44:b5:16:84:86:f4:5d:22:69:9d'
+    }
 
     class { '::salt::minion':
         id            => $::fqdn,
