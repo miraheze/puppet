861f0b173f505158540e51a0e9e0acf0b172fedc
paladox <paladox@users.noreply.github.com>
2020-02-14 16:51:33 +0000

    Update cloud.pp

--- modules/role/manifests/cloud.pp
+++ modules/role/manifests/cloud.pp
@@ -1,8 +1,16 @@
 # class: role::db
 class role::db {
-    include ::cloud
+    class { '::cloud':
+        main_ip4_address   => hiera('role::db::main_ip4_address'),
+        main_ip4_netmask   => hiera('role::db::main_ip4_netmask'),
+        main_ip4_broadcast => hiera('role::db::main_ip4_broadcast'),
+        main_ip4_gateway   => hiera('role::db::main_ip4_gateway'),
+        main_ip6_address   => hiera('role::db::main_ip6_address'),
+        main_ip6_gateway   => hiera('role::db::main_ip6_gateway'),
+    }
 
     # cloud1 and cloud2 respectivly
+    # TODO: Either automate this or move it so it's done in a file
     ['54.36.165.86', '2001:41d0:800:1056::1', '54.36.165.90', '2001:41d0:800:105a::1'].each |String $ip| {
         ufw::allow { "proxmox port 5900:5999 ${ip}":
             proto => 'tcp',
