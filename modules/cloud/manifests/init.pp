f7167e912b468f86c9a66a1eda9ab2d98871060f
paladox <paladox@users.noreply.github.com>
2020-02-14 17:07:11 +0000

    Update init.pp

--- modules/cloud/manifests/init.pp
+++ modules/cloud/manifests/init.pp
@@ -2,11 +2,11 @@
 
 class cloud (
     Stdlib::IP::Address $main_ip4_address   => hiera('role::db::main_ip4_address'),
-    Float               $main_ip4_netmask   => hiera('role::db::main_ip4_netmask'),
-    Float               $main_ip4_broadcast => hiera('role::db::main_ip4_broadcast'),
-    Float               $main_ip4_gateway   => hiera('role::db::main_ip4_gateway'),
+                        $main_ip4_netmask   => hiera('role::db::main_ip4_netmask'),
+                        $main_ip4_broadcast => hiera('role::db::main_ip4_broadcast'),
+                        $main_ip4_gateway   => hiera('role::db::main_ip4_gateway'),
     Stdlib::IP::Address $main_ip6_address   => hiera('role::db::main_ip6_address'),
-    Float               $main_ip6_gateway   => hiera('role::db::main_ip6_gateway'),
+                        $main_ip6_gateway   => hiera('role::db::main_ip6_gateway'),
 ) {
 
     file { '/etc/apt/trusted.gpg.d/proxmox.gpg':
