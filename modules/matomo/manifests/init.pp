c382662361f78521704670f086e716213906807c
paladox <paladox@users.noreply.github.com>
2020-02-27 00:45:16 +0000

    Update init.pp

--- modules/matomo/manifests/init.pp
+++ modules/matomo/manifests/init.pp
@@ -18,37 +18,35 @@ class matomo {
         require     => Git::Clone['matomo'],
     }
 
-    if !defined(Class['php::php_fpm']) {
-        class { 'php::php_fpm':
-            config  => {
-                'display_errors'            => 'Off',
-                'error_reporting'           => 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
-                'log_errors'                => 'On',
-                'memory_limit'              => '512M',
-                'opcache'                   => {
-                    'enable'                  => 1,
-                    'interned_strings_buffer' => 30,
-                    'memory_consumption'      => 112,
-                    'max_accelerated_files'   => 20000,
-                    'max_wasted_percentage'   => 10,
-                    'validate_timestamps'     => 1,
-                    'revalidate_freq'         => 10,
-                },
-                'enable_dl'           => 0,
-                'post_max_size'       => '60M',
-                'register_argc_argv'  => 'Off',
-                'request_order'       => 'GP',
-                'track_errors'        => 'Off',
-                'upload_max_filesize' => '100M',
-                'variables_order'     => 'GPCS',
+    ensure_resource_duplicate('class', 'php::php_fpm', {
+        'config'  => {
+            'display_errors'            => 'Off',
+            'error_reporting'           => 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
+            'log_errors'                => 'On',
+            'memory_limit'              => '512M',
+            'opcache'                   => {
+                'enable'                  => 1,
+                'interned_strings_buffer' => 30,
+                'memory_consumption'      => 112,
+                'max_accelerated_files'   => 20000,
+                'max_wasted_percentage'   => 10,
+                'validate_timestamps'     => 1,
+                'revalidate_freq'         => 10,
             },
-            config_cli => {
-                'memory_limit' => hiera('php::cli::memory_limit', '2G'),
-            },
-            fpm_min_child => 4,
-            version => hiera('php::php_version', '7.3'),
-        }
-    }
+            'enable_dl'           => 0,
+            'post_max_size'       => '60M',
+            'register_argc_argv'  => 'Off',
+            'request_order'       => 'GP',
+            'track_errors'        => 'Off',
+            'upload_max_filesize' => '100M',
+            'variables_order'     => 'GPCS',
+        },
+        'config_cli' => {
+            'memory_limit' => '2G',
+        },
+        'fpm_min_child' => 4,
+        'version' => hiera('php::php_version', '7.3'),
+    })
 
     include ssl::wildcard
 
@@ -78,7 +76,7 @@ class matomo {
 
     cron { 'archive_matomo':
         ensure  => present,
-        command => '/usr/local/bin/fileLockScript.sh /tmp/matomo_file_lock "/usr/bin/nice -19 /usr/bin/php /srv/matomo/console core:archive --url=https://matomo.miraheze.org/ --concurrent-requests-per-website=1" > /srv/matomo-archive.log',
+        command => '/usr/local/bin/fileLockScript.sh /tmp/matomo_file_lock "/usr/bin/nice -19 /usr/bin/php /srv/matomo/console core:archive --url=https://matomo.miraheze.org/" > /srv/matomo-archive.log',
         user    => 'www-data',
         minute  => '30',
         hour    => '*/24',
