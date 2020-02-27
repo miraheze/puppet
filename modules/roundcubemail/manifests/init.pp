5e80395951c4f061e40548ae527d08ff54e2c971
paladox <paladox@users.noreply.github.com>
2020-02-27 00:27:44 +0000

    Update init.pp

--- modules/roundcubemail/manifests/init.pp
+++ modules/roundcubemail/manifests/init.pp
@@ -5,39 +5,40 @@ class roundcubemail (
     String $db_user_password      = undef,
     String $roundcubemail_des_key = undef,
 ) {
-
     include ::nodejs
 
     $php_version = hiera('php::php_version', '7.3')
-    ensure_resource_duplicate('class', 'php::php_fpm', {
-        'config'  => {
-            'display_errors'            => 'Off',
-            'error_reporting'           => 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
-            'log_errors'                => 'On',
-            'memory_limit'              => '512M',
-            'opcache'                   => {
-                'enable'                  => 1,
-                'interned_strings_buffer' => 30,
-                'memory_consumption'      => 112,
-                'max_accelerated_files'   => 20000,
-                'max_wasted_percentage'   => 10,
-                'validate_timestamps'     => 1,
-                'revalidate_freq'         => 10,
+    if !defined(Class['php::php_fpm']) {
+        class { 'php::php_fpm':
+            'config'  => {
+                'display_errors'            => 'Off',
+                'error_reporting'           => 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
+                'log_errors'                => 'On',
+                'memory_limit'              => '512M',
+                'opcache'                   => {
+                    'enable'                  => 1,
+                    'interned_strings_buffer' => 30,
+                    'memory_consumption'      => 112,
+                    'max_accelerated_files'   => 20000,
+                    'max_wasted_percentage'   => 10,
+                    'validate_timestamps'     => 1,
+                    'revalidate_freq'         => 10,
+                },
+                'enable_dl'           => 0,
+                'post_max_size'       => '60M',
+                'register_argc_argv'  => 'Off',
+                'request_order'       => 'GP',
+                'track_errors'        => 'Off',
+                'upload_max_filesize' => '100M',
+                'variables_order'     => 'GPCS',
             },
-            'enable_dl'           => 0,
-            'post_max_size'       => '60M',
-            'register_argc_argv'  => 'Off',
-            'request_order'       => 'GP',
-            'track_errors'        => 'Off',
-            'upload_max_filesize' => '100M',
-            'variables_order'     => 'GPCS',
-        },
-        'config_cli' => {
-            'memory_limit' => '400M',
-        },
-        'fpm_min_child' => 4,
-        'version' => $php_version
-    })
+            'config_cli' => {
+                'memory_limit' => '400M',
+            },
+            'fpm_min_child' => 4,
+            'version' => $php_version
+        }
+    }
 
     require_package("php${php_version}-pspell")
 
