26875a1a558f735d4b058af12709205bdb161514
paladox <paladox@users.noreply.github.com>
2020-03-02 15:46:44 +0000

    Update php.pp

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -5,36 +5,39 @@ class mediawiki::php (
     $php_version = hiera('php::php_version', '7.2'),
     Optional[Boolean] $use_tideways = undef,
 ) {
-    ensure_resource_duplicate('class', '::php::php_fpm', {
-        'config'  => {
-            'display_errors'            => 'Off',
-            'error_log'                 => '/var/log/mediawiki/debuglogs/php-error.log',
-            'error_reporting'           => 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
-            'log_errors'                => 'On',
-            'opcache'                   => {
-                'enable'                  => 1,
-                'interned_strings_buffer' => 50,
-                'memory_consumption'      => 512,
-                'max_accelerated_files'   => 20000,
-                'max_wasted_percentage'   => 10,
-                'validate_timestamps'     => 1,
-                'revalidate_freq'         => 10,
+    if !defined(Class['php::php_fpm']) {
+        class { 'php::php_fpm':
+            config  => {
+                'display_errors'            => 'Off',
+                'error_log'                 => '/var/log/mediawiki/debuglogs/php-error.log',
+                'error_reporting'           => 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
+                'log_errors'                => 'On',
+                'memory_limit'              => '512M',
+                'opcache'                   => {
+                    'enable'                  => 1,
+                    'interned_strings_buffer' => 50,
+                    'memory_consumption'      => 512,
+                    'max_accelerated_files'   => 20000,
+                    'max_wasted_percentage'   => 10,
+                    'validate_timestamps'     => 1,
+                    'revalidate_freq'         => 10,
+                },
+                'enable_dl'           => 0,
+                'post_max_size'       => '250M',
+                'register_argc_argv'  => 'Off',
+                'request_order'       => 'GP',
+                'track_errors'        => 'Off',
+                'upload_max_filesize' => '250M',
+                'variables_order'     => 'GPCS',
             },
-            'enable_dl'           => 0,
-            'post_max_size'       => '250M',
-            'register_argc_argv'  => 'Off',
-            'request_order'       => 'GP',
-            'track_errors'        => 'Off',
-            'upload_max_filesize' => '250M',
-            'variables_order'     => 'GPCS',
-        },
-        'fpm_pool_config' => {
-            'request_terminate_timeout_track_finished' => 'yes',
-        },
-        'fpm_min_child' => $php_fpm_childs,
-        'fpm_min_restart_threshold' => $fpm_min_restart_threshold,
-        'version' => $php_version
-    })
+            fpm_pool_config => {
+                'request_terminate_timeout_track_finished' => 'yes',
+            },
+            fpm_min_child => $php_fpm_childs,
+            fpm_min_restart_threshold => $fpm_min_restart_threshold,
+            version => $php_version
+        }
+    }
 
     $profiling_ensure =  $use_tideways ? {
         true    => 'present',
