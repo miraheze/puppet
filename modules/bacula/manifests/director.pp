a5683c4bc3755826bfb62cfbc37d3edf94c07716
paladox <paladox@users.noreply.github.com>
2020-03-08 00:51:30 +0000

    bacula: Remove db5 (#1280)
    * bacula: Remove db5
    
    * Update director.pp
    
    * Update nrpe.cfg.erb

--- modules/bacula/manifests/director.pp
+++ modules/bacula/manifests/director.pp
@@ -106,14 +106,6 @@ class bacula::director {
         },
     }
 
-    monitoring::services { 'Bacula Databases db5':
-        check_command => 'nrpe',
-        vars          => {
-            nrpe_command => 'check_bacula_databasesdb5',
-            nrpe_timeout => '60s',
-        },
-    }
-
     monitoring::services { 'Bacula Databases db6':
         check_command => 'nrpe',
         vars          => {
