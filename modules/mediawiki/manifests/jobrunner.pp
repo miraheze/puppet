bd4434e77c71c84d29df9dca80a109aa4143b1fc
paladox <paladox@users.noreply.github.com>
2020-03-10 21:13:26 +0000

    jobrunner: Use new jobrunner redis if using new servers (#1283)
    * jobrunner: Use new jobrunner redis if using new servers
    
    * Update jobrunner.json.erb

--- modules/mediawiki/manifests/jobrunner.pp
+++ modules/mediawiki/manifests/jobrunner.pp
@@ -7,6 +7,7 @@ class mediawiki::jobrunner {
         origin    => 'https://github.com/wikimedia/mediawiki-services-jobrunner',
     }
 
+    $new_servers = hiera('new_servers', false)
     $redis_password = hiera('passwords::redis::master')
 
     file { '/srv/jobrunner/jobrunner.json':
