181e6e7c1205715c9593345b142b5921cc41718e
paladox <paladox@users.noreply.github.com>
2020-03-28 02:12:17 +0000

    Backup dbt1 (#1298)
    * Backup dbt1
    
    * Update director.pp
    
    * Update nrpe.cfg.erb
    
    * Update bacula-dir.conf

--- modules/bacula/manifests/director.pp
+++ modules/bacula/manifests/director.pp
@@ -114,6 +114,14 @@ class bacula::director {
         },
     }
 
+    monitoring::services { 'Bacula Databases dbt1':
+        check_command => 'nrpe',
+        vars          => {
+            nrpe_command => 'check_bacula_databasesdbt1',
+            nrpe_timeout => '60s',
+        },
+    }
+
     monitoring::services { 'Bacula Static':
         check_command => 'nrpe',
         vars          => {
