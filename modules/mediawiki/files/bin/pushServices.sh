492eb6bd6a30f1bccf15d058c5b131b5d60cdabc
paladox <paladox@users.noreply.github.com>
2020-03-29 20:37:31 +0100

    Update pushServices.sh

--- modules/mediawiki/files/bin/pushServices.sh
+++ modules/mediawiki/files/bin/pushServices.sh
@@ -5,9 +5,9 @@ set -e
 set -u
 
 if [ ! -d /srv/services/services/ ]; then
-        cd /srv/services/ && GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -i /srv/services/id_rsa -F /dev/null' git clone git@github.com:miraheze/services.git && cd /srv/services/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /srv/services/id_rsa -F /dev/null"
+        cd /srv/services/ && GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -i /srv/services/id_ed25519 -F /dev/null' git clone git@github.com:miraheze/services.git && cd /srv/services/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /srv/services/id_ed25519 -F /dev/null"
 else
-        cd /srv/services/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /srv/services/id_rsa -F /dev/null" && git reset --hard origin/master && git pull
+        cd /srv/services/services/ && git config --local core.sshCommand "ssh -o StrictHostKeyChecking=no -i /srv/services/id_ed25519 -F /dev/null" && git reset --hard origin/master && git pull
 fi
 
 git -C /srv/services/services/ config user.email "noreply@miraheze.org"
