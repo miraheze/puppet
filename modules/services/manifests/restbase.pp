6f528dce3f43c6d774a46150428f14f7080f20ce
paladox <paladox@users.noreply.github.com>
2020-03-31 19:04:04 +0100

    Update restbase.pp

--- modules/services/manifests/restbase.pp
+++ modules/services/manifests/restbase.pp
@@ -71,9 +71,16 @@ class services::restbase {
         notify  => Service['restbase'],
     }
 
-    file { '/etc/mediawiki/restbase/miraheze_project.yaml':
+    file { '/etc/mediawiki/restbase/miraheze_project_v1.yaml':
         ensure  => present,
-        source  => 'puppet:///modules/services/restbase/miraheze_project.yaml',
+        source  => 'puppet:///modules/services/restbase/miraheze_project_v1.yaml',
+        require => File['/etc/mediawiki/restbase'],
+        notify  => Service['restbase'],
+    }
+
+    file { '/etc/mediawiki/restbase/miraheze_project_sys.yaml':
+        ensure  => present,
+        source  => 'puppet:///modules/services/restbase/miraheze_project_sys.yaml',
         require => File['/etc/mediawiki/restbase'],
         notify  => Service['restbase'],
     }
