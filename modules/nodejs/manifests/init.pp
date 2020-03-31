9811b1fcb2cb71d2ac54b0760f22b442a03c84d4
paladox <paladox@users.noreply.github.com>
2020-03-31 14:49:45 +0100

    nodejs: Upgrade to NodeJS 12

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -4,7 +4,7 @@ class nodejs {
 
     apt::source { 'nodejs_apt':
         comment  => 'NODEJS',
-        location => 'https://deb.nodesource.com/node_8.x',
+        location => 'https://deb.nodesource.com/node_12.x',
         release  => "${::lsbdistcodename}",
         repos    => 'main',
         key      => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
