d9c32859c48c63203bc445746b14fd2f294c4c8a
paladox <paladox@users.noreply.github.com>
2020-04-21 09:44:34 +0100

    Switch db6 to being the db master

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,7 +28,7 @@ node 'db4.miraheze.org' {
 
 node 'db6.miraheze.org' {
     include base
-    include role::dbreplication
+    include role::db
     include bacula::client
     include role::salt::minions
     include prometheus::mysqld_exporter
