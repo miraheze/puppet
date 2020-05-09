0971ef53aab421cb096a4507c0b22dca009e2799
paladox <paladox@users.noreply.github.com>
2020-05-09 16:40:53 +0100

    Update init.pp

--- modules/matomo/manifests/init.pp
+++ modules/matomo/manifests/init.pp
@@ -1,6 +1,6 @@
 # class: matomo
 class matomo (
-    String $ldap_password = lookup('password::matomo::ldap_password'),
+    String $ldap_password = lookup('passwords::matomo::ldap_password'),
 ) {
     git::clone { 'matomo':
         directory          => '/srv/matomo',
