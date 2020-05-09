c4aa5e6f32a4adba8c28a95497e93ce4ac6cd191
paladox <paladox@users.noreply.github.com>
2020-05-09 23:16:57 +0100

    Update apt.pp

--- modules/salt/manifests/apt.pp
+++ modules/salt/manifests/apt.pp
@@ -5,11 +5,11 @@ class salt::apt {
         $os_version = $facts['os']['release']['major']
         apt::key { 'salt_key':
           id     => '754A1A7AE731F165D5E6D4BD0E08A149DE57BFBE',
-          source => "https://repo.saltstack.com/py3/debian/${os_version}/amd64/3000/SALTSTACK-GPG-KEY.pub",
+          source => "https://repo.saltstack.com/py3/debian/${os_version}/amd64/2019.2/SALTSTACK-GPG-KEY.pub",
         }
 
         apt::source { 'salt_apt':
-          location => "http://repo.saltstack.com/py3/debian/${os_version}/amd64/3000",
+          location => "http://repo.saltstack.com/py3/debian/${os_version}/amd64/2019.2",
           release  => "${::lsbdistcodename}",
           repos    => 'main',
           notify   => Exec['apt_update_salt'],
