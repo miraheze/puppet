4eeaf8092cf9819625ee23719e597fb8428f08b1
paladox <paladox@users.noreply.github.com>
2020-07-01 17:07:27 +0100

    Update salt.pp

--- modules/role/manifests/salt.pp
+++ modules/role/manifests/salt.pp
@@ -1,9 +1,9 @@
 class role::salt (
-    String $salt_state_roots    = '/srv/salt'
-    String $salt_file_roots     = '/srv/salt'
-    String $salt_pillar_roots   = '/srv/pillars'
-    String $salt_module_roots   = '/srv/salt/_modules'
-    String $salt_returner_roots = '/srv/salt/_returners'
+    String $salt_state_roots    = '/srv/salt',
+    String $salt_file_roots     = '/srv/salt',
+    String $salt_pillar_roots   = '/srv/pillars',
+    String $salt_module_roots   = '/srv/salt/_modules',
+    String $salt_returner_roots = '/srv/salt/_returners',
 ) {
 
     class { '::salt':
