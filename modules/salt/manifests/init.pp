b073ef796e76bfdc7114dec98c5ec89d5e640d1c
paladox <paladox@users.noreply.github.com>
2020-07-01 20:02:15 +0100

    salt: Maintain known_hosts for root too
    Turns out that when running salt-user as root, it uses /root/.ssh not the user from the roster config.

--- modules/salt/manifests/init.pp
+++ modules/salt/manifests/init.pp
@@ -11,29 +11,44 @@ class salt {
         mode    => '0444',
         require => Package['salt-ssh'],
     }
-
-     file { '/home/salt-user/.ssh':
+ 
+    file { '/home/salt-user/.ssh':
         ensure  => directory,
         mode    => '0700',
         owner   => 'salt-user',
         group   => 'salt-user',
         require => User['salt-user'],
-     }
+    }
  
-     file { '/home/salt-user/.ssh/id_ed25519':
-         source    => 'puppet:///private/base/user/salt-user-ssh-key',
-         owner     => 'salt-user',
-         group     => 'salt-user',
-         mode      => '400',
-         show_diff => false,
-         require   => File['/home/salt-user/.ssh'],
-     }
+    file { '/home/salt-user/.ssh/id_ed25519':
+        source    => 'puppet:///private/base/user/salt-user-ssh-key',
+        owner     => 'salt-user',
+        group     => 'salt-user',
+        mode      => '400',
+        show_diff => false,
+        require   => File['/home/salt-user/.ssh'],
+    }
      
     file { '/home/salt-user/.ssh/known_hosts':
-         content   => template('salt/salt-user-known-hosts.erb'),
-         owner     => 'salt-user',
-         group     => 'salt-user',
-         mode      => '644',
-         require   => File['/home/salt-user/.ssh'],
-     }
+        content   => template('salt/salt-user-known-hosts.erb'),
+        owner     => 'salt-user',
+        group     => 'salt-user',
+        mode      => '644',
+        require   => File['/home/salt-user/.ssh'],
+    }
+
+    file { '/root/.ssh':
+        ensure  => directory,
+        mode    => '0700',
+        owner   => 'root',
+        group   => 'root',
+    }
+     
+    file { '/root/.ssh/known_hosts':
+        content   => template('salt/salt-user-known-hosts.erb'),
+        owner     => 'root',
+        group     => 'root',
+        mode      => '644',
+        require   => File['/root/.ssh'],
+    }
 }
