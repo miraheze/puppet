db38faff3d9b47414e562d3fa8e6fc75e2ca8fa0
paladox <paladox@users.noreply.github.com>
2020-07-01 16:47:03 +0100

    Update init.pp

--- modules/salt/manifests/init.pp
+++ modules/salt/manifests/init.pp
@@ -14,98 +14,23 @@ class salt (
     String $salt_module_roots             = '/srv/salt/_modules',
     String $salt_returner_roots           = '/srv/salt/_returners',
 ) {
-    include salt::apt
-
-    include ssl::wildcard
-
-    # May be installed in the future again,
-    # but with proper measures in place.
-    package { 'salt-master':
+    package { 'salt-ssh':
         ensure  => 'absent',
     }
 
-    # Do NOT install and run this service
-    # on any Miraheze server. Salt is NOT safe
-    # for usage here, EVEN with the patch in place.
-    service { 'salt-master':
-        ensure  => stopped,
-        enable  => true,
-        require => Package['salt-master'],
-    }
-
-    file { '/etc/salt/master':
-        content => template('salt/master.erb'),
+    $host = query_nodes("domain='$domain'", 'fqdn')
+    file { '/etc/salt/roster':
+        content => template('salt/roster.erb'),
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
-        notify  => Service['salt-master'],
-        require => Package['salt-master'],
+        notify  => Service['salt-ssh'],
+        require => Package['salt-ssh'],
     }
 
-    file { $salt_runner_dirs:
-        ensure => directory,
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
-    }
-
-    file { "${salt_runner_dirs}/keys.py":
-        ensure => present,
-        source => 'puppet:///modules/salt/keys.py',
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
-    }
-
-    file { $salt_reactor_root:
-        ensure => directory,
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
-    }
-
-    # this also is the same as $salt_state_roots
-    file { $salt_file_roots:
-        ensure => directory,
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
-    }
-
-    file { $salt_pillar_roots:
-        ensure => directory,
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
-    }
-
-    file { $salt_module_roots:
-        ensure => directory,
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
-    }
-
-    file { $salt_returner_roots:
-        ensure => directory,
-        mode   => '0755',
-        owner  => 'root',
-        group  => 'root',
-    }
-
-    sysctl::parameters { 'salt-master':
-        values => {
-            'net.core.somaxconn'          => 4096,
-            'net.core.netdev_max_backlog' => 4096,
-            'net.ipv4.tcp_mem'            => '16777216 16777216 16777216',
-        }
-    }
-
-    # Reducing permissions on the master cache, by default is 0755
-    file { '/var/cache/salt/master':
-        ensure => directory,
-        mode   => '0750',
-        owner  => 'root',
-        group  => 'root',
+    service { 'salt-ssh':
+        ensure  => running,
+        enable  => true,
+        require => Package['salt-ssh'],
     }
 }
