b4e6c4dae27df28a8aee76b7b78806cb78c1be72
paladox <paladox@users.noreply.github.com>
2020-07-07 19:47:49 +0100

    Update config.pp

--- modules/mariadb/manifests/config.pp
+++ modules/mariadb/manifests/config.pp
@@ -110,4 +110,13 @@ class mariadb::config(
             mysql_ssl      => true,
         },
     }
+
+    if $server_role == 'slave' {
+        monitoring::services { 'Check Mysql Replication':
+            check_command => 'nrpe',
+            vars          => {
+                nrpe_command => "check_mysql-replication_${::fqdn}",
+            },
+        }
+    }
 }
