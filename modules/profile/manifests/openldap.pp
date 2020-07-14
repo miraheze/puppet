4d750bb8143a78deb7bf4431e515055810c04ac9
paladox <paladox@users.noreply.github.com>
2020-07-14 18:45:32 +0100

    Fix password for ldapvi

--- modules/profile/manifests/openldap.pp
+++ modules/profile/manifests/openldap.pp
@@ -1,6 +1,7 @@
 # openldap server
 class profile::openldap (
-    String $password = lookup('profile::openldap::password'),
+    String $admin_password = lookup('profile::openldap::admin_password'),
+    String $ldapvi_password = lookup('profile::openldap::ldapvi_password'),
 ) {
     include ssl::wildcard
 
