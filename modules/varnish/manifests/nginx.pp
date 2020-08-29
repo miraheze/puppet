0bfa2d5c432287988e21262c55a1fba244f4fad8
paladox <paladox@users.noreply.github.com>
2020-08-28 20:10:02 -0400

    Revert "Revert "varnish: Fix rate limiter (#1486)" (#1487)"
    This reverts commit 3b635041779250aabbf3d426a505b8eede8cd155.

--- modules/varnish/manifests/nginx.pp
+++ modules/varnish/manifests/nginx.pp
@@ -3,6 +3,10 @@ class varnish::nginx {
     $sslcerts = loadyaml('/etc/puppetlabs/puppet/ssl-cert/certs.yaml')
     $sslredirects = loadyaml('/etc/puppetlabs/puppet/ssl-cert/redirects.yaml')
 
+    # Used to whitelist miraheze ips within the rate limiter
+    $ip_4 = query_nodes("domain='$domain'", 'ipaddress')
+    $ip_6 = query_nodes("domain='$domain'", 'ipaddress6')
+
     nginx::site { 'mediawiki':
         ensure       => present,
         content      => template('varnish/mediawiki.conf'),
