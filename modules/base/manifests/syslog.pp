4a7e6be4bf71ebb2fa1992b7368bc5dcb431c2cc
Ferran Tufan <southparkfan223@hotmail.com>
2020-10-04 20:58:53 +0200

    base::syslog: do not use mutual authentication, only verify graylog's certificate

--- modules/base/manifests/syslog.pp
+++ modules/base/manifests/syslog.pp
@@ -49,8 +49,6 @@ class base::syslog (
                                                 'tls' => [
                                                         { 'peer-verify' => 'required-trusted' },
                                                         { 'ca-dir' => '/etc/ssl/certs' },
-                                                        { 'cert-file' => '/etc/ssl/certs/wildcard.miraheze.org-2020.crt' },
-                                                        { 'key-file' => '/etc/ssl/private/wildcard.miraheze.org-2020.key' },
                                                         { 'ssl-options' => [ 'no-sslv2', 'no-sslv3', 'no-tlsv1', 'no-tlsv11' ] }
                                                 ]
                                         },
