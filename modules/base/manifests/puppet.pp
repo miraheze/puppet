3bf1d8d3ea674bc11145786339a77ccfb852e7ce
paladox <paladox@users.noreply.github.com>
2020-12-05 22:28:44 +0000

    base::puppet: Remove support for puppet4 (#1541)
    * base::puppet: Remove support for puppet4
    
    * Delete puppet.4.conf.erb

--- modules/base/manifests/puppet.pp
+++ modules/base/manifests/puppet.pp
@@ -1,107 +1,69 @@
 # class base::puppet
-class base::puppet {
+class base::puppet (
+    $puppet_major_version = lookup('puppet_major_version', {'default_value' => 6}),
+    $puppetserver_hostname = lookup('puppetserver_hostname', {'default_value' => 'puppet2.miraheze.org'}),
+) {
 
-    $puppet_major_version = lookup('puppet_major_version', {'default_value' => 6})
+    apt::source { 'puppetlabs':
+        location => 'http://apt.puppetlabs.com',
+        repos    => "puppet${puppet_major_version}",
+        key      => {
+            'id'     => '6F6B15509CF8E59E6E469F327F438280EF8D349F',
+            'server' => 'keyserver.ubuntu.com',
+        },
+    }
 
-    if $puppet_major_version == 6 {
-        $puppetserver_hostname = lookup('puppetserver_hostname', {'default_value' => 'puppet2.miraheze.org'})
+    package { 'puppet-agent':
+        ensure  => present,
+        require => Apt::Source['puppetlabs'],
+    }
 
-        apt::source { 'puppetlabs':
-            location => 'http://apt.puppetlabs.com',
-            repos    => "puppet${puppet_major_version}",
-            key      => {
-                'id'     => '6F6B15509CF8E59E6E469F327F438280EF8D349F',
-                'server' => 'keyserver.ubuntu.com',
-            },
-        }
+    # facter needs this for proper "virtual"/"is_virtual" resolution
+    require_package('virt-what')
 
-        package { 'puppet-agent':
+    file { '/usr/bin/facter':
+        ensure  => link,
+        target  => '/opt/puppetlabs/bin/facter',
+        require => Package['puppet-agent'],
+    }
+
+    file { '/usr/bin/hiera':
+        ensure  => link,
+        target  => '/opt/puppetlabs/bin/hiera',
+        require => Package['puppet-agent'],
+    }
+
+    file { '/usr/bin/puppet':
+        ensure  => 'link',
+        target  => '/opt/puppetlabs/bin/puppet',
+        require => Package['puppet-agent'],
+    }
+
+    file { '/var/log/puppet':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0750',
+    }
+
+    cron { 'puppet-agent':
+        command => '/usr/bin/puppet agent -tv >> /var/log/puppet/puppet.log',
+        user    => 'root',
+        hour    => '*',
+        minute  => '*/10',
+    }
+
+    logrotate::conf { 'puppet':
+        ensure => present,
+        source => 'puppet:///modules/base/puppet/puppetlabs.puppet.logrotate.conf',
+    }
+
+    if !lookup('puppetserver') {
+        file { '/etc/puppetlabs/puppet/puppet.conf':
             ensure  => present,
-            require => Apt::Source['puppetlabs'],
-        }
-
-        # facter needs this for proper "virtual"/"is_virtual" resolution
-        require_package('virt-what')
-
-        file { '/usr/bin/facter':
-            ensure  => link,
-            target  => '/opt/puppetlabs/bin/facter',
+            content => template("base/puppet/puppet.${puppet_major_version}.conf.erb"),
+            mode    => '0444',
             require => Package['puppet-agent'],
-        }
-
-        file { '/usr/bin/hiera':
-            ensure  => link,
-            target  => '/opt/puppetlabs/bin/hiera',
-            require => Package['puppet-agent'],
-        }
-
-        file { '/usr/bin/puppet':
-            ensure  => 'link',
-            target  => '/opt/puppetlabs/bin/puppet',
-            require => Package['puppet-agent'],
-        }
-
-        file { '/var/log/puppet':
-            ensure => directory,
-            owner  => 'root',
-            group  => 'root',
-            mode   => '0750',
-        }
-
-        cron { 'puppet-agent':
-            command => '/usr/bin/puppet agent -tv >> /var/log/puppet/puppet.log',
-            user    => 'root',
-            hour    => '*',
-            minute  => '*/10',
-        }
-
-        logrotate::conf { 'puppet':
-            ensure => present,
-            source => 'puppet:///modules/base/puppet/puppetlabs.puppet.logrotate.conf',
-        }
-
-        if !lookup('puppetserver') {
-            file { '/etc/puppetlabs/puppet/puppet.conf':
-                ensure  => present,
-                content => template("base/puppet/puppet.${puppet_major_version}.conf.erb"),
-                mode    => '0444',
-                require => Package['puppet-agent'],
-            }
-        }
-    } else {
-        # deprecated
-        $puppetmaster_hostname = lookup('puppetmaster_hostname', {'default_value' => 'puppet1.miraheze.org'})
-
-        require_package('puppet', 'facter')
-
-        # facter needs this for proper "virtual"/"is_virtual" resolution
-        require_package('virt-what')
-
-        file { '/var/log/puppet':
-            ensure => directory,
-            owner  => 'puppet',
-            group  => 'puppet',
-            mode   => '0750',
-        }
-
-        cron { 'puppet-agent':
-            command => '/usr/bin/puppet agent -tv >> /var/log/puppet/puppet.log',
-            user    => 'root',
-            hour    => '*',
-            minute  => '*/10',
-        }
-
-        logrotate::conf { 'puppet':
-            ensure => present,
-            source => 'puppet:///modules/base/puppet/puppet.logrotate.conf',
-        }
-
-        if !lookup('puppetmaster') {
-            file { '/etc/puppet/puppet.conf':
-                ensure => present,
-                content => template("base/puppet/puppet.${puppet_major_version}.conf.erb"),
-                mode   => '0444',
-            }
         }
     }
 
