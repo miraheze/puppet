ec0180452faddaf5b0394ef77d2b6754ab53a8e1
paladox <paladox@users.noreply.github.com>
2020-12-08 02:09:41 +0000

    Update role.rb

--- modules/vmlib/lib/puppet/functions/role.rb
+++ modules/vmlib/lib/puppet/functions/role.rb
@@ -42,7 +42,7 @@ Puppet::Functions.create_function(:role) do
     scope = closure_scope
     # Check if the function has already been called
     if scope.include? '_role'
-      raise Puppet::ParseError, "role() can only be called once per node"
+      raise(Puppet::ParseError, "role() can only be called once per node")
     end
     role = main_role.gsub(/^::/, '')
     # Backwards compat
