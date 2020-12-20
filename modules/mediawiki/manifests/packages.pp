2e5a0c8e18c2166319fad8c5cba6af1d03bf7f55
Ferran Tufan <southparkfan223@hotmail.com>
2020-12-20 18:07:45 +0100

    mediawiki: install fatrace

--- modules/mediawiki/manifests/packages.pp
+++ modules/mediawiki/manifests/packages.pp
@@ -5,6 +5,7 @@ class mediawiki::packages {
         'dvipng',
         'htmldoc',
         'inkscape',
+        'fatrace',
         'fonts-freefont-ttf',
         'ffmpeg',
         'ffmpeg2theora',
