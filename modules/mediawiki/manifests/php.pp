cf488bde3f4592a0e280ac27bb0139a30542b595
paladox <paladox@users.noreply.github.com>
2020-12-21 23:22:43 +0000

    mediawiki: Switch to syslog for php-error log if using syslog_ng

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -1,15 +1,21 @@
 # mediawiki::php
 class mediawiki::php (
-    $php_fpm_childs = lookup('mediawiki::php::fpm::childs', {'default_value' => 26}),
-    $fpm_min_restart_threshold = lookup('mediawiki::php::fpm::fpm_min_restart_threshold', {'default_value' => 6}),
-    $php_version = lookup('php::php_version', {'default_value' => '7.2'}),
+    Integer $php_fpm_childs = lookup('mediawiki::php::fpm::childs', {'default_value' => 26}),
+    Integer $fpm_min_restart_threshold = lookup('mediawiki::php::fpm::fpm_min_restart_threshold', {'default_value' => 6}),
+    String $php_version = lookup('php::php_version', {'default_value' => '7.2'}),
     Optional[Boolean] $use_tideways = undef,
+    String $syslog_daemon = lookup('base::syslog::syslog_daemon', {'default_value' => 'rsyslog'}),
 ) {
+    
     if !defined(Class['php::php_fpm']) {
+        $syslog =  $syslog_daemon ? {
+            'syslog_ng' => 'syslog',
+            default => '/var/log/mediawiki/debuglogs/php-error.log'
+        }
         class { 'php::php_fpm':
             config  => {
                 'display_errors'            => 'Off',
-                'error_log'                 => '/var/log/mediawiki/debuglogs/php-error.log',
+                'error_log'                 => $syslog,
                 'error_reporting'           => 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
                 'log_errors'                => 'On',
                 'memory_limit'              => '512M',
