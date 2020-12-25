279c747fbaf926921c5d2f34db8edb1a3a9f1ca3
paladox <paladox@users.noreply.github.com>
2020-12-25 00:02:37 +0000

    git: Add allow_unrelated_histories param (#1572)

--- modules/git/manifests/clone.pp
+++ modules/git/manifests/clone.pp
@@ -19,9 +19,10 @@ define git::clone(
     String  $group = 'root',
     String  $timeout = '600',
     String  $depth = 'full',
-    Boolean $recurse_submodules=false,
+    Boolean $recurse_submodules = false,
     String  $mode = '0755',
     String  $umask = '022',
+    Boolean $allow_unrelated_histories = false,
 ) {
 
     case $ensure {
@@ -40,14 +41,15 @@ define git::clone(
                 true    => '--recurse-submodules ',
                 default => '',
             }
+
             # if branch was specified
             if !empty($branch) {
                 $brancharg = "-b ${branch} "
-            }
-            # else don't checkout a non-default branch
-            else {
+            } else {
+                # else don't checkout a non-default branch
                 $brancharg = ''
             }
+
             if !empty($ssh) {
                 $env = "GIT_SSH=${ssh}"
             } else {
@@ -59,11 +61,16 @@ define git::clone(
                 default => " --depth=${depth}"
             }
 
+            $allow_unrelated_histories_arg = $allow_unrelated_histories ?  {
+                true    => '--allow-unrelated-histories',
+                default => ''
+            }
+
             $git = '/usr/bin/git'
 
             # clone the repository
             exec { "git_clone_${title}":
-                command     => "${git} clone ${recurse_submodules_arg}${brancharg}${origin}${deptharg} ${directory}",
+                command     => "${git} clone ${recurse_submodules_arg}${brancharg}${origin}${deptharg} ${allow_unrelated_histories_arg} ${directory}",
                 provider    => shell,
                 logoutput   => on_failure,
                 cwd         => '/tmp',
@@ -127,7 +134,7 @@ define git::clone(
                 }
                 exec { "git_pull_${title}":
                     cwd       => $directory,
-                    command   => "${git} pull ${recurse_submodules_arg}--quiet${deptharg}",
+                    command   => "${git} pull ${recurse_submodules_arg}--quiet${deptharg} ${allow_unrelated_histories_arg}",
                     provider  => shell,
                     logoutput => on_failure,
                     # git diff --quiet will exit 1 (return false)
