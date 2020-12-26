9e264ebbb2422bca7d6f536a17c541f970995800
paladox <paladox@users.noreply.github.com>
2020-12-26 22:58:53 +0000

    Create sitemap.php

---
+++ modules/mediawiki/files/sitemap.php
@@ -0,0 +1,43 @@
+<?php
+
+$databaseJsonFileName = '/srv/mediawiki/w/cache/databases.json';
+$databasesArray = file_exists( $databaseJsonFileName ) ?
+	json_decode( file_get_contents( $databaseJsonFileName ), true ) : [ 'combi' => [] ];
+
+if ( $databasesArray['combi'] ) {
+	if ( preg_match( '/^(.+)\.miraheze\.org$/', $_SERVER['HTTP_HOST'], $matches ) ) {
+		$wiki = "{$matches[1]}wiki";
+		if ( !isset( $databasesArray['combi']["{$wiki}"] ) ) {
+			return;
+		}
+
+		header( "Location: https://static.miraheze.org/{$wiki}/sitemaps/sitemap.xml", true, 302 );
+	} else {
+		$customDomainFound = false;
+		$suffixes = [ 'wiki' ];
+		$suffixMatch = array_flip( [ 'miraheze.org' => 'wiki' ] );
+		foreach ( $databasesArray['combi'] as $db => $data ) {
+			foreach ( $suffixes as $suffix ) {
+				if ( substr( $db, -strlen( $suffix ) == $suffix ) ) {
+					$url = $data['u'] ?? 'https://' . substr( $db, 0, -strlen( $suffix ) ) . '.' . $suffixMatch[$suffix];
+
+					if ( !isset( $url ) || !$url ) {
+						continue;
+					}
+
+					if ( $url === "https://{$_SERVER['HTTP_HOST']}" ) {
+						$customDomainFound = $db;
+					}
+				}
+			}
+
+			continue;
+		}
+
+		if ( $customDomainFound ) {
+			header( "Location: https://static.miraheze.org/{$customDomainFound}/sitemaps/sitemap.xml", true, 302 );
+		}
+	}
+}
+
+exit();
