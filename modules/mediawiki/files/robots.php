c2ddc67d90ad1d8cb99c5c3a1282d003eebbcd17
paladox <paladox@users.noreply.github.com>
2020-12-26 17:22:28 +0000

    Fix sitemap in robots.php

--- modules/mediawiki/files/robots.php
+++ modules/mediawiki/files/robots.php
@@ -1,6 +1,10 @@
 <?php
 
-header('Content-Type:text/plain');
+header( 'Content-Type: text/plain' );
+
+$databaseJsonFileName = '/srv/mediawiki/w/cache/databases.json';
+$databasesArray = fil_exists( $$databaseJsonFileName ) ?
+    json_decode( file_get_contents( $$databaseJsonFileName ), true ) : [];
 
 # Disallow API and special pages
 echo "# Disallow API and special pages" . "\r\n";
@@ -24,6 +28,40 @@ echo "# Block SemrushBot" . "\r\n";
 echo "User-Agent: SemrushBot" . "\r\n";
 echo "Disallow: /" . "\r\n\n";
 
+if ( isset( $databasesArray['combi'] ) && $databasesArray['combi'] ) {
+  $wikis = array_keys( $databasesArray['combi'] );
+  if ( preg_match( '/^(.+)\.miraheze\.org$/', $_SERVER['HTTP_HOST'], $matches ) ) {
+    if ( !isset( $wikis["{$matches[0]}wiki"] ) ) {
+      return;
+    }
+
+    # Dynamic sitemap url
+    echo "# Dynamic sitemap url" . "\r\n";
+    echo "Sitemap: https://static.miraheze.org/{$wikis["{$matches[0]}wiki"]}/sitemaps/sitemap.xml" . "\r\n\n";
+  } else {
+    $customDomainFound = false;
+    $suffixes = [ 'wiki' ];
+    $suffixMatch = array_flip( [ 'miraheze.org' => 'wiki' ] );
+		foreach ( $databasesArray['combi'] as $db => $data ) {
+			foreach ( $suffixes as $suffix ) {
+				if ( substr( $db, -strlen( $suffix ) == $suffix ) ) {
+          if ( substr( $db, 0, -strlen( $suffix ) ) === $_SERVER['HTTP_HOST'] ) {
+            $customDomainFound = $db;
+            return;
+          }
+				}
+			}
+
+			continue;
+		}
+    
+    if ( $customDomainFound ) {
+      # Dynamic sitemap url
+      echo "# Dynamic sitemap url" . "\r\n";
+      echo "Sitemap: https://static.miraheze.org/{$customDomainFound}/sitemaps/sitemap.xml" . "\r\n\n";
+    }
+  }
+}
 # Dynamic sitemap url
 echo "# Dynamic sitemap url" . "\r\n";
 echo "Sitemap: https://$_SERVER[HTTP_HOST]/wiki/Special:RedirectSitemap" . "\r\n\n";
