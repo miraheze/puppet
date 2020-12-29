93742aa79af984cd355725be5ce98cc0474095b3
paladox <paladox@users.noreply.github.com>
2020-12-29 17:31:23 +0000

    ssl: Remove legacy certificate

--- modules/ssl/manifests/wildcard.pp
+++ modules/ssl/manifests/wildcard.pp
@@ -3,25 +3,6 @@ class ssl::wildcard (
     $ssl_cert_path = '/etc/ssl/certs',
     $ssl_cert_key_private_path = '/etc/ssl/private',
 ) {
-    # Legacy kept for 2020 TLS certificate switchover
-    if !defined(File['wildcard.miraheze.org']) {
-        file { 'wildcard.miraheze.org':
-            ensure => 'absent',
-            source => 'puppet:///ssl/certificates/wildcard.miraheze.org.crt',
-            path   => "${ssl_cert_path}/wildcard.miraheze.org.crt",
-        }
-    }
-
-    if !defined(File['wildcard.miraheze.org_private']) {
-        file { 'wildcard.miraheze.org_private':
-            ensure => 'absent',
-            source => 'puppet:///ssl-keys/wildcard.miraheze.org.key',
-            path   => "${ssl_cert_key_private_path}/wildcard.miraheze.org.key",
-            owner  => 'root',
-            group  => 'ssl-cert',
-            mode   => '0660',
-        }
-    }
 
     # New certificate for 2020 switchover
     if !defined(File['wildcard.miraheze.org-2020']) {
