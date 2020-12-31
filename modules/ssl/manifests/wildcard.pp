b566b42a2259fedc8cf5eab3dad40406fa7d0e31
paladox <thomasmulhall410@yahoo.com>
2020-12-31 13:17:07 +0000

    Revert "ssl: Purge wildcard.miraheze.org-2020"
    This reverts commit 442cbb91040ca4f4bd666a8cf661b5ed8dcd36af.

--- modules/ssl/manifests/wildcard.pp
+++ modules/ssl/manifests/wildcard.pp
@@ -7,13 +7,20 @@ class ssl::wildcard (
     # New certificate for 2020 switchover
     if !defined(File['wildcard.miraheze.org-2020']) {
         file { 'wildcard.miraheze.org-2020':
-            ensure => 'absent',
+            ensure => 'present',
+            source => 'puppet:///ssl/certificates/wildcard.miraheze.org-2020.crt',
+            path   => "${ssl_cert_path}/wildcard.miraheze.org-2020.crt",
         }
     }
 
     if !defined(File['wildcard.miraheze.org-2020_private']) {
         file { 'wildcard.miraheze.org-2020_private':
-            ensure => 'absent',
+            ensure => 'present',
+            source => 'puppet:///ssl-keys/wildcard.miraheze.org-2020.key',
+            path   => "${ssl_cert_key_private_path}/wildcard.miraheze.org-2020.key",
+            owner  => 'root',
+            group  => 'ssl-cert',
+            mode   => '0660',
         }
     }
 
