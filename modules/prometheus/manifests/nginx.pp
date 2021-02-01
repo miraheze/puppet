4d5080e6f1052d85e567355e1badfc4ab5db6617
paladox <paladox@users.noreply.github.com>
2021-02-01 01:42:27 +0000

    Update nginx.pp

--- modules/prometheus/manifests/nginx.pp
+++ modules/prometheus/manifests/nginx.pp
@@ -23,13 +23,13 @@ class prometheus::nginx {
 
     $firewall = query_facts('Class[Prometheus]', ['ipaddress', 'ipaddress6'])
     $firewall.each |$key, $value| {
-        ufw::allow { "prometheus 9113 ipv4 ${value['ipaddress']}":
+        ufw::allow { "prometheus 9113 ${value['ipaddress']}":
             proto => 'tcp',
             port  => 9113,
             from  => $value['ipaddress'],
         }
 
-        ufw::allow { "prometheus 9113 ipv6 ${value['ipaddress6']}":
+        ufw::allow { "prometheus 9113 ${value['ipaddress6']}":
             proto => 'tcp',
             port  => 9113,
             from  => $value['ipaddress6'],
