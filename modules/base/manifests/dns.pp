c2fb09a6323b52b45b0971baad422c68a998e8cf
John Lewis <john@miraheze.org>
2021-02-25 15:55:46 +0000

    add monitoring + resolvconf

--- modules/base/manifests/dns.pp
+++ modules/base/manifests/dns.pp
@@ -8,11 +8,24 @@ class base::dns {
         mode   => '0444',
         owner  => 'pdns',
         group  => 'pdns',
+        notify => Service['pdns-recursor'],
         source => 'puppet:///modules/base/dns/recursor.conf',
     }
 
     service { 'pdns-recursor':
         ensure  => running,
+        require => Package['pdns-recursor'],
+    }
+
+    monitoring::services { 'PowerDNS Recursor':
+        check_command => 'nrpe',
+        vars          => {
+            nrpe_command => 'check_pdns_recursor',
+        },
+    }
+
+    file { '/etc/resolv.conf':
+        source  => 'puppet:///modules/base/dns/resolv.conf',
         require => Package['pdns-recursor'],
     }
 }
