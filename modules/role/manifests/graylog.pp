5e334e385cd6d54071b022aa6b7772a9ef67e053
paladox <paladox@users.noreply.github.com>
2021-02-27 01:35:40 +0000

    graylog: upgrade mongodb to 4.4.4

--- modules/role/manifests/graylog.pp
+++ modules/role/manifests/graylog.pp
@@ -10,7 +10,7 @@ class role::graylog {
 
     class { 'mongodb::globals':
         manage_package_repo => true,
-        version             => '4.4.3',
+        version             => '4.4.4',
     }->
     class { 'mongodb::server':
         bind_ip => ['127.0.0.1'],
