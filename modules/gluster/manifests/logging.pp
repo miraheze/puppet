f5baab4065bc8bc2c11dec7b60895d2fa46d449b
paladox <paladox@users.noreply.github.com>
2021-03-01 20:57:39 +0000

    Introduce gluster::logging
    This is to allow us to read files such as glusterd.log and send to gluster.
    
    TODO: Possibly need to add support for templates.

---
+++ modules/gluster/manifests/logging.pp
@@ -0,0 +1,30 @@
+# == Define: gluster::logging
+#
+# Read log file and sends to centralised logger (e.g reads /var/log/glusterfs/glusterd.log and sends to gluster).
+#
+# === Parameters
+#
+# [file_source_options*]
+#   Options for the file source for example [ '/var/log/glusterfs/glusterd.log' ].
+#   This essentially reads from the log file and sends it to gluster.
+#
+define gluster::logging (
+	Array[String] $file_source_options
+) {
+	syslog_ng::source { 's_file_${title}':
+		params => {
+			'type'    => 'file',
+			'options' => $file_source_options,
+		},
+	} ->
+	syslog_ng::log { "s_file_${title} to d_graylog_syslog_tls":
+		params => [
+			{
+				'source' => "s_file_${title}",
+			},
+			{
+				'destination' => 'd_graylog_syslog_tls',
+			},
+		],
+	}
+}
