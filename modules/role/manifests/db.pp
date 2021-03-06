edebaeff6f110a0d8daa58b3770a3a6c62f98b95
paladox <paladox@users.noreply.github.com>
2021-03-06 22:09:06 +0000

    Update db.pp

--- modules/role/manifests/db.pp
+++ modules/role/manifests/db.pp
@@ -12,6 +12,8 @@ class role::db {
     $roundcubemail_password = lookup('passwords::roundcubemail')
     $icingaweb2_db_user_password = lookup('passwords::icingaweb2')
  
+    include ssl::wildcard
+
     file { '/etc/ssl/private':
         ensure  => directory,
         owner   => 'root',
@@ -19,12 +21,10 @@ class role::db {
         mode    => '0750',
     }
 
-    include ssl::wildcard
-
     class { 'mariadb::config':
-        config      => 'mariadb/config/mw.cnf.erb',
-        password    => lookup('passwords::db::root'),
-        server_role => 'master',
+        config          => 'mariadb/config/mw.cnf.erb',
+        password        => lookup('passwords::db::root'),
+        server_role     => 'master',
         icinga_password => $icinga_password,
     }
 
@@ -61,23 +61,23 @@ class role::db {
     $fwPort3306 = query_facts("domain='$domain' and (Class[Role::Db] or Class[Role::Dbbackup] or Class[Role::Mediawiki] or Class[Role::Icinga2] or Class[Role::Roundcubemail] or Class[Role::Phabricator])", ['ipaddress', 'ipaddress6'])
     $fwPort3306.each |$key, $value| {
         ufw::allow { "mariadb inbound 3306/tcp for ${value['ipaddress']}":
-            proto   => 'tcp',
-            port    => 3306,
-            from    => $value['ipaddress'],
+            proto => 'tcp',
+            port  => 3306,
+            from  => $value['ipaddress'],
         }
 
         ufw::allow { "mariadb inbound 3306/tcp for ${value['ipaddress6']}":
-            proto   => 'tcp',
-            port    => 3306,
-            from    => $value['ipaddress6'],
+            proto => 'tcp',
+            port  => 3306,
+            from  => $value['ipaddress6'],
         }
     }
 
     # Create a user to allow db transfers between servers
     users::user { 'dbcopy':
-        ensure      => present,
-        uid         => 3000,
-        ssh_keys    => [
+        ensure   => present,
+        uid      => 3000,
+        ssh_keys => [
     		'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFX1yvcRAMqwlbkkhMPhK1GFYrLYM18qC1YUcuUEErxz dbcopy@db6'
         ],
     }
