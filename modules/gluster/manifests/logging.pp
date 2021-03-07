743cfdda3bc8734c51242dab5b26b602ccf9ee42
paladox <paladox@users.noreply.github.com>
2021-03-07 23:51:32 +0000

    Update logging.pp

--- modules/gluster/manifests/logging.pp
+++ modules/gluster/manifests/logging.pp
@@ -11,6 +11,15 @@
 define gluster::logging (
 	Array[String] $file_source_options
 ) {
+	syslog_ng::rewrite { 'r_application_name':
+		params => {
+			'type'      => 'set',
+			'options'   => [
+				'glusterd',
+				{ 'value' => 'APPLICATION_NAME' }
+			],
+		},
+	} ->
 	syslog_ng::source { "s_file_${title}":
 		params => {
 			'type'    => 'file',
@@ -22,6 +31,9 @@ define gluster::logging (
 			{
 				'source' => "s_file_${title}",
 			},
+                        {
+                            'rewrite' => 'r_application_name',
+                        },
 			{
 				'destination' => 'd_graylog_syslog_tls',
 			},
