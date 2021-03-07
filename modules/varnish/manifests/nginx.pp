b26a874ecbd986526f6ef889404ee04bf7fe9eae
paladox <paladox@users.noreply.github.com>
2021-03-07 16:03:50 +0000

    Update nginx.pp

--- modules/varnish/manifests/nginx.pp
+++ modules/varnish/manifests/nginx.pp
@@ -8,20 +8,7 @@ class varnish::nginx {
     $ip_6 = query_nodes("domain='$domain'", 'ipaddress6')
 
     nginx::site { 'mediawiki':
-        ensure       => present,
-        content      => template('varnish/mediawiki.conf'),
-        notify_site  => Exec['nginx-syntax'],
-    }
-
-    exec { 'nginx-syntax':
-        command     => '/usr/sbin/nginx -t',
-        notify      => Exec['nginx-reload'],
-        refreshonly => true,
-    }
-
-    exec { 'nginx-reload':
-        command     => '/usr/sbin/service nginx reload',
-        refreshonly => true,
-        require     => Exec['nginx-syntax'],
+        ensure  => present,
+        content => template('varnish/mediawiki.conf'),
     }
 }
