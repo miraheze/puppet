1840b265fa9fff1261965449a9aa76d5966eb1ea
paladox <paladox@users.noreply.github.com>
2021-03-07 23:25:58 +0000

    puppetserver: Switch to systemd::service (#1692)
    Bug: T6802

--- modules/puppetserver/manifests/init.pp
+++ modules/puppetserver/manifests/init.pp
@@ -153,22 +153,7 @@ class puppetserver(
         group   => 'puppet-users',
         mode    => '0770',
     }
-
-    exec { 'puppetserver reload systemd':
-        command     => '/bin/systemctl daemon-reload',
-        refreshonly => true,
-    }
-
-    file { '/lib/systemd/system/puppetserver.service':
-        ensure  => present,
-        source  => 'puppet:///modules/puppetserver/puppetserver.systemd',
-        notify  => [
-            Exec['puppetserver reload systemd'],
-            Service['puppetserver'],
-        ],
-        require => Package['puppetserver'],
-    }
-
+ 
     if $puppetdb_enable {
         class { 'puppetserver::puppetdb::client':
             puppetdb_hostname => $puppetdb_hostname,
@@ -181,9 +166,10 @@ class puppetserver(
         }
     }
 
-    service { 'puppetserver':
-        ensure  => running,
-        enable  => true,
+    systemd::service { 'puppetserver':
+        ensure  => present,
+        content => systemd_template('puppetserver'),
+        restart => true,
         require => Package['puppetserver'],
     }
 
