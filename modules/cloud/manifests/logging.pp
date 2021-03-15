a21611544222f50c8166d043d312c9a2a8790ce2
paladox <paladox@users.noreply.github.com>
2021-03-15 23:36:21 +0000

    Introduce cloud::logging

---
+++ modules/cloud/manifests/logging.pp
@@ -0,0 +1,46 @@
+# == Define: cloud::logging
+#
+# Read log file and sends to centralised logger.
+#
+# === Parameters
+#
+# [file_source_options*]
+#   Options for the file source for example [ '/var/log/pveproxy/access.log' ].
+#
+# [program_name*]
+#   This sets the program name. Sometimes the log is parsed wrong and the
+#   program name is set wrong. This corrects the issue.
+#
+define cloud::logging (
+	Array  $file_source_options,
+	String $program_name,
+) {
+	syslog_ng::rewrite { 'r_program':
+		params => {
+			'type'      => 'set',
+			'options'   => [
+				$program_name,
+				{ 'value' => 'PROGRAM' }
+			],
+		},
+	} ->
+	syslog_ng::source { "s_file_${title}":
+		params => {
+			'type'    => 'file',
+			'options' => $file_source_options,
+		},
+	} ->
+	syslog_ng::log { "s_file_${title} to d_graylog_syslog_tls":
+		params => [
+			{
+				'source' => "s_file_${title}",
+			},
+			{
+				'rewrite' => 'r_program'
+			},
+			{
+				'destination' => 'd_graylog_syslog_tls',
+			},
+		],
+	}
+}
