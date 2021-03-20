b3fd2accff2308ebac9d78a045067109fbc37630
paladox <paladox@users.noreply.github.com>
2021-03-20 20:16:48 +0000

    Update generateVmtouch.py

--- modules/varnish/files/vmtouch/generateVmtouch.py
+++ modules/varnish/files/vmtouch/generateVmtouch.py
@@ -1,43 +1,12 @@
 #! /usr/bin/python3
 
 import os.path
-from glob import glob
 
 files = [
-    '/srv/mediawiki/w/cache/databases.json'
-]
-
-l10nLangs = [
-    'de',
-    'en',
-    'es',
-    'it',
-    'ja',
-    'ko',
-    'nl',
-    'zh',
-    'zh-cn',
-    'zh-tw'
-]
-
-globFiles = [
     '/etc/ssl/certs',
     '/etc/ssl/localcerts',
-    '/srv/mediawiki/w/cache/*wiki.json',
-    '/srv/mediawiki/w/extensions/*/extension.json',
-    '/srv/mediawiki/w/skins/*/skin.json',
     '/usr/share/ca-certificates/mozilla'
 ]
-
-for globFile in globFiles:
-    for file in glob(globFile):
-        if os.path.isfile(file):
-            files.append(file)
-        elif os.path.isdir(file):
-            files.append(file)
-
-for lang in l10nLangs:
-    files.append("/srv/mediawiki/w/cache/l10n/l10n_cache-{}.cdb".format(lang))
 
 with open('/etc/vmtouch-files.list', mode='wt') as filesList:
     filesList.write('\n'.join(files))
