7429c584a9bf6c02767f84f58613d570358ebf69
John Lewis <john@miraheze.org>
2021-04-07 00:32:00 +0100

    add jobQueueCollector script to Redis Prometheus exporter

---
+++ modules/prometheus/files/redis/jobQueueCollector.lua
@@ -0,0 +1,25 @@
+-- JobQueue additional metric collector
+-- Author: John Lewis, Miraheze
+
+local result = {}
+local queues = { 'l-unclaimed', 'z-claimed', 'z-abandoned', 'z-delayed' }
+-- Below is a list of jobs we want to monitor specifically
+local jobs = { '*', 'CreateWikiJob', 'RequestWikiAIJob', 'DataDumpGenerateJob', 'MWScriptJob', 'NamespaceMigrationJob', 'LocalRenameUserJob', 'LocalGlobalUserPageCacheUpdateJob', 'replaceText', 'webVideoTranscode', 'refreshLinks', 'htmlCacheUpdate', 'recentChangesUpdate' }
+
+for _,job in ipairs(jobs) do
+	for _,queue in ipairs(queues) do
+		local lsum = 0
+		local lkeys = redis.call( 'KEYS', 'global:jobqueue:' .. job .. ':' .. queue )
+		for _,lkey in ipairs(lkeys) do
+			if queue ~= 'l-unclaimed' then
+				lsum = lsum + tonumber(redis.call('ZCARD', lkey))
+			else
+				lsum = lsum + tonumber(redis.call('LLEN', lkey))
+			end
+		end
+		table.insert(result, job .. '-' .. queue )
+		table.insert(result, tostring(lsum) )
+	end
+end
+
+return result
