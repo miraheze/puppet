adcfd9f89f8fdf043a77ccd32d114502011f7e0d
John Lewis <john@miraheze.org>
2021-04-07 01:25:13 +0100

    wiki:jobqueue not global:jobqueue

--- modules/prometheus/files/redis/jobQueueCollector.lua
+++ modules/prometheus/files/redis/jobQueueCollector.lua
@@ -9,7 +9,7 @@ local jobs = { '*', 'CreateWikiJob', 'RequestWikiAIJob', 'DataDumpGenerateJob', 
 for _,job in ipairs(jobs) do
 	for _,queue in ipairs(queues) do
 		local lsum = 0
-		local lkeys = redis.call( 'KEYS', 'global:jobqueue:' .. job .. ':' .. queue )
+		local lkeys = redis.call( 'KEYS', '*:jobqueue:' .. job .. ':' .. queue )
 		for _,lkey in ipairs(lkeys) do
 			if queue ~= 'l-unclaimed' then
 				lsum = lsum + tonumber(redis.call('ZCARD', lkey))
