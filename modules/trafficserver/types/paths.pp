4b65bb38d7d5ec564ada233f615271e3b429cabd
paladox <paladox@users.noreply.github.com>
2021-05-15 21:58:01 +0100

    Fix

--- modules/trafficserver/types/paths.pp
+++ modules/trafficserver/types/paths.pp
@@ -1,9 +1,7 @@
 type Trafficserver::Paths = Struct[{
     'sysconfdir'    => Stdlib::Absolutepath,
     'datadir'       => Stdlib::Absolutepath,
-    'includedir'    => Stdlib::Absolutepath,
     'libdir'        => Stdlib::Absolutepath,
-    'libexecdir'    => Stdlib::Absolutepath,
     'runtimedir'    => Stdlib::Absolutepath,
     'logdir'        => Stdlib::Absolutepath,
     'cachedir'      => Stdlib::Absolutepath,
