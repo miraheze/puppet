0c7d3d48b7c792692b1ed49bed3534000f44cefc
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-08-10 04:27:34 -0600

    Use --quiet for LocalisationUpdate cron (#1855)

--- modules/mediawiki/manifests/cron.pp
+++ modules/mediawiki/manifests/cron.pp
@@ -4,7 +4,7 @@
 class mediawiki::cron {
     cron { 'update.php for LocalisationUpdate':
         ensure  => present,
-        command => '/usr/bin/nice -n 15 /usr/bin/php /srv/mediawiki/w/extensions/LocalisationUpdate/update.php --wiki loginwiki > /var/log/mediawiki/debuglogs/l10nupdate.log',
+        command => '/usr/bin/nice -n 15 /usr/bin/php /srv/mediawiki/w/extensions/LocalisationUpdate/update.php --quiet --wiki=loginwiki > /var/log/mediawiki/debuglogs/l10nupdate.log',
         user    => 'www-data',
         minute  => '0',
         hour    => '23',
