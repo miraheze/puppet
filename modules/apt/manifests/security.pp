802f59c6e33458dd891bdb7beb53a675f202fca6
paladox <paladox@users.noreply.github.com>
2021-08-28 23:03:27 -0400

    apt::security: Do not use os_version for bullseye

--- modules/apt/manifests/security.pp
+++ modules/apt/manifests/security.pp
@@ -15,7 +15,7 @@ class apt::security (
     $_repos = $repos
   }
   if ($facts['os']['name'] == 'Debian' or $facts['os']['name'] == 'Ubuntu') {
-    if os_version('debian >= bullseye') {
+    if debian::codename::ge('bullseye') {
       $securityDist = "-security"
     } else {
       $securityDist = ""
