b27b53b1ed352a835184d4930734b3df1078bc5a
paladox <paladox@users.noreply.github.com>
2021-08-28 22:23:38 -0400

    mediawiki: Purge texvc package (#1901)
    * mediawiki: Purge texvc
    
    * Delete modules/mediawiki/files/packages/texvc directory
    
    * Update packages.pp

--- modules/mediawiki/manifests/packages.pp
+++ modules/mediawiki/manifests/packages.pp
@@ -28,7 +28,6 @@ class mediawiki::packages {
         'xvfb',
         'timidity',
         'librsvg2-bin',
-        'texlive-latex-extra',
         'python3-requests',
         'rsync',
     ]
@@ -46,18 +45,11 @@ class mediawiki::packages {
     }
 
     file { '/opt/texvc_3.0.0+git20160613-1_amd64.deb':
-        ensure  => present,
-        source  => 'puppet:///modules/mediawiki/packages/texvc/texvc_3.0.0+git20160613-1_amd64.deb',
+        ensure  => absent,
     }
 
     package { 'texvc':
-        ensure      => installed,
-        provider    => dpkg,
-        source      => '/opt/texvc_3.0.0+git20160613-1_amd64.deb',
-        require     => [
-            File['/opt/texvc_3.0.0+git20160613-1_amd64.deb'],
-            Package['texlive-latex-extra'],
-        ],
+        ensure      => purged,
     }
 
     package { [ 'ocaml' ]:
