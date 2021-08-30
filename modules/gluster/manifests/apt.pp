179532d206f1387b3b5d620f096e391d8c2417a3
paladox <thomasmulhall410@yahoo.com>
2021-08-30 11:07:55 -0400

    Revert "gluster: work around an issue with the apt repo (#1903)"
    This reverts commit c714d8e3bb89e09f159993ec5a20b895f9685fa1.

--- modules/gluster/manifests/apt.pp
+++ modules/gluster/manifests/apt.pp
@@ -1,11 +1,7 @@
 class gluster::apt {
-    # For now work around an issue using amd64/apt failing when using version 8 of gluster
-    # This should be "https://download.gluster.org/pub/gluster/glusterfs/8/LATEST/Debian/${::lsbdistcodename}/amd64/apt"
-    # when https://github.com/gluster/glusterfs/issues/2741 is resolved.
-    # Version 9 unaffected.
     apt::source { 'gluster_apt':
         comment  => 'GlusterFS',
-        location => "https://download.gluster.org/pub/gluster/glusterfs/8/LATEST/Debian/${::lsbdistcodename}",
+        location => "https://download.gluster.org/pub/gluster/glusterfs/8/LATEST/Debian/${::lsbdistcodename}/amd64/apt",
         release  => "${::lsbdistcodename}",
         repos    => 'main',
         key      => 'F9C958A3AEE0D2184FAD1CBD43607F0DC2F8238C',
