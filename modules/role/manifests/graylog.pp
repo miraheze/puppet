f8ab4cca32d65611b96ffdb522c77e0afe2fb061
paladox <paladox@users.noreply.github.com>
2021-09-02 16:47:43 -0400

    graylog: Upgrade elasticsearch to 7.14.1

--- modules/role/manifests/graylog.pp
+++ modules/role/manifests/graylog.pp
@@ -21,7 +21,7 @@ class role::graylog {
     }
 
     class { 'elasticsearch':
-        version         => '7.13.4',
+        version         => '7.14.1',
         manage_repo     => true,
         config          => {
             'cluster.name'  => 'graylog',
