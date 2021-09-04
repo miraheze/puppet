30da89c51046e13bb87e2f0ebe0b6a12847745fa
RhinosF1 <rhinosf1@miraheze.org>
2021-09-04 20:27:28 +0100

    mwdeploy: update user tool (#1942)
    * mwdeploy: update user tool
    
    * Update init.pp
    
    * Update init.pp
    
    * Update init.pp

--- modules/mediawiki/manifests/init.pp
+++ modules/mediawiki/manifests/init.pp
@@ -28,9 +28,17 @@ class mediawiki(
     }
     
     if lookup(mediawiki::remote_sync) {
-        users::key { 'www-data':
-            ensure  => present,
-            content => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDktIRXHBi4hDZvb6tBrPZ0Ag6TxLbXoQ7CkisQqOY6V MediaWikiDeploy',
+        users::user { 'www-data':
+            ensure   => present,
+            uid      => 33,
+            gid      => 33,
+            system   => true,
+            homedir  => '/var/www',
+            shell    => '/bin/bash',
+            system   => true,
+            ssh_keys => [
+                'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDktIRXHBi4hDZvb6tBrPZ0Ag6TxLbXoQ7CkisQqOY6V MediaWikiDeploy'
+            ],
         }
         file { '/var/www/.ssh':
             ensure => directory,
