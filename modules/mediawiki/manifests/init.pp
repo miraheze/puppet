df153263b1317e42c08156d2181a877d4d8a579b
RhinosF1 <rhinosf1@miraheze.org>
2021-09-04 16:35:29 +0100

    Update init.pp

--- modules/mediawiki/manifests/init.pp
+++ modules/mediawiki/manifests/init.pp
@@ -32,6 +32,18 @@ class mediawiki(
             ensure  => present,
             content => 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDktIRXHBi4hDZvb6tBrPZ0Ag6TxLbXoQ7CkisQqOY6V MediaWikiDeploy',
         }
+        file { '/var/www/.ssh':
+            ensure => directory,
+            owner  => 'www-data',
+            group  => 'www-data',
+            mode   => '0400',
+        }
+        file { '/var/www/.ssh/authorized_keys':
+            ensure => file,
+            owner  => 'www-data',
+            group  => 'www-data',
+            mode   => '0400',
+        }
     }
     
     if lookup(mediawiki::is_canary) {
