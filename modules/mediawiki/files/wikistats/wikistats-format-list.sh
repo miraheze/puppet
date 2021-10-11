a2bc01114f63fe2b3122d7303b90f93a60c64937
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-10-11 13:11:15 -0600

    Remove unused wikistats stuff

--- modules/mediawiki/files/wikistats/wikistats-format-list.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while read line
-do
-	wiki=`/bin/echo $line | /usr/bin/cut -d "|" -f1`
-	if ! /bin/grep -q "$wiki" /srv/mediawiki/dblist/private.dblist; then
-		/bin/echo ${wiki%????}
-	fi
-done < $1
