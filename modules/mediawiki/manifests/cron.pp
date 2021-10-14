97e3dd03fcd6b56aed867ee403ecd7ceed86737b
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-10-14 02:10:19 -0600

    ensure absent on LocalisationUpdate cron

--- modules/mediawiki/manifests/cron.pp
+++ modules/mediawiki/manifests/cron.pp
@@ -3,7 +3,7 @@
 # Used for CORE crons which should be ran on every MediaWiki server.
 class mediawiki::cron {
     cron { 'update.php for LocalisationUpdate':
-        ensure  => present,
+        ensure  => absent,
         command => '/usr/bin/nice -n 15 /usr/bin/php /srv/mediawiki/w/extensions/LocalisationUpdate/update.php --quiet --wiki=loginwiki > /var/log/mediawiki/debuglogs/l10nupdate.log',
         user    => 'www-data',
         minute  => '0',
