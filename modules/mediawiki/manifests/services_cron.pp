5be3f5ad6b134edd16c650d1839c0755493d66ab
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-10-16 11:41:41 -0600

    Update services_cron.pp

--- modules/mediawiki/manifests/services_cron.pp
+++ modules/mediawiki/manifests/services_cron.pp
@@ -1,21 +1,5 @@
 # class: mediawiki::services_cron
 class mediawiki::services_cron {
-    file { '/srv/services':
-        ensure => 'directory',
-        owner  => 'www-data',
-        group  => 'www-data',
-        mode   => '0755',
-    }
-
-    file { '/srv/services/id_rsa':
-        ensure  => absent,
-        source  => 'puppet:///private/acme/id_rsa',
-        owner   => 'www-data',
-        group   => 'www-data',
-        mode    => '0400',
-        require => File['/srv/services'],
-    }
-
     cron { 'generate_services':
         ensure  => absent,
         command => '/bin/bash /usr/local/bin/pushServices.sh',
