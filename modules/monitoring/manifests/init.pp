60a840d41fc663674184a57989008e52fda83d96
paladox <paladox@users.noreply.github.com>
2021-10-16 18:14:26 +0100

    monitoring: Update mariadb to 10.4

--- modules/monitoring/manifests/init.pp
+++ modules/monitoring/manifests/init.pp
@@ -13,27 +13,27 @@ class monitoring (
         allowdupe => false,
     }
 
-    $version = lookup('mariadb::version', {'default_value' => '10.2'})
+    $version = lookup('mariadb::version', {'default_value' => '10.4'})
     apt::source { 'mariadb_apt':
         comment     => 'MariaDB stable',
         location    => "http://ams2.mirrors.digitalocean.com/mariadb/repo/${version}/debian",
         release     => "${::lsbdistcodename}",
         repos       => 'main',
         key         => '177F4010FE56CA3336300305F1656F24C74CD1D8',
-        notify      => Exec['apt_update_mariadb'],
     }
 
     apt::pin { 'mariadb_pin':
         priority        => 600,
-        origin          => 'ams2.mirrors.digitalocean.com'
+        origin          => 'ams2.mirrors.digitalocean.com',
+        require         => Apt::Source['mariadb_apt'],
+        notify          => Exec['apt_update_mariadb'],
     }
 
     # First installs can trip without this
-    exec {'apt_update_mariadb':
+    exec { 'apt_update_mariadb':
         command     => '/usr/bin/apt-get update',
         refreshonly => true,
         logoutput   => true,
-        require     => Apt::Pin['mariadb_pin'],
     }
 
     package { "mariadb-client-${version}":
