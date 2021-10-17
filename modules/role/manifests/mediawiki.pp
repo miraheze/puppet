1fa9d4d02b185604c8660806621877d8c7b23bf9
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-10-17 09:33:46 -0600

    Remove test4 (#2050)

--- modules/role/manifests/mediawiki.pp
+++ modules/role/manifests/mediawiki.pp
@@ -33,31 +33,6 @@ class role::mediawiki {
                 from  => $key,
             }
         }
-
-        # Temporarily to test ats (apache traffic server)
-        ufw::allow { 'http port 80 51.195.236.214':
-            proto => 'tcp',
-            port  => 80,
-            from  => '51.195.236.214',
-        }
-
-        ufw::allow { 'https port 80 2001:41d0:800:178a::12':
-            proto => 'tcp',
-            port  => 80,
-            from  => '2001:41d0:800:178a::12',
-        }
-
-        ufw::allow { 'http port 443 51.195.236.214':
-            proto => 'tcp',
-            port  => 443,
-            from  => '51.195.236.214',
-        }
-
-        ufw::allow { 'https port 443 2001:41d0:800:178a::12':
-            proto => 'tcp',
-            port  => 443,
-            from  => '2001:41d0:800:178a::12',
-        }
     } else {
         ufw::allow { 'http port tcp':
             proto => 'tcp',
