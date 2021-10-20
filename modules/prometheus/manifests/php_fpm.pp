92eb22ae260800f02eeae38f33061427a03b71f8
paladox <paladox@users.noreply.github.com>
2021-10-20 19:28:03 +0100

    Update php_fpm.pp

--- modules/prometheus/manifests/php_fpm.pp
+++ modules/prometheus/manifests/php_fpm.pp
@@ -18,31 +18,13 @@ class prometheus::php_fpm {
         require => File['/usr/local/bin/prometheus-phpfpm-exporter'],
     }
 
-    # TODO: Remove once all modules use ferm.
-    $firewall_mode = lookup('base::firewall::mode', {'default_value' => 'ufw'})
     $firewall_rules = query_facts('Class[Prometheus]', ['ipaddress', 'ipaddress6'])
-    if $firewall_mode == 'ufw' {
-        $firewall_rules.each |$key, $value| {
-            ufw::allow { "Prometheus php-fpm 9253 ${value['ipaddress']}":
-                proto => 'tcp',
-                port  => 9253,
-                from  => $value['ipaddress'],
-            }
+    $firewall_rules_mapped = $firewall_rules.map |$key, $value| { "${value['ipaddress']} ${value['ipaddress6']}" }
+    $firewall_rules_str = join($firewall_rules_mapped, ' ')
 
-            ufw::allow { "Prometheus php-fpm 9253 ${value['ipaddress6']}":
-                proto => 'tcp',
-                port  => 9253,
-                from  => $value['ipaddress6'],
-            }
-        }
-    } else {
-        $firewall_rules_mapped = $firewall_rules.map |$key, $value| { "${value['ipaddress']} ${value['ipaddress6']}" }
-        $firewall_rules_str = join($firewall_rules_mapped, ' ')
-
-        ferm::service { 'prometheus php-fpm':
-            proto  => 'tcp',
-            port   => '9253',
-            srange => "(${firewall_rules_str})",
-        }
+    ferm::service { 'prometheus php-fpm':
+        proto  => 'tcp',
+        port   => '9253',
+        srange => "(${firewall_rules_str})",
     }
 }
