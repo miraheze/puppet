b11fadd0c2f74fb1b0ea966ec2adf5744aa3c307
paladox <paladox@users.noreply.github.com>
2021-10-20 01:07:05 +0100

    redis: Migrate to ferm

--- modules/role/manifests/redis.pp
+++ modules/role/manifests/redis.pp
@@ -6,19 +6,13 @@ class role::redis {
         maxmemory => $redis_heap,
     }
 
-    $firewall = query_facts("domain='$domain' and (Class[Role::Mediawiki] or Class[Role::Icinga2])", ['ipaddress', 'ipaddress6'])
-    $firewall.each |$key, $value| {
-        ufw::allow { "Redis port - ${value['ipaddress']}":
-            proto => 'tcp',
-            port  => 6379,
-            from  => $value['ipaddress'],
-        }
-
-        ufw::allow { "Redis port - ${value['ipaddress6']}":
-            proto => 'tcp',
-            port  => 6379,
-            from  => $value['ipaddress6'],
-        }
+    $firewall_rules = query_facts('Class[Role::Mediawiki] or Class[Role::Icinga2]', ['ipaddress', 'ipaddress6'])
+    $firewall_rules_mapped = $firewall_rules.map |$key, $value| { "${value['ipaddress']} ${value['ipaddress6']}" }
+    $firewall_rules_str = join($firewall_rules_mapped, ' ')
+    ferm::service { 'redis':
+        proto  => 'tcp',
+        port   => '6379',
+        srange => "(${firewall_rules_str})",
     }
 
     motd::role { 'role::redis':
