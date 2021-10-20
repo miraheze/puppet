ca51ee2150059f57df548ba3ecd36bcdd5ac4e3a
paladox <paladox@users.noreply.github.com>
2021-10-20 01:41:49 +0100

    Update varnish.pp

--- modules/role/manifests/varnish.pp
+++ modules/role/manifests/varnish.pp
@@ -2,33 +2,26 @@
 class role::varnish {
     include ::varnish
 
-    ufw::allow { 'http port tcp':
-        proto => 'tcp',
-        port  => 80,
-    }
-
-    ufw::allow { 'https port tcp':
-        proto => 'tcp',
-        port  => 443,
-    }
-
     ferm::service { 'http':
-        proto => 'tcp',
-        port  => '80',
+        proto   => 'tcp',
+        port    => '80',
+        notrack => true,
     }
 
     ferm::service { 'https':
-        proto => 'tcp',
-        port  => '443',
+        proto   => 'tcp',
+        port    => '443',
+        notrack => true,
     }
 
     $firewall_rules = query_facts('Class[Role::Mediawiki]', ['ipaddress', 'ipaddress6'])
     $firewall_rules_mapped = $firewall_rules.map |$key, $value| { "${value['ipaddress']} ${value['ipaddress6']}" }
     $firewall_rules_str = join($firewall_rules_mapped, ' ')
     ferm::service { 'direct varnish access':
-        proto  => 'tcp',
-        port   => '81',
-        srange => "(${firewall_rules_str})",
+        proto   => 'tcp',
+        port    => '81',
+        srange  => "(${firewall_rules_str})",
+        notrack => true,
     }
 
     motd::role { 'role::varnish':
