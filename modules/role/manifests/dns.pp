b41199265f8ad36d33ab554205e11e9d6eebf356
paladox <paladox@users.noreply.github.com>
2021-10-20 01:36:10 +0100

    Migrate dns to ferm (#2066)
    * Migrate dns to ferm
    
    * Update ns1.yaml
    
    * Update ns2.yaml

--- modules/role/manifests/dns.pp
+++ modules/role/manifests/dns.pp
@@ -2,14 +2,28 @@
 class role::dns {
     include ::dns
 
-    ufw::allow { 'dns port udp':
+    ferm::service { 'dns_udp':
         proto => 'udp',
-        port  => 53,
+        port  => '53',
     }
 
-    ufw::allow { 'dns port tcp':
+    ferm::service { 'dns_tcp':
         proto => 'tcp',
-        port  => 53,
+        port  => '53',
+    }
+
+    ferm::rule { 'skip_dns_conntrack-out':
+        desc  => 'Skip DNS outgoing connection tracking',
+        table => 'raw',
+        chain => 'OUTPUT',
+        rule  => 'proto udp sport 53 NOTRACK;',
+    }
+
+    ferm::rule { 'skip_dns_conntrack-in':
+        desc  => 'Skip DNS incoming connection tracking',
+        table => 'raw',
+        chain => 'PREROUTING',
+        rule  => 'proto udp dport 53 NOTRACK;',
     }
 
     motd::role { 'role::dns':
