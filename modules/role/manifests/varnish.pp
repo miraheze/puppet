ff815c7f35d95b8f1bb1f28d904aeeafe08f4544
paladox <paladox@users.noreply.github.com>
2021-10-20 01:43:32 +0100

    Migrate varnish to ferm (#2067)
    * Migrate varnish to ferm
    
    * Update varnish.pp
    
    * Update varnish_prometheus_exporter.pp

--- modules/role/manifests/varnish.pp
+++ modules/role/manifests/varnish.pp
@@ -2,29 +2,26 @@
 class role::varnish {
     include ::varnish
 
-    ufw::allow { 'http port tcp':
-        proto => 'tcp',
-        port  => 80,
+    ferm::service { 'http':
+        proto   => 'tcp',
+        port    => '80',
+        notrack => true,
     }
 
-    ufw::allow { 'https port tcp':
-        proto => 'tcp',
-        port  => 443,
+    ferm::service { 'https':
+        proto   => 'tcp',
+        port    => '443',
+        notrack => true,
     }
 
-    $firewall = query_facts('Class[Role::Mediawiki]', ['ipaddress', 'ipaddress6'])
-    $firewall.each |$key, $value| {
-        ufw::allow { "Direct Varnish access ipv4 ${value['ipaddress']}":
-            proto => 'tcp',
-            port  => 81,
-            from  => $value['ipaddress'],
-        }
-
-        ufw::allow { "Direct Varnish access ipv6 ${value['ipaddress6']}":
-            proto => 'tcp',
-            port  => 81,
-            from  => $value['ipaddress6'],
-        }
+    $firewall_rules = query_facts('Class[Role::Mediawiki]', ['ipaddress', 'ipaddress6'])
+    $firewall_rules_mapped = $firewall_rules.map |$key, $value| { "${value['ipaddress']} ${value['ipaddress6']}" }
+    $firewall_rules_str = join($firewall_rules_mapped, ' ')
+    ferm::service { 'direct varnish access':
+        proto   => 'tcp',
+        port    => '81',
+        srange  => "(${firewall_rules_str})",
+        notrack => true,
     }
 
     motd::role { 'role::varnish':
