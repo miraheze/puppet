4ad858ab3456145ab1a8216209de9bc693a63e0f
paladox <paladox@users.noreply.github.com>
2021-12-05 22:15:07 +0000

    base: If running bullseye or above remove /var/log/journal
    It made looking at the unit slower and also we didn't need a persistent journal.

--- modules/base/manifests/syslog.pp
+++ modules/base/manifests/syslog.pp
@@ -1,7 +1,17 @@
 # class base::syslog
 class base::syslog (
         String $syslog_daemon = lookup('base::syslog::syslog_daemon', {'default_value' => 'syslog_ng'}),
-    ) {
+) {
+
+        if os_version('debian >= bullseye') {
+                file { '/var/log/journal' :
+                        ensure  => absent,
+                        recurse => true,
+                        force   => true,
+                        notify  => Service['systemd-journald'],
+                }
+        }
+
         if $syslog_daemon == 'rsyslog' {
                 include ::rsyslog
 
