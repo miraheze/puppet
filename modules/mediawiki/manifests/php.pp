b6ef8639f799ffdd5e9917a8d76f7e223c8691d0
RhinosF1 <rhinosf1@miraheze.org>
2021-12-13 22:23:23 +0000

    Update php.pp

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -2,6 +2,7 @@
 class mediawiki::php (
     Integer $php_fpm_childs = lookup('mediawiki::php::fpm::childs', {'default_value' => 26}),
     Integer $fpm_min_restart_threshold = lookup('mediawiki::php::fpm::fpm_min_restart_threshold', {'default_value' => 6}),
+    Integer $fpm_max_memory = lookup('mediawiki::php::fpm::fpm_max_memory', {'default_value' => 512}),
     String $php_version = lookup('php::php_version', {'default_value' => '7.2'}),
     Boolean $use_tideways = lookup('mediawiki::php::use_tideways', {'default_value' => false}),
 ) {
@@ -16,11 +17,11 @@ class mediawiki::php (
                 'error_log'                 => 'syslog',
                 'error_reporting'           => 'E_ALL & ~E_DEPRECATED & ~E_STRICT',
                 'log_errors'                => 'On',
-                'memory_limit'              => '512M',
+                'memory_limit'              => "${$fpm_max_memory}M",
                 'opcache'                   => {
                     'enable'                  => 1,
                     'interned_strings_buffer' => 50,
-                    'memory_consumption'      => 512,
+                    'memory_consumption'      =  $fpm_max_memory,
                     'max_accelerated_files'   => 24000,
                     'max_wasted_percentage'   => 10,
                     'validate_timestamps'     => 1,
