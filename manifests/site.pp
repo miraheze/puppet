9817fbc11f5a48b88886a989a128976557409cd9
RhinosF1 <rhinosf1@miraheze.org>
2021-12-29 22:20:57 +0000

    regexes

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,19 +33,12 @@ node 'graylog2.miraheze.org' {
     include prometheus::es_exporter
 }
 
-node 'jobchron1.miraheze.org' {
+node /^jobchron(1|121)\.miraheze\.org$/ {
     include base
     include role::redis
     include prometheus::redis_exporter
     include mediawiki::jobqueue::chron
-}
 
-node 'jobchron121.miraheze.org' {
-    include base
-    include role::redis
-    include prometheus::redis_exporter
-    include mediawiki::jobqueue::chron
-}
 
 node 'ldap2.miraheze.org' {
     include base
@@ -58,7 +51,7 @@ node 'mail2.miraheze.org' {
     include role::roundcubemail
 }
 
-node /^mem([12]|1([02]1)\.miraheze\.org$/ {
+node /^mem([12]|1([02]1))\.miraheze\.org$/ {
     include base
     include role::memcached
 }
