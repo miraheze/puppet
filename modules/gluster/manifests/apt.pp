418f32b83c1e520744d6587afae2c90dd998b122
paladox <paladox@users.noreply.github.com>
2021-12-31 14:35:44 +0000

    gluster: Support setting a http_proxy for apt

--- modules/gluster/manifests/apt.pp
+++ modules/gluster/manifests/apt.pp
@@ -1,10 +1,14 @@
 class gluster::apt {
+    $http_proxy = lookup('http_proxy', {'default_value' => undef})
     apt::source { 'gluster_apt':
         comment  => 'GlusterFS',
         location => "https://download.gluster.org/pub/gluster/glusterfs/9/LATEST/Debian/${::lsbdistcodename}/amd64/apt",
         release  => "${::lsbdistcodename}",
         repos    => 'main',
-        key      => 'F9C958A3AEE0D2184FAD1CBD43607F0DC2F8238C',
+        key      => {
+            'id' => 'F9C958A3AEE0D2184FAD1CBD43607F0DC2F8238C',
+            'options' => "http-proxy='${http_proxy}'",
+        },
         notify   => Exec['apt_update_gluster'],
     }
 
