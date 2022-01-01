6dbeae5e48b6068a7fbec561289dfbb5132e63eb
paladox <paladox@users.noreply.github.com>
2022-01-01 18:05:38 +0000

    monitoring: Remove ipv4 address from host

--- modules/monitoring/manifests/hosts.pp
+++ modules/monitoring/manifests/hosts.pp
@@ -2,22 +2,10 @@ define monitoring::hosts (
     $ensure      = present,
     $contacts    = lookup('contactgroups', {'default_value' => [ 'sre' ]}),
 ) {
-
-    # If on a container instead of a physical machine or real VM,
-    # use the custom fact to get the IP.
-    if $facts['virtual'] == 'openvz' {
-        $ipaddress4 = $facts['virtual_ip_address']
-    } else {
-        $ipaddress4 = $facts['ipaddress']
-    }
-
-    $ipaddress6 = $facts['ipaddress6']
-
     @@icinga2::object::host { $title:
         ensure   => $ensure,
         import   => ['generic-host'],
-        address  => $ipaddress4,
-        address6 => $ipaddress6,
+        address6 => $facts['ipaddress6'],
         target   => '/etc/icinga2/conf.d/puppet_hosts.conf',
         vars     => {
             notification => {
