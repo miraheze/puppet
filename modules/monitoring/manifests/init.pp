5cff16415edbcb7dea7d078155ad43dec33be017
paladox <paladox@users.noreply.github.com>
2022-01-01 02:24:57 +0000

    Fix installing mariadb apt repo

--- modules/monitoring/manifests/init.pp
+++ modules/monitoring/manifests/init.pp
@@ -23,6 +23,7 @@ class monitoring (
         key      => {
                 'id' => '177F4010FE56CA3336300305F1656F24C74CD1D8',
                 'options' => "http-proxy='${http_proxy}'",
+                'server'  => 'hkp://keyserver.ubuntu.com:80',
         },
     }
 
