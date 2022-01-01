8e36558348fd6d72e5a43f7e5b715c91f4c1f0d2
paladox <paladox@users.noreply.github.com>
2022-01-01 00:32:52 +0000

    mariadb: Support http proxy (#2202)
    * mariadb: Support http proxy
    
    * Update db101.yaml

--- modules/mariadb/manifests/packages.pp
+++ modules/mariadb/manifests/packages.pp
@@ -10,12 +10,16 @@ class mariadb::packages(
         ensure => present,
     }
 
+    $http_proxy = lookup('http_proxy', {'default_value' => undef})
     apt::source { 'mariadb_apt':
         comment     => 'MariaDB stable',
         location    => "http://ams2.mirrors.digitalocean.com/mariadb/repo/${version}/debian",
         release     => "${::lsbdistcodename}",
         repos       => 'main',
-        key         => '177F4010FE56CA3336300305F1656F24C74CD1D8',
+        key      => {
+                'id' => '177F4010FE56CA3336300305F1656F24C74CD1D8',
+                'options' => "http-proxy='${http_proxy}'",
+        },
     }
 
     apt::pin { 'mariadb_pin':
