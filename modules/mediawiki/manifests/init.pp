2e7ec6394013655741af5794b54815d974b42889
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-01-03 19:15:21 -0700

    Update init.pp

--- modules/mediawiki/manifests/init.pp
+++ modules/mediawiki/manifests/init.pp
@@ -225,6 +225,27 @@ class mediawiki(
             group              => 'www-data',
             mode               => '0755',
         }
+
+        exec { 'shellbox_composer':
+            command     => 'composer install --no-dev',
+            creates     => '/srv/shellbox/vendor',
+            cwd         => '/srv/shellbox',
+            path        => '/usr/bin',
+            environment => 'HOME=/srv/shellbox',
+            user        => 'www-data',
+            require     => Git::Clone['shellbox'],
+        }
+
+        file { '/srv/shellbox/config':
+            ensure  => directory,
+            require => Git::Clone['shellbox'],
+        }
+
+        file { '/srv/shellbox/config/config.json':
+            ensure  => present,
+            source  => 'puppet:///modules/mediawiki/shellbox_config.json',
+            require => File['/srv/shellbox/config'],
+        }
     }    
 
     file { [
