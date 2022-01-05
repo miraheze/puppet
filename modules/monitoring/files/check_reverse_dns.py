d25c812e0a10c8c56f8a307ce97f840556ece9ca
paladox <paladox@users.noreply.github.com>
2022-01-05 14:13:10 +0000

    Fix

--- modules/monitoring/files/check_reverse_dns.py
+++ modules/monitoring/files/check_reverse_dns.py
@@ -100,7 +100,7 @@ def get_reverse_dnshostname(hostname):
 
         resolved_ip_addr = str(dns_resolver.query(hostname, 'AAAA')[0])
         ptr_record = reversename.from_address(resolved_ip_addr)
-        rev_host = str(resolver.query(ptr_record, "PTR")[0]).rstrip('.')
+        rev_host = str(dns_resolver.query(ptr_record, "PTR")[0]).rstrip('.')
 
         return rev_host
     except (resolver.NXDOMAIN, resolver.NoAnswer):
