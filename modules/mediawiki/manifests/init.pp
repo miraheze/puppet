817cffa77f22f654e5af87d39f2ec00b45e84cea
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-01-05 16:15:54 -0700

    Add user and group

--- modules/mediawiki/manifests/init.pp
+++ modules/mediawiki/manifests/init.pp
@@ -216,21 +216,38 @@ class mediawiki(
     }
 
     if lookup(mediawiki::use_shellbox) {
+        group { 'shellbox':
+            ensure => present,
+            system => true,
+        }
+
+        user { 'shellbox':
+            ensure => present,
+            gid    => 'shellbox',
+            system => true,
+            home   => '/nonexistent',
+            shell  => '/usr/sbin/nologin',
+        }
+
         php::fpm::pool { 'shellbox':
+            user   => 'shellbox',
+            group  => 'shellbox',
             config => {
+                'listen.owner' => 'www-data',
+                'listen.group' => 'www-data',
                 'pm' => 'static',
                 'pm.max_children' => 1,
             },
         }
 
         git::clone { 'shellbox':
-            ensure             => present,
-            directory          => '/srv/shellbox',
-            origin             => 'https://gerrit.wikimedia.org/r/mediawiki/libs/Shellbox',
-            branch             => 'master',
-            owner              => 'www-data',
-            group              => 'www-data',
-            mode               => '0755',
+            ensure    => present,
+            directory => '/srv/shellbox',
+            origin    => 'https://gerrit.wikimedia.org/r/mediawiki/libs/Shellbox',
+            branch    => 'master',
+            owner     => 'www-data',
+            group     => 'www-data',
+            mode      => '0755',
         }
 
         exec { 'shellbox_composer':
