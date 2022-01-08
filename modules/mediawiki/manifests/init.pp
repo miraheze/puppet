db1018157bdef07cc9899fa7eb200836c4d63581
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-01-08 09:48:01 -0700

    Update init.pp

--- modules/mediawiki/manifests/init.pp
+++ modules/mediawiki/manifests/init.pp
@@ -19,6 +19,10 @@ class mediawiki(
         include mediawiki::jobqueue::chron
     }
 
+    if lookup(mediawiki::use_shellbox) {
+        include mediawiki::shellbox
+    }
+
     file { '/etc/mathoid':
         ensure  => directory,
     }
@@ -213,64 +217,7 @@ class mediawiki(
         mode               => '0755',
         recurse_submodules => true,
         require            => Package['libjpeg-dev'],
-    }
-
-    if lookup(mediawiki::use_shellbox) {
-        group { 'shellbox':
-            ensure => present,
-            system => true,
-        }
-
-        user { 'shellbox':
-            ensure => present,
-            gid    => 'shellbox',
-            system => true,
-            home   => '/nonexistent',
-            shell  => '/usr/sbin/nologin',
-        }
-
-        php::fpm::pool { 'shellbox':
-            user   => 'shellbox',
-            group  => 'shellbox',
-            config => {
-                'listen.owner' => 'www-data',
-                'listen.group' => 'www-data',
-                'pm' => 'static',
-                'pm.max_children' => 1,
-            },
-        }
-
-        git::clone { 'shellbox':
-            ensure    => present,
-            directory => '/srv/shellbox',
-            origin    => 'https://gerrit.wikimedia.org/r/mediawiki/libs/Shellbox',
-            branch    => 'master',
-            owner     => 'www-data',
-            group     => 'www-data',
-            mode      => '0755',
-        }
-
-        exec { 'shellbox_composer':
-            command     => 'composer install --no-dev',
-            creates     => '/srv/shellbox/vendor',
-            cwd         => '/srv/shellbox',
-            path        => '/usr/bin',
-            environment => 'HOME=/srv/shellbox',
-            user        => 'www-data',
-            require     => Git::Clone['shellbox'],
-        }
-
-        file { '/srv/shellbox/config':
-            ensure  => directory,
-            require => Git::Clone['shellbox'],
-        }
-
-        file { '/srv/shellbox/config/config.json':
-            ensure  => present,
-            source  => 'puppet:///modules/mediawiki/shellbox_config.json',
-            require => File['/srv/shellbox/config'],
-        }
-    }    
+    }  
 
     file { [
         '/srv/mediawiki',
