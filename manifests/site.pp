5b89871d828a900104f757d69ffbd668a46e1109
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-01-14 19:13:40 -0700

    wrong branch

--- manifests/site.pp
+++ manifests/site.pp
@@ -64,6 +64,13 @@ node 'graylog121.miraheze.org' {
     include role::graylog
 }
 
+node 'jobchron1.miraheze.org' {
+    include base
+    include role::redis
+    include prometheus::redis_exporter
+    include mediawiki::jobqueue::chron
+}
+
 node 'jobchron121.miraheze.org' {
     include base
     include role::redis
@@ -93,9 +100,24 @@ node 'mail121.miraheze.org' {
     include role::roundcubemail
 }
 
+node /^mem[12]\.miraheze\.org$/ {
+    include base
+    include role::memcached
+}
+
 node /^mem1[02]1\.miraheze\.org$/ {
     include base
     include role::memcached
+}
+
+node 'mon2.miraheze.org' {
+    include base
+    include role::grafana
+    include role::icinga2
+    include role::irc
+    include role::matomo
+    include role::prometheus
+    include prometheus::php_fpm
 }
 
 node 'mon111.miraheze.org' {
@@ -108,7 +130,19 @@ node 'mon111.miraheze.org' {
     include prometheus::php_fpm
 }
 
+node /^mw([89]|1[0123])\.miraheze\.org$/ {
+    include base
+    include role::mediawiki
+    include prometheus::php_fpm
+}
+
 node /^mw1[012][12]\.miraheze\.org$/ {
+    include base
+    include role::mediawiki
+    include prometheus::php_fpm
+}
+
+node 'mwtask1.miraheze.org' {
     include base
     include role::mediawiki
     include prometheus::php_fpm
@@ -155,6 +189,12 @@ node 'puppet111.miraheze.org' {
     include puppetdb::database
     include role::puppetserver
     include role::salt
+}
+
+node 'test3.miraheze.org' {
+    include base
+    include role::mediawiki
+    include prometheus::php_fpm
 }
 
 node 'test101.miraheze.org' {
