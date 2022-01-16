80e70372ec5f300959076bb76e528cef97f6c937
paladox <paladox@users.noreply.github.com>
2022-01-16 18:04:51 +0000

    mediawiki::cgroup: Force use of cgroups v1 (#2347)

--- modules/mediawiki/manifests/cgroup.pp
+++ modules/mediawiki/manifests/cgroup.pp
@@ -33,4 +33,9 @@ class mediawiki::cgroup {
     grub::bootparam { 'cgroup.memory':
         value => 'nokmem',
     }
+
+    # Force use of cgroups v1
+    grub::bootparam { 'systemd.unified_cgroup_hierarchy':
+        value => '0',
+    }
 }
