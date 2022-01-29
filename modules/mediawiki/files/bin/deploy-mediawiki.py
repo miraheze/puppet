dff635f659ad52d45d7087fae730268f23163cb9
RhinosF1 <rhinosf1@gmail.com>
2022-01-29 16:03:26 +0000

    support multiple cw clusters

--- modules/mediawiki/files/bin/deploy-mediawiki.py
+++ modules/mediawiki/files/bin/deploy-mediawiki.py
@@ -4,10 +4,42 @@ import argparse
 import os
 import time
 import requests
+import socket
 
 
 repos = {'config': 'config', 'world': 'w', 'landing': 'landing', 'errorpages': 'ErrorPages'}
 DEPLOYUSER = 'www-data'
+ENVIRONMENTS = {
+    'beta': {
+        'wikidbname': 'betawiki',
+        'wikiurl': 'beta.betaheze.org',
+        'servers': [],
+        'canary': 'test101',
+    },
+    'prod': {
+        'wikidbname': 'testwiki',  # don't use loginwiki anymore - we want this to be an experimental wiki
+        'wikiurl': 'publictestwiki.com',
+        'servers': ['mw101', 'mw102', 'mw111', 'mw112', 'mw121', 'mw122'],
+        'canary': 'mwtask111',
+    }
+}
+HOSTNAME = socket.gethostname()
+
+def get_environment_info():
+    if HOSTNAME.split('.').startswith('test'):
+        return ENVIRONMENTS['beta']
+    return ENVIRONMENTS['prod']
+
+
+def get_server_list(envinfo, servers):
+    if servers in ('all', 'scsvg'):
+        slist = envinfo['servers']
+        return slist.append(envinfo['canary'])
+    if servers == 'skip':
+        return [envinfo['canary']]
+    return servers.split(',')
+    
+    
 
 
 def run_command(cmd):
@@ -59,13 +91,14 @@ def check_up(Debug=None, Host=None, domain='https://meta.miraheze.org', verify=T
     return up
 
 
-def remote_sync_file(time, serverlist, path, recursive=True, force=False):
+def remote_sync_file(time, serverlist, path, envinfo, recursive=True, force=False):
     print(f'Start {path} deploys.')
     for server in serverlist:
-        print(f'Deploying {path} to {server}.')
-        ec = run_command(_construct_rsync_command(time=time, local=False, dest=path, server=server, recursive=recursive))
-        check_up(Debug=server, force=force)
-        print(f'Deployed {path} to {server}.')
+        if envinfo['canary'] == server:
+            print(f'Deploying {path} to {server}.')
+            ec = run_command(_construct_rsync_command(time=time, local=False, dest=path, server=server, recursive=recursive))
+            check_up(Debug=server, force=force, domain=envinfo['wikiurl'])
+            print(f'Deployed {path} to {server}.')
     print(f'Finished {path} deploys.')
     return ec
 
@@ -106,107 +139,99 @@ def _construct_git_pull(repo, submodules=False):
 
 
 def run(args, start):
-    loginfo = {}
-    exitcodes = []
-    for arg in vars(args).items():
-        if arg[1] is not None and arg[1] is not False:
-            loginfo[arg[0]] = arg[1]
-    synced = loginfo['servers']
-    del loginfo['servers']
-    text = f'starting deploy of "{str(loginfo)}" to {synced}'
-    rsyncpaths = []
-    rsyncfiles = []
-    rsync = []
-    rebuild = []
-    postinstall = []
-    stage = []
-    if not args.nolog:
-        os.system(f'/usr/local/bin/logsalmsg {text}')
-    else:
-        print(text)
-    if args.world and not args.pull:
-        pull = ['world']
-    if args.pull or args.world:
-        if args.pull:
-            pull = str(args.pull).split(',')
-        elif args.world and 'world' not in pull:
-            pull.append('world')
-        for repo in pull:
-            if repo == 'world':
-                sm = True
-            else:
-                sm = False
-            try:
-                stage.append(_construct_git_pull(repo, submodules=sm))
-            except KeyError:
-                print(f'Failed to pull {repo} due to invalid name')
+    envinfo = get_environment_info()
+    servers = get_server_list(envinfo, args.servers)
+    if envinfo['canary'] in servers:
+        loginfo = {}
+        exitcodes = []
+        for arg in vars(args).items():
+            if arg[1] is not None and arg[1] is not False:
+                loginfo[arg[0]] = arg[1]
+        synced = loginfo['servers']
+        del loginfo['servers']
+        text = f'starting deploy of "{str(loginfo)}" to {synced}'
+        rsyncpaths = []
+        rsyncfiles = []
+        rsync = []
+        rebuild = []
+        postinstall = []
+        stage = []
+        if not args.nolog:
+            os.system(f'/usr/local/bin/logsalmsg {text}')
+        else:
+            print(text)
+        if args.world and not args.pull:
+            pull = ['world']
+        if args.pull or args.world:
+            if args.pull:
+                pull = str(args.pull).split(',')
+            elif args.world and 'world' not in pull:
+                pull.append('world')
+            for repo in pull:
+                if repo == 'world':
+                    sm = True
+                else:
+                    sm = False
+                try:
+                    stage.append(_construct_git_pull(repo, submodules=sm))
+                except KeyError:
+                    print(f'Failed to pull {repo} due to invalid name')
 
-    options = {'config': args.config, 'world': args.world, 'landing': args.landing, 'errorpages': args.errorpages}
-    for cmd in stage:  # setup env, git pull etc
-        exitcodes.append(run_command(cmd))
-    non_zero_code(exitcodes)
-    for option in options:  # configure rsync & custom data for repos
-        if options[option]:
-            if option == 'world':  # install steps for w
-                os.chdir(_get_staging_path('world'))
-                exitcodes.append(run_command('sudo -u www-data composer install --no-dev --quiet'))
-                rebuild.append('sudo -u www-data php /srv/mediawiki/w/extensions/MirahezeMagic/maintenance/rebuildVersionCache.php --save-gitinfo --wiki=loginwiki')
-                rsyncpaths.append('/srv/mediawiki/cache/gitinfo/')
-            rsync.append(_construct_rsync_command(time=args.ignoretime, location=f'{_get_staging_path(option)}*', dest=_get_deployed_path(option)))
-            rsyncpaths.append(_get_deployed_path(option))
-    non_zero_code(exitcodes)
-    if args.files:  # specfic extra files
-        files = str(args.files).split(',')
-        for file in files:
-            rsync.append(_construct_rsync_command(time=args.ignoretime, recursive=False, location=f'/srv/mediawiki-staging/{file}', dest=f'/srv/mediawiki/{file}'))
-            rsyncfiles.append(f'/srv/mediawiki/{file}')
-    if args.folders:  # specfic extra folders
-        folders = str(args.folders).split(',')
-        for folder in folders:
-            rsync.append(_construct_rsync_command(time=args.ignoretime, location=f'/srv/mediawiki-staging/{folder}/*', dest='/srv/mediawiki/{folder}/'))
-            rsyncpaths.append(f'/srv/mediawiki/{folder}/')
+        options = {'config': args.config, 'world': args.world, 'landing': args.landing, 'errorpages': args.errorpages}
+        for cmd in stage:  # setup env, git pull etc
+            exitcodes.append(run_command(cmd))
+        non_zero_code(exitcodes)
+        for option in options:  # configure rsync & custom data for repos
+            if options[option]:
+                if option == 'world':  # install steps for w
+                    os.chdir(_get_staging_path('world'))
+                    exitcodes.append(run_command('sudo -u www-data composer install --no-dev --quiet'))
+                    rebuild.append(f'sudo -u www-data php /srv/mediawiki/w/extensions/MirahezeMagic/maintenance/rebuildVersionCache.php --save-gitinfo --wiki={envinfo["wikidbname"]}')
+                    rsyncpaths.append('/srv/mediawiki/cache/gitinfo/')
+                rsync.append(_construct_rsync_command(time=args.ignoretime, location=f'{_get_staging_path(option)}*', dest=_get_deployed_path(option)))
+                rsyncpaths.append(_get_deployed_path(option))
+        non_zero_code(exitcodes)
+        if args.files:  # specfic extra files
+            files = str(args.files).split(',')
+            for file in files:
+                rsync.append(_construct_rsync_command(time=args.ignoretime, recursive=False, location=f'/srv/mediawiki-staging/{file}', dest=f'/srv/mediawiki/{file}'))
+                rsyncfiles.append(f'/srv/mediawiki/{file}')
+        if args.folders:  # specfic extra folders
+            folders = str(args.folders).split(',')
+            for folder in folders:
+                rsync.append(_construct_rsync_command(time=args.ignoretime, location=f'/srv/mediawiki-staging/{folder}/*', dest='/srv/mediawiki/{folder}/'))
+                rsyncpaths.append(f'/srv/mediawiki/{folder}/')
 
-    if args.extensionlist:  # when adding skins/exts
-        rebuild.append('sudo -u www-data php /srv/mediawiki/w/extensions/CreateWiki/maintenance/rebuildExtensionListCache.php --wiki=loginwiki')
-        rsyncfiles.append('/srv/mediawiki/cache/extension-list.json')
+        if args.extensionlist:  # when adding skins/exts
+            rebuild.append(f'sudo -u www-data php /srv/mediawiki/w/extensions/CreateWiki/maintenance/rebuildExtensionListCache.php --wiki={envinfo["wikidbname"]}')
+            rsyncfiles.append('/srv/mediawiki/cache/extension-list.json')
 
-    for cmd in rsync:  # move staged content to live
-        exitcodes.append(run_command(cmd))
-    non_zero_code(exitcodes)
-    # These need to be setup late because dodgy
-    if args.l10nupdate:  # used by automated maint
-        run_command('sudo -u www-data ionice -c idle /usr/bin/nice -n 15 /usr/bin/php /srv/mediawiki/w/extensions/LocalisationUpdate/update.php --wiki=loginwiki')  # gives garbage errors
-        args.l10n = True  # imply --l10n
-    if args.l10n:  # setup l10n
-        postinstall.append('sudo -u www-data php /srv/mediawiki/w/maintenance/mergeMessageFileList.php --quiet --wiki=loginwiki --output /srv/mediawiki/config/ExtensionMessageFiles.php')
-        rebuild.append('sudo -u www-data php /srv/mediawiki/w/maintenance/rebuildLocalisationCache.php --quiet --wiki=loginwiki')
-        rsyncpaths.append('/srv/mediawiki/cache/l10n/')
+        for cmd in rsync:  # move staged content to live
+            exitcodes.append(run_command(cmd))
+        non_zero_code(exitcodes)
+        # These need to be setup late because dodgy
+        if args.l10nupdate:  # used by automated maint
+            run_command(f'sudo -u www-data ionice -c idle /usr/bin/nice -n 15 /usr/bin/php /srv/mediawiki/w/extensions/LocalisationUpdate/update.php {envinfo["wikidbname"]}')  # gives garbage errors
+            args.l10n = True  # imply --l10n
+        if args.l10n:  # setup l10n
+            postinstall.append(f'sudo -u www-data php /srv/mediawiki/w/maintenance/mergeMessageFileList.php --quiet --wiki={envinfo["wikidbname"]} --output /srv/mediawiki/config/ExtensionMessageFiles.php')
+            rebuild.append(f'sudo -u www-data php /srv/mediawiki/w/maintenance/rebuildLocalisationCache.php --quiet --wiki={envinfo["wikidbname"]}')
+            rsyncpaths.append('/srv/mediawiki/cache/l10n/')
 
-    for cmd in postinstall:  # cmds to run after rsync & install (like mergemessage)
-        exitcodes.append(run_command(cmd))
-    non_zero_code(exitcodes)
-    for cmd in rebuild:  # update ext list + l10n
-        exitcodes.append(run_command(cmd))
-    non_zero_code(exitcodes)
+        for cmd in postinstall:  # cmds to run after rsync & install (like mergemessage)
+            exitcodes.append(run_command(cmd))
+        non_zero_code(exitcodes)
+        for cmd in rebuild:  # update ext list + l10n
+            exitcodes.append(run_command(cmd))
+        non_zero_code(exitcodes)
 
-    # see if we are online - exit code 3 if not
-    check_up(Debug=None, Host='beta.betaheze.org', verify=False, force=args.force)
+        # see if we are online - exit code 3 if not
+        check_up(Debug=None, Host={envinfo["wikiurl"]}, verify=False, force=args.force)
 
-    # decide what servers to remote on
-    sync = True
-    if args.servers == 'skip':
-        sync = False
-        print('Sync skipped. Mediawiki deploy has not passed canary stage.')
-    elif args.servers in ('all', 'scsvg'):
-        serverlist = ['mw101', 'mw102', 'mw111', 'mw112', 'mw121', 'mw122']
-    else:
-        serverlist = str(args.servers).split(',')
-
-    if sync:
-        for path in rsyncpaths:
-            exitcodes.append(remote_sync_file(time=args.ignoretime, serverlist=serverlist, path=path, force=args.force))
-        for file in rsyncfiles:
-            exitcodes.append(remote_sync_file(time=args.ignoretime, serverlist=serverlist, path=file, recursive=False, force=args.force))
+    for path in rsyncpaths:
+        exitcodes.append(remote_sync_file(time=args.ignoretime, serverlist=servers, path=path, force=args.force, envinfo=envinfo))
+    for file in rsyncfiles:
+        exitcodes.append(remote_sync_file(time=args.ignoretime, serverlist=servers, path=file, recursive=False, force=args.force, envinfo=envinfo))
 
     fintext = f'finished deploy of "{str(loginfo)}" to {synced}'
 
