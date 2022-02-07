84f8db5dc17a350e4bb75a124d14f50f1dc3ced1
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-02-07 15:25:42 -0700

    Create favicon.php

---
+++ modules/mediawiki/files/favicon.php
@@ -0,0 +1,45 @@
+<?php
+
+// Based on the version created by Wikimedia
+
+define( 'MW_NO_SESSION', 1 );
+require_once '/srv/mediawiki/w/includes/WebStart.php';
+
+use MediaWiki\MediaWikiServices;
+
+function streamFavicon() {
+	global $wgFavicon;
+	wfResetOutputBuffers();
+
+	$favicon = $wgFavicon;
+	if ( $favicon === '/favicon.ico' ) {
+		$favicon = '/default.ico';
+	}
+
+	$req = RequestContext::getMain()->getRequest();
+	if ( $req->getHeader( 'X-Favicon-Loop' ) !== false ) {
+		header( 'HTTP/1.1 500 Internal Server Error' );
+		return;
+	}
+
+	$url = wfExpandUrl( $favicon, PROTO_CANONICAL );
+	$client = MediaWikiServices::getInstance()
+		->getHttpRequestFactory()
+		->create( $url );
+	$client->setHeader( 'X-Favicon-Loop', '1' );
+
+	$status = $client->execute();
+	if ( !$status->isOK() ) {
+		header( 'HTTP/1.1 500 Internal Server Error' );
+		return;
+	}
+
+	$content = $client->getContent();
+	header( 'Content-Length: ' . strlen( $content ) );
+	header( 'Content-Type: ' . $client->getResponseHeader( 'Content-Type' ) );
+	header( 'Cache-Control: public' );
+	header( 'Expires: ' . gmdate( 'r', time() + 86400 ) );
+	echo $content;
+}
+
+streamFavicon();
