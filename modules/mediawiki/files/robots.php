cbd595c07ec772805e908f7b48a111bd37a6c03f
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-02-17 11:59:11 -0700

    Simplify robots.php to use `$wgDBname`

--- modules/mediawiki/files/robots.php
+++ modules/mediawiki/files/robots.php
@@ -1,7 +1,6 @@
 <?php
 
 define( 'MW_NO_SESSION', 1 );
-
 require_once '/srv/mediawiki/w/includes/WebStart.php';
 
 use MediaWiki\MediaWikiServices;
@@ -10,10 +9,6 @@ $wikiPageFactory = MediaWikiServices::getInstance()->getWikiPageFactory();
 $titleFactory = MediaWikiServices::getInstance()->getTitleFactory();
 
 $page = $wikiPageFactory->newFromTitle( $titleFactory->newFromText( 'Robots.txt', NS_MEDIAWIKI ) );
-
-$databaseJsonFileName = '/srv/mediawiki/cache/databases.json';
-$databasesArray = file_exists( $databaseJsonFileName ) ?
-	json_decode( file_get_contents( $databaseJsonFileName ), true ) : [ 'combi' => [] ];
 
 header( 'Content-Type: text/plain; charset=utf-8' );
 header( 'X-Miraheze-Robots: Default' );
@@ -37,47 +32,9 @@ echo "# Throttle MJ12Bot" . "\r\n";
 echo "User-agent: MJ12bot" . "\r\n";
 echo "Crawl-Delay: 10" . "\r\n\n";
 
-if ( $databasesArray['combi'] ) {
-	if ( preg_match( '/^(.+)\.miraheze\.org$/', $_SERVER['HTTP_HOST'], $matches ) ) {
-		$wiki = "{$matches[1]}wiki";
-
-		if ( !isset( $databasesArray['combi']["{$wiki}"] ) ) {
-			return;
-		}
-
-		# Dynamic sitemap url
-		echo "# Dynamic sitemap url" . "\r\n";
-		echo "Sitemap: https://static.miraheze.org/{$wiki}/sitemaps/sitemap.xml" . "\r\n\n";
-	} else {
-		$customDomainFound = false;
-		$suffixes = [ 'wiki' ];
-		$suffixMatch = array_flip( [ 'miraheze.org' => 'wiki' ] );
-
-		foreach ( $databasesArray['combi'] as $db => $data ) {
-			foreach ( $suffixes as $suffix ) {
-				if ( substr( $db, -strlen( $suffix ) == $suffix ) ) {
-					$url = $data['u'] ?? 'https://' . substr( $db, 0, -strlen( $suffix ) ) . '.' . $suffixMatch[$suffix];
-
-					if ( !$url ) {
-						continue;
-					}
-
-					if ( $url === "https://{$_SERVER['HTTP_HOST']}" ) {
-						$customDomainFound = $db;
-					}
-				}
-			}
-
-			continue;
-		}
-
-		if ( $customDomainFound ) {
-			# Dynamic sitemap url
-			echo "# Dynamic sitemap url" . "\r\n";
-			echo "Sitemap: https://static.miraheze.org/{$customDomainFound}/sitemaps/sitemap.xml" . "\r\n\n";
-		}
-	}
-}
+# Dynamic sitemap url
+echo "# Dynamic sitemap url" . "\r\n";
+echo "Sitemap: https://static.miraheze.org/{$wgDBname}/sitemaps/sitemap.xml" . "\r\n\n";
 
 if ( $page->exists() ) {
 	header( 'X-Miraheze-Robots: Custom' );
