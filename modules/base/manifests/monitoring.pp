48028d94c729f31b8f3da5f06caa1213d9dc0a1c
John Lewis <john@miraheze.org>
2022-02-27 21:02:31 +0000

    T8843: add docs for base monitoring

--- modules/base/manifests/monitoring.pp
+++ modules/base/manifests/monitoring.pp
@@ -53,29 +53,35 @@ class base::monitoring {
     monitoring::hosts { $::hostname: }
 
     monitoring::nrpe { 'Disk Space':
-        command => '/usr/lib/nagios/plugins/check_disk -w 10% -c 5% -p /'
+        command => '/usr/lib/nagios/plugins/check_disk -w 10% -c 5% -p /',
+        docs    => 'https://meta.miraheze.org/wiki/Tech:Icinga/Base_Monitoring#Disk_Space'
     }
 
     $loadCrit = $facts['virtual_processor_count'] * 2.0
     $loadWarn = $facts['virtual_processor_count'] * 1.7
     monitoring::nrpe { 'Current Load':
-        command => "/usr/lib/nagios/plugins/check_load -w ${loadWarn} -c ${loadCrit}"
+        command => "/usr/lib/nagios/plugins/check_load -w ${loadWarn} -c ${loadCrit}",
+        docs    => 'https://meta.miraheze.org/wiki/Tech:Icinga/Base_Monitoring#Current_Load'
     }
 
     monitoring::nrpe { 'Puppet':
-        command => '/usr/bin/sudo /usr/lib/nagios/plugins/check_puppet_run -w 3600 -c 43200'
+        command => '/usr/bin/sudo /usr/lib/nagios/plugins/check_puppet_run -w 3600 -c 43200',
+        docs    => 'https://meta.miraheze.org/wiki/Tech:Icinga/Base_Monitoring#Puppet'
     }
 
     monitoring::services { 'SSH':
-        check_command => 'ssh'
+        check_command => 'ssh',
+        docs          => 'https://meta.miraheze.org/wiki/Tech:Icinga/Base_Monitoring#SSH'
     }
 
     monitoring::nrpe { 'APT':
-        command => '/usr/lib/nagios/plugins/check_apt -o'
+        command => '/usr/lib/nagios/plugins/check_apt -o',
+        docs    => 'https://meta.miraheze.org/wiki/Tech:Icinga/Base_Monitoring#APT'
     }
 
     monitoring::nrpe { 'NTP time':
-        command => '/usr/lib/nagios/plugins/check_ntp_time -H time.cloudflare.com -w 5 -c 10'
+        command => '/usr/lib/nagios/plugins/check_ntp_time -H time.cloudflare.com -w 0.1 -c 0.5',
+        docs    => 'https://meta.miraheze.org/wiki/Tech:Icinga/Base_Monitoring#NTP'
     }
 
     # Collect all NRPE command files
