ce53bea289dfaa4bec9b7a055b950514be4b30c2
RhinosF1 <rhinosf1@gmail.com>
2022-04-04 12:53:54 +0100

    add tests

--- modules/mediawiki/files/bin/test_mw_deploy.py
+++ modules/mediawiki/files/bin/test_mw_deploy.py
@@ -1,4 +1,8 @@
 import importlib
+import time
+import argparse
+import socket
+import pytest
 mwd = importlib.import_module('deploy-mediawiki')
 
 
@@ -30,6 +34,10 @@ def test_check_up_no_debug_host() -> None:
         assert str(e) == 'Host or Debug must be specified'
         failed = True
     assert failed
+
+
+def test_check_up_debug_fail() -> None:
+    assert not mwd.check_up(nolog=True, Debug='mwtask111', domain='httpstat.us/500', force=True)
 
 
 def test_check_up_debug() -> None:
@@ -134,3 +142,105 @@ def test_run_command() -> None:
 
 def test_batched_command() -> None:
     assert mwd.run_batch_command(['echo test 1', 'echo test 2'], 'testrun', []) == [0, 0]
+
+def test_get_envinfo() -> None:
+    assert mwd.get_environment_info() == {'servers':
+                                          [
+                                              'mw101',
+                                              'mw102',
+                                              'mw111',
+                                              'mw112',
+                                              'mw121',
+                                              'mw122',
+                                              'mwtask111'
+                                           ],
+                                          'wikidbname': 'testwiki',
+                                          'wikiurl': 'publictestwiki.com'
+                                          }
+
+def test_get_servers_all() -> None:
+    assert mwd.get_server_list(mwd.get_environment_info(), 'all') == [
+                                              'mw101',
+                                              'mw102',
+                                              'mw111',
+                                              'mw112',
+                                              'mw121',
+                                              'mw122',
+                                              'mwtask111'
+                                           ]
+
+
+def test_get_servers_two() -> None:
+    assert mwd.get_server_list(mwd.get_environment_info(), 'mw101,mw111') == ['mw101', 'mw111']
+
+
+def test_run() -> None:
+    parser = argparse.ArgumentParser()
+    args, unknown = parser.parse_known_args()
+    del unknown
+    args.servers = 'all'
+    args.config = False
+    args.world = False
+    args.landing = False
+    args.errorpages = False
+    args.files = ''
+    args.folders = ''
+    args.extensionlist = False
+    args.l10n = False
+    args.nolog = True
+    assert mwd.run(args, time.time()) == 0
+
+
+def test_run_log() -> None:
+    parser = argparse.ArgumentParser()
+    args, unknown = parser.parse_known_args()
+    del unknown
+    args.servers = 'all'
+    args.config = False
+    args.world = False
+    args.landing = False
+    args.errorpages = False
+    args.files = ''
+    args.folders = ''
+    args.extensionlist = False
+    args.l10n = False
+    args.nolog = False
+    assert mwd.run(args, time.time()) == 0
+
+@pytest.mark.server(url='/w/api.php?action=query&meta=siteinfo&formatversion=2&format=json', response=[{'id': 1}], method='GET')
+def test_run_full_suites() -> None:
+    parser = argparse.ArgumentParser()
+    args, unknown = parser.parse_known_args()
+    del unknown
+    args.servers = socket.gethostname().split('.')[0]
+    args.config = False
+    args.world = False
+    args.landing = False
+    args.errorpages = False
+    args.files = ''
+    args.folders = ''
+    args.extensionlist = False
+    args.l10n = False
+    args.nolog = True
+    args.pull = ''
+    args.l10nupdate = False
+    args.force = True
+    args.port = 5000
+    assert mwd.run(args, time.time()) == 0
+    args.nolog = False
+    assert mwd.run(args, time.time()) == 0
+    args.config = True
+    args.landing = True
+    args.errorpages = True
+    args.ignoretime = False
+    args.l10n = True
+    args.l10nupdate = True
+    args.extensionlist = True
+    args.files = 'test'
+    args.folders = 'myfolder'
+    args.pull = 'config,world'
+    assert mwd.run(args, time.time()) == 1
+    args.pull = 'garbage'
+    assert mwd.run(args, time.time()) == 1
+    args.servers = f'{socket.gethostname().split(".")[0]}, mygarbageserver.local'
+    assert mwd.run(args, time.time()) == 1
