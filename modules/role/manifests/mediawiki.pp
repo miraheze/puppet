d0236afc711186e759c3ea2af8dfd3113ed3fc20
paladox <paladox@users.noreply.github.com>
2022-04-22 16:59:42 +0100

    mediawiki: allow prometheus server to connect to port 80/443

--- modules/role/manifests/mediawiki.pp
+++ modules/role/manifests/mediawiki.pp
@@ -7,7 +7,7 @@ class role::mediawiki (
 
     if $strict_firewall {
         $firewall_rules_str = join(
-            query_facts('Class[Role::Mediawiki] or Class[Role::Varnish] or Class[Role::Icinga2]', ['ipaddress', 'ipaddress6'])
+            query_facts('Class[Role::Mediawiki] or Class[Role::Varnish] or Class[Role::Icinga2] or Class[Role::Prometheus]', ['ipaddress', 'ipaddress6'])
             .map |$key, $value| {
                 "${value['ipaddress']} ${value['ipaddress6']}"
             }
