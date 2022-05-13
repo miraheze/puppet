fbaa80499af6a077e2635ae407b28c56abe7882d
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-05-13 14:51:25 -0600

    Update php.pp

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -2,11 +2,11 @@
 class mediawiki::php (
     Float $fpm_workers_multiplier      = lookup('mediawiki::php::fpm::fpm_workers_multiplier', {'default_value' => 1.5}),
     Integer $php_fpm_childs            = lookup('mediawiki::php::fpm::childs', {'default_value' => 26}),
-    Integer $max_memory                = lookup('mediawiki::php::max_memory', {'default_value' => 256}),
     Integer $request_timeout           = lookup('mediawiki::php::request_timeout', {'default_value' => 60}),
     VMlib::Php_version $php_version    = lookup('php::php_version', {'default_value' => '7.4'}),
     Boolean $enable_fpm                = lookup('mediawiki::php::enable_fpm', {'default_value' => true}),
     Boolean $enable_request_profiling  = lookup('mediawiki::php::enable_request_profiling', {'default_value' => false}),
+    String $memory_limit               = lookup('mediawiki::php::memory_limit', {'default_value' => '256M'}),
     Optional[Hash] $fpm_config         = lookup('mediawiki::php::fpm_config', {default_value => undef}),
 ) {
     
@@ -16,7 +16,7 @@ class mediawiki::php (
         'pcre.backtrack_limit'   => 5000000,
         'date.timezone'          => 'UTC',
         'display_errors'         => 'stderr',
-        'memory_limit'           => "${$max_memory}M",
+        'memory_limit'           => $memory_limit,
         'error_reporting'        => 'E_ALL & ~E_STRICT',
         'mysql'                  => { 'connect_timeout' => 3 },
         'default_socket_timeout' => 60,
