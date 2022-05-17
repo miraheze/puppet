edb53195ad7f9b8a68e4fc964906e1a12a4e429d
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-05-17 11:41:10 -0600

    Update monitoring.pp

--- modules/nutcracker/manifests/monitoring.pp
+++ modules/nutcracker/manifests/monitoring.pp
@@ -4,26 +4,15 @@
 #
 class nutcracker::monitoring(
     Stdlib::Port $port = 11212,
-    Optional[Stdlib::Unixpath] $socket = undef,
 ) {
-    nrpe::monitor_service { 'nutcracker':
-        description  => 'nutcracker process',
-        nrpe_command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u nutcracker -C nutcracker',
-        notes_url    => 'https://wikitech.wikimedia.org/wiki/Nutcracker',
+    monitoring::nrpe { 'nutcracker process':
+        command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u nutcracker -C nutcracker',
     }
 
-    if ($port != 0) {
-        nrpe::monitor_service { 'nutcracker_port':
-            description  => 'nutcracker port',
-            nrpe_command => "/usr/lib/nagios/plugins/check_tcp -H 127.0.0.1 -p ${port} --timeout=2",
-            notes_url    => 'https://wikitech.wikimedia.org/wiki/Nutcracker',
-        }
-    }
-    if ($socket != undef) {
-        nrpe::monitor_service { 'nutcracker_socket':
-            description  => 'nutcracker socket',
-            nrpe_command => "/usr/lib/nagios/plugins/check_tcp -H ${socket} --timeout=2",
-            notes_url    => 'https://wikitech.wikimedia.org/wiki/Nutcracker',
+    monitoring::services { 'nutcracker port':
+        check_command => 'tcp',
+        vars          => {
+            tcp_port    => $port,
         }
     }
 }
