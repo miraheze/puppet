e0db6486d35793f1a87c066b919808d63e6e5b7f
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-05-24 10:45:50 -0600

    Update test_mw_deploy.py

--- modules/mediawiki/files/bin/test_mw_deploy.py
+++ modules/mediawiki/files/bin/test_mw_deploy.py
@@ -125,7 +125,7 @@ def test_construct_git_pull() -> None:
 
 
 def test_construct_git_pull_branch() -> None:
-    assert mwd._construct_git_pull('config', branch='myfunbranch') == 'sudo -u www-data git -C /srv/mediawiki-staging/config/ pull --branch=myfunbranch --quiet'
+    assert mwd._construct_git_pull('config', branch='myfunbranch') == 'sudo -u www-data git -C /srv/mediawiki-staging/config/ pull  --branch=myfunbranch --quiet'
 
 
 def test_construct_git_pull_branch_sm() -> None:
