4e6ecaf1965c774a493dd095acad9624d254b199
RhinosF1 <rhinosf1@gmail.com>
2022-05-24 16:55:45 +0100

    Add tests (#7)

--- modules/mediawiki/files/bin/test_mw_deploy.py
+++ modules/mediawiki/files/bin/test_mw_deploy.py
@@ -122,3 +122,11 @@ def test_construct_git_pull_sm() -> None:
 
 def test_construct_git_pull() -> None:
     assert mwd._construct_git_pull('config') == 'sudo -u www-data git -C /srv/mediawiki-staging/config/ pull  --quiet'
+
+
+def test_construct_git_pull_branch() -> None:
+    assert mwd._construct_git_pull('config', branch='myfunbranch') == 'sudo -u www-data git -C /srv/mediawiki-staging/config/ pull --branch=myfunbranch --quiet'
+
+
+def test_construct_git_pull_branch_sm() -> None:
+    assert mwd._construct_git_pull('config', submodules=True, branch='test') == 'sudo -u www-data git -C /srv/mediawiki-staging/config/ pull --recurse-submodules --branch=test --quiet'
