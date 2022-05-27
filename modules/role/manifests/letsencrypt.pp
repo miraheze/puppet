01928095eb14382d5c64466be318e3d662b5f836
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-05-27 13:24:28 -0600

    Update letsencrypt.pp

--- modules/role/manifests/letsencrypt.pp
+++ modules/role/manifests/letsencrypt.pp
@@ -2,29 +2,20 @@
 class role::letsencrypt {
     include ::letsencrypt
 
-    $firewall_srange = join(
-        query_facts('Class[Role::Varnish] or Class[Role::Icinga2]', ['ipaddress', 'ipaddress6'])
-        .map |$key, $value| {
-            "${value['ipaddress']} ${value['ipaddress6']}"
+    if !defined(Ferm::Service['http']) {
+        ferm::service { 'http':
+            proto   => 'tcp',
+            port    => '80',
+            notrack => true,
         }
-        .flatten()
-        .unique()
-        .sort(),
-        ' '
-    )
-
-    ferm::service { 'http':
-        proto   => 'tcp',
-        port    => '80',
-        srange  => "(${firewall_srange})",
-        notrack => true,
     }
 
-    ferm::service { 'https':
-        proto   => 'tcp',
-        port    => '443',
-        srange  => "(${firewall_srange})",
-        notrack => true,
+    if !defined(Ferm::Service['https']) {
+        ferm::service { 'https':
+            proto   => 'tcp',
+            port    => '443',
+            notrack => true,
+        }
     }
 
     motd::role { 'role::letsencrypt':
