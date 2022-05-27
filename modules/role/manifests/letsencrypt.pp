08499329e708e4021b8b269a1e5faf9afc8c7627
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-05-27 13:37:05 -0600

    Update letsencrypt.pp

--- modules/role/manifests/letsencrypt.pp
+++ modules/role/manifests/letsencrypt.pp
@@ -2,10 +2,22 @@
 class role::letsencrypt {
     include ::letsencrypt
 
+    $firewall_srange = join(
+        query_facts('Class[Role::Varnish] or Class[Role::Icinga2]', ['ipaddress', 'ipaddress6'])
+        .map |$key, $value| {
+            "${value['ipaddress']} ${value['ipaddress6']}"
+        }
+        .flatten()
+        .unique()
+        .sort(),
+        ' '
+    )
+
     if !defined(Ferm::Service['http']) {
         ferm::service { 'http':
             proto   => 'tcp',
             port    => '80',
+            srange  => "(${firewall_srange})",
             notrack => true,
         }
     }
@@ -14,6 +26,7 @@ class role::letsencrypt {
         ferm::service { 'https':
             proto   => 'tcp',
             port    => '443',
+            srange  => "(${firewall_srange})",
             notrack => true,
         }
     }
