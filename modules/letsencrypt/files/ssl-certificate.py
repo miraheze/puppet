8aecb586a0c90011f8fdd5bce1090a1e4d1b60e6
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-05-29 13:41:01 -0600

    ssl-certificate: fix print output directory (#2607)

--- modules/letsencrypt/files/ssl-certificate.py
+++ modules/letsencrypt/files/ssl-certificate.py
@@ -124,7 +124,7 @@ class SslCertificate:
         os.system(f'/bin/cat /etc/letsencrypt/live/{self.domain}/fullchain.pem')
 
         if self.private:
-            print('Private key is being copied and pushed to /etc/puppetlabs/puppet/ssl-keys')
+            print('Private key is being copied and pushed to /home/puppet-users/ssl-keys')
             os.system(f'cp /etc/letsencrypt/live/{self.domain}/privkey.pem /home/puppet-users/ssl-keys/{self.domain}.key')
             os.system(f"cd /home/puppet-users/ssl-keys/ && git add . && git commit -m 'add {self.domain} key' && git push origin master")
 
