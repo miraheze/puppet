d8b6175b655f6b1e5e4b353a0cbc7ebafb8dbaba
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-06 10:45:35 -0600

    Update and rename modules/ssl/manifests/hiera.pp to modules/sslcert/manifests/hiera.pp

--- modules/sslcert/manifests/hiera.pp
+++ modules/sslcert/manifests/hiera.pp
@@ -1,10 +1,10 @@
 # class for auto loading SSL certs onto machines needing them
-class ssl::hiera {
+class sslcert::hiera {
     $ssldomains = loadyaml('/etc/puppetlabs/puppet/ssl-cert/certs.yaml')
     $sslredirects = loadyaml('/etc/puppetlabs/puppet/ssl-cert/redirects.yaml')
 
     $certs = merge( $ssldomains, $sslredirects )
 
     # resource handler to create certs via a definied type and hash
-    create_resources('ssl::hiera::certs', $certs)
+    create_resources('sslcert::hiera::certs', $certs)
 }
