e100d239ec0579179f37c8784e1eda1e0aa99e85
RhinosF1 <rhinosf1@gmail.com>
2022-06-09 21:25:58 +0100

    Update disable-puppet.py

--- modules/mediawiki/files/cookbooks/disable-puppet.py
+++ modules/mediawiki/files/cookbooks/disable-puppet.py
@@ -1,7 +1,8 @@
 #! /usr/bin/python3
 
-from os import system
 import sys
+from os import system
+
 system(f'logsalmsg Disabling puppet for {sys.argv[1]}')
 system('sudo puppet agent -tv')
 system(f'sudo puppet agent --disable "{sys.argv[1]}"')
