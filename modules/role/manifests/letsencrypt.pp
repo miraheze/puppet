10cc6bae884efcbedae7db0012d7f15365a16ff9
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-09 15:45:21 -0600

    letsencrypt: ensure GitHub host key is present

--- modules/role/manifests/letsencrypt.pp
+++ modules/role/manifests/letsencrypt.pp
@@ -31,6 +31,13 @@ class role::letsencrypt {
         }
     }
 
+    @@sshkey { 'github.com':
+        ensure       => present,
+        type         => 'ecdsa-sha2-nistp256',
+        key          => 'AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=',
+        host_aliases => [ 'github.com' ],
+    }
+
     motd::role { 'role::letsencrypt':
         description => 'LetsEncrypt server',
     }
