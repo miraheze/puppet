e3ba213b5908fa404b443d9bdaeafe16c8535ea0
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-10 10:00:31 -0600

    Update ssl.pp

--- modules/role/manifests/ssl.pp
+++ modules/role/manifests/ssl.pp
@@ -31,6 +31,13 @@ class role::ssl {
         }
     }
 
+    @@sshkey { 'github.com':
+        ensure       => present,
+        type         => 'ecdsa-sha2-nistp256',
+        key          => 'AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=',
+        host_aliases => [ 'github.com' ],
+    }
+
     motd::role { 'role::ssl':
         description => 'SSL management server',
     }
