64539509c41bf68fa5bde0b5043e7e29f9c124df
paladox <paladox@users.noreply.github.com>
2022-06-13 20:22:06 +0100

    graylog: upgrade to 4.3.1

--- modules/role/manifests/graylog.pp
+++ modules/role/manifests/graylog.pp
@@ -21,10 +21,10 @@ class role::graylog {
     $http_proxy = lookup('http_proxy', {'default_value' => undef})
     class { 'graylog::repository':
         proxy => $http_proxy,
-        version => '4.2',
+        version => '4.3',
     }->
     class { 'graylog::server':
-        package_version => '4.2.9-1',
+        package_version => '4.3.1-1',
         config          => {
             'password_secret'          => lookup('passwords::graylog::password_secret'),
             'root_password_sha2'       => lookup('passwords::graylog::root_password_sha2'),
