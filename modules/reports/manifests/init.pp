5302fe970e75e42d061648f96438b154a9245315
Reception123 <utilizator.receptie123@gmail.com>
2022-07-03 22:12:09 +0200

    reports: update to v4

--- modules/reports/manifests/init.pp
+++ modules/reports/manifests/init.pp
@@ -5,7 +5,7 @@ class reports {
     git::clone { 'TSPortal':
         directory          => '/srv/TSPortal',
         origin             => 'https://github.com/miraheze/TSPortal',
-        branch             => 'v3',
+        branch             => 'v4',
         recurse_submodules => true,
         owner              => 'www-data',
         group              => 'www-data',
