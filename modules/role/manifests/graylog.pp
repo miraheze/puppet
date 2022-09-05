05722a215c47e54e2eee6e644a3eaaa019548cff
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-09-05 15:03:30 -0600

    Update graylog.pp

--- modules/role/manifests/graylog.pp
+++ modules/role/manifests/graylog.pp
@@ -92,10 +92,10 @@ class role::graylog {
     }
 
     rsyslog::input::file { 'graylog':
-         path              => '/var/log/graylog-server/server.log',
-         syslog_tag_prefix => '',
-         use_udp           => true,
-     }
+        path              => '/var/log/graylog-server/server.log',
+        syslog_tag_prefix => '',
+        use_udp           => true,
+    }
 
     motd::role { 'role::graylog':
         description => 'central logging server',
