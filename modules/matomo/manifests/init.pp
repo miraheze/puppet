4fd5ff6ba176e3bcf874995c8f55ab4a64c1e2c6
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-09-05 14:50:19 -0600

    Update init.pp

--- modules/matomo/manifests/init.pp
+++ modules/matomo/manifests/init.pp
@@ -87,8 +87,8 @@ class matomo (
 
     # Requires igbinary to be installed
     php::extension { 'redis':
-         ensure => present
-     }
+        ensure => present
+    }
 
     class { '::php::fpm':
         ensure => present,
@@ -107,10 +107,10 @@ class matomo (
             package_name => "php${php_version}-xml",
             priority     => 15;
         'igbinary':
-             config   => {
-                 'extension'                => 'igbinary.so',
-                 'igbinary.compact_strings' => 'Off',
-             };
+            config   => {
+                'extension'                => 'igbinary.so',
+                'igbinary.compact_strings' => 'Off',
+            };
         'mysqlnd':
             package_name => '',
             priority     => 10;
