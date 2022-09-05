a56ea97356ab2ce85d93179df48f02b512361c48
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-09-05 14:54:10 -0600

    Update shared.pp

--- modules/mediawiki/manifests/jobqueue/shared.pp
+++ modules/mediawiki/manifests/jobqueue/shared.pp
@@ -2,25 +2,24 @@
 #
 # JobQueue resources for both runner & chron
 class mediawiki::jobqueue::shared {
+    git::clone { 'JobRunner':
+        ensure    => latest,
+        directory => '/srv/jobrunner',
+        origin    => 'https://github.com/miraheze/jobrunner-service',
+    }
 
-  git::clone { 'JobRunner':
-      ensure    => latest,
-      directory => '/srv/jobrunner',
-      origin    => 'https://github.com/miraheze/jobrunner-service',
-  }
+    $redis_password = lookup('passwords::redis::master')
+    $redis_server_ip = lookup('mediawiki::jobqueue::runner::redis_ip', {'default_value' => '[2a10:6740::6:306]:6379'})
 
-  $redis_password = lookup('passwords::redis::master')
-  $redis_server_ip = lookup('mediawiki::jobqueue::runner::redis_ip', {'default_value' => '[2a10:6740::6:306]:6379'})
+    if lookup('jobrunner::intensive', {'default_value' => false}) {
+        $config = 'jobrunner-hi.json.erb'
+    } else {
+        $config = 'jobrunner.json.erb'
+    }
 
-  if lookup('jobrunner::intensive', {'default_value' => false}) {
-      $config = 'jobrunner-hi.json.erb'
-   } else {
-      $config = 'jobrunner.json.erb'
-   }
-
-   file { '/srv/jobrunner/jobrunner.json':
-      ensure  => present,
-      content => template("mediawiki/${config}"),
-      require => Git::Clone['JobRunner'],
-   }
+    file { '/srv/jobrunner/jobrunner.json':
+        ensure  => present,
+        content => template("mediawiki/${config}"),
+        require => Git::Clone['JobRunner'],
+    }
 }
