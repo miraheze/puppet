90d93451cb89f8ab4530ff5d45c7626a779c3f17
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-09-08 17:57:09 -0600

    Update site.pp

--- modules/nginx/manifests/site.pp
+++ modules/nginx/manifests/site.pp
@@ -1,4 +1,33 @@
 # == Define: nginx::site
+#
+# Provisions an Nginx vhost. Like file resources, this resource type takes
+# either a 'content' parameter with a string literal value or 'source'
+# parameter with a Puppet file reference. The resource title is used as the
+# site name.
+#
+# === Parameters
+#
+# [*content*]
+#   The Nginx site configuration as a string literal.
+#   Either this or 'source' must be set.
+#
+# [*source*]
+#   The Nginx site configuration as a Puppet file reference.
+#   Either this or 'content' must be set.
+#
+# [*ensure*]
+#   'present' or 'absent'; whether the site configuration is
+#   installed or removed in sites-available/
+#
+# [*monitor*]
+#   Boolean; Whether HTTPS monitoring should be enabled for the site.
+#
+# === Examples
+#
+#  nginx::site { 'mediawiki':
+#    content => template('mediawiki/mediawiki.conf.erb'),
+#  }
+#
 define nginx::site(
     VMlib::Ensure                $ensure  = 'present',
     Boolean                      $monitor = true,
