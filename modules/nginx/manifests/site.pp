a6a987b5dc8f2904d8a6c66768908a17fc2c1c06
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-09-08 18:35:20 -0600

    nginx: minor cleanup (#2815)

--- modules/nginx/manifests/site.pp
+++ modules/nginx/manifests/site.pp
@@ -1,11 +1,40 @@
-# nginx::site
+# == Define: nginx::site
+#
+# Provisions an Nginx vhost. Like file resources, this resource type takes
+# either a 'content' parameter with a string literal value or 'source'
+# parameter with a Puppet file reference. The resource title is used as the
+# site name.
+#
+# === Parameters
+#
+# [*content*]
+#   The Nginx site configuration as a string literal.
+#   Either this or 'source' must be set.
+#
+# [*source*]
+#   The Nginx site configuration as a Puppet file reference.
+#   Either this or 'content' must be set.
+#
+# [*ensure*]
+#   'present' or 'absent'; whether the site configuration is
+#   installed or removed in sites-available/
+#
+# [*monitor*]
+#   Boolean; Whether HTTPS monitoring should be enabled for the site.
+#
+# === Examples
+#
+#  nginx::site { 'mediawiki':
+#    content => template('mediawiki/mediawiki.conf.erb'),
+#  }
+#
 define nginx::site(
-    VMlib::Ensure $ensure     = present,
-    Optional[String] $content = undef,
-    VMlib::Sourceurl $source  = undef,
-    Boolean $monitor          = true,
+    VMlib::Ensure                $ensure  = 'present',
+    Boolean                      $monitor = true,
+    Optional[String]             $content = undef,
+    Optional[Stdlib::Filesource] $source  = undef,
 ) {
-    include ::nginx
+    include nginx
 
     $basename = regsubst($title, '[\W_]', '-', 'G')
 
