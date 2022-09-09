0ce715d97068e0c364ef9b24a49aad4317ee34b8
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-09-09 12:00:23 -0600

    Convert memcached_servers to single nutcracker config in hiera (#2755)

--- modules/role/manifests/mediawiki/nutcracker.pp
+++ modules/role/manifests/mediawiki/nutcracker.pp
@@ -1,35 +1,23 @@
 # === Class role::mediawiki::nutcracker
 class role::mediawiki::nutcracker (
-    Array[Variant[Stdlib::Host,String]] $memcached_servers_1 = lookup('memcached_servers_1', {'default_value' => []}),
-    Array[Variant[Stdlib::Host,String]] $memcached_servers_3 = lookup('memcached_servers_3', {'default_value' => []}),
+    Hash $nutcracker_config = lookup('role::mediawiki::nutcracker::config', {'default_value' => undef}),
 ) {
-
-    if $memcached_servers_1 != [] and $memcached_servers_3 != [] {
+    if $nutcracker_config != undef {
         $nutcracker_pools = {
-            'memcached_1'     => {
-                auto_eject_hosts     => false,
-                distribution         => 'ketama',
-                hash                 => 'md5',
-                listen               => '127.0.0.1:11212',
-                preconnect           => true,
-                server_connections   => 1,
-                server_failure_limit => 3,
-                server_retry_timeout => 30000,  # milliseconds
-                timeout              => 500,    # milliseconds
-                servers              => $memcached_servers_1,
-            },
-            'memcached_3'     => {
-                auto_eject_hosts     => false,
-                distribution         => 'ketama',
-                hash                 => 'md5',
-                listen               => '127.0.0.1:11214',
-                preconnect           => true,
-                server_connections   => 1,
-                server_failure_limit => 3,
-                server_retry_timeout => 30000,  # milliseconds
-                timeout              => 500,    # milliseconds
-                servers              => $memcached_servers_3,
-            },
+            $nutcracker_config.each |String $pool_name, Hash $pool_data| {
+                $pool_name => {
+                    auto_eject_hosts     => false,
+                    distribution         => 'ketama',
+                    hash                 => 'md5',
+                    listen               => "127.0.0.1:${pool_data['port']}",
+                    preconnect           => true,
+                    server_connections   => 1,
+                    server_failure_limit => 3,
+                    server_retry_timeout => 30000, # milliseconds
+                    timeout              => 500,   # milliseconds
+                    servers              => $pool_data['servers'],
+                },
+            }
         }
 
         # Ship a tmpfiles.d configuration to create /run/nutcracker
@@ -52,32 +40,20 @@ class role::mediawiki::nutcracker (
             docs    => 'https://meta.miraheze.org/wiki/Tech:Icinga/MediaWiki_Monitoring#Nutcracker'
         }
 
-        ferm::rule { 'skip_nutcracker_conntrack_out_1':
-            desc  => 'Skip outgoing connection tracking for Nutcracker',
-            table => 'raw',
-            chain => 'OUTPUT',
-            rule  => 'proto tcp sport (6378:6382 11212) NOTRACK;',
-        }
+        $nutcracker_config.each |String $pool_name, Hash $pool_data| {
+            ferm::rule { "skip_nutcracker_conntrack_out_${pool_name}":
+                desc  => 'Skip outgoing connection tracking for Nutcracker',
+                table => 'raw',
+                chain => 'OUTPUT',
+                rule  => "proto tcp sport (6378:6382 ${pool_data['port']}) NOTRACK;",
+            }
 
-        ferm::rule { 'skip_nutcracker_conntrack_in_1':
-            desc  => 'Skip incoming connection tracking for Nutcracker',
-            table => 'raw',
-            chain => 'PREROUTING',
-            rule  => 'proto tcp dport (6378:6382 11212) NOTRACK;',
-        }
-
-        ferm::rule { 'skip_nutcracker_conntrack_out_2':
-            desc  => 'Skip outgoing connection tracking for Nutcracker',
-            table => 'raw',
-            chain => 'OUTPUT',
-            rule  => 'proto tcp sport (6378:6382 11214) NOTRACK;',
-        }
-
-        ferm::rule { 'skip_nutcracker_conntrack_in_2':
-            desc  => 'Skip incoming connection tracking for Nutcracker',
-            table => 'raw',
-            chain => 'PREROUTING',
-            rule  => 'proto tcp dport (6378:6382 11214) NOTRACK;',
+            ferm::rule { "skip_nutcracker_conntrack_in_${pool_name}":
+                desc  => 'Skip incoming connection tracking for Nutcracker',
+                table => 'raw',
+                chain => 'PREROUTING',
+                rule  => "proto tcp dport (6378:6382 ${pool_data['port']}) NOTRACK;",
+            }
         }
     }
 }
