1b6e30537af94bdf7956b1eddb8f658054685502
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-09-09 18:13:44 -0600

    varnish: set net.ipv4.tcp_tw_reuse (#2818)

--- modules/role/manifests/varnish.pp
+++ modules/role/manifests/varnish.pp
@@ -32,6 +32,23 @@ class role::varnish {
         notrack => true,
     }
 
+    # tcp_tw_(reuse|recycle): both are off by default
+    # http://vincent.bernat.im/en/blog/2014-tcp-time-wait-state-linux.html
+    #    _recycle is dangerous: it violates RFCs, and probably breaks
+    # clients when many clients are behind a single NAT gateway, and
+    # affects the recycling of TIME_WAIT slots for both incoming and
+    # outgoing connections.
+    #    _reuse is not-so-dangerous: it only affects outgoing
+    # connections, and looks at timestamp and other state information to
+    # gaurantee that the reuse doesn't cause issues within reasonable
+    # constraints.
+    #    This helps prevent TIME_WAIT issues for our $localip<->$localip
+    # connections from nginx to varnish:80 - some of our caches reach
+    # connection volume/rate spikes where this is a real issue.
+    sysctl::parameters { 'tcp_tw_reuse':
+        values => { 'net.ipv4.tcp_tw_reuse' => 1 },
+    }
+
     motd::role { 'role::varnish':
         description => 'Varnish caching server',
     }
