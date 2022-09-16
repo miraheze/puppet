ef1186be0980c154a6a528ec0afcfb23164db788
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-09-16 15:45:36 -0600

    varnish: add some more network tuning
    This disables `net.ipv4.tcp_slow_start_after_idle` and sets `net.ipv4.tcp_notsent_lowat`

--- modules/role/manifests/varnish.pp
+++ modules/role/manifests/varnish.pp
@@ -42,24 +42,43 @@ class role::varnish {
         values => {
             # Increase the number of ephemeral ports
             'net.ipv4.ip_local_port_range'       => [ 4001, 65534 ],
-        }
-    }
 
-    # tcp_tw_(reuse|recycle): both are off by default
-    # http://vincent.bernat.im/en/blog/2014-tcp-time-wait-state-linux.html
-    #    _recycle is dangerous: it violates RFCs, and probably breaks
-    # clients when many clients are behind a single NAT gateway, and
-    # affects the recycling of TIME_WAIT slots for both incoming and
-    # outgoing connections.
-    #    _reuse is not-so-dangerous: it only affects outgoing
-    # connections, and looks at timestamp and other state information to
-    # gaurantee that the reuse doesn't cause issues within reasonable
-    # constraints.
-    #    This helps prevent TIME_WAIT issues for our $localip<->$localip
-    # connections from nginx to varnish:80 - some of our caches reach
-    # connection volume/rate spikes where this is a real issue.
-    sysctl::parameters { 'tcp_tw_reuse':
-        values => { 'net.ipv4.tcp_tw_reuse' => 1 },
+            # tcp_tw_(reuse|recycle): both are off by default
+            # http://vincent.bernat.im/en/blog/2014-tcp-time-wait-state-linux.html
+            #    _recycle is dangerous: it violates RFCs, and probably breaks
+            # clients when many clients are behind a single NAT gateway, and
+            # affects the recycling of TIME_WAIT slots for both incoming and
+            # outgoing connections.
+            #    _reuse is not-so-dangerous: it only affects outgoing
+            # connections, and looks at timestamp and other state information to
+            # gaurantee that the reuse doesn't cause issues within reasonable
+            # constraints.
+            #    This helps prevent TIME_WAIT issues for our $localip<->$localip
+            # connections from nginx to varnish:80 - some of our caches reach
+            # connection volume/rate spikes where this is a real issue.
+            'net.ipv4.tcp_tw_reuse'              => 1,
+
+            # tcp_slow_start_after_idle: SSR resets the congestion window of
+            # connections that have gone idle, which means it has a tendency to
+            # reset the congestion window of HTTP keepalive and HTTP/2
+            # connections, which are characterized by short bursts of activity
+            # separated by long idle times.
+            'net.ipv4.tcp_slow_start_after_idle' => 0,
+
+            # tcp_notsent_lowat: Default is -1 (unset).  The default behavior is
+            # to keep the socket writeable until the whole socket buffer fills.
+            # With this set, even if there's buffer space, the kernel doesn't
+            # notify of writeability (e.g. via epoll()) until the amount of
+            # unsent data (as opposed to unacked) in the socket buffer is less
+            # than this value.  This reduces local buffer bloat on our server's
+            # sending side, which may help with HTTP/2 prioritization.  The
+            # magic value for tuning is debateable, but arguably even setting a
+            # conservative (higher) value here is better than not setting it
+            # all, in almost all cases for any kind of TCP traffic.  ~128K seems
+            # to be a common recommendation for something close-ish to optimal
+            # for internet-facing things.
+            'net.ipv4.tcp_notsent_lowat'         => 131072,
+        }
     }
 
     motd::role { 'role::varnish':
