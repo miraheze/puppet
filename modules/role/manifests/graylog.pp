12380d59bd556d31a0debbeeb69638be453b5ec3
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-09-20 10:46:34 -0600

    graylog: upgrade to 4.3.7

--- modules/role/manifests/graylog.pp
+++ modules/role/manifests/graylog.pp
@@ -24,7 +24,7 @@ class role::graylog {
         version => '4.3',
     }
     -> class { 'graylog::server':
-        package_version => '4.3.6-1',
+        package_version => '4.3.7-1',
         config          => {
             'password_secret'     => lookup('passwords::graylog::password_secret'),
             'root_password_sha2'  => lookup('passwords::graylog::root_password_sha2'),
