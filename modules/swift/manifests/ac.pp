7c150302da9558ab3cffac50d4250bbbab427074
paladox <thomasmulhall410@yahoo.com>
2022-09-22 18:07:56 +0100

    swift: Add missing container-reconciler file

--- modules/swift/manifests/ac.pp
+++ modules/swift/manifests/ac.pp
@@ -33,6 +33,8 @@ class swift::ac {
             content => template('swift/account-server.conf.erb');
         '/etc/swift/container-server.conf':
             content => template('swift/container-server.conf.erb');
+        '/etc/swift/container-reconciler.conf':
+            content => template('swift/container-reconciler.conf.erb');
         # The uwsgi configurations are similar to what Debian ships but logging to syslog
         '/etc/swift/swift-account-server-uwsgi.ini':
             content => template('swift/swift-account-server-uwsgi.ini.erb');
