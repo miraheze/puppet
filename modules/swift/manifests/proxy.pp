fa4d8d2fe04156650b1103b204b456a640ca0b8e
paladox <paladox@users.noreply.github.com>
2022-09-25 00:16:56 +0100

    swift: cleanup proxy-server (#2870)

--- modules/swift/manifests/proxy.pp
+++ modules/swift/manifests/proxy.pp
@@ -1,7 +1,8 @@
 # == Class: swift::proxy
 
 class swift::proxy (
-    Hash[String, String] $user_accounts = lookup('swift_accounts', {'default_value' => []})
+    Hash[String, String] $user_accounts = lookup('swift_accounts', {'default_value' => []}),
+    Integer              $num_workers   = lookup('swift::proxy::num_workers', {'default_value' => $::processorcount}),
 ){
 
     ensure_packages(['swift-proxy'])
