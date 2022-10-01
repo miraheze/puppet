a682991a9520820765428547e0f2af43295d3890
paladox <paladox@users.noreply.github.com>
2022-10-02 00:12:10 +0100

    Update proxy.pp

--- modules/swift/manifests/proxy.pp
+++ modules/swift/manifests/proxy.pp
@@ -3,6 +3,8 @@
 class swift::proxy (
     Hash[String, String] $user_accounts = lookup('swift_accounts', {'default_value' => []}),
     Integer              $num_workers   = lookup('swift::proxy::num_workers', {'default_value' => $::processorcount}),
+    Hash                 $accounts      = lookup('swift::accounts'),
+    Hash                 $accounts_keys = lookup('swift::accounts_keys'),
 ){
 
     ensure_packages(['swift-proxy'])
