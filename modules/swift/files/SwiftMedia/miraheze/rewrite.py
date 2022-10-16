8c10b0f97152fea9e36c45fc9f074eb96c319388
paladox <paladox@users.noreply.github.com>
2022-10-16 17:27:01 +0100

    Fix

--- modules/swift/files/SwiftMedia/miraheze/rewrite.py
+++ modules/swift/files/SwiftMedia/miraheze/rewrite.py
@@ -202,7 +202,7 @@ class _MirahezeRewriteContext(WSGIContext):
         match = re.match(
             (r'^/(?P<proj>[^/]+)/'
              r'((?P<zone>transcoded|thumb)/)?'
-             r'(?P<path>((temp|archive)/)?[0-9a-f]/[0-9a-f]{2}/.+)$')
+             r'(?P<path>((temp|archive)/)?[0-9a-f]/[0-9a-f]{2}/.+)$'),
             req.path)
         if match:
             proj = match.group('proj')
