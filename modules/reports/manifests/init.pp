a6deabe456409a299e64e1c264f946696ad3ce35
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-10-23 15:17:09 -0600

    Upgrade TSPortal to v9

--- modules/reports/manifests/init.pp
+++ modules/reports/manifests/init.pp
@@ -5,7 +5,7 @@ class reports {
     git::clone { 'TSPortal':
         directory => '/srv/TSPortal',
         origin    => 'https://github.com/miraheze/TSPortal',
-        branch    => 'v8',
+        branch    => 'v9',
         owner     => 'www-data',
         group     => 'www-data',
     }
