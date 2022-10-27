afb952e7a1a91535ae293da61a467c2483e5ddd3
paladox <paladox@users.noreply.github.com>
2022-10-27 22:13:52 +0100

    Update proxy.pp

--- modules/swift/manifests/proxy.pp
+++ modules/swift/manifests/proxy.pp
@@ -51,7 +51,7 @@ class swift::proxy (
         check_command => 'check_http',
         vars          => {
             address6         => $facts['ipaddress6'],
-            http_vhost       => $::fqdn,
+            http_vhost       => 'swift-lb.miraheze.org',
             http_ignore_body => true,
             # We redirect / in varnish so the 404 is expected in the backend.
             # We don't serve index page.
