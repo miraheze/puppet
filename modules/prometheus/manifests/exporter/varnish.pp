0e0432ea6e8400baae30af3cad8d806b49b0c24f
paladox <paladox@users.noreply.github.com>
2022-11-21 15:58:09 +0000

    prometheus::exporter::varnish: cleanup firewall rules

--- modules/prometheus/manifests/exporter/varnish.pp
+++ modules/prometheus/manifests/exporter/varnish.pp
@@ -9,9 +9,16 @@ class prometheus::exporter::varnish (
         restart => true,
     }
 
-    $firewall_rules = query_facts('Class[Role::Prometheus]', ['ipaddress', 'ipaddress6'])
-    $firewall_rules_mapped = $firewall_rules.map |$key, $value| { "${value['ipaddress']} ${value['ipaddress6']}" }
-    $firewall_rules_str = join($firewall_rules_mapped, ' ')
+    $firewall_rules_str = join(
+        query_facts('Class[Prometheus]', ['ipaddress', 'ipaddress6'])
+        .map |$key, $value| {
+            "${value['ipaddress']} ${value['ipaddress6']}"
+        }
+        .flatten()
+        .unique()
+        .sort(),
+        ' '
+    )
     ferm::service { 'prometheus varnish_exporter':
         proto  => 'tcp',
         port   => $listen_port,
