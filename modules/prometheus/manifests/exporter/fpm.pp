01d50ced86d38403a060deca180e7090a1eedd75
paladox <paladox@users.noreply.github.com>
2022-11-21 16:00:01 +0000

    prometheus::exporter::fpm: cleanup firewall rule

--- modules/prometheus/manifests/exporter/fpm.pp
+++ modules/prometheus/manifests/exporter/fpm.pp
@@ -18,10 +18,16 @@ class prometheus::exporter::fpm {
         require => File['/usr/local/bin/prometheus-phpfpm-exporter'],
     }
 
-    $firewall_rules = query_facts('Class[Prometheus]', ['ipaddress', 'ipaddress6'])
-    $firewall_rules_mapped = $firewall_rules.map |$key, $value| { "${value['ipaddress']} ${value['ipaddress6']}" }
-    $firewall_rules_str = join($firewall_rules_mapped, ' ')
-
+    $firewall_rules_str = join(
+        query_facts('Class[Prometheus]', ['ipaddress', 'ipaddress6'])
+        .map |$key, $value| {
+            "${value['ipaddress']} ${value['ipaddress6']}"
+        }
+        .flatten()
+        .unique()
+        .sort(),
+        ' '
+    )
     ferm::service { 'prometheus php-fpm':
         proto  => 'tcp',
         port   => '9253',
