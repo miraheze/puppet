75234dfdec2d0c960e5144e9d01e73a172e4592b
paladox <paladox@users.noreply.github.com>
2022-11-26 20:27:56 +0000

    Update client.pp

--- modules/puppetserver/manifests/puppetdb/client.pp
+++ modules/puppetserver/manifests/puppetdb/client.pp
@@ -39,29 +39,15 @@ class puppetserver::puppetdb::client(
 
     class { 'puppetdb': }
 
-    $syslog_daemon = lookup('base::syslog::syslog_daemon', {'default_value' => 'syslog_ng'})
-    if $syslog_daemon == 'syslog_ng' {
-        puppetserver::logging { 'puppetdb':
-            file_path           => '/etc/puppetlabs/puppetdb/logback.xml',
-            file_source         => 'puppet:///modules/puppetserver/puppetdb_logback.xml',
-            file_source_options => [
-                '/var/log/puppetlabs/puppetdb/puppetdb.log.json',
-                { 'flags' => 'no-parse' }
-            ],
-            program_name        => 'puppetdb',
-            notify              => Service['puppetdb'],
-        }
-    } else {
-        file { '/etc/puppetlabs/puppetdb/logback.xml':
-            ensure => present,
-            source => 'puppet:///modules/puppetserver/puppetdb_logback.xml',
-            notify => Service['puppetdb'],
-        }
+    file { '/etc/puppetlabs/puppetdb/logback.xml':
+        ensure => present,
+        source => 'puppet:///modules/puppetserver/puppetdb_logback.xml',
+        notify => Service['puppetdb'],
+    }
 
-        rsyslog::input::file { 'puppetdb':
-            path              => '/var/log/puppetlabs/puppetdb/puppetdb.log.json',
-            syslog_tag_prefix => '',
-            use_udp           => true,
-        }
+    rsyslog::input::file { 'puppetdb':
+        path              => '/var/log/puppetlabs/puppetdb/puppetdb.log.json',
+        syslog_tag_prefix => '',
+        use_udp           => true,
     }
 }
