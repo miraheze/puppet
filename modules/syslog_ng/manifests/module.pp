72e073c154909c07c1b2cbad2b26ff774c823307
paladox <thomasmulhall410@yahoo.com>
2022-11-26 20:22:30 +0000

    Remove syslog_ng

--- modules/syslog_ng/manifests/module.pp
+++
@@ -1,8 +0,0 @@
-#
-define syslog_ng::module {
-  include ::syslog_ng
-  $module_prefix = $::syslog_ng::module_prefix
-  package { "${module_prefix}${title}":
-    ensure => $::syslog_ng::package_ensure
-  }
-}
