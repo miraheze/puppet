8d67f29268261711eee4b9f7c67fae5f3da5ecb8
paladox <paladox@users.noreply.github.com>
2022-12-16 16:45:56 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 # servers
 
-node /^bast1[024]1\.miraheze\.org$/ {
+node /^bast1[24]1\.miraheze\.org$/ {
     include base
     include role::bastion
 }
@@ -52,7 +52,7 @@ node 'matomo131.miraheze.org' {
     include role::matomo
 }
 
-node /^mem1[034]1\.miraheze\.org$/ {
+node /^mem1[34]1\.miraheze\.org$/ {
     include base
     include role::memcached
 }
