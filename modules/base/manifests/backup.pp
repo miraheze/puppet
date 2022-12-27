96da5ce94fc4d0a8e84b5433c569ac47b5893914
John F. Lewis <john@miraheze.org>
2022-12-27 17:18:24 +0000

    Create backup.pp

---
+++ modules/base/manifests/backup.pp
@@ -0,0 +1,13 @@
+# class base::backup
+class base::backup (
+    String $pca_password = lookup('private::passwords::pca')
+) {
+    package { ['python3-fabric', 'python3-decorator']:
+        ensure => present,
+    }
+
+    file { '/usr/local/bin/miraheze-backup':
+        mode    => '0555',
+        content => template('base/backups/miraheze-backup.py.erb),
+    }
+}
