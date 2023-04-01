93455b89917e308c86517a7091beb9dd4d177b4e
RhinosF1 <rhinosf1@gmail.com>
2023-04-01 18:12:23 +0100

    Update deploy-mediawiki.py

--- modules/mediawiki/files/bin/deploy-mediawiki.py
+++ modules/mediawiki/files/bin/deploy-mediawiki.py
@@ -76,7 +76,7 @@ def non_zero_code(ec: list[int], nolog: bool = True, leave: bool = True) -> bool
     return False
 
 
-def check_up(nolog: bool, Debug: Optional[str] = None, Host: Optional[str] = None, domain: str = 'meta.miraheze.org', verify: bool = True, force: bool = False, port: int = 443) -> bool:
+def check_up(nolog: bool, Debug: str | None = None, Host: str | None = None, domain: str = 'meta.miraheze.org', verify: bool = True, force: bool = False, port: int = 443) -> bool:
     if verify is False:
         os.environ['PYTHONWARNINGS'] = 'ignore:Unverified HTTPS request'
     if not Debug and not Host:
@@ -139,7 +139,7 @@ def _get_deployed_path(repo: str) -> str:
     return f'/srv/mediawiki/{repos[repo]}/'
 
 
-def _construct_rsync_command(time: str, dest: str, recursive: bool = True, local: bool = True, location: Optional[str] = None, server: Optional[str] = None) -> str:
+def _construct_rsync_command(time: str, dest: str, recursive: bool = True, local: bool = True, location: str | None = None, server: str | None = None) -> str:
     if time:
         params = '--inplace'
     else:
@@ -158,7 +158,7 @@ def _construct_rsync_command(time: str, dest: str, recursive: bool = True, local
     raise Exception(f'Error constructing command. Either server was missing or {location} != {dest}')  # noqa: R503
 
 
-def _construct_git_pull(repo: str, submodules: bool = False, branch: Optional[str] = None) -> str:
+def _construct_git_pull(repo: str, submodules: bool = False, branch: str | None = None) -> str:
     extrap = ' '
     if submodules:
         extrap += '--recurse-submodules '
