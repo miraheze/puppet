87c6658f35f4566e5e87c7c4ee99230c80e789dc
RhinosF1 <rhinosf1@gmail.com>
2023-04-16 13:49:02 +0100

    update

--- modules/mediawiki/files/bin/mwscript.py
+++ modules/mediawiki/files/bin/mwscript.py
@@ -1,13 +1,28 @@
 #! /usr/bin/python3
 
+from __future__ import annotations
+
 import argparse
 import os
 import sys
+from typing import TYPE_CHECKING, TypedDict
+if TYPE_CHECKING:
+    from typing import Optional
 
 
-def run(args: argparse.Namespace) -> None:
+class CommandInfo(TypedDict):
+    command: str
+    generate: Optional[str]
+    long: bool
+    nolog: bool
+    confirm: bool
+
+
+
+def get_commands(args: argparse.Namespace) -> CommandInfo:
     longscripts = ('compressOld.php', 'deleteBatch.php', 'importDump.php', 'importImages.php', 'nukeNS.php', 'rebuildall.php', 'rebuildImages.php', 'refreshLinks.php', 'runJobs.php', 'purgeList.php', 'cargoRecreateData.php')
     long = False
+    generate = None
 
     script = args.script
     if not script.endswith('.php'):
@@ -20,7 +35,7 @@ def run(args: argparse.Namespace) -> None:
         runner = '/srv/mediawiki/w/maintenance/run.php '
     else:
         runner = ''
-    if args.runner and script.endswith('.php'):  # assume class if not
+    if script.endswith('.php'):  # assume class if not
         scriptsplit = script.split('/')
         if script in longscripts:
             long = True
@@ -34,11 +49,23 @@ def run(args: argparse.Namespace) -> None:
             script = f'{runner}/srv/mediawiki/w/{scriptsplit[0]}/{scriptsplit[1]}/maintenance/{scriptsplit[2]}'
             if scriptsplit[2] in longscripts:
                 long = True
-    else:
+    elif args.runner:
         print('WARNING: Use of classes is not well tested. Please use with caution.')
 
-    wiki = args.wiki
     validDBLists = ('active', 'beta')
+    try:
+        # We don't handle errror cases first as that's simply a failback and it would not be simpler.
+        if args.extension:  # noqa: SIM106
+            wiki = ''
+        elif args.arguments[0].endswith('wiki') or args.arguments[0] in [*['all'], *validDBLists]:  # noqa: SIM106
+            wiki = args.arguments[0]
+            args.arguments.remove(wiki)
+            if args.arguments == []:
+                args.arguments = False
+        else:
+            raise ValueError(f'First argument should be a valid wiki if --extension not given DEBUG: {args.arguments[0]} / {args.extension} / {[*["all"], *validDBLists]}')
+    except IndexError:
+        raise ValueError('Not enough Arguments given.')
     if wiki == 'all':
         long = True
         command = f'sudo -u www-data /usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.json {script}'
@@ -53,19 +80,23 @@ def run(args: argparse.Namespace) -> None:
         command = f'sudo -u www-data php {script} --wiki={wiki}'
     if args.arguments:
         command += ' ' + ' '.join(args.arguments)
-    logcommand = f'/usr/local/bin/logsalmsg "{command}'
+    return {'long': long, 'generate': generate, 'command': command, 'nolog': args.nolog, 'confirm': args.confirm}
+
+
+def run(info: CommandInfo) -> None:  # pragma: no cover
+    logcommand = f'/usr/local/bin/logsalmsg {info["command"]}'
     print('Will execute:')
-    if 'generate' in locals():
-        print(generate)
-    print(command)
-    if args.confirm or input("Type 'Y' to confirm: ").upper() == 'Y':
-        if long and not args.nolog:
+    if info['generate']:
+        print(info['generate'])
+    print(info['command'])
+    if info['confirm'] or input("Type 'Y' to confirm: ").upper() == 'Y':
+        if info['long'] and not info['nolog']:
             os.system(f'{logcommand} (START)"')
-        if 'generate' in locals():
-            os.system(generate)
-        return_value = os.system(command)
+        if info['generate']:
+            os.system(info['generate'])  # type: ignore
+        return_value = os.system(info['command'])
         logcommand += f' (END - exit={str(return_value)})"'
-        if not args.nolog:
+        if not info['nolog']:
             print(f'Logging via {logcommand}')
             os.system(logcommand)
         print('Done!')
@@ -73,10 +104,9 @@ def run(args: argparse.Namespace) -> None:
         print('Aborted!')
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process some integers.')
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Run a MediaWiki Script')
     parser.add_argument('script')
-    parser.add_argument('wiki')
     parser.add_argument('arguments', nargs='*', default=[])
     parser.add_argument('--extension', '--skin', dest='extension')
     parser.add_argument('--no-log', dest='nolog', action='store_true')
@@ -85,5 +115,8 @@ if __name__ == '__main__':
 
     args = parser.parse_known_args()[0]
     args.arguments += parser.parse_known_args()[1]
+    return args
 
-    run(args)
+if __name__ == '__main__':
+
+    run(get_commands(get_args()))
---
+++ modules/mediawiki/files/bin/test_mwscript.py
@@ -0,0 +1,51 @@
+import os
+
+import mwscript
+
+import pytest
+
+
+def test_get_command_simple():
+    args = mwscript.get_args()
+    args.script = 'test.php'
+    args.arguments = ['metawiki']
+    assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data php /srv/mediawiki/w/maintenance/test.php --wiki=metawiki', 'generate': None, 'long': False, 'nolog': False}
+
+
+def test_get_command_extension():
+    args = mwscript.get_args()
+    args.script = 'extensions/CheckUser/test.php'
+    args.arguments = ['metawiki']
+    assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data php /srv/mediawiki/w/extensions/CheckUser/maintenance/test.php --wiki=metawiki', 'generate': None, 'long': False, 'nolog': False}
+
+
+def test_get_command_extension_list():
+    args = mwscript.get_args()
+    args.script = 'test.php'
+    args.extension = 'CheckUser'
+    try:
+        assert mwscript.get_commands(args) == {'confirm': False, 'command': f'sudo -u www-data /usr/local/bin/foreachwikiindblist /home/{os.environ["LOGNAME"]}/CheckUser.json /srv/mediawiki/w/maintenance/test.php', 'generate': 'php /srv/mediawiki/w/extensions/MirahezeMagic/maintenance/generateExtensionDatabaseList.php --wiki=loginwiki --extension=CheckUser',
+'long': True, 'nolog': False}
+    except KeyError:
+        pytest.skip('You have a stupid environment')
+
+
+def test_get_command_all():
+    args = mwscript.get_args()
+    args.script = 'test.php'
+    args.arguments = ['all']
+    assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data /usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.json /srv/mediawiki/w/maintenance/test.php', 'generate': None, 'long': True, 'nolog': False}
+
+
+def test_get_command_beta():
+    args = mwscript.get_args()
+    args.script = 'test.php'
+    args.arguments = ['beta']
+    assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data /usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/beta.json /srv/mediawiki/w/maintenance/test.php', 'generate': None, 'long': True, 'nolog': False}
+
+
+def test_get_command_args():
+    args = mwscript.get_args()
+    args.script = 'test.php'
+    args.arguments = ['metawiki', '--test']
+    assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data php /srv/mediawiki/w/maintenance/test.php --wiki=metawiki --test', 'generate': None, 'long': False, 'nolog': False}
