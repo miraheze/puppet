28a4c5f7627b04a9367d14005597db7d6910af50
paladox <paladox@users.noreply.github.com>
2023-04-17 15:50:37 -0400

    Install matomo121 (#3198)

--- manifests/site.pp
+++ manifests/site.pp
@@ -52,6 +52,11 @@ node 'matomo131.miraheze.org' {
     include role::matomo
 }
 
+node 'matomo121.miraheze.org' {
+    include base
+    include role::matomo
+}
+
 node /^mem1[34]1\.miraheze\.org$/ {
     include base
     include role::memcached
