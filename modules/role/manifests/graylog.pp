b5363a0ed9ca5d3f5ee93cc6e9496e1955d7eea6
paladox <paladox@users.noreply.github.com>
2023-04-17 18:52:43 +0100

    graylog: set ignore_migration_failures
    For some reason migrating from graylog121 to 131 is causing issues with the index (migration).
    
    https://github.com/Graylog2/graylog2-server/issues/12963

--- modules/role/manifests/graylog.pp
+++ modules/role/manifests/graylog.pp
@@ -30,6 +30,7 @@ class role::graylog {
             'password_secret'     => lookup('passwords::graylog::password_secret'),
             'root_password_sha2'  => lookup('passwords::graylog::root_password_sha2'),
             'elasticsearch_hosts' => $elasticsearch_host,
+            'ignore_migration_failures' => true,
         }
     }
 
