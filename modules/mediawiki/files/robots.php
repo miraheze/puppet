8091aaed81047358f5b229986786a640621da192
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2023-06-19 15:23:36 -0400

    Update robots.php (#3226)
    add some extra troublemakers and restrictions

--- modules/mediawiki/files/robots.php
+++ modules/mediawiki/files/robots.php
@@ -13,7 +13,73 @@ $page = $wikiPageFactory->newFromTitle( $titleFactory->newFromText( 'Robots.txt'
 header( 'Content-Type: text/plain; charset=utf-8' );
 header( 'X-Miraheze-Robots: Default' );
 
-# Throttle YandexBot
+# Throttle access to certain pages
+echo "User-Agent: *" . "\r\n";
+echo "Disallow: /api.php" . "\r\n";
+echo "Disallow: /cors/" . "\r\n";
+echo "Disallow: /geoip$" . "\r\n";
+echo "Disallow: /rest_v1/" . "\r\n";
+echo "Disallow: /w/Property:" . "\r\n";
+echo "Disallow: /w/Property%3A" . "\r\n";
+echo "Disallow: /w/property:" . "\r\n";
+echo "Disallow: /*?title=Property:" . "\r\n";
+echo "Disallow: /*?title=Property%3A" . "\r\n";
+echo "Disallow: /*?*&title=Property:" . "\r\n";
+echo "Disallow: /*?*&title=Property%3A" . "\r\n";
+echo "Disallow: /w/Special:" . "\r\n";
+echo "Disallow: /w/Special%3A" . "\r\n";
+echo "Disallow: /w/special:" . "\r\n";
+echo "Disallow: /*?title=Special:" . "\r\n";
+echo "Disallow: /*?title=Special%3A" . "\r\n";
+echo "Disallow: /*?*&title=Special:" . "\r\n";
+echo "Disallow: /*?*&title=Special%3A" . "\r\n";
+echo "Disallow: /w/Especial:" . "\r\n";
+echo "Disallow: /w/Especial%3A" . "\r\n";
+echo "Disallow: /w/especial:" . "\r\n";
+echo "Disallow: /*?title=Especial:" . "\r\n";
+echo "Disallow: /*?title=Especial%3A" . "\r\n";
+echo "Disallow: /*?*&title=Especial:" . "\r\n";
+echo "Disallow: /*?*&title=Especial%3A" . "\r\n";
+echo "Disallow: /*?action=" . "\r\n";
+echo "Disallow: /*?*&action=" . "\r\n";
+echo "Disallow: /*?feed=" . "\r\n";
+echo "Disallow: /*?*&feed=" . "\r\n";
+echo "Disallow: /*?from=" . "\r\n";
+echo "Disallow: /*?*&from=" . "\r\n";
+echo "Disallow: /*?mobileaction=" . "\r\n";
+echo "Disallow: /*?*&mobileaction=" . "\r\n";
+echo "Disallow: /*?oldid=" . "\r\n";
+echo "Disallow: /*?*&oldid=" . "\r\n";
+echo "Disallow: /*?printable=" . "\r\n";
+echo "Disallow: /*?*&printable=" . "\r\n";
+echo "Disallow: /*?redirect=" . "\r\n";
+echo "Disallow: /*?*&redirect=" . "\r\n";
+echo "Disallow: /*?uselang=" . "\r\n";
+echo "Disallow: /*?*&uselang=" . "\r\n";
+echo "Disallow: /*?useskin=" . "\r\n";
+echo "Disallow: /*?*&useskin=" . "\r\n";
+echo "Disallow: /*?veaction=" . "\r\n";
+echo "Disallow: /*?*&veaction=" . "\r\n";
+echo "Disallow: /*?filefrom=" . "\r\n";
+echo "Disallow: /*?*&filefrom=" . "\r\n";
+echo "Disallow: /*?fileuntil=" . "\r\n";
+echo "Disallow: /*?*&fileuntil=" . "\r\n";
+echo "Disallow: /*?navbox=" . "\r\n";
+echo "Disallow: /*?*&navbox=" . "\r\n";
+echo "Disallow: /*?pageuntil=" . "\r\n";
+echo "Disallow: /*?*&pageuntil=" . "\r\n";
+echo "Disallow: /*?pagefrom=" . "\r\n";
+echo "Disallow: /*?*&pagefrom=" . "\r\n";
+echo "Disallow: /*?diff=" . "\r\n";
+echo "Disallow: /*?*&diff=" . "\r\n";
+echo "Disallow: /*?curid=" . "\r\n";
+echo "Disallow: /*?*&curid=" . "\r\n";
+echo "Disallow: /*?search=" . "\r\n";
+echo "Disallow: /*?*&search=" . "\r\n";
+echo "Disallow: /*?section=" . "\r\n";
+echo "Disallow: /*?*&section=" . "\r\n\n";
+
+# Throttle YandexBot TODO: Crawl-delay is not respected since 2018
 echo "# Throttle YandexBot" . "\r\n";
 echo "User-Agent: YandexBot" . "\r\n";
 echo "Crawl-Delay: 2.5" . "\r\n\n";
@@ -21,16 +87,57 @@ echo "Crawl-Delay: 2.5" . "\r\n\n";
 # Throttle BingBot
 echo "# Throttle BingBot" . "\r\n";
 echo "User-agent: bingbot" . "\r\n";
-echo "Crawl-delay: 1" . "\r\n\n";
+echo "Crawl-delay: 5" . "\r\n\n";
 
 # Block SemrushBot
 echo "# Block SemrushBot" . "\r\n";
 echo "User-Agent: SemrushBot" . "\r\n";
 echo "Disallow: /" . "\r\n\n";
 
+# Throttle MJ12Bot
 echo "# Throttle MJ12Bot" . "\r\n";
 echo "User-agent: MJ12bot" . "\r\n";
 echo "Crawl-Delay: 10" . "\r\n\n";
+
+# Block AhrefsBot
+echo "# Block AhrefsBot" . "\r\n";
+echo "User-agent: AhrefsBot" . "\r\n";
+echo "Disallow: /" . "\r\n\n";
+
+# Block Bytespider
+echo "# Block Bytespider" . "\r\n";
+echo "User-agent: Bytespider" . "\r\n";
+echo "Disallow: /" . "\r\n\n";
+
+# Block PetalBot
+echo "# Block PetalBot" . "\r\n";
+echo "User-agent: PetalBot" . "\r\n";
+echo "Disallow: /" . "\r\n\n";
+
+# Block DotBot
+echo "# Block DotBot" . "\r\n";
+echo "User-agent: DotBot" . "\r\n";
+echo "Disallow: /" . "\r\n\n";
+
+# Block MegaIndex
+echo "# Block MegaIndex" . "\r\n";
+echo "User-agent: MegaIndex" . "\r\n";
+echo "Disallow: /" . "\r\n\n";
+
+# Block serpstatbot
+echo "# Block serpstatbot" . "\r\n";
+echo "User-agent: serpstatbot" . "\r\n";
+echo "Disallow: /" . "\r\n\n";
+
+# Block Barkrowler
+echo "# Block Barkrowler" . "\r\n";
+echo "User-agent: Barkrowler" . "\r\n";
+echo "Disallow: /" . "\r\n\n";
+
+# Block SeekportBot
+echo "# Block SeekportBot" . "\r\n";
+echo "User-agent: SeekportBot" . "\r\n";
+echo "Disallow: /" . "\r\n\n";
 
 # Dynamic sitemap url
 echo "# Dynamic sitemap url" . "\r\n";
