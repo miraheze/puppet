23acbe64e3bb656d860d592531e1aae199f9ddfb
paladox <paladox@users.noreply.github.com>
2023-07-01 12:46:17 +0100

    mediawiki: run composer install for jobrunner (#3249)

--- modules/mediawiki/manifests/jobqueue/shared.pp
+++ modules/mediawiki/manifests/jobqueue/shared.pp
@@ -2,10 +2,29 @@
 #
 # JobQueue resources for both runner & chron
 class mediawiki::jobqueue::shared {
+    if !defined(Package['composer']) {
+        ensure_packages('composer')
+    }
+
     git::clone { 'JobRunner':
         ensure    => latest,
         directory => '/srv/jobrunner',
         origin    => 'https://github.com/miraheze/jobrunner-service',
+        owner     => 'www-data',
+        group     => 'www-data',
+    }
+
+    exec { 'jobrunner_composer':
+        command     => 'composer install --no-dev',
+        creates     => '/srv/jobrunner/vendor',
+        cwd         => '/srv/jobrunner',
+        path        => '/usr/bin',
+        environment => [
+            'HOME=/srv/jobrunner',
+            'HTTP_PROXY=http://bast.miraheze.org:8080'
+        ],
+        user        => 'www-data',
+        require     => Git::Clone['JobRunner'],
     }
 
     $redis_password = lookup('passwords::redis::master')
