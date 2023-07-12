0565459c1f30d170b37975edb696b4ad0a3e0bb2
paladox <paladox@users.noreply.github.com>
2023-07-12 17:25:20 +0100

    matomo: update to 4.15.0

--- modules/matomo/manifests/init.pp
+++ modules/matomo/manifests/init.pp
@@ -8,7 +8,7 @@ class matomo (
     git::clone { 'matomo':
         directory          => '/srv/matomo',
         origin             => 'https://github.com/matomo-org/matomo',
-        branch             => '4.14.2', # Current stable
+        branch             => '4.15.0', # Current stable
         recurse_submodules => true,
         owner              => 'www-data',
         group              => 'www-data',
