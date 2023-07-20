0ca29ab42b2af752cce14a22720cdf5838b079a6
paladox <paladox@users.noreply.github.com>
2023-07-19 20:46:40 -0400

    Update cgroup.pp

--- modules/mediawiki/manifests/cgroup.pp
+++ modules/mediawiki/manifests/cgroup.pp
@@ -13,6 +13,7 @@ class mediawiki::cgroup {
         mode   => '0755',
     }
 
+    $php_version = lookup('php::php_version')
     systemd::service { 'cgroup':
         ensure  => present,
         content => systemd_template('cgroup'),
