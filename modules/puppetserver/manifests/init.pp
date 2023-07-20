16230068dc36725ecc41198c7b5a83461d4b013a
paladox <paladox@users.noreply.github.com>
2023-07-20 11:28:31 -0400

    puppetserver: make sure service starts on boot

--- modules/puppetserver/manifests/init.pp
+++ modules/puppetserver/manifests/init.pp
@@ -178,6 +178,7 @@ class puppetserver(
 
     service { 'puppetserver':
         ensure   => running,
+        enable   => true,
         provider => 'systemd',
         require  => Package['puppetserver'],
     }
