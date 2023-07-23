bc2d3480fa9fdf3e82e94424585308ed638fcbd4
paladox <paladox@users.noreply.github.com>
2023-07-23 15:11:06 -0400

    Update init.pp

--- modules/salt/manifests/init.pp
+++ modules/salt/manifests/init.pp
@@ -3,7 +3,7 @@ class salt {
         ensure  => present,
     }
 
-    $host = query_nodes("domain='${domain}'", 'facts.networking.fqdn')
+    $host = query_nodes("domain='${facts['networking']['domain']}'", 'networking.fqdn')
     file { '/etc/salt/roster':
         content => template('salt/roster.erb'),
         owner   => 'root',
