a9c59aae38880b27c53bcaf25618f2fedded7208
paladox <paladox@users.noreply.github.com>
2023-07-23 15:31:59 -0400

    Update varnish.pp

--- modules/role/manifests/varnish.pp
+++ modules/role/manifests/varnish.pp
@@ -16,9 +16,9 @@ class role::varnish {
     }
 
     $firewall_rules_str = join(
-        query_facts('Class[Role::Mediawiki]', ['ipaddress', 'ipaddress6'])
+        query_facts("networking.domain='${facts['networking']['domain']}' and Class[Role::Mediawiki]", ['networking'])
         .map |$key, $value| {
-            "${value['ipaddress']} ${value['ipaddress6']}"
+            "${value['networking']['ip']} ${value['networking']['ip6']}"
         }
         .flatten()
         .unique()
