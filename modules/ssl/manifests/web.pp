3be72641d416277914bbcd48206a780d810d4ade
paladox <paladox@users.noreply.github.com>
2023-07-23 15:56:16 -0400

    Update web.pp

--- modules/ssl/manifests/web.pp
+++ modules/ssl/manifests/web.pp
@@ -18,9 +18,9 @@ class ssl::web {
     }
 
     $firewall_rules_str = join(
-        query_facts('Class[Role::Icinga2]', ['ipaddress', 'ipaddress6'])
+        query_facts("networking.domain='${facts['networking']['domain']}' and Class[Role::Icinga2]", ['networking'])
         .map |$key, $value| {
-            "${value['ipaddress']} ${value['ipaddress6']}"
+            "${value['networking']['ip']} ${value['networking']['ip6']}"
         }
         .flatten()
         .unique()
