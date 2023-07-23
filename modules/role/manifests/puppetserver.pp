94141338f74dddf9c5d455f4d914711a18a3e58b
paladox <paladox@users.noreply.github.com>
2023-07-23 19:24:25 -0400

    Update puppetserver.pp

--- modules/role/manifests/puppetserver.pp
+++ modules/role/manifests/puppetserver.pp
@@ -27,7 +27,7 @@ class role::puppetserver (
     String  $puppetserver_java_options = lookup('puppetserver_java_opts', {'default_value' => '-Xms300m -Xmx300m'}),
 ) {
 
-    $puppetserver_java_opts = "${puppetserver_java_options} -javaagent:/usr/share/java/prometheus/jmx_prometheus_javaagent.jar=${::fqdn}:9400:/etc/puppetlabs/puppetserver/jvm_prometheus_jmx_exporter.yaml"
+    $puppetserver_java_opts = "${puppetserver_java_options} -javaagent:/usr/share/java/prometheus/jmx_prometheus_javaagent.jar=${facts['networking']['fqdn']}:9400:/etc/puppetlabs/puppetserver/jvm_prometheus_jmx_exporter.yaml"
     class { '::puppetserver':
         puppetdb_hostname      => $puppetdb_hostname,
         puppetdb_enable        => $puppetdb_enable,
