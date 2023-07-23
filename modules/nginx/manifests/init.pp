7451ef2f715d6b25aa7891bb4120b4ac590b4ec5
paladox <paladox@users.noreply.github.com>
2023-07-23 15:11:40 -0400

    Update init.pp

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -41,7 +41,7 @@ class nginx (
 
     $module_path = get_module_path('varnish')
 
-    $cache_proxies = query_facts("domain='${domain}' and Class['Role::Varnish']", ['networking.ip', 'networking.ip6'])
+    $cache_proxies = query_facts("Class['Role::Varnish']",  ['networking'])
     file { '/etc/nginx/nginx.conf':
         content => template('nginx/nginx.conf.erb'),
         require => Package['nginx'],
