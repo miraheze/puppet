5e32e7639eb470b2bc394dd3779b81998afc1993
paladox <paladox@users.noreply.github.com>
2023-08-09 14:51:29 -0400

    Update all_certs.pp

--- modules/ssl/manifests/all_certs.pp
+++ modules/ssl/manifests/all_certs.pp
@@ -8,9 +8,18 @@ class ssl::all_certs {
 
     file { '/etc/ssl/localcerts':
         ensure  => directory,
+        owner   => 'root',
+        group   => 'ssl-cert',
         source  => 'puppet:///ssl/certificates',
+        mode    => '0775',
         recurse => remote,
         purge   => true,
+        ignore  => [
+            # TODO: Move to seperate directory to allow us
+            # to not use this hack.
+            'opensearch-admin-cert.pem',
+            'opensearch-node.crt'
+        ],
         notify  => $restart_nginx,
     }
 
