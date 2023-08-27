a38592d6f47ca9dbf041a3d5d03c046edcdc4e86
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2023-08-27 17:44:24 -0400

    ircrcbot (and related) lookup ipv6 for network

--- modules/role/manifests/irc.pp
+++ modules/role/manifests/irc.pp
@@ -5,11 +5,7 @@ class role::irc {
 
     class { 'irc::ircrcbot':
         nickname     => 'MirahezeRC',
-        # irc.libera.chat ipv6 address; we have to hardcode it.
-        # This is because it's either picking up the ipv4 address
-        # for the hostname or it doesn't support getting the ipv6
-        # address from hostname.
-        network      => '2001:470:1:dea::95',
+        network      => 'irc.libera.chat',
         network_port => '6697',
         channel      => '#miraheze-feed',
         udp_port     => '5070',
@@ -17,11 +13,7 @@ class role::irc {
 
     class { 'irc::irclogserverbot':
         nickname     => 'MirahezeLSBot',
-        # irc.libera.chat ipv6 address; we have to hardcode it.
-        # This is because it's either picking up the ipv4 address
-        # for the hostname or it doesn't support getting the ipv6
-        # address from hostname.
-        network      => '2001:470:1:dea::95',
+        network      => 'irc.libera.chat',
         network_port => '6697',
         channel      => '#miraheze-sre',
         udp_port     => '5071',
