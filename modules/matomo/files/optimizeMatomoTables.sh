d60ac7551d69b8261c8b5c5a71ec78d753b1af65
paladox <paladox@users.noreply.github.com>
2023-09-27 14:05:35 +0100

    matomo: remove runMatomoArchive and optimizeMatomoTables (#3443)

--- modules/matomo/files/optimizeMatomoTables.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/srv/matomo/console database:optimize-archive-tables last2 > /srv/matomo-optimize.log 2>&1
-/srv/matomo/console database:optimize-archive-tables january > /srv/matomo-optimize.log 2>&1
--- modules/matomo/files/runMatomoArchive.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/bin/flock -w 10 /tmp/matomo_file_lock /usr/bin/php /srv/matomo/console core:archive --url=https://matomo.miraheze.org/ > /srv/matomo-archive.log 2>&1
