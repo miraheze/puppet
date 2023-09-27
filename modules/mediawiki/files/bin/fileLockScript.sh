511fc995c9cb3c5aa68449034c5bed339f7974b1
paladox <paladox@users.noreply.github.com>
2023-09-27 13:47:02 +0100

    Fix fileLockScript
    Improve locking.

--- modules/mediawiki/files/bin/fileLockScript.sh
+++ modules/mediawiki/files/bin/fileLockScript.sh
@@ -1,19 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 
 # To run this script do the following fileLockScript.sh <lock_name> "<script>"
 
-LOCK_FILE=$1
+exec 100>$1 || exit 1
+flock -n 100 || exit 1
 
-if ( set -o noclobber; echo "$$" > "$LOCK_FILE") 2> /dev/null; then
+trap "rm -f $1" EXIT
 
-        trap 'rm -f "$LOCK_FILE"; exit $?' INT TERM EXIT
-
-        # do stuff here
-        $2
-
-        # clean up after yourself, and release your trap
-        rm -f "$LOCK_FILE"
-        trap - INT TERM EXIT
-else
-        echo "Lock Exists: $lockfile owned by $(cat $LOCK_FILE)"
-fi
+$2
