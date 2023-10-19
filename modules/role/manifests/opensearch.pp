af9171593536ac80be3df0db0655b7c18ca1cea5
paladox <paladox@users.noreply.github.com>
2023-10-19 12:11:52 +0100

    opensearch: upgrade to 2.11

--- modules/role/manifests/opensearch.pp
+++ modules/role/manifests/opensearch.pp
@@ -32,7 +32,7 @@ class role::opensearch (
             'plugins.security.nodes_dn'                             => ['CN=*.miraheze.org'],
             'plugins.security.restapi.roles_enabled'                => ['all_access', 'security_rest_api_access'],
         },
-        version     => '2.6.0',
+        version     => '2.11.0',
         manage_repo => true,
         jvm_options => [ '-Xms2g', '-Xmx2g' ],
         templates   => {
