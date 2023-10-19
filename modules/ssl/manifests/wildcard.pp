b7cc72cd435d30440c69d354ce3dcafd79599a36
paladox <thomasmulhall410@yahoo.com>
2023-10-19 16:21:02 +0100

    Revert "ssl::wildcard: fix reloading nginx"
    This reverts commit e877b77afd4617a5d7710ddc1262470833df881c.

--- modules/ssl/manifests/wildcard.pp
+++ modules/ssl/manifests/wildcard.pp
@@ -2,7 +2,6 @@
 define ssl::wildcard (
     $ssl_cert_path = '/etc/ssl/localcerts',
     $ssl_cert_key_private_path = '/etc/ssl/private',
-    $reload_nginx = true,
 ) {
 
     if !defined(File['/etc/ssl/localcerts']) {
@@ -15,8 +14,7 @@ define ssl::wildcard (
         }
     }
 
-
-    if $reload_nginx {
+    if defined(Service['nginx']) {
         $restart_nginx = Service['nginx']
     } else {
         $restart_nginx = undef
