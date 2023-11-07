1ddf54e75b277cd3dad7a515410d1c4062a3203d
RhinosF1 <rhinosf1@gmail.com>
2023-11-07 09:02:08 +0000

    Update test_mwscript.py

--- modules/mediawiki/files/bin/test_mwscript.py
+++ modules/mediawiki/files/bin/test_mwscript.py
@@ -7,6 +7,7 @@ def test_get_command_simple():
     args = mwscript.get_args()
     args.script = 'test.php'
     args.arguments = ['metawiki']
+    args.norunphp = True
     assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data php /srv/mediawiki/w/maintenance/test.php --wiki=metawiki', 'generate': None, 'long': False, 'nolog': False}
 
 
@@ -14,6 +15,7 @@ def test_get_command_extension():
     args = mwscript.get_args()
     args.script = 'extensions/CheckUser/test.php'
     args.arguments = ['metawiki']
+    args.norunphp = True
     assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data php /srv/mediawiki/w/extensions/CheckUser/maintenance/test.php --wiki=metawiki', 'generate': None, 'long': False, 'nolog': False}
 
 
@@ -24,6 +26,7 @@ def test_get_command_extension_list(mock_getlogin):
     args = mwscript.get_args()
     args.script = 'test.php'
     args.extension = 'CheckUser'
+    args.norunphp = True
     assert mwscript.get_commands(args) == {
         'confirm': False,
         'command': f'sudo -u www-data /usr/local/bin/foreachwikiindblist /home/{os.environ["LOGNAME"]}/CheckUser.json /srv/mediawiki/w/maintenance/test.php',
@@ -37,6 +40,7 @@ def test_get_command_all():
     args = mwscript.get_args()
     args.script = 'test.php'
     args.arguments = ['all']
+    args.norunphp = True
     assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data /usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.json /srv/mediawiki/w/maintenance/test.php', 'generate': None, 'long': True, 'nolog': False}
 
 
@@ -44,6 +48,7 @@ def test_get_command_beta():
     args = mwscript.get_args()
     args.script = 'test.php'
     args.arguments = ['beta']
+    args.norunphp = True
     assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data /usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/beta.json /srv/mediawiki/w/maintenance/test.php', 'generate': None, 'long': True, 'nolog': False}
 
 
@@ -51,6 +56,7 @@ def test_get_command_args():
     args = mwscript.get_args()
     args.script = 'test.php'
     args.arguments = ['metawiki', '--test']
+    args.norunphp = True
     assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data php /srv/mediawiki/w/maintenance/test.php --wiki=metawiki --test', 'generate': None, 'long': False, 'nolog': False}
 
 
@@ -58,6 +64,7 @@ def test_get_command_subdir():
     args = mwscript.get_args()
     args.script = 'subdir/test.php'
     args.arguments = ['metawiki']
+    args.norunphp = True
     assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data php /srv/mediawiki/w/maintenance/subdir/test.php --wiki=metawiki', 'generate': None, 'long': False, 'nolog': False}
 
 
@@ -65,7 +72,6 @@ def test_get_command_simple_runner():
     args = mwscript.get_args()
     args.script = 'test.php'
     args.arguments = ['metawiki']
-    args.runner = True
     assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data php /srv/mediawiki/w/maintenance/run.php /srv/mediawiki/w/maintenance/test.php --wiki=metawiki', 'generate': None, 'long': False, 'nolog': False}
 
 
@@ -73,7 +79,6 @@ def test_get_command_extension_runner():
     args = mwscript.get_args()
     args.script = 'extensions/CheckUser/test.php'
     args.arguments = ['metawiki']
-    args.runner = True
     assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data php /srv/mediawiki/w/maintenance/run.php /srv/mediawiki/w/extensions/CheckUser/maintenance/test.php --wiki=metawiki', 'generate': None, 'long': False, 'nolog': False}
 
 
@@ -84,7 +89,6 @@ def test_get_command_extension_list_runner(mock_getlogin):
     args = mwscript.get_args()
     args.script = 'test.php'
     args.extension = 'CheckUser'
-    args.runner = True
     assert mwscript.get_commands(args) == {
         'confirm': False,
         'command': f'sudo -u www-data /usr/local/bin/foreachwikiindblist /home/{os.environ["LOGNAME"]}/CheckUser.json /srv/mediawiki/w/maintenance/run.php /srv/mediawiki/w/maintenance/test.php',
@@ -98,7 +102,6 @@ def test_get_command_all_runner():
     args = mwscript.get_args()
     args.script = 'test.php'
     args.arguments = ['all']
-    args.runner = True
     assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data /usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.json /srv/mediawiki/w/maintenance/run.php /srv/mediawiki/w/maintenance/test.php', 'generate': None, 'long': True, 'nolog': False}
 
 
@@ -106,7 +109,6 @@ def test_get_command_beta_runner():
     args = mwscript.get_args()
     args.script = 'test.php'
     args.arguments = ['beta']
-    args.runner = True
     assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data /usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/beta.json /srv/mediawiki/w/maintenance/run.php /srv/mediawiki/w/maintenance/test.php', 'generate': None, 'long': True, 'nolog': False}
 
 
@@ -114,7 +116,6 @@ def test_get_command_args_runner():
     args = mwscript.get_args()
     args.script = 'test.php'
     args.arguments = ['metawiki', '--test']
-    args.runner = True
     assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data php /srv/mediawiki/w/maintenance/run.php /srv/mediawiki/w/maintenance/test.php --wiki=metawiki --test', 'generate': None, 'long': False, 'nolog': False}
 
 
@@ -122,7 +123,6 @@ def test_get_command_subdir_runner():
     args = mwscript.get_args()
     args.script = 'subdir/test.php'
     args.arguments = ['metawiki']
-    args.runner = True
     assert mwscript.get_commands(args) == {'confirm': False, 'command': 'sudo -u www-data php /srv/mediawiki/w/maintenance/run.php /srv/mediawiki/w/maintenance/subdir/test.php --wiki=metawiki', 'generate': None, 'long': False, 'nolog': False}
 
 
@@ -130,6 +130,5 @@ def test_get_command_class():
     args = mwscript.get_args()
     args.script = 'test'
     args.arguments = ['metawiki', '--test']
-    args.runner = True
     args.confirm = True
     assert mwscript.get_commands(args) == {'confirm': True, 'command': 'sudo -u www-data php /srv/mediawiki/w/maintenance/run.php test --wiki=metawiki --test', 'generate': None, 'long': False, 'nolog': False}
