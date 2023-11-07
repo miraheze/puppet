70240c30bce7878b542bf3804ccd6d3ea2009453
paladox <paladox@users.noreply.github.com>
2023-11-07 14:26:23 +0000

    mediawiki: Fix jobchron config
    https://www.mediawiki.org/wiki/Redis#Automatic_handling_of_job_recycling_and_abandons

--- modules/mediawiki/manifests/jobqueue/shared.pp
+++ modules/mediawiki/manifests/jobqueue/shared.pp
@@ -42,4 +42,10 @@ class mediawiki::jobqueue::shared {
         content => template("mediawiki/${config}"),
         require => Git::Clone['JobRunner'],
     }
+
+    file { '/srv/jobrunner/jobrunner-cleanup.json':
+        ensure  => present,
+        content => template('mediawiki/jobrunner-cleanup.json.erb'),
+        require => Git::Clone['JobRunner'],
+    }
 }
