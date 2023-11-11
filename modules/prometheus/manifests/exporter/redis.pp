45cfa5648116b06f339668889d2c1185ba4a895b
paladox <paladox@users.noreply.github.com>
2023-11-11 22:18:59 +0000

    Update redis.pp

--- modules/prometheus/manifests/exporter/redis.pp
+++ modules/prometheus/manifests/exporter/redis.pp
@@ -2,7 +2,7 @@
 #
 class prometheus::exporter::redis (
     String $redis_password = lookup('passwords::redis::master'),
-    Optional[Boolean] $use_script = lookup('prometheus::exporter::redis::use_script', {default => false}),
+    Optional[Boolean] $use_script = lookup('prometheus::exporter::redis::use_script', {default => undef}),
 ) {
 
     file { '/etc/redis_exporter':
