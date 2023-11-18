fd479bcd08023d2cb78f52c1d1a4ba9d5a968721
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2023-11-17 22:41:59 -0500

    proxy pass through bastion servers (#3505)

--- modules/role/manifests/bastion.pp
+++ modules/role/manifests/bastion.pp
@@ -26,4 +26,41 @@ class role::bastion {
         port   => '8080',
         srange => "(${squid_access_hosts_str})",
     }
+
+    $backends = lookup('varnish::backends')
+    $firewall_rules_str = join(
+        query_facts("networking.domain='${facts['networking']['domain']}' and Class[Role::Varnish]", ['networking'])
+        .map |$key, $value| {
+            "${value['networking']['ip']} ${value['networking']['ip6']}"
+        }
+        .flatten()
+        .unique()
+        .sort(),
+        ' '
+    )
+    $firewall_rules_ports = join(
+        $backends.map |$key, $value| {
+            "${value['port']}"
+        }
+        .flatten()
+        .unique()
+        .sort(),
+        ' '
+    )
+    ferm::service { 'direct varnish access':
+        proto   => 'tcp',
+        port    => "(${firewall_rules_ports})",
+        srange  => "(${firewall_rules_str})",
+        notrack => true,
+    }
+
+    file { '/etc/nginx/sites-enabled/default':
+        ensure => absent,
+        notify => Service['nginx'],
+    }
+
+    nginx::site { 'mediawiki':
+        ensure  => present,
+        content => template('role/bastion/mediawiki.conf.erb'),
+    }
 }
