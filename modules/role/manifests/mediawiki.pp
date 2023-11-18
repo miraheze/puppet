d5b4327b8ba75276d885ff4531dd9c61e1b0c9cb
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2023-11-17 23:09:16 -0500

    add bastion to mw firewall

--- modules/role/manifests/mediawiki.pp
+++ modules/role/manifests/mediawiki.pp
@@ -9,7 +9,7 @@ class role::mediawiki (
 
     if $strict_firewall {
         $firewall_rules_str = join(
-            query_facts("networking.domain='${facts['networking']['domain']}' and Class[Role::Mediawiki] or Class[Role::Varnish] or Class[Role::Icinga2] or Class[Role::Prometheus]", ['networking'])
+            query_facts("networking.domain='${facts['networking']['domain']}' and Class[Role::Mediawiki] or Class[Role::Varnish] or Class[Role::Icinga2] or Class[Role::Prometheus] or Class[Role::Bastion]", ['networking'])
             .map |$key, $value| {
                 "${value['networking']['ip']} ${value['networking']['ip6']}"
             }
