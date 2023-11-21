498911d2521037003f9ed903fb3dcdae0199974b
paladox <paladox@users.noreply.github.com>
2023-11-21 18:39:30 -0500

    bastion: Put proxy ipv4 to ipv6 stuff behind flag (#3512)

--- modules/role/manifests/bastion.pp
+++ modules/role/manifests/bastion.pp
@@ -1,4 +1,6 @@
-class role::bastion {
+class role::bastion (
+    $enable_proxy_ipv4_ipv6 = lookup('role::bastion::enable_proxy_ipv4_ipv6', {'default_value' => undef})
+) {
     include squid
 
     motd::role { 'role::bastion':
@@ -27,40 +29,42 @@ class role::bastion {
         srange => "(${squid_access_hosts_str})",
     }
 
-    $backends = lookup('varnish::backends')
-    $firewall_rules_str = join(
-        query_facts("networking.domain='${facts['networking']['domain']}' and Class[Role::Varnish]", ['networking'])
-        .map |$key, $value| {
-            "${value['networking']['ip']} ${value['networking']['ip6']}"
+    if $enable_proxy_ipv4_ipv6 {
+        $backends = lookup('varnish::backends')
+        $firewall_rules_str = join(
+            query_facts("networking.domain='${facts['networking']['domain']}' and Class[Role::Varnish]", ['networking'])
+            .map |$key, $value| {
+                "${value['networking']['ip']} ${value['networking']['ip6']}"
+            }
+            .flatten()
+            .unique()
+            .sort(),
+            ' '
+        )
+        $firewall_rules_ports = join(
+            $backends.map |$key, $value| {
+                "${value['port']}"
+            }
+            .flatten()
+            .unique()
+            .sort(),
+            ' '
+        )
+        ferm::service { 'direct varnish access':
+            proto   => 'tcp',
+            port    => "(${firewall_rules_ports})",
+            srange  => "(${firewall_rules_str})",
+            notrack => true,
         }
-        .flatten()
-        .unique()
-        .sort(),
-        ' '
-    )
-    $firewall_rules_ports = join(
-        $backends.map |$key, $value| {
-            "${value['port']}"
+    
+        file { '/etc/nginx/sites-enabled/default':
+            ensure => absent,
+            notify => Service['nginx'],
         }
-        .flatten()
-        .unique()
-        .sort(),
-        ' '
-    )
-    ferm::service { 'direct varnish access':
-        proto   => 'tcp',
-        port    => "(${firewall_rules_ports})",
-        srange  => "(${firewall_rules_str})",
-        notrack => true,
-    }
-
-    file { '/etc/nginx/sites-enabled/default':
-        ensure => absent,
-        notify => Service['nginx'],
-    }
-
-    nginx::site { 'mediawiki':
-        ensure  => present,
-        content => template('role/bastion/mediawiki.conf.erb'),
+    
+        nginx::site { 'mediawiki':
+            ensure  => present,
+            content => template('role/bastion/mediawiki.conf.erb'),
+        }
     }
 }
