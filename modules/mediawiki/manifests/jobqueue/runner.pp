3fde070c48ea209bc31de42a7e0811c10e187a4b
paladox <thomasmulhall410@yahoo.com>
2023-11-25 00:09:39 -0500

    Revert "Lower parsercache to 5 days"
    This reverts commit 10143ee09a3d805952f898e0e6290285e310a7f4.

--- modules/mediawiki/manifests/jobqueue/runner.pp
+++ modules/mediawiki/manifests/jobqueue/runner.pp
@@ -98,7 +98,7 @@ class mediawiki::jobqueue::runner {
 
             cron { 'purge_parsercache':
                 ensure  => present,
-                command => '/usr/bin/php /srv/mediawiki/w/maintenance/purgeParserCache.php --wiki loginwiki --tag pc1 --age 432000 --msleep 200',
+                command => '/usr/bin/php /srv/mediawiki/w/maintenance/purgeParserCache.php --wiki loginwiki --tag pc1 --age 604800 --msleep 200',
                 user    => 'www-data',
                 special => 'daily',
             }
