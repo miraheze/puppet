7e7c8bbe6b101e85c23c5e8d9527d221b5c5834b
paladox <paladox@users.noreply.github.com>
2023-12-07 01:58:46 -0500

    varnish: Temporarily disable varnish-connlimits

--- modules/role/manifests/varnish.pp
+++ modules/role/manifests/varnish.pp
@@ -4,10 +4,13 @@ class role::varnish {
     include fail2ban
     include prometheus::exporter::varnishreqs
 
-    ferm::conf { 'varnish-connlimits':
-        prio    => '01',
-        source  => 'puppet:///modules/role/firewall/varnish-connlimits.conf'
-    }
+    # Temporarily disabling this due to
+    # service keep being restarted on
+    # puppet runs.
+    # ferm::conf { 'varnish-connlimits':
+    #    prio    => '01',
+    #    source  => 'puppet:///modules/role/firewall/varnish-connlimits.conf'
+    # }
 
     ferm::service { 'http':
         proto   => 'tcp',
