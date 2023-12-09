6a581ccac08871800d76550799c686be0b2ac8ce
paladox <paladox@users.noreply.github.com>
2023-12-09 16:10:13 -0500

    mcrouter: install missing packages on bookworm

--- modules/mcrouter/manifests/init.pp
+++ modules/mcrouter/manifests/init.pp
@@ -105,13 +105,16 @@ class mcrouter(
             require     => File['/opt/mcrouter_2022.01.31.00-1_amd64.deb'],
         }
     } else {
-        stdlib::ensure_packages(['libboost-program-options1.74.0', 'libboost-regex1.74.0-icu72', 'libfmt9', 'libgflags2.2', 'libgoogle-glog0v6'])
+        stdlib::ensure_packages(['libboost-context1.74.0', 'libboost-filesystem1.74.0', 'libboost-program-options1.74.0', 'libjemalloc2', 'libboost-regex1.74.0-icu72', 'libfmt9', 'libgflags2.2', 'libgoogle-glog0v6'])
 
         file { '/opt/mcrouter_2023.07.17.00-1_amd64.deb':
             ensure  => present,
             source => 'puppet:///private/mcrouter/mcrouter_2023.07.17.00-1_amd64.deb',
             require => [
+                Package['libboost-context1.74.0'],
+                Package['libboost-filesystem1.74.0'],
                 Package['libboost-program-options1.74.0'],
+                Package['libjemalloc2'],
                 Package['libboost-regex1.74.0-icu72'],
                 Package['libfmt9'],
                 Package['libgflags2.2'],
