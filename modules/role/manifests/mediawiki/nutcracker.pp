0d9fd7c9ca1fedb298ad0136ba563073d8602549
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-01-02 16:25:54 -0700

    Fix spacing

--- modules/role/manifests/mediawiki/nutcracker.pp
+++ modules/role/manifests/mediawiki/nutcracker.pp
@@ -27,37 +27,37 @@ class role::mediawiki::nutcracker (
         },
     }
 
-	# Ship a tmpfiles.d configuration to create /run/nutcracker
-	systemd::tmpfile { 'nutcracker':
-		content => 'd /run/nutcracker 0755 nutcracker nutcracker - -'
-	}
+    # Ship a tmpfiles.d configuration to create /run/nutcracker
+    systemd::tmpfile { 'nutcracker':
+        content => 'd /run/nutcracker 0755 nutcracker nutcracker - -'
+    }
 
-	class { '::nutcracker':
-		mbuf_size => '64k',
-		pools     => $nutcracker_pools,
-	}
+    class { '::nutcracker':
+        mbuf_size => '64k',
+        pools     => $nutcracker_pools,
+    }
 
-	systemd::unit { 'nutcracker':
-		content  => "[Service]\nCPUAccounting=yes\nRestart=always\nNice=-19\n",
-		override => true,
-	}
+    systemd::unit { 'nutcracker':
+        content  => "[Service]\nCPUAccounting=yes\nRestart=always\nNice=-19\n",
+        override => true,
+    }
 
-	monitoring::nrpe { 'nutcracker process':
-		command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u nutcracker -C nutcracker',
-		docs    => 'https://meta.miraheze.org/wiki/Tech:Icinga/MediaWiki_Monitoring#Nutcracker'
-	}
+    monitoring::nrpe { 'nutcracker process':
+        command => '/usr/lib/nagios/plugins/check_procs -c 1:1 -u nutcracker -C nutcracker',
+        docs    => 'https://meta.miraheze.org/wiki/Tech:Icinga/MediaWiki_Monitoring#Nutcracker'
+    }
 
-	ferm::rule { 'skip_nutcracker_conntrack_out':
-		desc  => 'Skip outgoing connection tracking for Nutcracker',
-		table => 'raw',
-		chain => 'OUTPUT',
-		rule  => 'proto tcp sport 11212 NOTRACK;',
-	}
+    ferm::rule { 'skip_nutcracker_conntrack_out':
+        desc  => 'Skip outgoing connection tracking for Nutcracker',
+        table => 'raw',
+        chain => 'OUTPUT',
+        rule  => 'proto tcp sport 11212 NOTRACK;',
+    }
 
-	ferm::rule { 'skip_nutcracker_conntrack_in':
-		desc  => 'Skip incoming connection tracking for Nutcracker',
-		table => 'raw',
-		chain => 'PREROUTING',
-		rule  => 'proto tcp dport 11212 NOTRACK;',
-	}
+    ferm::rule { 'skip_nutcracker_conntrack_in':
+        desc  => 'Skip incoming connection tracking for Nutcracker',
+        table => 'raw',
+        chain => 'PREROUTING',
+        rule  => 'proto tcp dport 11212 NOTRACK;',
+    }
 }
