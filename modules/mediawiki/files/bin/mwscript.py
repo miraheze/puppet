ca048a01da4eb79e0bba50ac2362e021b6cf1ce7
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-01-02 20:04:18 -0700

    mwscript: support multiversion

--- modules/mediawiki/files/bin/mwscript.py
+++ modules/mediawiki/files/bin/mwscript.py
@@ -4,31 +4,40 @@ from __future__ import annotations
 
 import argparse
 import os
+import json
 import sys
-from typing import TYPE_CHECKING, TypedDict
-if TYPE_CHECKING:
-    from typing import Optional, Union
+from typing import TypedDict
 
 
 class CommandInfo(TypedDict):
     command: str
-    generate: Optional[str]
+    generate: str | None
     long: bool
     nolog: bool
     confirm: bool
 
 
-def syscheck(result: Union[CommandInfo, int]) -> CommandInfo:
+def syscheck(result: CommandInfo | int) -> CommandInfo:
     if isinstance(result, int):
         sys.exit(result)
     return result
 
 
-def get_commands(args: argparse.Namespace) -> Union[CommandInfo, int]:
-    validDBLists = ('active', 'beta')
+def get_commands(args: argparse.Namespace) -> CommandInfo | int:
+    mw_versions = os.popen('getMWVersions all').read().strip()
+    versions = {}
+    if mw_versions:
+        versions = json.loads(mw_versions)
+
+    del mw_versions
+
+    versionLists = tuple([f'{key}-wikis' for key in versions.keys()])
+    validDBLists = ('active',) + versionLists
+
     longscripts = ('compressold', 'deletebatch', 'importdump', 'importimages', 'nukens', 'rebuildall', 'rebuildimages', 'refreshlinks', 'runjobs', 'purgelist', 'cargorecreatedata')
     long = False
     generate = None
+
     try:
         if args.extension:
             wiki = ''
@@ -43,12 +52,21 @@ def get_commands(args: argparse.Namespace) -> Union[CommandInfo, int]:
     except IndexError:
         print('Not enough Arguments given.')
         return 2
+
+    if not args.version:
+        dbname = wiki
+        if not dbname:
+            dbname = 'default'
+        args.version = os.popen(f'getMWVersion {dbname}').read().strip()
+        if wiki and wiki in versionLists:
+            args.version = versions.get(wiki[:-6])
+
     script = args.script
-    if not script.endswith('.php') and args.norunphp:
-        print('Error: You can not use a class and specify --no-runner')
+    if not script.endswith('.php') and float(args.version) < 1.40:
+        print('Error: Use MediaWiki version 1.40 or greater (e.g. --version=1.40) to use a class for MaintenanceRunner')
         return 2
-    if not args.norunphp:
-        runner = '/srv/mediawiki/w/maintenance/run.php '
+    if float(args.version) >= 1.40:
+        runner = f'/srv/mediawiki/{args.version}/maintenance/run.php '
     else:
         runner = ''
     if script.endswith('.php'):  # assume class if not
@@ -56,13 +74,13 @@ def get_commands(args: argparse.Namespace) -> Union[CommandInfo, int]:
         if script.split('.')[0].lower() in longscripts:
             long = True
         if len(scriptsplit) == 1:
-            script = f'{runner}/srv/mediawiki/w/maintenance/{script}'
+            script = f'{runner}/srv/mediawiki/{args.version}/maintenance/{script}'
         elif len(scriptsplit) == 2:
-            script = f'{runner}/srv/mediawiki/w/maintenance/{scriptsplit[0]}/{scriptsplit[1]}'
+            script = f'{runner}/srv/mediawiki/{args.version}/maintenance/{scriptsplit[0]}/{scriptsplit[1]}'
             if scriptsplit[1].split('.')[0].lower() in longscripts:
                 long = True
         else:
-            script = f'{runner}/srv/mediawiki/w/{scriptsplit[0]}/{scriptsplit[1]}/maintenance/{scriptsplit[2]}'
+            script = f'{runner}/srv/mediawiki/{args.version}/{scriptsplit[0]}/{scriptsplit[1]}/maintenance/{scriptsplit[2]}'
             if scriptsplit[2].split('.')[0].lower() in longscripts:
                 long = True
     else:
@@ -73,12 +91,12 @@ def get_commands(args: argparse.Namespace) -> Union[CommandInfo, int]:
     if wiki == 'all':
         long = True
         command = f'sudo -u www-data /usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.json {script}'
-    elif wiki in validDBLists:
+    elif wiki and wiki in validDBLists:
         long = True
         command = f'sudo -u www-data /usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/{wiki}.json {script}'
     elif args.extension:
         long = True
-        generate = f'php {runner}/srv/mediawiki/w/extensions/MirahezeMagic/maintenance/generateExtensionDatabaseList.php --wiki=loginwiki --extension={args.extension}'
+        generate = f'php {runner}/srv/mediawiki/{args.version}/extensions/MirahezeMagic/maintenance/generateExtensionDatabaseList.php --wiki=loginwiki --extension={args.extension}'
         command = f'sudo -u www-data /usr/local/bin/foreachwikiindblist /home/{os.getlogin()}/{args.extension}.json {script}'
     else:
         command = f'sudo -u www-data php {script} --wiki={wiki}'
@@ -112,10 +130,10 @@ def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Run a MediaWiki Script')
     parser.add_argument('script')
     parser.add_argument('arguments', nargs='*', default=[])
+    parser.add_argument('--version', dest='version')
     parser.add_argument('--extension', '--skin', dest='extension')
     parser.add_argument('--no-log', dest='nolog', action='store_true')
     parser.add_argument('--confirm', '--yes', '-y', dest='confirm', action='store_true')
-    parser.add_argument('--no-use-runner', dest='norunphp', action='store_true')
 
     args = parser.parse_known_args()[0]
     args.arguments += parser.parse_known_args()[1]
@@ -123,5 +141,4 @@ def get_args() -> argparse.Namespace:
 
 
 if __name__ == '__main__':
-
     run(syscheck(get_commands(get_args())))
