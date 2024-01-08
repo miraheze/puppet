f3a7279708d44b1dfd3a0864aa6911a25a7bf46e
paladox <paladox@users.noreply.github.com>
2024-01-08 19:44:35 +0000

    salt: add support for Debian 12 (#3595)
    Co-authored-by: github-actions <github-actions@users.noreply.github.com>

--- modules/salt/manifests/init.pp
+++ modules/salt/manifests/init.pp
@@ -1,6 +1,39 @@
 class salt {
-    package { ['salt-ssh', 'salt-common']:
-        ensure  => present,
+    if $facts['os']['distro']['codename'] == 'bookworm' {
+        file { '/etc/apt/trusted.gpg.d/salt.gpg':
+            ensure => present,
+            source => 'puppet:///modules/salt/key/salt.gpg',
+        }
+
+        apt::source { 'salt_apt':
+            location => "https://repo.saltproject.io/salt/py3/debian/${$facts['os']['distro']['release']['major']}/amd64/latest",
+            release  => $facts['os']['distro']['codename'],
+            repos    => 'main',
+            require  => File['/etc/apt/trusted.gpg.d/salt.gpg'],
+            notify   => Exec['apt_update_salt'],
+        }
+
+        apt::pin { 'proxmox_pin':
+            priority => 600,
+            origin   => 'repo.saltproject.io'
+        }
+
+        # First installs can trip without this
+        exec {'apt_update_salt':
+            command     => '/usr/bin/apt-get update',
+            refreshonly => true,
+            logoutput   => true,
+            require     => Apt::Pin['salt_pin'],
+        }
+
+        package { ['salt-ssh', 'salt-common']:
+            ensure  => present,
+            require => Apt::Source['salt_apt']
+        }
+    } else {
+        package { ['salt-ssh', 'salt-common']:
+            ensure  => present,
+        }
     }
 
     $host = query_nodes("networking.domain='${facts['networking']['domain']}'", 'networking.fqdn')
