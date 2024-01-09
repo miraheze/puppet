aff2ef16719645f9a94eac02a22db30e1d4a5029
paladox <paladox@users.noreply.github.com>
2024-01-09 01:24:10 +0000

    Update memcached.pp

--- modules/prometheus/manifests/exporter/memcached.pp
+++ modules/prometheus/manifests/exporter/memcached.pp
@@ -35,7 +35,7 @@ class prometheus::exporter::memcached (
     }
 
     $firewall_rules_str = join(
-        query_facts("networking.domain='${facts['networking']['domain']}' and Class[Role::Prometheus]", ['networking'])
+        query_facts("Class[Role::Prometheus]", ['networking'])
         .map |$key, $value| {
             "${value['networking']['ip']} ${value['networking']['ip6']}"
         }
