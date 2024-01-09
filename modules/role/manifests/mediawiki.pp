8011896f38a94c584ed0adb7c83e38a413043dd2
paladox <paladox@users.noreply.github.com>
2024-01-09 16:43:08 +0000

    Temporarily don't install cadvisor
    There's no package on bookworm (won't install)

--- modules/role/manifests/mediawiki.pp
+++ modules/role/manifests/mediawiki.pp
@@ -3,7 +3,8 @@ class role::mediawiki (
     Boolean $strict_firewall = lookup('role::mediawiki::use_strict_firewall', {'default_value' => false}),
     Boolean $use_mcrouter = lookup('role::mediawiki::use_mcrouter', {'default_value' => false})
 ) {
-    include prometheus::exporter::cadvisor
+    # doesn't install on bookworm
+    # include prometheus::exporter::cadvisor
 
     if $use_mcrouter {
         include role::mediawiki::mcrouter
