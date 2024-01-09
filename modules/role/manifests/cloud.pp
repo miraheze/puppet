483eb531000571ab6f897d130d13e23e31e71ec3
paladox <paladox@users.noreply.github.com>
2024-01-09 00:22:08 +0000

    cloud: fix firewall

--- modules/role/manifests/cloud.pp
+++ modules/role/manifests/cloud.pp
@@ -5,7 +5,7 @@ class role::cloud {
     class { '::cpufrequtils': }
 
     $firewall_rules_str = join(
-        query_facts("networking.domain='${facts['networking']['domain']}' and Class[Role::Cloud]", ['networking'])
+        query_facts("Class[Role::Cloud]", ['networking'])
         .map |$key, $value| {
             "${value['networking']['ip']} ${value['networking']['ip6']}"
         }
