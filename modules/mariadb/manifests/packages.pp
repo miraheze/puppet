34b66622bd045a065ecf5129eeff0f30d1b3c2df
paladox <paladox@users.noreply.github.com>
2024-01-09 16:21:30 +0000

    mariadb: only use new apt repo on Debian 12

--- modules/mariadb/manifests/packages.pp
+++ modules/mariadb/manifests/packages.pp
@@ -11,16 +11,30 @@ class mariadb::packages(
     }
 
     $http_proxy = lookup('http_proxy', {'default_value' => undef})
-    apt::source { 'mariadb_apt':
-        comment  => 'MariaDB stable',
-        location => "http://ams2.mirrors.digitalocean.com/mariadb/repo/${version}/debian",
-        release  => $facts['os']['distro']['codename'],
-        repos    => 'main',
-        key      => {
-            'name'   => 'mariadb_release_signing_key.pgp',
-            'source' => 'https://mariadb.org/mariadb_release_signing_key.pgp',
-            'options' => "http-proxy='${http_proxy}'",
-        },
+    if $facts['os']['distro']['codename'] == 'bookworm' {
+        apt::source { 'mariadb_apt':
+            comment  => 'MariaDB stable',
+            location => "http://ams2.mirrors.digitalocean.com/mariadb/repo/${version}/debian",
+            release  => $facts['os']['distro']['codename'],
+            repos    => 'main',
+            key      => {
+                'name'   => 'mariadb_release_signing_key.pgp',
+                'source' => 'https://mariadb.org/mariadb_release_signing_key.pgp',
+                'options' => "http-proxy='${http_proxy}'",
+            },
+        }
+    } else {
+        apt::source { 'mariadb_apt':
+            comment  => 'MariaDB stable',
+            location => "http://ams2.mirrors.digitalocean.com/mariadb/repo/${version}/debian",
+            release  => $facts['os']['distro']['codename'],
+            repos    => 'main',
+            key      => {
+                'id'      => '177F4010FE56CA3336300305F1656F24C74CD1D8',
+                'options' => "http-proxy='${http_proxy}'",
+                'server'  => 'hkp://keyserver.ubuntu.com:80',
+            },
+        }
     }
 
     apt::pin { 'mariadb_pin':
