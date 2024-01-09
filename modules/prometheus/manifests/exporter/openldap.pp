176984edafd907c3f2d33d26a297edfab859c66c
paladox <paladox@users.noreply.github.com>
2024-01-09 01:25:30 +0000

    prometheus: fix firewall (#3598)

--- modules/prometheus/manifests/exporter/openldap.pp
+++ modules/prometheus/manifests/exporter/openldap.pp
@@ -29,7 +29,7 @@ class prometheus::exporter::openldap {
     }
 
     $firewall_rules_str = join(
-        query_facts("networking.domain='${facts['networking']['domain']}' and Class[Role::Prometheus]", ['networking'])
+        query_facts("Class[Role::Prometheus]", ['networking'])
         .map |$key, $value| {
             "${value['networking']['ip']} ${value['networking']['ip6']}"
         }
