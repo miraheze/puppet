3020823de1beda0d0ddc2f5cd2c504b91b57459d
paladox <paladox@users.noreply.github.com>
2024-01-09 22:01:48 +0000

    Fix

--- manifests/site.pp
+++ manifests/site.pp
@@ -80,7 +80,7 @@ node 'mon141.miraheze.org' {
     include role::irc
 }
 
-node 'mon181.miraheze.org' {
+node 'mon181.wikitide.net' {
     include base
     include role::grafana
     include role::icinga2
