40558e02c3c1f99cd7db46118338bcbe6c63c623
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-01-11 19:08:50 -0700

    Remove unused grafana_db_host

--- modules/grafana/manifests/init.pp
+++ modules/grafana/manifests/init.pp
@@ -3,7 +3,6 @@ class grafana (
     String $grafana_password = lookup('passwords::db::grafana'),
     String $mail_password = lookup('passwords::mail::noreply'),
     String $ldap_password = lookup('passwords::ldap_password'),
-    String $grafana_db_host = lookup('grafana_db_host', {'default_value' => 'db112.miraheze.org'}),
 ) {
 
     include ::apt
