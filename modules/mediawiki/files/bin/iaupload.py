e078fa2840343ff42bee70ba1bc9356e31e3cf7c
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-01-14 10:16:37 -0700

    Rewrite iaupload and add tests

--- modules/mediawiki/files/bin/iaupload.py
+++ modules/mediawiki/files/bin/iaupload.py
@@ -1,9 +1,13 @@
 #! /usr/bin/python3
 
+# Upload files to archive.org directly
+# from the command line.
+#
+# Universal Omega 2022
+
 import argparse
 import internetarchive
 import os
-
 from datetime import datetime
 
 # set HTTP proxy to use for getting the item from archive.org
@@ -12,52 +16,61 @@ from datetime import datetime
 os.environ['HTTP_PROXY'] = 'http://bast.miraheze.org:8080'
 os.environ['HTTPS_PROXY'] = 'http://bast.miraheze.org:8080'
 
-# add arguments
-parser = argparse.ArgumentParser(
-    description='Uploads a file to archive.org.')
-parser.add_argument(
-    '--title', dest='title', required=True,
-    help='The title of the file to be used on archive.org. Will be both the title and identifier. Required.')
-parser.add_argument(
-    '--description', dest='description', default='',
-    help='The description of the file to be used on archive.org. Optional. Default: empty')
-parser.add_argument(
-    '--mediatype', dest='mediatype', default='web',
-    help='The media type of the file to be used on archive.org. Optional. Default: web')
-parser.add_argument(
-    '--subject', dest='subject', default='miraheze;wikiteam',
-    help='Subject (topics) of the file for archive.org. Multiple topics can be separated by a semicolon. Optional. Default: miraheze;wikiteam')
-parser.add_argument(
-    '--collection', dest='collection', default='opensource',
-    help='The name of the collection to use on archive.org. Optional. Default: opensource')
-parser.add_argument(
-    '--file', dest='file', required=True,
-    help='The local path to the file to be uploaded to archive.org. Required.')
-args = parser.parse_args()
 
-item = internetarchive.get_item(args.title)
+class ArchiveUploader:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(
+            description='Uploads a file to archive.org.')
+        self.parser.add_argument(
+            '--title', dest='title', required=True,
+            help='The title of the file to be used on archive.org. Will be both the title and identifier. Required.')
+        self.parser.add_argument(
+            '--description', dest='description', default='',
+            help='The description of the file to be used on archive.org. Optional. Default: empty')
+        self.parser.add_argument(
+            '--mediatype', dest='mediatype', default='web',
+            help='The media type of the file to be used on archive.org. Optional. Default: web')
+        self.parser.add_argument(
+            '--subject', dest='subject', default='miraheze;wikiteam',
+            help='Subject (topics) of the file for archive.org. Multiple topics can be separated by a semicolon. Optional. Default: miraheze;wikiteam')
+        self.parser.add_argument(
+            '--collection', dest='collection', default='opensource',
+            help='The name of the collection to use on archive.org. Optional. Default: opensource')
+        self.parser.add_argument(
+            '--file', dest='file', required=True,
+            help='The local path to the file to be uploaded to archive.org. Required.')
 
-# set session proxy for uploading
-item.session.proxies = {
-    'http': 'http://bast.miraheze.org:8080',
-    'https': 'http://bast.miraheze.org:8080',
-}
+    def upload(self):
+        args = self.parser.parse_args()
 
-# get last modification time from file to use as the publication date in archive.org
-mtime = os.path.getmtime(args.file)
-dt = datetime.fromtimestamp(mtime)
-date = datetime.strftime(dt, '%Y-%m-%d')
+        item = internetarchive.get_item(args.title)
 
-# set metadata
-# see https://archive.org/developers/metadata-schema for valid options
-md = {
-    'collection': args.collection,
-    'date': date,
-    'description': args.description,
-    'mediatype': args.mediatype,
-    'subject': args.subject,
-    'title': args.title,
-}
+        # set session proxy for uploading
+        item.session.proxies = {
+            'http': 'http://bast.miraheze.org:8080',
+            'https': 'http://bast.miraheze.org:8080',
+        }
 
-# actually upload the file
-item.upload(args.file, metadata=md, verbose=True, queue_derive=False)
+        # get last modification time from file to use as the publication date in archive.org
+        mtime = os.path.getmtime(args.file)
+        dt = datetime.fromtimestamp(mtime)
+        date = datetime.strftime(dt, '%Y-%m-%d')
+
+        # set metadata
+        # see https://archive.org/developers/metadata-schema for valid options
+        md = {
+            'collection': args.collection,
+            'date': date,
+            'description': args.description,
+            'mediatype': args.mediatype,
+            'subject': args.subject,
+            'title': args.title,
+        }
+
+        # actually upload the file
+        item.upload(args.file, metadata=md, verbose=True, queue_derive=False)
+
+
+if __name__ == '__main__':
+    uploader = ArchiveUploader()
+    uploader.upload()
