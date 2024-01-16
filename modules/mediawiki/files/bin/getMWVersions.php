ed9621494990b5cb7b870daed639b0b8f5f15d51
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-01-16 14:55:53 -0700

    Use staging for versions
    If not deployed the first time, config won't exist

--- modules/mediawiki/files/bin/getMWVersions.php
+++ modules/mediawiki/files/bin/getMWVersions.php
@@ -3,7 +3,7 @@
 
 error_reporting( 0 );
 
-require_once '/srv/mediawiki/config/initialise/MirahezeFunctions.php';
+require_once '/srv/mediawiki-staging/config/initialise/MirahezeFunctions.php';
 
 if ( ( $argv[1] ?? false ) === 'all' ) {
 	echo json_encode( MirahezeFunctions::MEDIAWIKI_VERSIONS );
