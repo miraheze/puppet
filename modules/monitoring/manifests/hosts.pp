513bda5819b15119600efe431e9efb9a60979298
paladox <paladox@users.noreply.github.com>
2024-01-24 02:22:29 +0000

    monitoring: fix ip

--- modules/monitoring/manifests/hosts.pp
+++ modules/monitoring/manifests/hosts.pp
@@ -2,6 +2,14 @@ define monitoring::hosts (
     $ensure      = present,
     $contacts    = lookup('contactgroups', {'default_value' => [ 'sre' ]}),
 ) {
+    if ( $facts['networking']['interfaces']['ens19'] and $facts['networking']['interfaces']['ens18'] ) {
+        $address6 = $facts['networking']['interfaces']['ens18']['ip6']
+    } elsif ( $facts['networking']['interfaces']['ens18'] ) {
+        $address6 = $facts['networking']['interfaces']['ens18']['ip6']
+    } else {
+        $address6 = $facts['networking']['ip6']
+    }
+
     @@icinga2::object::host { $title:
         ensure   => $ensure,
         import   => ['generic-host'],
