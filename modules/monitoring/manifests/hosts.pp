6ebf20582966f932eaed2be741d60ed208ea9d55
paladox <paladox@users.noreply.github.com>
2024-01-24 02:27:05 +0000

    Update hosts.pp

--- modules/monitoring/manifests/hosts.pp
+++ modules/monitoring/manifests/hosts.pp
@@ -3,17 +3,17 @@ define monitoring::hosts (
     $contacts    = lookup('contactgroups', {'default_value' => [ 'sre' ]}),
 ) {
     if ( $facts['networking']['interfaces']['ens19'] and $facts['networking']['interfaces']['ens18'] ) {
-        $address6 = $facts['networking']['interfaces']['ens19']['ip']
+        $address = $facts['networking']['interfaces']['ens19']['ip']
     } elsif ( $facts['networking']['interfaces']['ens18'] ) {
-        $address6 = $facts['networking']['interfaces']['ens18']['ip6']
+        $address = $facts['networking']['interfaces']['ens18']['ip6']
     } else {
-        $address6 = $facts['networking']['ip6']
+        $address = $facts['networking']['ip6']
     }
 
     @@icinga2::object::host { $title:
         ensure   => $ensure,
         import   => ['generic-host'],
-        address6 => $address6,
+        address6 => $address,
         target   => '/etc/icinga2/conf.d/puppet_hosts.conf',
         vars     => {
             notification => {
