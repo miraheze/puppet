91d93b05dafdc371c98549a2b5fab59766beae6a
paladox <paladox@users.noreply.github.com>
2024-01-24 14:02:06 +0000

    PostgreSQL: support private network in firewall

--- modules/role/manifests/postgresql.pp
+++ modules/role/manifests/postgresql.pp
@@ -9,7 +9,13 @@ class role::postgresql {
     $firewall_rules_str = join(
         query_facts('Class[Role::Puppetserver]', ['networking'])
         .map |$key, $value| {
-            "${value['networking']['ip']} ${value['networking']['ip6']}"
+            if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } elsif ( $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } else {
+                "${value['networking']['ip']} ${value['networking']['ip6']}"
+            }
         }
         .flatten()
         .unique()
