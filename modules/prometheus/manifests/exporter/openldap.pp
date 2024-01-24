b22d733baab8b53e66f98df2c6fce9c232fd9b3d
paladox <paladox@users.noreply.github.com>
2024-01-23 21:19:11 -0500

    prometheus: add support for private network in firewall (#3672)

--- modules/prometheus/manifests/exporter/openldap.pp
+++ modules/prometheus/manifests/exporter/openldap.pp
@@ -31,7 +31,13 @@ class prometheus::exporter::openldap {
     $firewall_rules_str = join(
         query_facts('Class[Role::Prometheus]', ['networking'])
         .map |$key, $value| {
-            "${value['networking']['ip']} ${value['networking']['ip6']}"
+            if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } elsif ( $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } else {
+                "${value['networking']['ip']} ${value['networking']['ip6']}"
+            }
         }
         .flatten()
         .unique()
