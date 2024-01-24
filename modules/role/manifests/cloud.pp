a0ab2038ab709093ee0aa0ac13bc7e1eb8cfaf45
paladox <paladox@users.noreply.github.com>
2024-01-24 13:58:31 +0000

    cloud: support private network in firewall

--- modules/role/manifests/cloud.pp
+++ modules/role/manifests/cloud.pp
@@ -7,7 +7,13 @@ class role::cloud {
     $firewall_rules_str = join(
         query_facts('Class[Role::Cloud]', ['networking'])
         .map |$key, $value| {
-            "${value['networking']['ip']} ${value['networking']['ip6']}"
+            if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } elsif ( $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } else {
+                "${value['networking']['ip']} ${value['networking']['ip6']}"
+            }
         }
         .flatten()
         .unique()
