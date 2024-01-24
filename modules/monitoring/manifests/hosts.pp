929ba5942710c4639161344089dd70d99c09ee67
paladox <paladox@users.noreply.github.com>
2024-01-23 21:27:11 -0500

    monitoring: fix ip (#3673)

--- modules/monitoring/manifests/hosts.pp
+++ modules/monitoring/manifests/hosts.pp
@@ -2,10 +2,18 @@ define monitoring::hosts (
     $ensure      = present,
     $contacts    = lookup('contactgroups', {'default_value' => [ 'sre' ]}),
 ) {
+    if ( $facts['networking']['interfaces']['ens19'] and $facts['networking']['interfaces']['ens18'] ) {
+        $address = $facts['networking']['interfaces']['ens19']['ip']
+    } elsif ( $facts['networking']['interfaces']['ens18'] ) {
+        $address = $facts['networking']['interfaces']['ens18']['ip6']
+    } else {
+        $address = $facts['networking']['ip6']
+    }
+
     @@icinga2::object::host { $title:
         ensure   => $ensure,
         import   => ['generic-host'],
-        address6 => $facts['networking']['ip6'],
+        address6 => $address,
         target   => '/etc/icinga2/conf.d/puppet_hosts.conf',
         vars     => {
             notification => {
