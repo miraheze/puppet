a3266d0f3e52156157a4aa5f91bf0c949637e0a0
paladox <paladox@users.noreply.github.com>
2024-01-24 14:25:32 +0000

    mediawiki::monitoring: support private network

--- modules/mediawiki/manifests/monitoring.pp
+++ modules/mediawiki/manifests/monitoring.pp
@@ -1,11 +1,18 @@
 # === Class mediawiki::monitoring
 class mediawiki::monitoring {
+    if ( $facts['networking']['interfaces']['ens19'] and $facts['networking']['interfaces']['ens18'] ) {
+        $address = $facts['networking']['interfaces']['ens19']['ip']
+    } elsif ( $facts['networking']['interfaces']['ens18'] ) {
+        $address = $facts['networking']['interfaces']['ens18']['ip6']
+    } else {
+        $address = $facts['networking']['ip6']
+    }
     monitoring::services { 'MediaWiki Rendering':
         check_command => 'check_mediawiki',
         docs          => 'https://meta.miraheze.org/wiki/Tech:Icinga/MediaWiki_Monitoring#MediaWiki_Rendering',
         vars          => {
             host    => lookup('mediawiki::monitoring::host'),
-            address => $facts['networking']['ip6'],
+            address => $address,
         },
     }
 }
