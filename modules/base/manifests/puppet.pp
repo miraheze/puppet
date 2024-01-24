752376fe490a95eb44a783318a5251c57324fc63
paladox <paladox@users.noreply.github.com>
2024-01-24 22:20:53 +0000

    base::puppet: support using bookworm dust

--- modules/base/manifests/puppet.pp
+++ modules/base/manifests/puppet.pp
@@ -13,9 +13,6 @@ class base::puppet (
 
     apt::source { 'puppetlabs':
         location => 'http://apt.puppetlabs.com',
-        # TODO: Once a bookworm repo is available,
-        # remove this hack.
-        release  => 'bullseye',
         repos    => "puppet${puppet_major_version}",
         require  => File['/etc/apt/trusted.gpg.d/puppetlabs.gpg'],
         notify   => Exec['apt_update_puppetlabs'],
