1063cf6ab2779d5c6f25e6a3078763c864aafebb
paladox <paladox@users.noreply.github.com>
2024-01-25 22:02:18 +0000

    reports: upgrade to v12

--- modules/reports/manifests/init.pp
+++ modules/reports/manifests/init.pp
@@ -5,7 +5,7 @@ class reports {
     git::clone { 'TSPortal':
         directory => '/srv/TSPortal',
         origin    => 'https://github.com/miraheze/TSPortal',
-        branch    => 'v11',
+        branch    => 'v12',
         owner     => 'www-data',
         group     => 'www-data',
     }
