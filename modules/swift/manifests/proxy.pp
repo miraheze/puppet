342d4dcd9f9899c030f028ec3362aecc5f781a38
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-01-26 11:57:12 -0700

    swift: use swiftproxy171

--- modules/swift/manifests/proxy.pp
+++ modules/swift/manifests/proxy.pp
@@ -4,7 +4,7 @@ class swift::proxy (
     Integer              $num_workers   = lookup('swift::proxy::num_workers', {'default_value' => $facts['processors']['count']}),
     Hash                 $accounts      = lookup('swift::accounts'),
     Hash                 $accounts_keys = lookup('swift::accounts_keys'),
-    String               $swift_main_memcached = lookup('swift::proxy::swift_main_memcached', {'default_value' => '[2a10:6740::6:201]'}),
+    String               $swift_main_memcached = lookup('swift::proxy::swift_main_memcached', {'default_value' => '[2602:294:0:b23::109]'}),
 ) {
 
     stdlib::ensure_packages(['swift-proxy'])
