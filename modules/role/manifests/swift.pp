b0a6bd953e95e02c26279b66dd5916dca55c826b
paladox <paladox@users.noreply.github.com>
2024-01-26 23:42:28 +0000

    swift: allow varnish to access

--- modules/role/manifests/swift.pp
+++ modules/role/manifests/swift.pp
@@ -7,7 +7,7 @@ class role::swift (
     include ::swift::ring
 
     $firewall_rules_str = join(
-        query_facts('Class[Role::Swift] or Class[Role::Mediawiki] or Class[Role::Icinga2] or Class[Role::Prometheus] or Class[Role::Bastion]', ['networking'])
+        query_facts('Class[Role::Swift] or Class[Role::Mediawiki] or Class[Role::Icinga2] or Class[Role::Prometheus] or Class[Role::Bastion] or Class[Role::Varnish]', ['networking'])
         .map |$key, $value| {
             if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
                 "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
