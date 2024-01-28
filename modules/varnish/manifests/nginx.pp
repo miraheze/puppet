f8427b5a269b640d13839caa3bdba591fc65463e
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-01-28 16:44:24 -0700

    Update nginx.pp

--- modules/varnish/manifests/nginx.pp
+++ modules/varnish/manifests/nginx.pp
@@ -1,5 +1,24 @@
 # class to handle Varnish nginx (using hiera coolness)
 class varnish::nginx {
+    if ( $facts['networking']['interfaces']['ens19'] and $facts['networking']['interfaces']['ens18'] ) {
+        $address = $facts['networking']['interfaces']['ens19']['ip']
+    } elsif ( $facts['networking']['interfaces']['ens18'] ) {
+        $address = $facts['networking']['interfaces']['ens18']['ip6']
+    } else {
+        $address = $facts['networking']['ip6']
+    }
+
+    monitoring::services { 'HTTPS':
+        ensure        => present,
+        check_command => 'check_curl',
+        vars          => {
+            address6         => $address,
+            http_vhost       => $facts['networking']['fqdn'],
+            http_ssl         => true,
+            http_ignore_body => true,
+            http_expect      => 'HTTP/2 404',
+        },
+    }
 
     file { '/etc/nginx/sites-enabled/default':
         ensure => absent,
