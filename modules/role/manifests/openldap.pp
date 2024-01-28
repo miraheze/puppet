8602536c76293fe1c9fcb16f5216d2cb31fe0d56
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-01-28 16:08:00 -0700

    Fix ldap monitoring (#3722)

--- modules/role/manifests/openldap.pp
+++ modules/role/manifests/openldap.pp
@@ -195,6 +195,27 @@ class role::openldap (
         port   => '636',
         srange => "(${firewall_rules})",
     }
+    $firewall_rules_icinga = join(
+        query_facts('Class[Role::Icinga2]', ['networking'])
+        .map |$key, $value| {
+            if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } elsif ( $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } else {
+                "${value['networking']['ip']} ${value['networking']['ip6']}"
+            }
+        }
+        .flatten()
+        .unique()
+        .sort(),
+        ' '
+    )
+    ferm::service { 'ldap':
+        proto  => 'tcp',
+        port   => '389',
+        srange => "(${firewall_rules_icinga})",
+    }
 
     # restart slapd if it uses more than 50% of memory
     cron { 'restart_slapd':
