d934c0b0d935e4e67221a2254bad21081a2be169
paladox <paladox@users.noreply.github.com>
2024-02-02 16:44:15 +0000

    Update bastion.pp

--- modules/role/manifests/bastion.pp
+++ modules/role/manifests/bastion.pp
@@ -42,7 +42,9 @@ class role::bastion (
         $firewall_rules_str = join(
             query_facts('Class[Role::Varnish]', ['networking'])
             .map |$key, $value| {
-                if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
+                if ( $value['networking']['he-ipv6']['ipv6'] ) {
+                    "${value['networking']['ip']} ${value['networking']['he-ipv6']['ipv6']}"
+                } elsif ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
                     "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
                 } elsif ( $value['networking']['interfaces']['ens18'] ) {
                     "${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
