6a4e360b42f9afa340cd26e4194d3d41fb5ae8e3
paladox <paladox@users.noreply.github.com>
2024-02-02 16:41:51 +0000

    Update phabricator.pp

--- modules/role/manifests/phabricator.pp
+++ modules/role/manifests/phabricator.pp
@@ -5,7 +5,9 @@ class role::phabricator {
     $firewall_rules_str = join(
         query_facts('Class[Role::Varnish] or Class[Role::Icinga2]', ['networking'])
         .map |$key, $value| {
-            if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
+            if ( $value['networking']['he-ipv6']['ipv6'] ) {
+                "${value['networking']['ip']} ${value['networking']['he-ipv6']['ipv6']}"
+            } elsif ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
                 "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
             } elsif ( $value['networking']['interfaces']['ens18'] ) {
                 "${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
