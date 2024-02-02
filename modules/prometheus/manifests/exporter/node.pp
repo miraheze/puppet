9c193a889d45537d7552f52cc59736d350cb7ddc
paladox <paladox@users.noreply.github.com>
2024-02-02 12:00:02 -0500

    Fix (#3729)

--- modules/prometheus/manifests/exporter/node.pp
+++ modules/prometheus/manifests/exporter/node.pp
@@ -91,8 +91,8 @@ class prometheus::exporter::node (
     $firewall_rules_str = join(
         query_facts('Class[Prometheus]', ['networking'])
         .map |$key, $value| {
-            if ( $value['networking']['he-ipv6']['ipv6'] ) {
-                "${value['networking']['ip']} ${value['networking']['he-ipv6']['ipv6']}"
+            if ( $value['networking']['interfaces']['he-ipv6'] ) {
+                "${value['networking']['ip']} ${value['networking']['interfaces']['he-ipv6']['ip6']}"
             } elsif ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
                 "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
             } elsif ( $value['networking']['interfaces']['ens18'] ) {
