526c270f83a0be6f33595c9e79cd1ab52e7c7044
paladox <paladox@users.noreply.github.com>
2024-02-05 15:30:24 +0000

    Create expirer.pp

---
+++ modules/swift/manifests/expirer.pp
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: Apache-2.0
+class swift::expirer (
+    $ensure,
+    $statsd_host               = undef,
+    $statsd_port               = 8125,
+    $statsd_metric_prefix      = undef,
+    $statsd_sample_rate_factor = '1',
+    $memcached_servers         = ['localhost'],
+    $memcached_port            = 11211,
+    $swift_main_memcached      = lookup('swift::proxy::swift_main_memcached', {'default_value' => '[2602:294:0:b23::109]'}),
+) {
+
+    package { 'swift-object-expirer':
+        ensure => $ensure,
+    }
+
+    file { '/etc/swift/object-expirer.conf':
+        ensure  => $ensure,
+        content => template('swift/object-expirer.conf.erb'),
+        owner   => 'swift',
+        group   => 'swift',
+        mode    => '0440',
+        require => Package['swift-object-expirer'],
+    }
+
+    service { 'swift-object-expirer':
+        ensure => stdlib::ensure($ensure, 'service'),
+    }
+}
