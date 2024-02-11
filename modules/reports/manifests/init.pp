b3e910f85324500809bcd8e7b3b5816e9f990daa
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-11 16:36:21 -0700

    reports: upgrade to v16 (#3750)

--- modules/reports/manifests/init.pp
+++ modules/reports/manifests/init.pp
@@ -5,7 +5,7 @@ class reports {
     git::clone { 'TSPortal':
         directory => '/srv/TSPortal',
         origin    => 'https://github.com/miraheze/TSPortal',
-        branch    => 'v15',
+        branch    => 'v16',
         owner     => 'www-data',
         group     => 'www-data',
     }
