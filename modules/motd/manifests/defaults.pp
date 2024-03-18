eaa31a2a9ab60bb43e323bb0926eb28e1a97a77d
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-18 14:02:13 -0600

    Add

---
+++ modules/motd/manifests/defaults.pp
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: Apache-2.0
+# == Class: motd::defaults
+#
+# This provides defaults motd scripts, a generic but cleaned up (no help text)
+# mixture of what Debian usually ship in their motd. This is needed as
+# the motd module recursively manages the update-motd.d directory.
+#
+# This is meant to be generic. DO NOT INCLUDE SITE-SPECIFIC INFORMATION HERE.
+
+class motd::defaults {
+    # print uname & distribution release
+    motd::script { 'header':
+        ensure   => present,
+        priority => 0,
+        content  => "#!/bin/sh\nuname -snrvm\nlsb_release -s -d\n\n",
+    }
+
+    # print /etc/motd.tail, useful & harmless enough
+    motd::script { 'footer':
+        ensure   => present,
+        priority => 99,
+        content  => "#!/bin/sh\n[ -f /etc/motd.tail ] && cat /etc/motd.tail || true\n",
+    }
+}
