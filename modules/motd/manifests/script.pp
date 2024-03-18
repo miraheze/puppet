2a4d9ce9f3b0dfd912725d8e5957cef479cddb66
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-18 13:59:10 -0600

    Update motd module

--- modules/motd/manifests/script.pp
+++ modules/motd/manifests/script.pp
@@ -1,16 +1,16 @@
-# class: motd::script
-define motd::script(
-    VMlib::Ensure $ensure      = present,
+# == Define: motd::script
+define motd::script (
+    VMlib::Ensure $ensure    = present,
     Integer[0, 99] $priority  = 50,
     Optional[String] $content = undef,
-    VMlib::Sourceurl $source   = undef,
+    VMlib::Sourceurl $source  = undef,
 ) {
-    include ::motd
+    include motd
 
     if $source == undef and $content == undef  { fail('you must provide either "source" or "content"') }
     if $source != undef and $content != undef  { fail('"source" and "content" are mutually exclusive') }
 
-    $safe_name = regsubst($title, '[\W_]', '-', 'G')
+    $safe_name = regsubst($title, '[\W_]', '-', 'G').downcase
     $script    = sprintf('%02d-%s', $priority, $safe_name)
 
     file { "/etc/update-motd.d/${script}":
