bdc59670a868c5973166e1037790c7e9a8bdf2dc
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-18 14:09:22 -0600

    Add

---
+++ modules/vmlib/types/ansi/colour.pp
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: Apache-2.0
+type VMlib::Ansi::Colour = Enum[
+    'black',
+    'red',
+    'green',
+    'yellow',
+    'blue',
+    'magenta',
+    'cyan',
+    'white',
+]
