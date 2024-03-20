c9e162e2622899bbdbbfda65e27f1d2f8e20cab8
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-19 20:28:16 -0600

    Add statsd to Graphite (#3780)

---
+++ modules/statsite/manifests/init.pp
@@ -0,0 +1,64 @@
+# SPDX-License-Identifier: Apache-2.0
+# == Class: statsite
+#
+# Configure statsite https://github.com/statsite/statsite
+# To add individual instances, use statsite::instance
+#
+# === Parameters
+#
+# [*port*]
+#   Port to listen for messages on over UDP.
+#
+# [*graphite_host*]
+#   Send metrics to graphite on this host
+#
+# [*graphite_port*]
+#   Send metrics to graphite on this port
+#
+# [*input_counter*]
+#   Use this metric to report self-statistics
+#
+# [*extended_counters*]
+#   Export additional metrics for counters
+
+class statsite (
+  VMlib::Ensure $ensure = present,
+) {
+    # package { 'statsite':
+    #    ensure => $ensure,
+    # }
+
+    file { '/etc/statsite':
+        ensure => stdlib::ensure($ensure, 'directory'),
+        force  => true,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0555',
+    }
+
+    # stop the default service and rely on statsite::instance to do the
+    # right thing
+    exec { 'mask_statsite':
+        command => '/bin/systemctl mask statsite.service',
+        creates => '/etc/systemd/system/statsite.service',
+        # before  => Package['statsite'],
+    }
+
+    systemd::unit { 'statsite@':
+        ensure  => $ensure,
+        restart => true,
+        content => systemd_template('statsite@')
+    }
+
+    systemd::unit { 'statsite-instances':
+        ensure  => $ensure,
+        restart => true,
+        content => systemd_template('statsite-instances')
+    }
+
+    rsyslog::conf { 'statsite':
+        ensure   => $ensure,
+        source   => 'puppet:///modules/statsite/rsyslog.conf',
+        priority => 20,
+    }
+}
---
+++ modules/statsite/manifests/instance.pp
@@ -0,0 +1,59 @@
+# SPDX-License-Identifier: Apache-2.0
+# == Define: statsite
+#
+# Configure an instance of statsite
+#
+# === Parameters
+#
+# [*port*]
+#   Port to listen for messages on over UDP.
+#
+# [*graphite_host*]
+#   Send metrics to graphite on this host
+#
+# [*graphite_port*]
+#   Send metrics to graphite on this port
+#
+# [*input_counter*]
+#   Use this metric to report self-statistics
+#
+# [*extended_counters*]
+#   Export additional metrics for counters
+
+define statsite::instance(
+    VMlib::Ensure  $ensure            = present,
+    Stdlib::Port   $port              = 8125,
+    Stdlib::Host   $graphite_host     = 'localhost',
+    Stdlib::Port   $graphite_port     = 2003,
+    String         $input_counter     = "statsd.${facts['networking']['hostname']}.received",
+    Integer        $extended_counters = 1,
+) {
+    $python = $facts['os']['distro']['codename'] ? {
+        default => 'python3',
+    }
+
+    $stream_cmd = "${python} /usr/lib/statsite/sinks/graphite.py ${graphite_host} ${graphite_port} \"\""
+
+    file { "/etc/statsite/${port}.ini":
+        ensure  => $ensure,
+        content => template('statsite/statsite.ini.erb'),
+        # require => Package['statsite'],
+        notify  => Service["statsite@${port}"],
+    }
+
+    case $ensure {
+      'absent': {
+        $service_enable = false
+      }
+      default: {
+        $service_enable = true
+      }
+    }
+
+    service { "statsite@${port}":
+        ensure   => stdlib::ensure($ensure, 'service'),
+        provider => 'systemd',
+        enable   => $service_enable,
+        require  => File['/lib/systemd/system/statsite@.service'],
+    }
+}
