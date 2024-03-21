65f91348f84a363a51e4e70c19fca2e116b4cfaa
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-20 18:50:19 -0600

    zookeeper: configure logging

--- modules/role/manifests/kafka.pp
+++ modules/role/manifests/kafka.pp
@@ -1,7 +1,18 @@
 # role: irc
 class role::kafka {
     # We need zookeeper also
-    class { 'zookeeper': }
+    class { 'zookeeper':
+        log4j_prop      => 'INFO,SYSLOG',
+        extra_appenders => {
+            'Syslog' => {
+                'class'                    => 'org.apache.log4j.net.SyslogAppender',
+                'layout'                   => 'org.apache.log4j.PatternLayout',
+                'layout.conversionPattern' => "${hostname} zookeeper[id:%X{myid}] - %-5p [%t:%C{1}@%L][%x] - %m%n",
+                'syslogHost'               => 'localhost',
+                'facility'                 => 'user',
+            },
+        },
+    }
 
     class { 'kafka':
         kafka_version => '2.4.1',
