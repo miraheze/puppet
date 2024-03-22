7bcc60dc7ea30d96804f3a72f056b090117c3801
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-21 20:05:03 -0600

    Kafka: add firewall rule for eventgate

--- modules/role/manifests/kafka.pp
+++ modules/role/manifests/kafka.pp
@@ -33,6 +33,29 @@ class role::kafka {
         }
     }
 
+    $firewall_rules_str = join(
+        query_facts('Class[Role::Eventgate]', ['networking'])
+        .map |$key, $value| {
+            if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } elsif ( $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } else {
+                "${value['networking']['ip']} ${value['networking']['ip6']}"
+            }
+        }
+        .flatten()
+        .unique()
+        .sort(),
+        ' '
+    )
+    ferm::service { 'kafka':
+        proto   => 'tcp',
+        port    => '9092',
+        srange  => "(${firewall_rules_str})",
+        notrack => true,
+    }
+
     system::role { 'kafka':
         description => 'Kafka server',
     }
