f2644a34826051f5d602a37777ce1a806e99fab7
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-21 20:24:10 -0600

    eventgate: add firewall rule for icinga

--- modules/role/manifests/eventgate.pp
+++ modules/role/manifests/eventgate.pp
@@ -2,6 +2,29 @@
 class role::eventgate {
     include eventgate
 
+    $firewall_rules_str = join(
+        query_facts('Class[Role::Icinga2]', ['networking'])
+        .map |$key, $value| {
+            if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } elsif ( $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } else {
+                "${value['networking']['ip']} ${value['networking']['ip6']}"
+            }
+        }
+        .flatten()
+        .unique()
+        .sort(),
+        ' '
+    )
+    ferm::service { 'eventgate':
+        proto   => 'tcp',
+        port    => '8192',
+        srange  => "(${firewall_rules_str})",
+        notrack => true,
+    }
+
     system::role { 'role::eventgate':
         description => 'EventGate server',
     }
