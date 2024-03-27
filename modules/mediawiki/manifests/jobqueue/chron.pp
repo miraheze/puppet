d9d1cf27e0bb7dbef9d3676cab156aca94ebf0a4
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-27 12:45:32 -0600

    mediawiki: also disable redisJobChronService if using cpjobqueue

--- modules/mediawiki/manifests/jobqueue/chron.pp
+++ modules/mediawiki/manifests/jobqueue/chron.pp
@@ -6,25 +6,41 @@ class mediawiki::jobqueue::chron (
 ) {
     include mediawiki::php
 
-    if !defined(Class['mediawiki::jobqueue::shared']) {
-        $versions.each |$version, $params| {
-            if $params['default'] {
-                class { 'mediawiki::jobqueue::shared':
-                    version => $version,
+    if lookup('mediawiki::use_cpjobqueue', {'default_value' => false}) {
+        if !defined(Class['mediawiki::jobqueue::shared']) {
+            $versions.each |$version, $params| {
+                if $params['default'] {
+                    class { 'mediawiki::jobqueue::shared':
+                        ensure  => absent,
+                        version => $version,
+                    }
                 }
             }
         }
-    }
+        systemd::service { 'jobchron':
+            ensure    => absent,
+            content   => systemd_template('jobchron'),
+        }
+    } else {
+        if !defined(Class['mediawiki::jobqueue::shared']) {
+            $versions.each |$version, $params| {
+                if $params['default'] {
+                    class { 'mediawiki::jobqueue::shared':
+                        version => $version,
+                    }
+                }
+            }
+        }
+        systemd::service { 'jobchron':
+            ensure    => present,
+            content   => systemd_template('jobchron'),
+            subscribe => File['/srv/jobrunner/jobrunner.json'],
+            restart   => true,
+        }
 
-    systemd::service { 'jobchron':
-        ensure    => present,
-        content   => systemd_template('jobchron'),
-        subscribe => File['/srv/jobrunner/jobrunner.json'],
-        restart   => true,
-    }
-
-    monitoring::nrpe { 'JobChron Service':
-        command => '/usr/lib/nagios/plugins/check_procs -a redisJobChronService -c 1:1',
-        docs    => 'https://meta.miraheze.org/wiki/Tech:Icinga/MediaWiki_Monitoring#JobChron_Service'
+        monitoring::nrpe { 'JobChron Service':
+            command => '/usr/lib/nagios/plugins/check_procs -a redisJobChronService -c 1:1',
+            docs    => 'https://meta.miraheze.org/wiki/Tech:Icinga/MediaWiki_Monitoring#JobChron_Service'
+        }
     }
 }
