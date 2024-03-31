b7881c2fd381f9606ffffd40bdae7893089b07d5
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-31 12:04:03 -0600

    Update

--- modules/role/files/opensearch/opensearch-generate-admin-certificate.sh
+++ modules/role/files/opensearch/opensearch-generate-admin-certificate.sh
@@ -2,16 +2,16 @@
 
 # HELP: to run the script (make sure to ):
 # ./generate-admin-certificate.sh <path to store files, e.g. /root)>
-#  copy contents of miraheze-ca.pem
-# To convert ca file from crt to pem you do (this script already creates the ca in pem format but useful for converting Sectigo):
+#  copy contents of wikitide-ca.pem
+# To convert ca file from crt to pem you do (this script already creates the ca in pem format but useful for converting LetsEncrypt):
 # openssl x509 -in <ca>.crt -out <ca>.pem -outform PEM
 # to convert private key from rsa to PKCS8:
 # openssl pkcs8 -topk8 -in <old_key>.crt -out <new_key>.pem -nocrypt
-
-openssl genrsa -out $1/miraheze-ca-key.pem 2048
-openssl req -new -x509 -sha256 -key $1/miraheze-ca-key.pem -subj "/C=GB/ST=Nottinghamshire/L=Worksop/O=Miraheze LTD" -out $1/miraheze-ca.pem -days 730
+CN=ADMIN_WIKITIDE,O=WikiTide Foundation,L=Washington,ST=DC,C=US
+openssl genrsa -out $1/wikitide-ca-key.pem 2048
+openssl req -new -x509 -sha256 -key $1/wikitide-ca-key.pem -subj "/C=US/ST=DC/L=Washington/O=WikiTide Foundation" -out $1/wikitide-ca.pem -days 730
 
 openssl genrsa -out $1/admin-key-temp.pem 2048
 openssl pkcs8 -inform PEM -outform PEM -in $1/admin-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out $1/admin-key.pem
-openssl req -new -key $1/admin-key.pem -subj "/C=GB/ST=Nottinghamshire/L=Worksop/O=Miraheze LTD/CN=ADMIN_MIRAHEZE" -out $1/admin.csr
-openssl x509 -req -in $1/admin.csr -CA $1/miraheze-ca.pem -CAkey $1/miraheze-ca-key.pem -CAcreateserial -sha256 -out $1/admin.pem -days 730
+openssl req -new -key $1/admin-key.pem -subj "/C=US/ST=DC/L=Washington/O=WikiTide Foundation/CN=ADMIN_WIKITIDE" -out $1/admin.csr
+openssl x509 -req -in $1/admin.csr -CA $1/wikitide-ca.pem -CAkey $1/wikitide-ca-key.pem -CAcreateserial -sha256 -out $1/admin.pem -days 730
