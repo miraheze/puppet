3dbb0e7156b8b0be1492fa58c22dd581c0f328ea
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-04-04 11:15:08 -0600

    Add apt source for libssl1.1

--- modules/changeprop/manifests/init.pp
+++ modules/changeprop/manifests/init.pp
@@ -20,6 +20,20 @@ class changeprop (
     $redis_host                     = lookup('changeprop::redis_host', {'default_value' => 'localhost'}),
     $redis_password                 = lookup('passwords::redis::master')
 ) {
+    apt::source { 'bullseye':
+        location => 'http://deb.debian.org/debian/',
+        repos    => 'main',
+        release  => 'bullseye',
+        pin      => -10, 
+        include  => {
+            'src' => true,
+            'deb' => true,
+        }
+    }
+
+    # Need to update before trying to install libssl1.1 
+    Class['apt::update'] ->
+
     stdlib::ensure_packages(['nodejs', 'libssl1.1', 'libsasl2-dev'])
 
     group { 'changeprop':
