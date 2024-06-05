f58ed81fc3d7be27a4e6389d6514cb2fa96e2c4f
Alex <alex@blueselene.com>
2024-06-05 23:04:58 +0200

    Install ntp module (#3864)
    https://github.com/puppetlabs/puppetlabs-ntp
    
    The version installed in /modules/ntp is
    
    [v10.1.0](https://github.com/puppetlabs/puppetlabs-ntp/releases/tag/v10.1.0)
    
    This PR installs ntpd on all the servers.

---
+++ modules/ntp/examples/init.pp
@@ -0,0 +1,9 @@
+node default {
+  notify { 'enduser-before': }
+  notify { 'enduser-after': }
+
+  class { 'ntp':
+    require => Notify['enduser-before'],
+    before  => Notify['enduser-after'],
+  }
+}
