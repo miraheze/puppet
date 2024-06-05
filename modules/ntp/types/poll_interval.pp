2e5fced6e487b0d8ebc66e5aca64dad740fff464
Alex <alex@blueselene.com>
2024-06-05 20:02:28 +0200

    Revert "Install ntp module" (#3862)
    Reverts miraheze/puppet#3861

--- modules/ntp/types/poll_interval.pp
+++
@@ -1,3 +0,0 @@
-# See https://doc.ntp.org/documentation/4.2.6-series/confopt/#command-options for documentation
-# Alternatively: type Ntp::Poll_interval = Variant[Integer, Pattern['']]
-type Ntp::Poll_interval = Integer[3, 17]
