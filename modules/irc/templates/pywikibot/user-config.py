59e69cffb76281679a54b881bf186cb06a058dbb
Alex <alex@blueselene.com>
2024-07-25 12:54:18 +0200

    Pywikibot: Switch accounts to BeeBot & remove Windows line endings (#3887)

--- modules/irc/templates/pywikibot/user-config.py
+++ modules/irc/templates/pywikibot/user-config.py
@@ -1,4 +1,4 @@
 family = 'wikitide'
 mylang = 'meta'
-usernames['wikitide']['meta'] = 'Miraheze maintenance script'
+usernames['wikitide']['meta'] = 'BeeBot'
 authenticate['*.miraheze.org'] = ('<%= @consumer_token %>', '<%= @consumer_secret %>', '<%= @access_token %>', '<%= @access_secret %>')
