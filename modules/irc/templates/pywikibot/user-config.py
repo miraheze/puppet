428f44da4aa1d425cb0b303db5465ace42ca6a8a
Alex <alex@blueselene.com>
2024-08-10 17:20:40 +0200

    Pywikibot: Add user agent description (#3893)

--- modules/irc/templates/pywikibot/user-config.py
+++ modules/irc/templates/pywikibot/user-config.py
@@ -2,3 +2,4 @@ family = 'wikitide'
 mylang = 'meta'
 usernames['wikitide']['meta'] = 'BeeBot'
 authenticate['*.miraheze.org'] = ('<%= @consumer_token %>', '<%= @consumer_secret %>', '<%= @access_token %>', '<%= @access_secret %>')
+user_agent_description = 'https://meta.miraheze.org/wiki/Tech:Pywikibot; tech@wikitide.org'
