c2c8d5ca7b6e48fffbf21631860a08ba936229b5
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-09-12 05:34:22 +0800

    Blank line

--- modules/mediawiki/files/bin/test_mwdeploy.py
+++ modules/mediawiki/files/bin/test_mwdeploy.py
@@ -157,6 +157,7 @@ def test_check_up_no_debug_host() -> None:
 @patch('requests.get')
 def test_check_up_debug(mock_get) -> None:
     requests_get = requests.get
+
     def mock_requests_get(url, headers=None, verify=True, **kwargs):
         kwargs.pop('cert', None)
         return requests_get(url, headers=headers, verify=verify, cert=None, **kwargs)
