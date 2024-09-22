da4582dd93608b66e0c6095f63f2aefdca819158
paladox <paladox@users.noreply.github.com>
2024-09-22 18:41:00 +0100

    changeprop: Allow eventgate to access port 443

--- modules/role/manifests/changeprop.pp
+++ modules/role/manifests/changeprop.pp
@@ -27,6 +27,30 @@ class role::changeprop {
         notrack => true,
     }
 
+
+    $firewall_rules_eventgate_str = join(
+        query_facts('Class[Role::Eventgate] or Class[Role::Icinga2]', ['networking'])
+        .map |$key, $value| {
+            if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } elsif ( $value['networking']['interfaces']['ens18'] ) {
+                "${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
+            } else {
+                "${value['networking']['ip']} ${value['networking']['ip6']}"
+            }
+        }
+        .flatten()
+        .unique()
+        .sort(),
+        ' '
+    )
+
+    ferm::service { 'access port 443':
+        proto  => 'tcp',
+        port   => '443',
+        srange => "(${firewall_rules_eventgate_str})",
+    }
+
     system::role { 'role::changeprop':
         description => 'ChangeProp server',
     }
