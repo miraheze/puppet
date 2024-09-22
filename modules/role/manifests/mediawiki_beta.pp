ee0c64850f9df65e6be5ab6acaa76cbbbeaa1e63
paladox <paladox@users.noreply.github.com>
2024-09-22 14:48:30 +0100

    Migrate beta to use role based hiera

--- modules/role/manifests/mediawiki_beta.pp
+++ modules/role/manifests/mediawiki_beta.pp
@@ -3,6 +3,12 @@ class role::mediawiki_beta (
     Boolean $strict_firewall = lookup('role::mediawiki::use_strict_firewall', {'default_value' => false}),
     Boolean $use_mcrouter = lookup('role::mediawiki::use_mcrouter', {'default_value' => false})
 ) {
+
+    include base
+    include role::memcached
+    include role::poolcounter
+    include role::redis
+
     # doesn't install on bookworm
     # include prometheus::exporter::cadvisor
 
