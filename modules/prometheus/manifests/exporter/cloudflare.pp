1f554b668e98ebef046082728dfa23a23b739012
Agent Isai <43097272+AgentIsai@users.noreply.github.com>
2024-10-04 15:23:28 -0500

    Correct syntax again

--- modules/prometheus/manifests/exporter/cloudflare.pp
+++ modules/prometheus/manifests/exporter/cloudflare.pp
@@ -1,11 +1,14 @@
 class prometheus::exporter::cloudflare { 
+    $cf_api_token = lookup('passwords::cloudflare::api_token')
+
     file { '/etc/default/prometheus-cloudflare-exporter':
         ensure  => present,
         mode    => '0444',
         owner   => 'root',
         group   => 'root',
-        content => 'ARGS="-metrics_path='' -cf_api_token='${lookup(passwords::cloudflare::api_token)}' -listen=':9119'",
+        content => 'ARGS="-metrics_path \'\' -cf_api_token=\'${$cf_api_token}\' -listen :9119"',
     }
+
 
     systemd::service { 'prometheus-cloudflare-exporter':
         ensure  => present,
