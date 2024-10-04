ab4794abef660298aecc6b294c331d0a5b339287
Agent Isai <43097272+AgentIsai@users.noreply.github.com>
2024-10-04 14:44:08 -0500

    FIx syntax

--- modules/prometheus/manifests/exporter/cloudflare.pp
+++ modules/prometheus/manifests/exporter/cloudflare.pp
@@ -6,7 +6,7 @@ class prometheus::exporter::cloudflare {
         mode    => '0444',
         owner   => 'root',
         group   => 'root',
-        content => 'ARGS="-metrics_path='' -cf_api_token='${$cf_api_token}' -listen=':9119'',
+        content => 'ARGS="-metrics_path='' -cf_api_token='${$cf_api_token}' -listen=':9119'",
     }
 
     systemd::service { 'prometheus-cloudflare-exporter':
