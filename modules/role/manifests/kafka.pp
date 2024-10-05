008c38364228dc52882245d266a92b89bf484190
paladox <paladox@users.noreply.github.com>
2024-10-05 16:49:59 +0100

    kafka: set num.network.threads to 6

--- modules/role/manifests/kafka.pp
+++ modules/role/manifests/kafka.pp
@@ -68,6 +68,7 @@ class role::kafka {
             'log.retention.hours'               => '168', # 1 week
             'message.max.bytes'                 => '4194304',
             'num.io.threads'                    => '12',
+            'num.network.threads'               => '6',
             'num.partitions'                    => '24',
             'num.recovery.threads.per.data.dir' => '4',
             'offsets.retention.minutes'         => '10080', # 1 week
