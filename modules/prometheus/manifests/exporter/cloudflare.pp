2511a0148203939561eabfe0ee5aa6f876ffffbd
Agent Isai <43097272+AgentIsai@users.noreply.github.com>
2024-10-04 21:28:59 -0500

    Notify CloudFlare exporter when the config is updated

--- modules/prometheus/manifests/exporter/cloudflare.pp
+++ modules/prometheus/manifests/exporter/cloudflare.pp
@@ -6,7 +6,8 @@ class prometheus::exporter::cloudflare {
         mode    => '0444',
         owner   => 'root',
         group   => 'root',
-        content => "ARGS=\"--metrics_path '' --cf_api_token='${cf_api_token}' --scrape_delay 60 --listen :9119\"",
+        content => "ARGS=\"--metrics_path '' --cf_api_token='${cf_api_token}' --listen :9119\"",
+        notify  => Service['prometheus-cloudflare-exporter'],
     }
 
 
