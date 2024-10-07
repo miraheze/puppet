73b3005054f52f301a6a0bab671a3a11c7e74fb6
paladox <paladox@users.noreply.github.com>
2024-10-07 22:57:18 +0100

    Update jobrunner.pp

--- modules/mediawiki/manifests/jobrunner.pp
+++ modules/mediawiki/manifests/jobrunner.pp
@@ -111,7 +111,7 @@ class mediawiki::jobrunner {
     }
 
     $firewall_rules_str = join(
-        query_facts('Class[Role::Changeprop]', ['networking'])
+        query_facts('Class[Role::Changeprop] or Class[Role::Mediawiki_task] or Class[Role::Bastion]', ['networking'])
         .map |$key, $value| {
             if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
                 "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
