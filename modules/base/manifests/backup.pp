06001f8d590aaa5d2d586166eb05c688a06b7c26
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-10-11 11:27:15 +0800

    Temporarily add legacy wikitide-backup script (#4017)

--- modules/base/manifests/backup.pp
+++ modules/base/manifests/backup.pp
@@ -1,6 +1,7 @@
 # class base::backup
 class base::backup (
-    String $pca_password = lookup('private::passwords::pca')
+    String $pca_password = lookup('private::passwords::pca'),
+    String $pca_legacy_password = lookup('private::passwords::legacy_pca')
 ) {
     package { ['python3-fabric', 'python3-decorator']:
         ensure => present,
@@ -9,5 +10,10 @@ class base::backup (
     file { '/usr/local/bin/wikitide-backup':
         mode    => '0555',
         content => template('base/backups/wikitide-backup.py.erb'),
+    }
+
+    file { '/usr/local/bin/wikitide-backup-legacy':
+        mode    => '0555',
+        content => template('base/backups/wikitide-backup-legacy.py.erb'),
     }
 }
