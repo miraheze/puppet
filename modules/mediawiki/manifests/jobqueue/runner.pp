c0f9d16afe8a012d3ff2518be68bb97e458717eb
paladox <paladox@users.noreply.github.com>
2024-10-13 14:34:52 +0100

    Update runner.pp

--- modules/mediawiki/manifests/jobqueue/runner.pp
+++ modules/mediawiki/manifests/jobqueue/runner.pp
@@ -163,7 +163,7 @@ class mediawiki::jobqueue::runner (
         if $wiki == 'loginwikibeta' {
             cron { 'purge_parsercache':
                 ensure  => present,
-                command => "/usr/bin/php ${runner}/srv/mediawiki/${version}/maintenance/purgeParserCache.php --wiki loginwikibeta --tag pc1 --age 604800 --msleep 200",
+                command => "/usr/bin/php ${runner}/srv/mediawiki/${version}/maintenance/purgeParserCache.php --wiki loginwikibeta --tag pc1 --age 864000 --msleep 200",
                 user    => 'www-data',
                 special => 'daily',
             }
