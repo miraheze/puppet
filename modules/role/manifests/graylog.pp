602e74bbbedae4fac8a94b06a411d553b0e45471
paladox <paladox@users.noreply.github.com>
2024-11-02 15:54:57 +0000

    graylog: upgrade to 6.1.1

--- modules/role/manifests/graylog.pp
+++ modules/role/manifests/graylog.pp
@@ -22,10 +22,10 @@ class role::graylog {
     $http_proxy = lookup('http_proxy', {'default_value' => undef})
     class { 'graylog::repository':
         proxy   => $http_proxy,
-        version => '6.0',
+        version => '6.1',
     }
     -> class { 'graylog::server':
-        package_version        => '6.0.7-1',
+        package_version        => '6.1.1-1',
         config                 => {
             'password_secret'           => lookup('passwords::graylog::password_secret'),
             'root_password_sha2'        => lookup('passwords::graylog::root_password_sha2'),
