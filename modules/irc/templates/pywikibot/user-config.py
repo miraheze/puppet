96ade10bf5c61f51c1802d9cb5fa8bd63388ec90
Alex <alex@blueselene.com>
2024-11-11 21:00:42 +0100

    Remove stray comma

--- modules/irc/templates/pywikibot/user-config.py
+++ modules/irc/templates/pywikibot/user-config.py
@@ -1,6 +1,6 @@
 # MANAGED BY PUPPET
 family = 'wikitide'
 mylang = 'metawiki'
-usernames['wikitide']['*'] = 'BeeBot',
+usernames['wikitide']['*'] = 'BeeBot'
 authenticate['*'] = ('<%= @consumer_token %>', '<%= @consumer_secret %>', '<%= @access_token %>', '<%= @access_secret %>')
 user_agent_description = 'https://meta.miraheze.org/wiki/Tech:Pywikibot; tech@wikitide.org'
