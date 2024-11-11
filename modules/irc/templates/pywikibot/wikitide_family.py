b24748316dd81ff0561ad5bda2d1283730ef2bc8
Alex <alex@blueselene.com>
2024-11-11 20:30:45 +0100

    Fix syntax, again

--- modules/irc/templates/pywikibot/wikitide_family.py
+++ modules/irc/templates/pywikibot/wikitide_family.py
@@ -6,21 +6,21 @@ class Family(family.Family):  # noqa: D101
 
     name = 'wikitide'
     langs = {
-        <% @family_langs.each do |$dbname $params| {-%>
-        '<%= $dbname %>': '<%= $params["domain"] %>',
+        <% @family_langs.each do |dbname, params| {-%>
+        '<%= dbname %>': '<%= params["domain"] %>',
         <% end -%>
     }
 
     def scriptpath(self, code):
         return {
-            <% @family_langs.each do |$dbname, $params| {-%>
-            '<%= $dbname %>': '/w',
+            <% @family_langs.each do |dbname, params| {-%>
+            '<%= dbname %>': '/w',
             <% end -%>
         }[code]
 
     def protocol(self, code):
         return {
-            <% @family_langs.each do |$dbname $params| {-%>
-            '<%= $dbname %>': 'https',
+            <% @family_langs.each do |dbname, params| {-%>
+            '<%= dbname %>': 'https',
             <% end -%>
         }[code]
