084cb2f35cf330fd2e3de321b44f69e047beb143
paladox <paladox@users.noreply.github.com>
2024-11-12 16:50:40 +0000

    mediawiki::jobqueue::runner: convert cron to systemd timer (#4080)

--- modules/mediawiki/manifests/jobqueue/runner.pp
+++ modules/mediawiki/manifests/jobqueue/runner.pp
@@ -41,8 +41,92 @@ class mediawiki::jobqueue::runner (
     }
 
     if lookup('mediawiki::jobqueue::runner::cron', {'default_value' => false}) {
+        systemd::timer::job { 'purge-checkuser':
+            description       => 'Purges checkuser',
+            command           => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/extensions/CheckUser/maintenance/purgeOldData.php",
+            interval          => {
+                'start'    => 'OnCalendar',
+                'interval' => '*-*-* 06:05:00',
+            },
+            logfile_basedir   => '/var/log/mediawiki/cron',
+            logfile_name      => 'purge-checkuser.log',
+            syslog_identifier => 'purge-checkuser',
+            user              => 'www-data',
+            require           => File['/var/log/mediawiki/cron'],
+        }
+
+        systemd::timer::job { 'purge-abusefilter':
+            description       => 'Purges abusefilter',
+            command           => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/extensions/AbuseFilter/maintenance/PurgeOldLogIPData.php",
+            interval          => {
+                'start'    => 'OnCalendar',
+                'interval' => '*-*-* 18:05:00',
+            },
+            logfile_basedir   => '/var/log/mediawiki/cron',
+            logfile_name      => 'purge-abusefilter.log',
+            syslog_identifier => 'purge-abusefilter',
+            user              => 'www-data',
+            require           => File['/var/log/mediawiki/cron'],
+        }
+
+        systemd::timer::job { 'managewikis':
+            description       => 'Check for inactive wikis',
+            command           => "/usr/bin/php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/extensions/CreateWiki/maintenance/manageInactiveWikis.php --wiki ${wiki} --write",
+            interval          => {
+                'start'    => 'OnCalendar',
+                'interval' => '*-*-* 12:05:00',
+            },
+            logfile_basedir   => '/var/log/mediawiki/cron',
+            logfile_name      => 'managewikis.log',
+            syslog_identifier => 'managewikis',
+            user              => 'www-data',
+            require           => File['/var/log/mediawiki/cron'],
+        }
+
+        systemd::timer::job { 'generate-sitemaps':
+            description       => 'Create sitemaps for all wikis',
+            command           => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/extensions/MirahezeMagic/maintenance/generateMirahezeSitemap.php",
+            interval          => {
+                'start'    => 'OnCalendar',
+                'interval' => 'Sat *-*-* 00:00:00',
+            },
+            logfile_basedir   => '/var/log/mediawiki/cron',
+            logfile_name      => 'generate-sitemaps.log',
+            syslog_identifier => 'generate-sitemaps',
+            user              => 'www-data',
+            require           => File['/var/log/mediawiki/cron'],
+        }
+
+        systemd::timer::job { 'cleanup-upload-stash':
+            description       => 'Cleanup upload stash',
+            command           => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/maintenance/cleanupUploadStash.php",
+            interval          => {
+                'start'    => 'OnCalendar',
+                'interval' => '*-*-* 01:00:00',
+            },
+            logfile_basedir   => '/var/log/mediawiki/cron',
+            logfile_name      => 'cleanup-upload-stash.log',
+            syslog_identifier => 'cleanup-upload-stash',
+            user              => 'www-data',
+            require           => File['/var/log/mediawiki/cron'],
+        }
+
+        systemd::timer::job { 'purge-expired-blocks':
+            description       => 'Purge expired blocks',
+            command           => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/maintenance/purgeExpiredBlocks.php",
+            interval          => {
+                'start'    => 'OnCalendar',
+                'interval' => 'Mon *-*-* 00:00:00',
+            },
+            logfile_basedir   => '/var/log/mediawiki/cron',
+            logfile_name      => 'purge-expired-blocks.log',
+            syslog_identifier => 'purge-expired-blocks',
+            user              => 'www-data',
+            require           => File['/var/log/mediawiki/cron'],
+        }
+
         cron { 'purge_checkuser':
-            ensure  => present,
+            ensure  => absent,
             command => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/extensions/CheckUser/maintenance/purgeOldData.php >> /var/log/mediawiki/cron/purge_checkuser.log",
             user    => 'www-data',
             minute  => '5',
@@ -50,7 +134,7 @@ class mediawiki::jobqueue::runner (
         }
 
         cron { 'purge_abusefilter':
-            ensure  => present,
+            ensure  => absent,
             command => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/extensions/AbuseFilter/maintenance/PurgeOldLogIPData.php >> /var/log/mediawiki/cron/purge_abusefilter.log",
             user    => 'www-data',
             minute  => '5',
@@ -58,7 +142,7 @@ class mediawiki::jobqueue::runner (
         }
 
         cron { 'managewikis':
-            ensure  => present,
+            ensure  => absent,
             command => "/usr/bin/php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/extensions/CreateWiki/maintenance/manageInactiveWikis.php --wiki ${wiki} --write >> /var/log/mediawiki/cron/managewikis.log",
             user    => 'www-data',
             minute  => '5',
@@ -66,7 +150,7 @@ class mediawiki::jobqueue::runner (
         }
 
         cron { 'generate sitemaps for all wikis':
-            ensure  => present,
+            ensure  => absent,
             command => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/extensions/MirahezeMagic/maintenance/generateMirahezeSitemap.php",
             user    => 'www-data',
             minute  => '0',
@@ -76,7 +160,7 @@ class mediawiki::jobqueue::runner (
         }
 
         cron { 'cleanup_upload_stash':
-            ensure  => present,
+            ensure  => absent,
             command => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/maintenance/cleanupUploadStash.php",
             user    => 'www-data',
             hour    => 1,
@@ -84,7 +168,7 @@ class mediawiki::jobqueue::runner (
         }
 
         cron { 'purge expired blocks':
-            ensure  => present,
+            ensure  => absent,
             command => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/maintenance/purgeExpiredBlocks.php",
             user    => 'www-data',
             minute  => '0',
@@ -96,8 +180,50 @@ class mediawiki::jobqueue::runner (
         if $wiki == 'loginwiki' {
             $swift_password = lookup('mediawiki::swift_password')
 
+            systemd::timer::job { 'generate-sitemap-index':
+                description       => 'Create sitemap index',
+                command           => "/usr/bin/python3 /srv/mediawiki/${version}/extensions/MirahezeMagic/py/generateSitemapIndex.py -A https://swift-lb.miraheze.org/auth/v1.0 -U mw:media -K ${swift_password}",
+                interval          => {
+                    'start'    => 'OnCalendar',
+                    'interval' => 'Fri *-*-* 00:00:00',
+                },
+                logfile_basedir   => '/var/log/mediawiki/cron',
+                logfile_name      => 'generate-sitemap-index.log',
+                syslog_identifier => 'generate-sitemap-index',
+                user              => 'www-data',
+                require           => File['/var/log/mediawiki/cron'],
+            }
+
+            systemd::timer::job { 'purge-parsercache':
+                description       => 'Purge parsercache',
+                command           => "/usr/bin/php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/maintenance/purgeParserCache.php --wiki loginwiki --tag pc1 --age 864000 --msleep 200",
+                interval          => {
+                    'start'    => 'OnCalendar',
+                    'interval' => '*-*-* 00:00:00',
+                },
+                logfile_basedir   => '/var/log/mediawiki/cron',
+                logfile_name      => 'purge-parsercache.log',
+                syslog_identifier => 'purge-parsercache',
+                user              => 'www-data',
+                require           => File['/var/log/mediawiki/cron'],
+            }
+
+            systemd::timer::job { 'update-special-pages':
+                description       => 'Purge parsercache',
+                command           => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/maintenance/updateSpecialPages.php",
+                interval          => {
+                    'start'    => 'OnCalendar',
+                    'interval' => '*-1/3 05:00',
+                },
+                logfile_basedir   => '/var/log/mediawiki/cron',
+                logfile_name      => 'update-special-pages.log',
+                syslog_identifier => 'update-special-pages',
+                user              => 'www-data',
+                require           => File['/var/log/mediawiki/cron'],
+            }
+
             cron { 'generate sitemap index':
-                ensure  => present,
+                ensure  => absent,
                 command => "/usr/bin/python3 /srv/mediawiki/${version}/extensions/MirahezeMagic/py/generateSitemapIndex.py -A https://swift-lb.miraheze.org/auth/v1.0 -U mw:media -K ${swift_password} >> /var/log/mediawiki/cron/generate-sitemap-index.log",
                 user    => 'www-data',
                 minute  => '0',
@@ -107,14 +233,14 @@ class mediawiki::jobqueue::runner (
             }
 
             cron { 'purge_parsercache':
-                ensure  => present,
+                ensure  => absent,
                 command => "/usr/bin/php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/maintenance/purgeParserCache.php --wiki loginwiki --tag pc1 --age 864000 --msleep 200",
                 user    => 'www-data',
                 special => 'daily',
             }
 
             cron { 'update_special_pages':
-                ensure   => present,
+                ensure   => absent,
                 command  => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/maintenance/updateSpecialPages.php > /var/log/mediawiki/cron/updateSpecialPages.log 2>&1",
                 user     => 'www-data',
                 monthday => '*/3',
@@ -146,16 +272,58 @@ class mediawiki::jobqueue::runner (
         }
 
         if $wiki == 'loginwikibeta' {
+            systemd::timer::job { 'purge-parsercache':
+                description       => 'Purge parsercache',
+                command           => "/usr/bin/php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/maintenance/purgeParserCache.php --wiki loginwikibeta --tag pc1 --age 864000 --msleep 200",
+                interval          => {
+                    'start'    => 'OnCalendar',
+                    'interval' => '*-*-* 00:00:00',
+                },
+                logfile_basedir   => '/var/log/mediawiki/cron',
+                logfile_name      => 'purge-parsercache.log',
+                syslog_identifier => 'purge-parsercache',
+                user              => 'www-data',
+                require           => File['/var/log/mediawiki/cron'],
+            }
+
             cron { 'purge_parsercache':
-                ensure  => present,
+                ensure  => absent,
                 command => "/usr/bin/php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/maintenance/purgeParserCache.php --wiki loginwikibeta --tag pc1 --age 864000 --msleep 200",
                 user    => 'www-data',
                 special => 'daily',
             }
         }
 
+        systemd::timer::job { 'update-statistics':
+            description       => 'Update site statistics',
+            command           => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/maintenance/initSiteStats.php --update --active",
+            interval          => {
+                'start'    => 'OnCalendar',
+                'interval' => '*-*-1,15 05:00:00',
+            },
+            logfile_basedir   => '/var/log/mediawiki/cron',
+            logfile_name      => 'update-statistics.log',
+            syslog_identifier => 'update-statistics',
+            user              => 'www-data',
+            require           => File['/var/log/mediawiki/cron'],
+        }
+
+        systemd::timer::job { 'update-wikibase-sites-table':
+            description       => 'Update site statistics',
+            command           => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/extensions/MirahezeMagic/maintenance/populateWikibaseSitesTable.php",
+            interval          => {
+                'start'    => 'OnCalendar',
+                'interval' => '*-*-5,20 05:00:00',
+            },
+            logfile_basedir   => '/var/log/mediawiki/cron',
+            logfile_name      => 'update-wikibase-sites-table.log',
+            syslog_identifier => 'update-wikibase-sites-table',
+            user              => 'www-data',
+            require           => File['/var/log/mediawiki/cron'],
+        }
+
         cron { 'update_statistics':
-            ensure   => present,
+            ensure   => absent,
             command  => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/maintenance/initSiteStats.php --update --active > /dev/null",
             user     => 'www-data',
             minute   => '0',
@@ -164,7 +332,7 @@ class mediawiki::jobqueue::runner (
         }
 
         cron { 'update_sites':
-            ensure   => present,
+            ensure   => absent,
             command  => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/extensions/MirahezeMagic/maintenance/populateWikibaseSitesTable.php > /dev/null",
             user     => 'www-data',
             minute   => '0',
