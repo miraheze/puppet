6f24382a67d0baa8c5cbd014cf7cb8af1c878293
paladox <paladox@users.noreply.github.com>
2024-11-12 13:48:29 +0000

    mediawiki::jobqueue::runner: remove clean_gu_cache cron
    The documentation says you only need to run this once on wiki install.

--- modules/mediawiki/manifests/jobqueue/runner.pp
+++ modules/mediawiki/manifests/jobqueue/runner.pp
@@ -171,14 +171,5 @@ class mediawiki::jobqueue::runner (
             hour     => '5',
             monthday => [ '5', '20' ],
         }
-
-        cron { 'clean_gu_cache':
-            ensure   => present,
-            command  => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/extensions/GlobalUsage/maintenance/refreshGlobalimagelinks.php --pages=existing,nonexisting > /dev/null",
-            user     => 'www-data',
-            minute   => '0',
-            hour     => '5',
-            monthday => [ '6', '21' ],
-        }
     }
 }
