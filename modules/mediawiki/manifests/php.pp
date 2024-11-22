b0f4114e2258ce0bbaa88fa882342cf9511e202b
paladox <paladox@users.noreply.github.com>
2024-11-22 21:23:09 +0000

    mediawiki: increase opcache.memory_consumption to 3072 (#4101)

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -28,7 +28,7 @@ class mediawiki::php (
     $config_opcache = {
         'opcache.enable'                  => 1,
         'opcache.interned_strings_buffer' => 256,
-        'opcache.memory_consumption'      => 2048,
+        'opcache.memory_consumption'      => 3072,
         'opcache.max_accelerated_files'   => 65407,
         'opcache.max_wasted_percentage'   => 10,
         'opcache.validate_timestamps'     => 1,
