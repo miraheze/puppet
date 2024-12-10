db8ceb2c8357fcd3838178b7b422bec34f2e926d
paladox <paladox@users.noreply.github.com>
2024-12-10 15:24:17 +0000

    Update touch.php

--- modules/mediawiki/files/touch.php
+++ modules/mediawiki/files/touch.php
@@ -27,12 +27,16 @@ function streamAppleTouch() {
 
 	$url = wfExpandUrl( $touch, PROTO_CANONICAL );
 
+	$mediaWikiServices = MediaWikiServices::getInstance();
+	$urlUtils = $mediaWikiServices->getUrlUtils();
+	$parsedBaseUrl = $urlUtils->parse( $url );
+
 	$options = [];
-	if ( preg_match( '/static\.(miraheze\.org|wikitide\.net)\/(.+)/', $url ) ) {
+	if ( $parsedBaseUrl['host'] === 'static.wikitide.net' || $parsedBaseUrl['host'] === 'static.miraheze.org'  ) {
 		$options['followRedirects'] = true;
 	}
 	
-	$client = MediaWikiServices::getInstance()
+	$client = $mediaWikiServices
 		->getHttpRequestFactory()
 		->create( $url, $options );
 	$client->setHeader( 'X-Favicon-Loop', '1' );
