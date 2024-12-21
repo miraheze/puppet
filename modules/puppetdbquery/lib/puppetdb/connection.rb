02e83c46b34b903b25df0d5aa7f504d1f5c1f74e
paladox <paladox@users.noreply.github.com>
2024-12-21 15:48:41 +0000

    Fix deprecated

--- modules/puppetdbquery/lib/puppetdb/connection.rb
+++ modules/puppetdbquery/lib/puppetdb/connection.rb
@@ -60,7 +60,7 @@ EOT
     end
 
     uri = "/pdb/query/#{version}/#{endpoint}"
-    uri += URI.escape "?query=#{query.to_json}" unless query.nil? || query.empty?
+    uri += "?query=#{URI.encode_www_form_component(query.to_json)}" unless query.nil? || query.empty?
 
     debug("PuppetDB query: #{query.to_json}")
 
