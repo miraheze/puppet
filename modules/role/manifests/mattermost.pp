057056721c5a2a61d4752d389ba25ac45185b5ca
paladox <paladox@users.noreply.github.com>
2025-01-19 19:08:26 +0000

    mattermost: upgrade to 10.4.1

--- modules/role/manifests/mattermost.pp
+++ modules/role/manifests/mattermost.pp
@@ -23,7 +23,7 @@ class role::mattermost {
     }
     -> class { 'mattermost':
         edition          => 'enterprise',
-        version          => '10.3.1',
+        version          => '10.4.1',
         override_options => {
             'TeamSettings'    => {
                 'SiteName'                  => 'WikiTide Foundation',
