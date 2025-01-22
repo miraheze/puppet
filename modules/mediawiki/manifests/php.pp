d881d23e79f9f65d5c3c682582b4c16bf1be4e1b
paladox <paladox@users.noreply.github.com>
2025-01-22 13:38:26 +0000

    Update php.pp

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -1,7 +1,7 @@
 # === Class mediawiki::php
 class mediawiki::php (
     Float $fpm_workers_multiplier        = lookup('mediawiki::php::fpm::fpm_workers_multiplier', {'default_value' => 1.5}),
-    Integer $request_timeout             = lookup('mediawiki::php::request_timeout', {'default_value' => 60}),
+    Integer $request_timeout             = lookup('mediawiki::php::request_timeout', {'default_value' => 240}),
     String $apc_shm_size                 = lookup('mediawiki::php::apc_shm_size', {'default_value' => '3072M'}),
     VMlib::Php_version $php_version      = lookup('php::php_version', {'default_value' => '8.2'}),
     Boolean $enable_fpm                  = lookup('mediawiki::php::enable_fpm', {'default_value' => true}),
