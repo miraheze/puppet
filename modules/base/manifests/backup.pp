474e1c6da3f0ce847c60eeff2eb2b3f577a0a25f
paladox <paladox@users.noreply.github.com>
2025-02-26 02:45:09 +0000

    Update backup.pp

--- modules/base/manifests/backup.pp
+++ modules/base/manifests/backup.pp
@@ -2,7 +2,7 @@
 class base::backup (
     String $pca_password = lookup('private::passwords::pca'),
     String $pca_legacy_password = lookup('private::passwords::legacy_pca'),
-    String $gateway_host = lookup('gateway_host', {'default_value' => 'bastion.wikitide.net:8080'}),
+    String $use_gateway = lookup('use_gateway', {'default_value' => true),
 ) {
     package { ['python3-fabric', 'python3-decorator']:
         ensure => present,
