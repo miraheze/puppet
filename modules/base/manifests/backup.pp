efc6f90b794a1b582332a9fccaf3de9318361033
paladox <paladox@users.noreply.github.com>
2025-02-26 02:29:10 +0000

    wikitide-backup: allow setting gateway_host (#4201)

--- modules/base/manifests/backup.pp
+++ modules/base/manifests/backup.pp
@@ -1,7 +1,8 @@
 # class base::backup
 class base::backup (
     String $pca_password = lookup('private::passwords::pca'),
-    String $pca_legacy_password = lookup('private::passwords::legacy_pca')
+    String $pca_legacy_password = lookup('private::passwords::legacy_pca'),
+    String $gateway_host = lookup('gateway_host', {'default_value' => 'bastion.wikitide.net:8080'}),
 ) {
     package { ['python3-fabric', 'python3-decorator']:
         ensure => present,
