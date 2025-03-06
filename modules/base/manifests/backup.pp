4c2c8fc903364f6c5d6ebef188c6507a0cedec14
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-03-06 10:51:28 -0700

    Use stdlib::ensure_packages

--- modules/base/manifests/backup.pp
+++ modules/base/manifests/backup.pp
@@ -3,9 +3,7 @@ class base::backup (
     String $pca_password = lookup('private::passwords::pca'),
     Boolean $use_gateway = lookup('backups::use_gateway', {'default_value' => true}),
 ) {
-    package { ['python3-fabric', 'python3-decorator']:
-        ensure => present,
-    }
+    stdlib::ensure_packages(['python3-fabric', 'python3-decorator'])
 
     file { '/usr/local/bin/wikitide-backup':
         mode    => '0555',
