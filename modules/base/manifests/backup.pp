f930054a4e19be65180db8165c54331c1109d8e6
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-03-06 10:42:46 -0700

    Remove wikitide-backup-legacy and minor cleanup

--- modules/base/manifests/backup.pp
+++ modules/base/manifests/backup.pp
@@ -1,8 +1,7 @@
 # class base::backup
 class base::backup (
-    String $pca_password = lookup('private::passwords::pca'),
-    String $pca_legacy_password = lookup('private::passwords::legacy_pca'),
-    Boolean $use_gateway = lookup('use_gateway', {'default_value' => true}),
+    String  $pca_password = lookup('private::passwords::pca'),
+    Boolean $use_gateway  = lookup('base::backup::use_gateway', {'default_value' => true}),
 ) {
     package { ['python3-fabric', 'python3-decorator']:
         ensure => present,
@@ -11,10 +10,5 @@ class base::backup (
     file { '/usr/local/bin/wikitide-backup':
         mode    => '0555',
         content => template('base/backups/wikitide-backup.py.erb'),
-    }
-
-    file { '/usr/local/bin/wikitide-backup-legacy':
-        mode    => '0555',
-        content => template('base/backups/wikitide-backup-legacy.py.erb'),
     }
 }
