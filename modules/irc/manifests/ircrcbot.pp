bcf95a8d526557ea6a8e3890bc608f468c366687
RhinosF1 <rhinosf1@wikitide.org>
2025-03-13 13:32:56 +0000

    Update ircrcbot.pp

--- modules/irc/manifests/ircrcbot.pp
+++ modules/irc/manifests/ircrcbot.pp
@@ -10,14 +10,12 @@ define irc::ircrcbot(
 
     $mirahezebots_password = lookup('passwords::irc::mirahezebots')
 
-    if !defined(File['/usr/local/bin/ircrcbot.py']) {
-        file { '/usr/local/bin/ircrcbot.py':
+    file { "/usr/local/bin/ircrcbot-$nickname.py":
             ensure  => present,
             content => template('irc/ircrcbot.py'),
             mode    => '0755',
             notify  => Service['ircrcbot'],
         }
-    }
 
     systemd::service { "ircrcbot-$nickname":
         ensure  => present,
@@ -26,6 +24,6 @@ define irc::ircrcbot(
     }
 
     monitoring::nrpe { 'IRC RC Bot':
-        command => '/usr/lib/nagios/plugins/check_procs -a ircrcbot.py -c 1:1'
+        command => "/usr/lib/nagios/plugins/check_procs -a ircrcbot-$nickname.py -c 1:1"
     }
 }
