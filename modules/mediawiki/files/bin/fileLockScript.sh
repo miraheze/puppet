2a5de065fd23fdde4228408b4fd1a0f8c4254f7e
BlankEclair <blankeclair@disroot.org>
2025-03-15 00:04:23 +1100

    Run shellcheck
    This commit has various shell scripts fixed after running shellcheck. However:
    * I did not try to handle *.erb files
    * I didn't even test these (maybe production will blow up? who knows!)
    * I didn't bother with a few warnings lol

--- modules/mediawiki/files/bin/fileLockScript.sh
+++ modules/mediawiki/files/bin/fileLockScript.sh
@@ -2,9 +2,9 @@
 
 # To run this script do the following fileLockScript.sh <lock_name> "<script>"
 
-exec 100>$1 || exit 1
+exec 100>"$1" || exit 1
 flock -n 100 || exit 1
 
-trap "rm -f $1" EXIT
+trap 'rm -f $1' EXIT
 
 $2
