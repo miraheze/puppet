9586b08694cc0e0a493931ee3ed6e49d1e4bf2cb
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-03-17 12:12:42 -0600

    relaybot: fix selector_inside_resource

--- modules/irc/manifests/relaybot.pp
+++ modules/irc/manifests/relaybot.pp
@@ -48,11 +48,13 @@ define irc::relaybot (
         }
     }
 
+    $repo_ensure = $ensure ? {
+        present => latest,
+        default => $ensure,
+    }
+
     git::clone { "IRC-Discord-Relay-${title}":
-        ensure    => $ensure ? {
-            present => latest,
-            default => $ensure,
-        },
+        ensure    => $repo_ensure,
         origin    => 'https://github.com/miraheze/IRC-Discord-Relay',
         directory => $install_path,
         owner     => 'irc',
