10fbff61945680a8fd2c5fd467cd6360341896f3
Aeywoo <65846594+Aeywoo@users.noreply.github.com>
2025-03-28 00:25:11 +1030

    Update init.pp
    Beta this, beta that. When do we get Sigma this and Sigma that?

--- modules/mediawiki/manifests/init.pp
+++ modules/mediawiki/manifests/init.pp
@@ -104,7 +104,11 @@ class mediawiki {
     $openai_assistantid         = lookup('mediawiki::openai_assistantid')
     $turnstile_sitekey          = lookup('mediawiki::turnstile_sitekey')
     $turnstile_secreteky        = lookup('mediawiki::turnstile_secretkey')
-    $campaigneventsdashboard_apisecret = lookup('mediawiki::campaigneventsdashboard_apisecret')
+    if ( lookup('role::mediawiki::is_beta', {'default_value' => false)) ) {
+        $campaigneventsdashboard_apisecret = lookup('mediawiki::campaigneventsdashboard_apisecret_beta')
+    } else {
+        $campaigneventsdashboard_apisecret = lookup('mediawiki::campaigneventsdashboard_apisecret')
+    }
 
     file { '/srv/mediawiki/config/PrivateSettings.php':
         ensure  => 'present',
