b16aec0039e99488f56bf1b17d50f29fadfac015
paladox <paladox@users.noreply.github.com>
2025-04-06 20:49:19 +0100

    Update bastion.pp

--- modules/role/manifests/bastion.pp
+++ modules/role/manifests/bastion.pp
@@ -1,6 +1,4 @@
-class role::bastion (
-    $enable_proxy_ipv4_ipv6 = lookup('role::bastion::enable_proxy_ipv4_ipv6', {'default_value' => undef})
-) {
+class role::bastion {
     include base
     include squid
 
@@ -36,52 +34,5 @@ class role::bastion (
         proto  => 'tcp',
         port   => '8080',
         srange => "(${squid_access_hosts_str})",
-    }
-
-    if $enable_proxy_ipv4_ipv6 {
-        $backends = lookup('varnish::backends')
-        $firewall_rules_str = join(
-            query_facts('Class[Role::Varnish] or Class[Role::Cache::Cache]', ['networking'])
-            .map |$key, $value| {
-                if ( $value['networking']['interfaces']['he-ipv6'] ) {
-                    "${value['networking']['ip']} ${value['networking']['interfaces']['he-ipv6']['ip6']}"
-                } elsif ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
-                    "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
-                } elsif ( $value['networking']['interfaces']['ens18'] ) {
-                    "${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
-                } else {
-                    "${value['networking']['ip']} ${value['networking']['ip6']}"
-                }
-            }
-            .flatten()
-            .unique()
-            .sort(),
-            ' '
-        )
-        $firewall_rules_ports = join(
-            $backends.map |$key, $value| {
-                $value['port']
-            }
-            .flatten()
-            .unique()
-            .sort(),
-            ' '
-        )
-        ferm::service { 'direct varnish access':
-            proto   => 'tcp',
-            port    => "(${firewall_rules_ports})",
-            srange  => "(${firewall_rules_str})",
-            notrack => true,
-        }
-
-        file { '/etc/nginx/sites-enabled/default':
-            ensure => absent,
-            notify => Service['nginx'],
-        }
-
-        nginx::site { 'mediawiki':
-            ensure  => present,
-            content => template('role/bastion/mediawiki.conf.erb'),
-        }
     }
 }
