f096669a3a550587929afb44f138a2b2fd82b58b
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-14 10:00:30 -0600

    Update

---
+++ modules/mediawiki/manifests/techdocs.pp
@@ -0,0 +1,47 @@
+# === Class mediawiki::techdocs
+class mediawiki::techdocs {
+    stdlib::ensure_packages('python3-GitPython')
+
+    file { '/usr/local/bin/techdocs':
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        source => 'puppet:///modules/mediawiki/bin/techdocs.py',
+    }
+
+    file { '/srv/statichelp':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0770',
+    }
+
+    file { '/var/lib/nagios/id_ed25519':
+        ensure => present,
+        source => 'puppet:///private/acme/id_ed25519',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0400',
+    }
+
+    file { '/var/lib/nagios/id_ed25519.pub':
+        ensure => present,
+        source => 'puppet:///private/acme/id_ed25519.pub',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+    }
+
+    git::clone { 'statichelp':
+        ensure    => present,
+        directory => '/srv/statichelp',
+        origin    => 'git@github.com:miraheze/statichelp.git',
+        ssh       => 'ssh -i /var/lib/nagios/id_ed25519 -F /dev/null -o ProxyCommand=\'nc -X connect -x bastion.wikitide.net:8080 %h %p\'',
+        require   => [
+            File['/var/lib/nagios/id_ed25519'],
+            File['/var/lib/nagios/id_ed25519.pub'],
+            File['/srv/statichelp'],
+        ],
+    }
+}
