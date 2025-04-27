ae79869fb6f03e2c46068a5dcd73a10727390f74
RhinosF1 <rhinosf1@wikitide.org>
2025-04-27 16:13:08 +0100

    base::ssl: drop support for ca-certificates

--- modules/base/manifests/ssl.pp
+++ modules/base/manifests/ssl.pp
@@ -5,17 +5,4 @@ class base::ssl {
         'ssl-cert',
         'ca-certificates',
     ])
-
-    file { 'authority certificates':
-        path    => '/etc/ssl/certs',
-        source  => 'puppet:///ssl/ca/',
-        recurse => 'remote',
-        require => Package['ca-certificates'],
-    }
-
-    exec { 'update-cas':
-        command     => '/usr/sbin/update-ca-certificates',
-        refreshonly => true,
-        require     => File['authority certificates'],
-    }
 }
