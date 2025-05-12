870f574adf9c716e31c5b5953936ab10b76271e7
paladox <paladox@users.noreply.github.com>
2025-05-12 15:07:56 +0100

    Fix typo

--- modules/opensearch/lib/puppet/type/opensearch_index_template.rb
+++ modules/opensearch/lib/puppet/type/opensearch_index_template.rb
@@ -8,7 +8,7 @@ require 'puppet/file_serving/metadata'
 require 'puppet_x/opensearch/deep_implode'
 require 'puppet_x/opensearch/deep_to_i'
 require 'puppet_x/opensearch/deep_to_s'
-require 'puppet_x/opensearch/opsearch_rest_resource'
+require 'puppet_x/opensearch/opensearch_rest_resource'
 
 Puppet::Type.newtype(:opensearch_index_template) do
   extend OpensearchRESTResource
