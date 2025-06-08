74613cfafc9d05a983b633b03c2ab392b4490c67
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-06-08 11:33:09 -0600

    Always use ip

--- modules/mediawiki/manifests/monitoring.pp
+++ modules/mediawiki/manifests/monitoring.pp
@@ -25,26 +25,19 @@ class mediawiki::monitoring {
         mode   => '0555',
     }
 
-    if ( $facts['networking']['interfaces']['ens19'] and $facts['networking']['interfaces']['ens18'] ) {
-        $address = $facts['networking']['interfaces']['ens19']['ip']
-    } elsif ( $facts['networking']['interfaces']['ens18'] ) {
-        $address = $facts['networking']['interfaces']['ens18']['ip6']
-    } else {
-        $address = $facts['networking']['ip6']
-    }
     monitoring::services { 'MediaWiki Rendering':
         check_command => 'check_mediawiki',
         docs          => 'https://meta.miraheze.org/wiki/Tech:Icinga/MediaWiki_Monitoring#MediaWiki_Rendering',
         vars          => {
             host    => lookup('mediawiki::monitoring::host'),
-            address => $address,
+            address => $facts['networking']['ip'],
         },
     }
 
     monitoring::services { 'HTTPS':
         check_command => 'check_curl',
         vars          => {
-            address          => $address,
+            address          => $facts['networking']['ip'],
             http_vhost       => $facts['networking']['fqdn'],
             http_ssl         => true,
             http_ignore_body => true,
