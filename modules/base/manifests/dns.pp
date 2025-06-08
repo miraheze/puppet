41642b686b838c054ba053b6dff99382ae88e463
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-06-07 18:46:45 -0600

    Use fqdn

--- modules/base/manifests/dns.pp
+++ modules/base/manifests/dns.pp
@@ -30,7 +30,7 @@ class base::dns {
     }
 
     monitoring::nrpe { 'PowerDNS Recursor':
-        command  => "/usr/lib/nagios/plugins/check_dns -s ::1 -H ${facts['networking']['hostname']}",
+        command  => "/usr/lib/nagios/plugins/check_dns -s ::1 -H ${facts['networking']['fqdn']}",
         docs     => 'https://meta.miraheze.org/wiki/Tech:Icinga/Base_Monitoring#PowerDNS_Recursor',
         critical => true
     }
