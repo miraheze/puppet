bd04db66745cbe69935292658c621f4c9a16cc71
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-06-14 14:38:36 -0600

    mediawiki: use mediawiki::periodic_job for systemd timers

--- modules/mediawiki/manifests/logging.pp
+++ modules/mediawiki/manifests/logging.pp
@@ -2,18 +2,12 @@
 class mediawiki::logging {
     file { [
         '/var/log/mediawiki',
-        '/var/log/mediawiki/cron',
         '/var/log/mediawiki/debuglogs'
     ]:
         ensure => 'directory',
         owner  => 'www-data',
         group  => 'www-data',
         mode   => '0755',
-    }
-
-    logrotate::conf { 'mediawiki_cron_logs':
-        ensure => present,
-        source => 'puppet:///modules/mediawiki/logging/mediawiki_cron_logs.logrotate.conf',
     }
 
     logrotate::conf { 'mediawiki_debug_logs':
