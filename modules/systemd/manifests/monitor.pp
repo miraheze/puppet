79a9fbd6d2f3a6c62bc4c42e6e6876bc46a75180
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-06-15 18:41:33 -0600

    systemd: add monitoring for timers

---
+++ modules/systemd/manifests/monitor.pp
@@ -0,0 +1,12 @@
+define systemd::monitor {
+    file { '/usr/lib/nagios/plugins/check_systemd_unit_status':
+        source  => 'puppet:///modules/systemd/check_systemd_unit_status',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+    }
+
+    monitoring::nrpe { "Check unit status of ${title}":
+        command => '/usr/bin/sudo /usr/lib/nagios/plugins/check_systemd_unit_status'
+    }
+}
