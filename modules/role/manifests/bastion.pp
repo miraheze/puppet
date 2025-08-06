749f798497abba512611f79f5f97f04915552665
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-08-06 12:07:12 -0600

    Allow all of 10.0.0.0/8 to squid firewall

--- modules/role/manifests/bastion.pp
+++ modules/role/manifests/bastion.pp
@@ -28,9 +28,21 @@ class role::bastion {
         ' '
     )
 
+    # Add entire 10.0.0.0/8 (private IP) range and
+    # remove individual private IPs.
+    $squid_access_hosts_combined = join(
+        (
+            split($squid_access_hosts_str, ' ')
+                .filter |$ip| { $ip !~ /^10\./ } + ['10.0.0.0/8']
+        )
+        .unique()
+        .sort(),
+        ' '
+    )
+
     ferm::service { 'bastion-squid':
         proto  => 'tcp',
         port   => '8080',
-        srange => "(${squid_access_hosts_str})",
+        srange => "(${squid_access_hosts_combined})",
     }
 }
