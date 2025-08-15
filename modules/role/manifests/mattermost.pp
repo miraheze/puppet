ffaef82730dc7a91c0b05a5d95daa72e6a3a2448
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-08-14 20:53:09 -0600

    Update mattermost to 10.10.2

--- modules/role/manifests/mattermost.pp
+++ modules/role/manifests/mattermost.pp
@@ -23,7 +23,7 @@ class role::mattermost {
     }
     -> class { 'mattermost':
         edition          => 'enterprise',
-        version          => '10.10.1',
+        version          => '10.10.2',
         override_options => {
             'TeamSettings'    => {
                 'SiteName'                  => 'WikiTide Foundation',
