e875d87f9d3b65a6ff9add3736a916f21606887d
Alex <alex@blueselene.com>
2025-08-15 22:04:36 +0200

    Fix var name

--- modules/irc/templates/pywikibot/user-config.py
+++ modules/irc/templates/pywikibot/user-config.py
@@ -4,4 +4,4 @@ mylang = 'metawiki'
 usernames['wikitide']['*'] = 'BeeBot'
 authenticate['*'] = ('<%= @consumer_token %>', '<%= @consumer_secret %>', '<%= @access_token %>', '<%= @access_secret %>')
 user_agent_description = 'https://meta.miraheze.org/wiki/Tech:Pywikibot; tech@wikitide.org'
-user_scripts_path = ['wikitide-userscripts']
+user_script_path = ['wikitide-userscripts']
