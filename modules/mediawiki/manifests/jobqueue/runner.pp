92b847bc09761c75b909cc599f4b77f055c4eba7
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-10-07 02:33:47 -0600

    Re-enable MediaModeration job

--- modules/mediawiki/manifests/jobqueue/runner.pp
+++ modules/mediawiki/manifests/jobqueue/runner.pp
@@ -67,7 +67,6 @@ class mediawiki::jobqueue::runner (
         }
 
         mediawiki::periodic_job { 'mediamoderation-scan':
-            ensure   => absent,
             interval => '*-*-* *:34:00',
             command  => "/usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.php /srv/mediawiki/${version}/maintenance/run.php /srv/mediawiki/${version}/extensions/MediaModeration/maintenance/scanFilesInScanTable.php --use-jobqueue --sleep=1 --poll-sleep=10 --last-checked=never --verbose",
         }
