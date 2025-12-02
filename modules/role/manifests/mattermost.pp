bec4665f8e10dc28874e2bc599cf73f19dac37f6
paladox <paladox@users.noreply.github.com>
2025-12-02 11:46:53 -0500

    Update Mattermost version to 11.1.1

--- modules/role/manifests/mattermost.pp
+++ modules/role/manifests/mattermost.pp
@@ -23,7 +23,7 @@ class role::mattermost {
     }
     -> class { 'mattermost':
         edition          => 'enterprise',
-        version          => '11.1.0',
+        version          => '11.1.1',
         override_options => {
             'TeamSettings'    => {
                 'SiteName'                  => 'WikiTide Foundation',
