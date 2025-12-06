fafe70ccf64fb90b1ad3a988ecd3e6c1db0d8cc9
paladox <paladox@users.noreply.github.com>
2025-12-06 16:14:39 -0500

    Update kafka config

--- modules/role/manifests/kafka.pp
+++ modules/role/manifests/kafka.pp
@@ -68,8 +68,7 @@ class role::kafka {
             'log.message.timestamp.type'        => 'CreateTime',
             'log.retention.hours'               => '168', # 1 week
             'message.max.bytes'                 => '4194304',
-            'num.io.threads'                    => '12',
-            'num.network.threads'               => '6',
+            'num.io.threads'                    => '8',
             'num.partitions'                    => '1',
             'num.recovery.threads.per.data.dir' => '4',
             'offsets.retention.minutes'         => '10080', # 1 week
