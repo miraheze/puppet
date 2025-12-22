6cab9ba3d057ff3289663231f88b720a9ec28b4e
paladox <paladox@users.noreply.github.com>
2025-12-22 01:14:22 +0000

    Remove queue ID '2' from queued tracking jobs

--- modules/matomo/manifests/init.pp
+++ modules/matomo/manifests/init.pp
@@ -221,7 +221,7 @@ class matomo (
 
     $queuedtracking_command = '/usr/bin/php /srv/matomo/console queuedtracking:process'
 
-    ['0', '1', '2'].each | $key | {
+    ['0', '1'].each | $key | {
         systemd::timer::job { "matomo-queuedtracking-${key}":
             description       => "Runs the Matomo's Plugin QueuedTracking process.",
             command           => "/bin/bash -c '${queuedtracking_command} --queue-id=${key} -c 10 -s 2 -d 5'",
