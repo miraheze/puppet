ac02debe3206b6f2b80b916165ca7e293408d98e
paladox <paladox@users.noreply.github.com>
2026-01-09 16:12:48 +0000

    dns: Make sure /usr/share/GeoIP exists (#4654)

--- modules/dns/manifests/init.pp
+++ modules/dns/manifests/init.pp
@@ -13,11 +13,16 @@ class dns {
         notify    => Exec['gdnsd-syntax'],
     }
 
+    file { '/usr/share/GeoIP':
+        ensure => directory,
+    }
+
     file { '/usr/share/GeoIP/GeoLite2-Country.mmdb':
-        ensure => present,
-        source => 'puppet:///private/geoip/GeoLite2-Country.mmdb',
-        mode   => '0444',
-        notify => Exec['gdnsd-syntax'],
+        ensure  => present,
+        source  => 'puppet:///private/geoip/GeoLite2-Country.mmdb',
+        mode    => '0444',
+        notify  => Exec['gdnsd-syntax'],
+        require => File['/usr/share/GeoIP']
     }
 
     exec { 'gdnsd-syntax':
