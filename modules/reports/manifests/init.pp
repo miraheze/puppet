8f65f0e830ea32b99e44e3109f9531c71e56bcfa
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-01-23 18:32:31 -0700

    Update TSPortal to v24

--- modules/reports/manifests/init.pp
+++ modules/reports/manifests/init.pp
@@ -5,7 +5,7 @@ class reports {
     git::clone { 'TSPortal':
         directory => '/srv/TSPortal',
         origin    => 'https://github.com/miraheze/TSPortal',
-        branch    => 'v23',
+        branch    => 'v24',
         owner     => 'www-data',
         group     => 'www-data',
     }
