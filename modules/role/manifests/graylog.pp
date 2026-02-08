06a96669a1c7478d92df332c6a5d02b883f294f8
paladox <paladox@users.noreply.github.com>
2026-02-08 12:32:59 +0000

    Update MongoDB version to 7.0.29

--- modules/role/manifests/graylog.pp
+++ modules/role/manifests/graylog.pp
@@ -12,7 +12,7 @@ class role::graylog {
     class { 'mongodb::globals':
         manage_package_repo => true,
         repo_version        => lookup('mongodb_repo_version', {'default_value' => '7.0'}),
-        version             => lookup('mongodb_version', {'default_value' => '7.0.28'}),
+        version             => lookup('mongodb_version', {'default_value' => '7.0.29'}),
     }
     -> class { 'mongodb::server':
         bind_ip => ['127.0.0.1'],
