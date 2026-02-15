a5a94edaa7e290c37c04448498a22522de813b5f
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-02-15 12:19:03 -0700

    Support cgroup v2

--- modules/mediawiki/manifests/cgroup.pp
+++ modules/mediawiki/manifests/cgroup.pp
@@ -2,45 +2,15 @@
 class mediawiki::cgroup {
     stdlib::ensure_packages('cgroup-tools')
 
-    # The cgroup-mediawiki-clean script is used as the release_agent
-    # script for the cgroup. When the last task in the cgroup exits,
-    # the kernel will run the script.
-
-    file { '/usr/local/bin/cgroup-mediawiki-clean':
-        source => 'puppet:///modules/mediawiki/cgroup/cgroup-mediawiki-clean',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-    }
-
-    $php_version = lookup('php::php_version', {'default_value' => '8.2'})
     systemd::service { 'cgroup':
-        ensure  => present,
-        content => systemd_template('cgroup'),
+        ensure  => absent,
+        content => '',
         restart => false,
     }
 
-    grub::bootparam { 'cgroup_enable':
-        value => 'memory',
-    }
-
-    grub::bootparam { 'swapaccount':
-        value => '1',
-    }
-
-    # Disable cgroup memory accounting
-    grub::bootparam { 'cgroup.memory':
-        value => 'nokmem',
-    }
-
-    # Force use of cgroups v1
-    grub::bootparam { 'systemd.unified_cgroup_hierarchy':
-        value => '0',
-    }
-
-    if ($facts['os']['distro']['codename'] == 'trixie') {
-        grub::bootparam { 'SYSTEMD_CGROUP_ENABLE_LEGACY_FORCE':
-            value => '1',
-        }
+    systemd::unit { 'mediawiki.slice':
+        ensure  => present,
+        content => systemd_template('cgroup'),
+        restart => false,
     }
 }
