06e8db112ad740843662843c9734659583e0bcb9
paladox <paladox@users.noreply.github.com>
2026-02-24 19:47:50 +0000

    ircrcbot: Fix "builtins.UnicodeDecodeError: 'utf-8' codec can't decode byte"

--- modules/irc/templates/ircrcbot.py
+++ modules/irc/templates/ircrcbot.py
@@ -43,6 +43,15 @@ class RCBot(irc.IRCClient):
         self.quit('')
     irc_905 = irc_904
 
+    def lineReceived(self, line):
+        # Attempt to decode incoming strings; if they are neither UTF-8 or
+        # CP1252 they will get mangled as whatever CP1252 thinks they are.
+        try:
+            line = line.decode('utf-8')
+        except UnicodeDecodeError:
+            line = line.decode('cp1252')
+        irc.IRCClient.lineReceived(self, line)
+
     def signedOn(self):
         global recver
         self.join(self.channel)
