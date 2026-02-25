86ed144b6fb4d7a178991303735ca1bdbb554454
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-02-25 13:12:18 -0700

    prometheus: drop mediawiki_action_api_modules_latency_.* metrics

--- modules/role/manifests/prometheus.pp
+++ modules/role/manifests/prometheus.pp
@@ -313,6 +313,14 @@ class role::prometheus {
         'file_sd_configs' => [
           { 'files' => [ 'targets/statsd_exporter.yaml' ] },
         ],
+        'metric_relabel_configs' => [
+          # Very high cardinality
+          {
+            'action'        => 'drop',
+            'source_labels' => [ '__name__' ],
+            'regex'         => 'mediawiki_action_api_modules_latency_.*',
+          },
+        ],
       },
     ]
 
