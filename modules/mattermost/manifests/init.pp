d26b412c1eef7d4208257a04e431a3be1254862d
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-02-26 16:13:23 -0700

    mattermost: some cleanup and param validation

--- modules/mattermost/manifests/init.pp
+++ modules/mattermost/manifests/init.pp
@@ -1,42 +1,42 @@
 # See README.md.
 class mattermost (
-  $install_from_pkg     = $mattermost::params::install_from_pkg,
-  $pkg                  = $mattermost::params::pkg,
-  $base_url             = $mattermost::params::base_url,
-  $edition              = $mattermost::params::edition,
-  $version              = $mattermost::params::version,
-  $filename             = $mattermost::params::filename,
-  $full_url             = $mattermost::params::full_url,
-  $dir                  = $mattermost::params::dir,
-  $symlink              = $mattermost::params::symlink,
-  $create_user          = $mattermost::params::create_user,
-  $create_group         = $mattermost::params::create_group,
-  $user                 = $mattermost::params::user,
-  $group                = $mattermost::params::group,
-  $uid                  = $mattermost::params::uid,
-  $gid                  = $mattermost::params::gid,
-  $conf                 = $mattermost::params::conf,
-  $override_options     = $mattermost::params::override_options,
-  $env_conf             = $mattermost::params::env_conf,
-  $override_env_options = $mattermost::params::override_env_options,
-  $manage_data_dir      = $mattermost::params::manage_data_dir,
-  $manage_log_dir       = $mattermost::params::manage_log_dir,
-  $depend_service       = $mattermost::params::depend_service,
-  $install_service      = $mattermost::params::install_service,
-  $manage_service       = $mattermost::params::manage_service,
-  $service_name         = $mattermost::params::service_name,
-  $service_template     = $mattermost::params::service_template,
-  $service_path         = $mattermost::params::service_path,
-  $service_provider     = $mattermost::params::service_provider,
-  $purge_conf           = $mattermost::params::purge_conf,
-  $purge_env_conf       = $mattermost::params::purge_env_conf,
+  Boolean $install_from_pkg           = $mattermost::params::install_from_pkg,
+  String $pkg                         = $mattermost::params::pkg,
+  String $base_url                    = $mattermost::params::base_url,
+  Enum['team', 'enterprise'] $edition = $mattermost::params::edition,
+  String $version                     = $mattermost::params::version,
+  String $filename                    = $mattermost::params::filename,
+  String $full_url                    = $mattermost::params::full_url,
+  Stdlib::Absolutepath $dir           = $mattermost::params::dir,
+  Stdlib::Absolutepath $symlink       = $mattermost::params::symlink,
+  Boolean $create_user                = $mattermost::params::create_user,
+  Boolean $create_group               = $mattermost::params::create_group,
+  String $user                        = $mattermost::params::user,
+  String $group                       = $mattermost::params::group,
+  Integer $uid                        = $mattermost::params::uid,
+  Integer $gid                        = $mattermost::params::gid,
+  String $conf                        = $mattermost::params::conf,
+  Hash $override_options              = $mattermost::params::override_options,
+  String $env_conf                    = $mattermost::params::env_conf,
+  Hash $override_env_options          = $mattermost::params::override_env_options,
+  Boolean $manage_data_dir            = $mattermost::params::manage_data_dir,
+  Boolean $manage_log_dir             = $mattermost::params::manage_log_dir,
+  String $depend_service              = $mattermost::params::depend_service,
+  Boolean $install_service            = $mattermost::params::install_service,
+  Boolean $manage_service             = $mattermost::params::manage_service,
+  String $service_name                = $mattermost::params::service_name,
+  String $service_template            = $mattermost::params::service_template,
+  String $service_path                = $mattermost::params::service_path,
+  String $service_provider            = $mattermost::params::service_provider,
+  Boolean $purge_conf                 = $mattermost::params::purge_conf,
+  Boolean $purge_env_conf             = $mattermost::params::purge_env_conf,
 ) inherits mattermost::params {
   if $override_env_options['MM_FILESETTINGS_DIRECTORY'] {
-    $data_dir = $override_env_options['MM_FILESETTINGS_DIRECTORY']
+    $data_dir = assert_type(Stdlib::Absolutepath, $override_env_options['MM_FILESETTINGS_DIRECTORY'])
   }
   elsif $override_options['FileSettings'] {
     if $override_options['FileSettings']['Directory'] {
-      $data_dir = $override_options['FileSettings']['Directory']
+      $data_dir = assert_type(Stdlib::Absolutepath, $override_options['FileSettings']['Directory'])
     }
     else {
       $data_dir = undef
@@ -46,11 +46,11 @@ class mattermost (
     $data_dir = undef
   }
   if $override_env_options['MM_LOGSETTINGS_FILELOCATION'] {
-    $log_dir = $override_env_options['MM_LOGSETTINGS_FILELOCATION']
+    $log_dir = assert_type(Stdlib::Absolutepath, $override_env_options['MM_LOGSETTINGS_FILELOCATION'])
   }
   elsif $override_options['LogSettings'] {
     if $override_options['LogSettings']['FileLocation'] {
-      $log_dir = $override_options['LogSettings']['FileLocation']
+      $log_dir = assert_type(Stdlib::Absolutepath, $override_options['LogSettings']['FileLocation'])
     }
     else {
       $log_dir = undef
@@ -66,8 +66,8 @@ class mattermost (
     $executable = 'platform'
   }
   anchor { 'mattermost::begin': }
-  -> class { '::mattermost::install': }
-  -> class { '::mattermost::config': }
-  ~> class { '::mattermost::service': }
+  -> class { 'mattermost::install': }
+  -> class { 'mattermost::config': }
+  ~> class { 'mattermost::service': }
   -> anchor { 'mattermost::end': }
 }
