2e4c2128908f11c1c5f1890ff2516b20f5ebb289
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-02-26 16:04:53 -0700

    mattermost: upgrade to 11.4.2

--- modules/role/manifests/mattermost.pp
+++ modules/role/manifests/mattermost.pp
@@ -23,7 +23,7 @@ class role::mattermost {
     }
     -> class { 'mattermost':
         edition          => 'enterprise',
-        version          => '11.4.1',
+        version          => '11.4.2',
         override_options => {
             'TeamSettings'    => {
                 'SiteName'                  => 'WikiTide Foundation',
