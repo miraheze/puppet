707d3f8bd9c6f9ed4a6c850476153867cfebf028
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-02-26 16:07:56 -0700

    mattermost: remove cp from firewall rules

--- modules/role/manifests/mattermost.pp
+++ modules/role/manifests/mattermost.pp
@@ -111,7 +111,7 @@ class role::mattermost {
     $cloudflare_ipv6 = split(file('/etc/puppetlabs/puppet/private/files/firewall/cloudflare_ipv6'), /[\r\n]/)
 
     $firewall_rules_cloudflare_str = join(
-        $cloudflare_ipv4 + $cloudflare_ipv6 + query_facts('Class[Role::Varnish] or Class[Role::Cache::Cache] or Class[Role::Icinga2]', ['networking'])
+        $cloudflare_ipv4 + $cloudflare_ipv6 + query_facts('Class[Role::Icinga2]', ['networking'])
         .map |$key, $value| {
             if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
                 "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
