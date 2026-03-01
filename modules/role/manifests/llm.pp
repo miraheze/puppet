49e971770f11edf778917177632064ef98ef1943
paladox <thomasmulhall410@yahoo.com>
2026-03-01 17:06:30 +0000

    Update

--- modules/role/manifests/llm.pp
+++ modules/role/manifests/llm.pp
@@ -31,22 +31,14 @@ class role::llm (
         port             => $port,
     }
 
-    $firewall_rules_str = join(
-        query_facts('Class[Role::MediaWiki] or Class[Role::Mediawiki_task] or Class[Role::Mediawiki_beta] or Class[Role::Bastion] or Class[Role::Icinga2]', ['networking'])
-        .map |$key, $value| {
-            if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
-                "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
-            } elsif ( $value['networking']['interfaces']['ens18'] ) {
-                "${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
-            } else {
-                "${value['networking']['ip']} ${value['networking']['ip6']}"
-            }
-        }
-        .flatten()
-        .unique()
-        .sort(),
-        ' '
-    )
+    $subquery = @("PQL")
+    (resources { type = 'Class' and title = 'Role::MediaWiki' } or
+    resources { type = 'Class' and title = 'Role::Mediawiki_task' } or 
+    resources { type = 'Class' and title = 'Role::Mediawiki_beta' } or
+    resources { type = 'Class' and title = 'Role::Bastion' } or
+    resources { type = 'Class' and title = 'Role::Icinga2' })
+    | PQL
+    $firewall_rules_str = vmlib::generate_firewall_ip($subquery)
 
     ferm::service { 'http':
         proto   => 'tcp',
