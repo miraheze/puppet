2413acdbfc12f60cd9999cce08e7011650de3228
paladox <thomasmulhall410@yahoo.com>
2026-03-01 17:44:57 +0000

    Update

--- modules/varnish/manifests/init.pp
+++ modules/varnish/manifests/init.pp
@@ -196,22 +196,12 @@ class varnish (
         }
     }
 
-    $firewall_rules_str = join(
-        query_facts('Class[Role::Icinga2] or Class[Role::Mediawiki] or Class[Role::Mediawiki_task]', ['networking'])
-        .map |$key, $value| {
-            if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
-                "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
-            } elsif ( $value['networking']['interfaces']['ens18'] ) {
-                "${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
-            } else {
-                "${value['networking']['ip']} ${value['networking']['ip6']}"
-            }
-        }
-        .flatten()
-        .unique()
-        .sort(),
-        ' '
-    )
+    $subquery = @("PQL")
+    (resources { type = 'Class' and title = 'Role::Icinga2' } or
+    resources { type = 'Class' and title = 'Role::Mediawiki' } or
+    resources { type = 'Class' and title = 'Role::Mediawiki_task' })
+    | PQL
+    $firewall_rules_str = vmlib::generate_firewall_ip($subquery)
     ferm::service { "${facts['networking']['fqdn']} varnish depool service port 5001":
         proto  => 'tcp',
         port   => '5001',
