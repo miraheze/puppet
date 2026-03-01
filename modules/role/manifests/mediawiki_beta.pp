546c1b0241e344b52d185df2aab65c42291e75c6
paladox <paladox@users.noreply.github.com>
2026-03-01 16:32:20 +0000

    Update mediawiki_beta.pp

--- modules/role/manifests/mediawiki_beta.pp
+++ modules/role/manifests/mediawiki_beta.pp
@@ -20,22 +20,15 @@ class role::mediawiki_beta (
     include role::mediawiki::php::restarts
 
     if $strict_firewall {
-        $firewall_rules_str = join(
-            query_facts('Class[Role::Mediawiki_beta] or Class[Role::Varnish] or Class[Role::Cache::Cache] or Class[Role::Icinga2] or Class[Role::Prometheus] or Class[Role::Bastion]', ['networking'])
-            .map |$key, $value| {
-                if ( $value['networking']['interfaces']['ens19'] and $value['networking']['interfaces']['ens18'] ) {
-                    "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
-                } elsif ( $value['networking']['interfaces']['ens18'] ) {
-                    "${value['networking']['interfaces']['ens18']['ip']} ${value['networking']['interfaces']['ens18']['ip6']}"
-                } else {
-                    "${value['networking']['ip']} ${value['networking']['ip6']}"
-                }
-            }
-            .flatten()
-            .unique()
-            .sort(),
-            ' '
-        )
+        $subquery = @("PQL")
+        (resources { type = 'Class' and title = 'Role::Mediawiki_beta' } or
+        resources { type = 'Class' and title = 'Role::Varnish' } or
+        resources { type = 'Class' and title = 'Role::Cache::Cache]' } or
+        resources { type = 'Class' and title = 'Role::Icinga2' } or
+        resources { type = 'Class' and title = 'Role::Prometheus' } or
+        resources { type = 'Class' and title = 'Role::Bastion' })
+        | PQL
+        $firewall_rules_str = vmlib::generate_firewall_ip($subquery)
 
         ferm::service { 'http':
             proto   => 'tcp',
