802120a1888c76ed0733a72a14700b53d74c3721
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-03-11 17:35:03 -0600

    /

--- modules/mediawiki/files/cookbooks/cleanup-old-mediawiki.py
+++ modules/mediawiki/files/cookbooks/cleanup-old-mediawiki.py
@@ -7,7 +7,7 @@ version = sys.argv[1]
 
 system('sudo rm /srv/mediawiki/cache/legacy-wikis.php')
 system(f'sudo rm /srv/mediawiki/config/ExtensionMessageFiles-{version}.php')
-system(f'sudo rm -rf /srv/mediawiki/cache/{version}')
-system(f'sudo rm -rf /srv/mediawiki/femiwiki-deploy/{version}')
-system(f'sudo rm -rf /srv/mediawiki/{version}')
-system(f'sudo rm -rf /srv/mediawiki-staging/{version}')
+system(f'sudo rm -rf /srv/mediawiki/cache/{version}/')
+system(f'sudo rm -rf /srv/mediawiki/femiwiki-deploy/{version}/')
+system(f'sudo rm -rf /srv/mediawiki/{version}/')
+system(f'sudo rm -rf /srv/mediawiki-staging/{version}/')
