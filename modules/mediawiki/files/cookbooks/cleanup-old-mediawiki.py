a1f6074a67441f7ba874c7c89bde18e538abd50a
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-03-11 19:22:05 -0600

    Use www-data

--- modules/mediawiki/files/cookbooks/cleanup-old-mediawiki.py
+++ modules/mediawiki/files/cookbooks/cleanup-old-mediawiki.py
@@ -5,9 +5,9 @@ import sys
 
 version = sys.argv[1]
 
-system('sudo rm /srv/mediawiki/cache/legacy-wikis.php')
-system(f'sudo rm /srv/mediawiki/config/ExtensionMessageFiles-{version}.php')
-system(f'sudo rm -rf /srv/mediawiki/cache/{version}/')
-system(f'sudo rm -rf /srv/mediawiki/femiwiki-deploy/{version}/')
-system(f'sudo rm -rf /srv/mediawiki/{version}/')
-system(f'sudo rm -rf /srv/mediawiki-staging/{version}/')
+system('sudo -u www-data rm /srv/mediawiki/cache/legacy-wikis.php')
+system(f'sudo -u www-data rm /srv/mediawiki/config/ExtensionMessageFiles-{version}.php')
+system(f'sudo -u www-data rm -rf /srv/mediawiki/cache/{version}/')
+system(f'sudo -u www-data rm -rf /srv/mediawiki/femiwiki-deploy/{version}/')
+system(f'sudo -u www-data rm -rf /srv/mediawiki/{version}/')
+system(f'sudo -u www-data rm -rf /srv/mediawiki-staging/{version}/')
