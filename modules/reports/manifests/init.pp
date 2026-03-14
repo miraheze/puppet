0c3af3a9ca13d2a882158efc15c1d6e1b6c264ba
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-03-13 19:58:21 -0600

    Update TSPortal to v33

--- modules/reports/manifests/init.pp
+++ modules/reports/manifests/init.pp
@@ -5,7 +5,7 @@ class reports {
     git::clone { 'TSPortal':
         directory => '/srv/TSPortal',
         origin    => 'https://github.com/miraheze/TSPortal',
-        branch    => 'v32',
+        branch    => 'v33',
         owner     => 'www-data',
         group     => 'www-data',
     }
