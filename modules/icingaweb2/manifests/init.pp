ea42b6e111c6e6761ec16c96e8779843733a307d
paladox <thomasmulhall410@yahoo.com>
2026-06-16 21:59:46 +0100

    Fix

--- modules/icingaweb2/manifests/init.pp
+++ modules/icingaweb2/manifests/init.pp
@@ -14,7 +14,6 @@ class icingaweb2 (
     String $icingadb_redis_host      = 'localhost',
     Stdlib::Port $icingadb_redis_port = '6379',
     String $icingadb_redis_password,
-    $icingadb_redis_
     String $ldap_password,
     String $icingaweb2_api_password,
 ) {
