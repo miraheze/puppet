d2997454b344741f035e068fe6d5e5d5dee5c570
paladox <paladox@users.noreply.github.com>
2026-06-28 16:07:17 -0400

    Fix

--- modules/matomo/manifests/init.pp
+++ modules/matomo/manifests/init.pp
@@ -237,7 +237,7 @@ class matomo (
         '0' => {
             ensure   => 'present',
         },
-        '2' => {
+        '1' => {
             ensure   => 'present',
         },
         '2' => {
