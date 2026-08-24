6ac0536bfe34f1efd5649b61f12c6c2b26c60088
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-08-24 14:08:38 -0600

    Update

--- modules/nftables/manifests/set.pp
+++ modules/nftables/manifests/set.pp
@@ -8,39 +8,34 @@ define nftables::set (
     $ipv4_addrs = $ips.flatten.unique.filter |$ip| { $ip =~ Stdlib::IP::Address::V4 }
     $ipv6_addrs = $ips.flatten.unique.filter |$ip| { $ip =~ Stdlib::IP::Address::V6 }
 
-    # each family's file is only ever present if that family actually has
-    # members, even when $ensure is present overall, so a rule referencing
-    # an empty family's set never gets created either. See
-    # nftables::file::input call sites for the matching half of this.
-    $ensure_v4 = ($ensure == 'present' and !$ipv4_addrs.empty) ? { true => 'present', default => 'absent' }
-    $ensure_v6 = ($ensure == 'present' and !$ipv6_addrs.empty) ? { true => 'present', default => 'absent' }
-
+    $v4_params = {
+        'name'     => "${title}_ipv4",
+        'set_type' => 'ipv4_addr',
+        'addrs'    => $ipv4_addrs,
+        'interval' => $ipv4_addrs.any |$addr| { '/' in $addr },
+    }
     @file { "/etc/nftables/sets/${name}_ipv4.nft":
-        ensure  => $ensure_v4,
+        ensure  => $ensure,
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
-        content => epp('nftables/set.epp', {
-            'name'     => "${title}_ipv4",
-            'set_type' => 'ipv4_addr',
-            'addrs'    => $ipv4_addrs,
-            'interval' => $ipv4_addrs.any |$addr| { '/' in $addr },
-        }),
+        content => epp('nftables/set.epp', $v4_params),
         require => File['/etc/nftables/sets'],
         tag     => 'nft',
     }
 
+    $v6_params = {
+        'name'     => "${title}_ipv6",
+        'set_type' => 'ipv6_addr',
+        'addrs'    => $ipv6_addrs,
+        'interval' => $ipv6_addrs.any |$addr| { '/' in $addr }
+    }
     @file { "/etc/nftables/sets/${name}_ipv6.nft":
-        ensure  => $ensure_v6,
+        ensure  => $ensure,
         owner   => 'root',
         group   => 'root',
         mode    => '0444',
-        content => epp('nftables/set.epp', {
-            'name'     => "${title}_ipv6",
-            'set_type' => 'ipv6_addr',
-            'addrs'    => $ipv6_addrs,
-            'interval' => $ipv6_addrs.any |$addr| { '/' in $addr },
-        }),
+        content => epp('nftables/set.epp', $v6_params),
         require => File['/etc/nftables/sets'],
         tag     => 'nft',
     }
